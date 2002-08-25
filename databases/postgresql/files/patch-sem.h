f2af7d09b25c6b5b1405f7767f604b3ee8cc3c77
Eric Melville <eric@macports.org>
2002-08-25 20:16:50 +0000

    Add PostgreSQL.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@278 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ databases/postgresql/files/patch-sem.h
@@ -0,0 +1,15 @@
+--- postgresql-7.2.1/src/include/port/darwin/sem.h.orig	Thu Nov  8 12:37:52 2001
++++ postgresql-7.2.1/src/include/port/darwin/sem.h	Thu Aug 22 18:36:58 2002
+@@ -63,6 +63,12 @@
+ 	short		sem_flg;		/* operation flags		*/
+ };
+ 
++union semun {
++        int     val;            /* value for SETVAL */
++        struct  semid_ds *buf;  /* buffer for IPC_STAT & IPC_SET */
++        u_short *array;         /* array for GETALL & SETALL */
++};
++
+ extern int	semctl(int semid, int semnum, int cmd, /* ... */ union semun arg);
+ extern int	semget(key_t key, int nsems, int semflg);
+ extern int	semop(int semid, struct sembuf * sops, size_t nsops);
