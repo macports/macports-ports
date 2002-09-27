04b30e4cb5974cef3bf6bf0a846798fb122043db
Michael Maibaum <michaelm@macports.org>
2002-09-27 23:51:58 +0000

    path to pid file was incorrect
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@814 d073be05-634f-4543-b044-5fe20cf6d1d6

--- www/apache/files/apache.sh
+++ www/apache/files/apache.sh
@@ -5,7 +5,7 @@ start)
 	[ -x %%PREFIX%%/sbin/apachectl ] && %%PREFIX%%/sbin/apachectl start > /dev/null && echo -n ' apache'
 	;;
 stop)
-	[ -r /var/run/httpd.pid ] && %%PREFIX%%/sbin/apachectl stop > /dev/null && echo -n ' apache'
+	[ -r %%PREFIX%%/var/run/httpd.pid ] && %%PREFIX%%/sbin/apachectl stop > /dev/null && echo -n ' apache'
 	;;
 *)
 	echo "Usage: `basename $0` {start|stop}" >&2
