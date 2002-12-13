ef55bfa101818743c271dd1cd5160a57b7c043ba
Jordan K. Hubbard <jkh@macports.org>
2002-12-13 01:46:34 +0000

    Final tweak to dynamic search code.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@1420 d073be05-634f-4543-b044-5fe20cf6d1d6

--- games/xracer/files/patch-craft.c
+++ games/xracer/files/patch-craft.c
@@ -25,12 +25,3 @@
  
    /* See if we can open this track. Make sure we resolve all link-time
     * errors now, and make sure that symbols from the track don't pollute
-@@ -130,7 +130,7 @@
-     }
- 
-   /* The shared library contains one symbol of interest: ``craft'' */
--  craft_struct = dlsym (lib, "craft");
-+  craft_struct = dlsym (lib, "_craft");
- 
-   /* This is OK. This symbol should never actually be NULL. */
-   if (craft_struct == NULL)
--- games/xracer/files/patch-track.c
+++ games/xracer/files/patch-track.c
@@ -9,12 +9,3 @@
  
    return xrTrackLoadByFilename (filename);
  }
-@@ -104,7 +104,7 @@
-     }
- 
-   /* The shared library contains one symbol of interest: ``track'' */
--  track_struct = dlsym (lib, "track");
-+  track_struct = dlsym (lib, "_track");
- 
-   /* This is OK. This symbol should never actually be NULL. */
-   if (track_struct == NULL)
