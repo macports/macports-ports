6b9886d041daf429839c1de891d45dc3b0eecca1
Jordan K. Hubbard <jkh@macports.org>
2002-12-13 01:04:42 +0000

    More dylib fix-ups.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@1417 d073be05-634f-4543-b044-5fe20cf6d1d6

--- games/xracer/files/patch-track.c
+++ games/xracer/files/patch-track.c
@@ -1,5 +1,14 @@
---- src/track.c.orig	Thu Dec 12 12:33:27 2002
-+++ src/track.c	Thu Dec 12 12:34:10 2002
+--- src/track.c.orig	Thu Dec 12 14:08:55 2002
++++ src/track.c	Thu Dec 12 14:08:34 2002
+@@ -76,7 +76,7 @@
+   char filename[PATH_MAX];
+ 
+   /* Try to construct the name of the shared library containing this track. */
+-  snprintf (filename, sizeof filename, "tracks/libtrack%s.so", name);
++  snprintf (filename, sizeof filename, "tracks/libtrack%s.dylib", name);
+ 
+   return xrTrackLoadByFilename (filename);
+ }
 @@ -104,7 +104,7 @@
      }
  
