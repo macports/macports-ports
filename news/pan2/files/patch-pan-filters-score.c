503f4e9bbffc242b7839b6f0adb33ce0f29dde4d
Jim Mock <mij@macports.org>
2003-05-24 19:58:15 +0000

    Update to version 0.14.0.  It would be wonderful if the authors of this software could actually bump a version without breaking something different every time.  *Sigh*
    Bug:
    Submitted by:
    Reviewed by:
    Approved by:
    Obtained from:
    
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@2683 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ news/pan2/files/patch-pan-filters-score.c
@@ -0,0 +1,11 @@
+--- pan/filters/score.c.orig	Tue Apr  1 12:07:45 2003
++++ pan/filters/score.c	Tue Apr  1 12:07:52 2003
+@@ -28,7 +28,7 @@
+ #include <string.h>
+ #include <math.h>
+ 
+-#include <regex.h>
++#include <gnuregex.h>
+ 
+ #include <glib.h>
+ 
