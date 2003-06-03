2b3382d0c1622942e8928e21e9061e2dc67b997c
Waqar Malik <waqar@macports.org>
2003-06-03 03:30:34 +0000

    Bug:            https://trac.macports.org/ticket/593 Submitted by:   wmalik@mac.com Reviewed by:    fkr@opendarwin.org Initical Checkin
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@2733 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ comms/minicom/files/patch-windiv.c
@@ -0,0 +1,10 @@
+--- src/windiv.c.orig	Wed May 28 13:11:22 2003
++++ src/windiv.c	Wed May 28 13:11:28 2003
+@@ -19,6 +19,7 @@
+ #include "rcsid.h"
+ RCSID("$Id: patch-windiv.c,v 1.1 2003/06/03 03:30:34 waqar Exp $")
+ 
++#include <stdio.h>
+ #include <dirent.h>
+ #include <sys/stat.h>
+ #include "port.h"
