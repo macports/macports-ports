e17f48c31ece3745da5ae46c4477d153cea5620c
Jim Mock <mij@macports.org>
2003-06-29 20:49:15 +0000

    Update to version 2.2.2.
    While I'm here, remove the lameness I added when I committed version
    2.2.1 of this port.  Instead forcing the user to run
    gdk-pixbuf-query-loaders manually, simply install an already generated
    gdk-pixbuf.loaders file.  This also fixes problems with uninstall since
    running gdk-pixbuf-query-loaders manually led to a different checksum
    and a failed uninstall.
    
    Bug:
    Submitted by:
    Reviewed by:
    Approved by:
    Obtained from:
    
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@2990 d073be05-634f-4543-b044-5fe20cf6d1d6

--- x11/gtk2/files/patch-ltmain.sh
+++ x11/gtk2/files/patch-ltmain.sh
@@ -1,16 +1,17 @@
---- ltmain.sh.orig	Sat Nov  9 05:40:11 2002
-+++ ltmain.sh	Mon Apr  7 02:00:39 2003
-@@ -4047,10 +4047,10 @@
+--- ltmain.sh.orig	Sun Jun 29 04:01:19 2003
++++ ltmain.sh	Sun Jun 29 04:01:34 2003
+@@ -5145,10 +5145,10 @@
  
  # Directory that this library needs to be installed in:
  libdir='$install_libdir'"
--	  if test "$installed" = no && test $need_relink = yes; then
+-	  if test "$installed" = no && test "$need_relink" = yes; then
 -	    $echo >> $output "\
 -relink_command=\"$relink_command\""
 -	  fi
-+#	  if test "$installed" = no && test $need_relink = yes; then
++#	  if test "$installed" = no && test "$need_relink" = yes; then
 +#	    $echo >> $output "\
 +#relink_command=\"$relink_command\""
 +#	  fi
  	done
        fi
+ 
