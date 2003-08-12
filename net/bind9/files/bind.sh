18bf1bfe2e84c99cc84d04b4882ee9eb3f30da01
Felix Kronlage <fkr@macports.org>
2003-08-12 08:43:09 +0000

    Bug:            https://trac.macports.org/ticket/611 Submitted by: bchesneau@mac.com (Benoit Chesneau) Reviewed by: Approved by: Obtained from:
    update to bind port. bind.sh now allows start/stop/reload, also
    default zones were added.
    
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@3207 d073be05-634f-4543-b044-5fe20cf6d1d6

--- net/bind9/files/bind.sh
+++ net/bind9/files/bind.sh
@@ -1,3 +1,29 @@
 #!/bin/sh
 
-%%PREFIX%%/sbin/named
+PIDFILE=named.pid
+CAT=/bin/cat
+KILL=/bin/kill
+
+case "$1" in
+
+start)
+  echo "starting bind"
+  %%PREFIX%%/sbin/named
+  ;;
+
+restart)
+  echo "restarting bind"
+  $KILL -HUP `$CAT %%PREFIX%%/var/run/$PIDFILE` 
+  ;;
+
+stop)
+  echo "stopping bind"
+  $KILL -15 `$CAT %%PREFIX%%/var/run/$PIDFILE` 
+  ;;
+
+*)
+  echo "Usage: %%PREFIX%%/etc/rc.d/bind {start|stop|restart}"
+  exit 1
+  ;;
+
+esac
