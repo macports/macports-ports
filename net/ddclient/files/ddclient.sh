1c6f83e30a0c4e36e733bf90f04a78662613cd44
Robert Shaw <rshaw@macports.org>
2003-09-28 14:45:21 +0000

    Added DarwinPortsStartup daemon script and run dependency.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@3559 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ net/ddclient/files/ddclient.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+NAME=ddclient
+
+CAT=/bin/cat
+KILL=/bin/kill
+
+CONFFILE=__PREFIX/etc/$NAME.conf
+PIDFILE=/var/run/$NAME.pid
+
+case "$1" in
+
+start)
+	if [ -f $CONFFILE ]; then
+		echo "Starting $NAME daemon"
+		__PREFIX/sbin/$NAME
+	else
+		echo "Cannot start $NAME daemon: $CONFFILE does not exist!"
+		exit 1
+	fi
+	;;
+
+stop)
+	if [ -f $PIDFILE ]; then
+		echo "Stopping $NAME daemon"
+		$KILL -15 `$CAT $PIDFILE`
+	else
+		echo "Cannot stop $NAME daemon: $PIDFILE does not exist!"
+		exit 1
+	fi
+	;;
+
+restart)
+	$0 stop
+	$0 start
+	;;
+
+*)
+	echo "Usage: $0 {start|stop|restart}"
+	exit 1
+	;;
+
+esac
+
