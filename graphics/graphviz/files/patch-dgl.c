f302685d6d7f1f795264b6078cd7e5e4666f59b0
Gregory Wright <gwright@macports.org>
2003-10-29 00:46:18 +0000

    Fix build under Panther. Bug: Submitted by: Reviewed by: Approved by: Obtained from:
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@3839 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ graphics/graphviz/files/patch-dgl.c
@@ -0,0 +1,7 @@
+--- tcldgl/dgl.c.sav	Tue Oct 28 18:35:52 2003
++++ tcldgl/dgl.c	Tue Oct 28 18:36:00 2003
+@@ -209,4 +209,3 @@
+ 	return Tcldgl_Init(interp);
+ }
+ 
+-void __eprintf(const char *a, const char *b, unsigned c, const char *d) {}
