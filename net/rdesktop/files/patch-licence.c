01c9934f126315d3635c5cfead0ec6d1718f9c1d
Ole Guldberg Jensen <olegb@macports.org>
2003-10-30 13:43:28 +0000

    Bug: Submitted by:   pguyot@kallisys.net (Paul Guyot) Reviewed by:    olegb@opendarwin.org Approved by: Obtained from:
    removed the mirrorsites subdir is it not needed, and added the new
    man location. Thanks Paul.
    
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@3858 d073be05-634f-4543-b044-5fe20cf6d1d6

--- net/rdesktop/files/patch-licence.c
+++ net/rdesktop/files/patch-licence.c
@@ -1,11 +1,11 @@
---- ../rdesktop.orig/licence.c	Thu Feb 13 14:04:01 2003
-+++ licence.c	Thu Feb 13 14:05:14 2003
-@@ -113,7 +113,9 @@
+--- ../rdesktop.orig/licence.c	Thu Oct 30 14:07:37 2003
++++ licence.c	Thu Oct 30 14:08:04 2003
+@@ -118,7 +118,9 @@
  
  	out_uint32_le(s, 1);
  	out_uint16(s, 0);
 -	out_uint16_le(s, 0xff01);
-+	
++
 +	/* 0xff01 == any Windows TSC, 0x0301 == Windows 2K */
 +	out_uint16_le(s, 0x0301);
  
