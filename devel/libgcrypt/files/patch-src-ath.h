b07e955a70982135a1fe1befc1f957c7877791e2
Jim Mock <mij@macports.org>
2003-11-22 21:24:09 +0000

    Attempt to fix the build on Jaguar/Darwin 6.
    Bug:			https://trac.macports.org/ticket/1181
    Submitted by:		Eric Lawrence <nrc@ucla.edu>
    Reviewed by:
    Approved by:
    Obtained from:
    
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@4014 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ devel/libgcrypt/files/patch-src-ath.h
@@ -0,0 +1,11 @@
+--- src/ath.h.orig	Sat Nov 22 13:17:54 2003
++++ src/ath.h	Sat Nov 22 13:19:32 2003
+@@ -18,6 +18,8 @@
+    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+    02111-1307, USA.  */
+ 
++#include <config.h>
++
+ #ifndef ATH_H
+ #define ATH_H
+ 
