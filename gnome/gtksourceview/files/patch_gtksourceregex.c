50c038a3f0de5fa6a0f14c5c8456ea59a34c7aca
Ole Guldberg Jensen <olegb@macports.org>
2003-12-05 23:05:35 +0000

    new port
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@4114 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ gnome/gtksourceview/files/patch_gtksourceregex.c
@@ -0,0 +1,15 @@
+--- gtksourceview/gtksourceregex.c.org	Fri Nov 28 11:23:58 2003
++++ gtksourceview/gtksourceregex.c	Fri Nov 28 11:24:18 2003
+@@ -26,12 +26,7 @@
+ #include <stdlib.h>
+ #include <glib.h>
+ 
+-#ifdef NATIVE_GNU_REGEX
+-#include <sys/types.h>
+-#include <regex.h>
+-#else
+ #include "gnu-regex/regex.h"
+-#endif
+ 
+ #include "gtksourceview-i18n.h"
+ #include "gtksourceregex.h"
