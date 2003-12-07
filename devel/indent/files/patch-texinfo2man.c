10d68845bd916e88e1722dc63f470e58c1e7aa73
Jordan K. Hubbard <jkh@macports.org>
2003-12-07 09:14:37 +0000

    Add gnu indent. Obtained from:  fink
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@4146 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ devel/indent/files/patch-texinfo2man.c
@@ -0,0 +1,8 @@
+--- man/texinfo2man.c.orig	Sun Dec  7 00:46:05 2003
++++ man/texinfo2man.c	Sun Dec  7 00:46:09 2003
+@@ -1,5 +1,4 @@
+ #include <stdio.h>
+-#include <malloc.h>
+ #include <string.h>
+ #include <ctype.h>
+ 
