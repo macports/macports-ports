423e29db5d39d03a37c13e9f2afe099efa0e69b0
Bill Fenner <fenner@macports.org>
2004-01-08 01:02:38 +0000

    D'oh!  Just because we needed this port when I did it in October doesn't mean jkh didn't add the same port in December =(
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@4514 d073be05-634f-4543-b044-5fe20cf6d1d6

--- textproc/gindent/files/patch-texinfo2man.c
+++
@@ -1,8 +0,0 @@
---- man/texinfo2man.c.orig	Sat Oct 18 20:48:48 2003
-+++ man/texinfo2man.c	Sat Oct 18 20:48:52 2003
-@@ -1,5 +1,4 @@
- #include <stdio.h>
--#include <malloc.h>
- #include <string.h>
- #include <ctype.h>
- 
