addc6f9085166406faccaceaf8fb8913c066d0ac
Ole Guldberg Jensen <olegb@macports.org>
2004-01-11 10:45:22 +0000

    Bug:            https://trac.macports.org/ticket/1412 Submitted by:   opendarwin.org@darkart.com Reviewed by:    olegb@ Approved by: Obtained from:
    port:		dcfldd
    version:	1.0
    
    initial check in, thanks eric!
    
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@4574 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ sysutils/dcfldd/files/patch-src-dcfldd.c
@@ -0,0 +1,11 @@
+--- src/dcfldd.c.orig	Sat Jan 10 22:03:52 2004
++++ src/dcfldd.c	Sat Jan 10 22:06:38 2004
+@@ -342,7 +342,7 @@
+   hashlog=FILE         send MD5 hash output to FILE instead of stderr\n\
+   status=[on|off]      display a continual status message on stderr\n\
+                          default state is \"on\"\n\
+-  sizeprobe=[if|of]    determine the size of the input or output file
++  sizeprobe=[if|of]    determine the size of the input or output file\n\
+                          for use with status messages. (this option\n\
+                          gives you a percentage indicator)\n\
+                          WARNING: Read the manual before using this\n\
