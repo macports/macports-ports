4ec62e8681dea4351f0dc47b4c54151c1edb06d3
Bill Fenner <fenner@macports.org>
2004-01-14 07:35:57 +0000

    Remove unused <varargs.h> #include.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@4632 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ graphics/xli/files/patch-rlelib.c
@@ -0,0 +1,10 @@
+--- rlelib.c.orig	Tue Jan 13 23:29:11 2004
++++ rlelib.c	Tue Jan 13 23:29:18 2004
+@@ -12,7 +12,6 @@
+  */
+ 
+ #include "xli.h"
+-#include <varargs.h>
+ #include <ctype.h>
+ #include "rle.h"
+ 
