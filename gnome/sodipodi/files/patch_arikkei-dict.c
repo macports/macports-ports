a75604dd4d73d8dec4614bb0ed5dfebbc19038ec
Ole Guldberg Jensen <olegb@macports.org>
2004-01-19 13:04:15 +0000

    port:		sodipodi version:	0.33
    initial checkin! wbb4@: it took some time, but here it is :)
    
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@4876 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ gnome/sodipodi/files/patch_arikkei-dict.c
@@ -0,0 +1,10 @@
+--- src/libarikkei/arikkei-dict.c.org	Sun Jan 18 14:34:40 2004
++++ src/libarikkei/arikkei-dict.c	Sun Jan 18 14:34:56 2004
+@@ -12,7 +12,6 @@
+  *
+  */
+ 
+-#include <malloc.h>
+ #include <string.h>
+ 
+ #include "arikkei-dict.h"
