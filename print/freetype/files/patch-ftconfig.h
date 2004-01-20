49dcfb0aef0113bc884e99fe0e09fb1e46f04fa5
Toby Peterson <toby@macports.org>
2004-01-20 14:45:26 +0000

    Roll back to version 2.1.4, 2.1.5 and 2.1.7 are BOTH broken.
    Bug:
    Submitted by:
    Reviewed by:
    Approved by:
    Obtained from:
    
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@4906 d073be05-634f-4543-b044-5fe20cf6d1d6

--- print/freetype/files/patch-ftconfig.h
+++ print/freetype/files/patch-ftconfig.h
@@ -1,12 +1,11 @@
---- include/freetype/config/ftconfig.h.orig	Mon Jan 19 20:57:13 2004
-+++ include/freetype/config/ftconfig.h	Mon Jan 19 20:57:36 2004
-@@ -108,9 +108,11 @@
+--- include/freetype/config/ftconfig.h.orig	Sat Jun 21 03:20:07 2003
++++ include/freetype/config/ftconfig.h	Sat Jun 21 03:20:51 2003
+@@ -108,8 +108,10 @@
    /*   This is the only necessary change, so it is defined here instead    */
    /*   providing a new configuration file.                                 */
    /*                                                                       */
-+#if 0  /* Undefine for now - breaks too many x11 ports in darwinports */
- #if ( defined( __APPLE__ ) && !defined( DARWIN_NO_CARBON ) ) || \
-     ( defined( __MWERKS__ ) && defined( macintosh )        )
++#if 0	/* Undefine for now - breaks too many x11 ports in darwinports. */
+ #if defined( __APPLE__ ) || ( defined( __MWERKS__ ) && defined( macintosh ) )
  #define FT_MACINTOSH 1
 +#endif
  #endif
