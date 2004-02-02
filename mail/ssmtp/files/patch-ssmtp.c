0deaf4c2483f0e60b7097fa6872d1bd2a0a7a47b
Toby Peterson <toby@macports.org>
2004-02-02 03:39:06 +0000

    version bump to 2.60.4, fix manpage locations
    Bug:
    Submitted by:
    Reviewed by:
    Approved by:
    Obtained from:
    
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@5138 d073be05-634f-4543-b044-5fe20cf6d1d6

--- mail/ssmtp/files/patch-ssmtp.c
+++ mail/ssmtp/files/patch-ssmtp.c
@@ -1,10 +1,9 @@
---- ssmtp.c.orig	Sun Jan  5 10:16:11 2003
-+++ ssmtp.c	Sun Jan  5 10:16:26 2003
-@@ -13,7 +13,7 @@
- 
+--- ssmtp.c.old	Sun Feb  1 22:28:40 2004
++++ ssmtp.c	Sun Feb  1 22:28:55 2004
+@@ -14,6 +14,7 @@
  */
- #define VERSION "2.60.1"
--
+ #define VERSION "2.60.4"
+ 
 +#include <sys/types.h>
  #include <sys/socket.h>
  #include <netinet/in.h>
