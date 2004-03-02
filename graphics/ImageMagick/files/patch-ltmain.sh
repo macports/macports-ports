13cbb6533d41de745608dac1142229501869946a
Bryan Blackburn <blb@macports.org>
2004-03-02 22:12:34 +0000

    Bug:		https://trac.macports.org/ticket/1528 Port:		ImageMagick Version:	5.5.6
    Update port graphics/ImageMagick
    As requested by bug https://trac.macports.org/ticket/1528, adds the ability to build libMagick++ (but via
    a different, simpler method than suggested in the bug).  Roll some of the
    variants which are generally useful (jpg, tiff, zlib) into the port proper.
    Add variants for jbig and jpeg2000.  Some cosmetic changes.
    
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@5695 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ graphics/ImageMagick/files/patch-ltmain.sh
@@ -0,0 +1,17 @@
+--- ltmain.sh.orig	Tue Jan 28 11:19:07 2003
++++ ltmain.sh	Sun Feb 29 17:49:23 2004
+@@ -4700,10 +4700,10 @@
+ 
+ # Directory that this library needs to be installed in:
+ libdir='$install_libdir'"
+-	  if test "$installed" = no && test "$need_relink" = yes; then
+-	    $echo >> $output "\
+-relink_command=\"$relink_command\""
+-	  fi
++#	  if test "$installed" = no && test "$need_relink" = yes; then
++#	    $echo >> $output "\
++#relink_command=\"$relink_command\""
++#	  fi
+ 	done
+       fi
+ 
