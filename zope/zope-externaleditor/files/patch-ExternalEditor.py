e39a3a6991e7326de47dda7ce85a8121cdc73360
Robert Shaw <rshaw@macports.org>
2004-03-06 00:47:17 +0000

    Patch update to allow Mac OS X usage without the need for the UserSniffer module. This patch is a cleaner and less dependent fix (same as posted on the ZopeEditManager homepage).
    Bumped revision.
    
    Submitted by:   rshaw
    
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@5731 d073be05-634f-4543-b044-5fe20cf6d1d6

--- zope/zope-externaleditor/files/patch-ExternalEditor.py
+++ zope/zope-externaleditor/files/patch-ExternalEditor.py
@@ -1,21 +1,44 @@
---- ExternalEditor.py.orig	Tue Mar  4 11:24:42 2003
-+++ ExternalEditor.py	Mon Jan 12 20:52:17 2004
-@@ -48,6 +48,9 @@
+--- ExternalEditor.py.orig	Fri Mar  5 12:02:24 2004
++++ ExternalEditor.py	Fri Mar  5 12:01:54 2004
+@@ -17,6 +17,7 @@
+ # Zope External Editor Product by Casey Duncan
+ 
+ from string import join # For Zope 2.3 compatibility
++import re
+ import urllib
+ import Acquisition
+ from Globals import InitializeClass
+@@ -32,6 +33,8 @@
+ 
+ ExternalEditorPermission = 'Use external editor'
+ 
++mac_user_agent_pattern = re.compile( '.*Mac OS X.*|.*Mac_PowerPC.*' )
++
+ class ExternalEditor(Acquisition.Implicit):
+     """Create a response that encapsulates the data needed by the
+        ZopeEdit helper application
+@@ -44,6 +47,8 @@
+         path = request['TraversalRequestNameStack']
+         if path:
+             target = path[-1]
++            if mac_user_agent_pattern.match( request['HTTP_USER_AGENT'] ):
++                target = target.replace( '.zem', '' )
+             request.set('target', target)
              path[:] = []
          else:
-             request.set('target', None)
-+        sniffer = self.UserSniffer()
-+        platform = sniffer['platform']
-+        request.set('platform', platform)
-     
-     def index_html(self, REQUEST, RESPONSE, path=None):
-         """Publish the object to the external editor helper app"""
-@@ -135,6 +138,8 @@
-             raise 'BadRequest', 'Object does not support external editing'
-         
-         RESPONSE.setHeader('Content-Type', 'application/x-zope-edit')    
-+        if 'Mac' == REQUEST['platform']:
-+            RESPONSE.setHeader('Content-Disposition', 'attachment; filename=%s.zem' % REQUEST['target'])
-         return join(r, '\n')
- 
- InitializeClass(ExternalEditor)
+@@ -148,8 +153,13 @@
+                 or hasattr(base, 'document_src')
+                 or hasattr(base, 'read'))
+     if editable and user.has_permission(ExternalEditorPermission, object):
+-        url = "%s/externalEdit_/%s" % (
+-            object.aq_parent.absolute_url(), urllib.quote(object.getId()))
++        ext = ( mac_user_agent_pattern.match(
++            object.REQUEST['HTTP_USER_AGENT'] )
++                and '.zem'
++                or '' )
++        url = "%s/externalEdit_/%s%s" % ( object.aq_parent.absolute_url()
++                                        , urllib.quote(object.getId())
++                                        , ext )
+         if borrow_lock:
+             url += '?borrow_lock=1'
+         return ('<a href="%s" '
