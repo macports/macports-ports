ea3532a0fccaa801109183fdd046047c98593596
Gregory Wright <gwright@macports.org>
2004-03-12 23:10:35 +0000

    Update of patch-Makefile-generic to use new stuff from Matt Kaufmann. Bug: Submitted by: Reviewed by: Approved by: Obtained from:
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@5790 d073be05-634f-4543-b044-5fe20cf6d1d6

--- math/acl2/files/patch-axioms.lisp
+++ math/acl2/files/patch-axioms.lisp
@@ -1,5 +1,19 @@
---- axioms.lisp.sav	Fri Apr 25 17:07:12 2003
-+++ axioms.lisp	Fri Apr 25 17:07:28 2003
+--- axioms.lisp.sav	Thu Mar 11 18:04:10 2004
++++ axioms.lisp	Thu Mar 11 18:06:44 2004
+@@ -643,11 +643,11 @@
+ ; explains why the code below is so cumbersome.
+ 
+                 #+(and non-standard-analysis mcl)
+-                "ACL2 Version 2.7(r)(mcl)"
++                "ACL2 Version 2.7(r)(mcl) Built with patches from darwinports to work with OpenMCL (not part of the official distribution)"
+                 #+(and non-standard-analysis (not mcl))
+                 "ACL2 Version 2.7(r)"
+                 #+(and (not non-standard-analysis) mcl)
+-                "ACL2 Version 2.7(mcl)"
++                "ACL2 Version 2.7(mcl) Built with patches from darwinports to work with OpenMCL (not part of the official distribution)"
+                 #+(and (not non-standard-analysis) (not mcl))
+                 "ACL2 Version 2.7")
+ 
 @@ -17472,7 +17472,7 @@
             #+unix :unix
             #+apple :apple
