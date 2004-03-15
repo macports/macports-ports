cf4ca4c60174ae678906ac71a30936166fd7c9cd
Robert Shaw <rshaw@macports.org>
2004-03-15 18:13:23 +0000

    Updated to latest release 1.3. No longer depends/uses the semi-broken LocalFS product.
    Submitted by:   rshaw
    
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@5825 d073be05-634f-4543-b044-5fe20cf6d1d6

--- zope/zope-zphotoslides/files/patch-__init__.py
+++ zope/zope-zphotoslides/files/patch-__init__.py
@@ -1,6 +1,6 @@
---- __init__.py.orig	Tue Dec 16 13:50:14 2003
-+++ __init__.py	Tue Dec 16 13:50:21 2003
-@@ -68,7 +68,7 @@
+--- __init__.py.orig	Mon Mar 15 08:47:41 2004
++++ __init__.py	Mon Mar 15 08:48:10 2004
+@@ -67,7 +67,7 @@
              contentConstructors = (ZPhotoSlides.manage_addZPhotoSlides, ZPhotoSlidesFolder.manage_addZPhotoSlidesFolder, ) 
              install_globals = globals() # Used only in the Extensions/Install.py scrip
              utils.ContentInit(
