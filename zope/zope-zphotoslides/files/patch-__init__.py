fbee8efcaaee460f8be376f314a5c67dd353832e
Robert Shaw <rshaw@macports.org>
2004-03-20 06:20:56 +0000

    Updates Portfile format/code to match newer Zope product ports. No installed files change, no revision bump required.
    Submitted by:   rshaw
    
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@5882 d073be05-634f-4543-b044-5fe20cf6d1d6

--- zope/zope-zphotoslides/files/patch-__init__.py
+++ zope/zope-zphotoslides/files/patch-__init__.py
@@ -1,5 +1,5 @@
---- __init__.py.orig	Mon Mar 15 08:47:41 2004
-+++ __init__.py	Mon Mar 15 08:48:10 2004
+--- ZPhotoSlides/__init__.py.orig	Mon Mar 15 08:47:41 2004
++++ ZPhotoSlides/__init__.py	Mon Mar 15 08:48:10 2004
 @@ -67,7 +67,7 @@
              contentConstructors = (ZPhotoSlides.manage_addZPhotoSlides, ZPhotoSlidesFolder.manage_addZPhotoSlidesFolder, ) 
              install_globals = globals() # Used only in the Extensions/Install.py scrip
