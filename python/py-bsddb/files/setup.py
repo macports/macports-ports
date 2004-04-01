a645d9ad34258ff4082ea670ebdb6fdabaf1ca14
Markus W. Weißmann <mww@macports.org>
2004-04-01 15:57:08 +0000

    new port python/py-bsddb
    bindings for db3+4
    
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@6160 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ python/py-bsddb/files/setup.py
@@ -0,0 +1,22 @@
+try:
+    import distutils
+    from distutils import sysconfig
+    from distutils.command.install import install
+    from distutils.core import setup, Extension
+except:
+    raise SystemExit, "Distutils problem"
+
+prefix = sysconfig.PREFIX
+inc_dirs = [prefix + "/include"]
+lib_dirs = [prefix + "/lib"]
+libs = ["db-4"]
+
+setup(name = "_bsddb",
+      version = "__VERSION__",
+      description = "BSDDB Extension to Python",
+      
+      ext_modules = [Extension('_bsddb', ['_bsddb.c'],
+                               include_dirs = inc_dirs,
+                               libraries = libs,
+                               library_dirs = lib_dirs)]
+      )
