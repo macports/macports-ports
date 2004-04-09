282fba3a1cc0129795440de93bbfa99fa99d705a
Ole Guldberg Jensen <olegb@macports.org>
2004-04-09 18:06:11 +0000

    gnome 2.6
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@6311 d073be05-634f-4543-b044-5fe20cf6d1d6

--- gnome/nautilus/files/patch_nautilus-program-choosing.c
+++ gnome/nautilus/files/patch_nautilus-program-choosing.c
@@ -1,6 +1,20 @@
---- libnautilus-private/nautilus-program-choosing.c.org	Thu Nov 27 17:43:45 2003
-+++ libnautilus-private/nautilus-program-choosing.c	Thu Nov 27 17:44:04 2003
-@@ -565,7 +565,12 @@
+--- libnautilus-private/nautilus-program-choosing.c.orig	Wed Feb 11 16:37:03 2004
++++ libnautilus-private/nautilus-program-choosing.c	Wed Apr  7 13:33:57 2004
+@@ -71,7 +71,12 @@
+ 
+ static GHashTable *choose_application_hash_table, *choose_component_hash_table;
+ 
+-extern char **environ;
++#ifdef __APPLE__
++# include <crt_externs.h>
++# define environ (*_NSGetEnviron())
++#elif
++ extern char **environ;
++#endif
+ 
+ /* Cut and paste from gdkspawn-x11.c */
+ static gchar **
+@@ -634,7 +639,12 @@
  	gdk_error_trap_pop ();
  }
  
