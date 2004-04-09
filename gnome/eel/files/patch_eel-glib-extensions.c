275f9c8780acd918798aae3f807a6f50d95293c2
Ole Guldberg Jensen <olegb@macports.org>
2004-04-09 18:04:47 +0000

    gnome 2.6
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@6310 d073be05-634f-4543-b044-5fe20cf6d1d6

--- gnome/eel/files/patch_eel-glib-extensions.c
+++ gnome/eel/files/patch_eel-glib-extensions.c
@@ -1,15 +1,16 @@
---- eel/eel-glib-extensions.c.org	Wed Nov 26 18:21:37 2003
-+++ eel/eel-glib-extensions.c	Wed Nov 26 18:23:51 2003
-@@ -54,7 +54,12 @@
+--- eel/eel-glib-extensions.c.org	Wed Apr  7 12:30:35 2004
++++ eel/eel-glib-extensions.c	Wed Apr  7 12:31:06 2004
+@@ -54,8 +54,11 @@
+ static GList *hash_tables_to_free_at_exit;
  
  /* We will need this for eel_unsetenv if there is no unsetenv. */
- #if !defined (HAVE_SETENV)
+-#if !defined (HAVE_UNSETENV)
+-extern char **environ;
 +#ifdef __APPLE__
 +# include <crt_externs.h>
 +# define environ (*_NSGetEnviron())
 +#elif
- extern char **environ;
-+#endif
++ extern char **environ;
  #endif
  
  /**
