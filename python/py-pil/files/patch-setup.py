2f786ab7c10e8a99655715cc623e3a951aba5921
Robert Shaw <rshaw@macports.org>
2004-05-06 21:37:20 +0000

    Updated to PortGroup code. Fixed compile problems with freetype (now properly finds Apple installed version of freetype).
    Submitted by:   rshaw
    
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@6924 d073be05-634f-4543-b044-5fe20cf6d1d6

--- python/py-pil/files/patch-setup.py
+++ python/py-pil/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	Fri Dec 13 06:06:28 2002
-+++ setup.py	Fri Dec 13 06:09:38 2002
+--- setup.py.orig	Thu May  6 13:42:34 2004
++++ setup.py	Thu May  6 14:32:29 2004
 @@ -30,7 +30,7 @@
  # on Windows, the following is used to control how and where to search
  # for Tcl/Tk files.  None enables automatic searching; to override, set
  # this to a directory name.
 -TCLROOT = None
-+TCLROOT = "@prefix@"
++TCLROOT = "/opt/local"
  
  from PIL.Image import VERSION
  
@@ -17,10 +17,10 @@
 -    INCLUDE_DIRS.append('/sw/include')
 -if os.path.exists('/sw/lib'):
 -    LIBRARY_DIRS.append('/sw/lib')
-+if os.path.exists('@prefix@/include'):
-+    INCLUDE_DIRS.append('@prefix@/include')
-+if os.path.exists('@prefix@/lib'):
-+    LIBRARY_DIRS.append('@prefix@/lib')
++if os.path.exists('/opt/local/include'):
++    INCLUDE_DIRS.append('/opt/local/include')
++if os.path.exists('/opt/local/lib'):
++    LIBRARY_DIRS.append('/opt/local/lib')
  
  HAVE_LIBJPEG = 0
  HAVE_LIBTIFF = 0
@@ -28,8 +28,8 @@
          lib = m.group(1)
          if lib == "JPEG":
              HAVE_LIBJPEG = 1
-+            INCLUDE_DIRS.append("@prefix@/include")
-+            LIBRARY_DIRS.append("@prefix@/lib")
++            INCLUDE_DIRS.append("/opt/local/include")
++            LIBRARY_DIRS.append("/opt/local/lib")
              if sys.platform == "win32":
                  LIBRARIES.append("jpeg")
                  INCLUDE_DIRS.append(JPEGDIR)
@@ -37,12 +37,22 @@
                  LIBRARIES.append("jpeg")
          elif lib == "TIFF":
              HAVE_LIBTIFF = 1
-+            INCLUDE_DIRS.append("@prefix@/include")
-+            LIBRARY_DIRS.append("@prefix@/lib")
++            INCLUDE_DIRS.append("/opt/local/include")
++            LIBRARY_DIRS.append("/opt/local/lib")
              LIBRARIES.append("tiff")
          elif lib == "Z":
              HAVE_LIBZ = 1
-@@ -204,10 +208,15 @@
+@@ -119,6 +123,9 @@
+ # --------------------------------------------------------------------
+ # configure imagingtk module
+ 
++EXTRA_COMPILE_ARGS = None
++EXTRA_LINK_ARGS = None
++
+ try:
+     import _tkinter
+     TCL_VERSION = _tkinter.TCL_VERSION[:3]
+@@ -204,10 +211,15 @@
                  EXTRA_LINK_ARGS = frameworks
                  HAVE_TCLTK = 1
  
@@ -58,20 +68,27 @@
  
      if HAVE_TCLTK:
          MODULES.append(
-@@ -266,13 +275,13 @@
+@@ -266,13 +278,20 @@
          # FIXME: search for libraries
          LIBRARIES.append("freetype")
          INCLUDE_DIRS.append("/usr/include/freetype2")
 -    elif os.path.isdir("/sw/include/freetype2"):
-+    elif os.path.isdir("@prefix@/include/freetype2"):
++    elif os.path.isdir("/usr/X11R6/include/freetype2"):
++        # assume that the freetype library is installed in a
++        # standard location
++        # FIXME: search for libraries
++        LIBRARIES.append("freetype")
++        INCLUDE_DIRS.append("/usr/X11R6/include/freetype2")
++        LIBRARY_DIRS.append("/usr/X11R6/lib")
++    elif os.path.isdir("/opt/local/include/freetype2"):
          # assume that the freetype library is installed in a
          # standard location
          # FIXME: search for libraries
          LIBRARIES.append("freetype")
 -        INCLUDE_DIRS.append("/sw/include/freetype2")
 -        LIBRARY_DIRS.append("/sw/lib")
-+        INCLUDE_DIRS.append("@prefix@/include/freetype2")
-+        LIBRARY_DIRS.append("@prefix@/lib")
++        INCLUDE_DIRS.append("/opt/local/include/freetype2")
++        LIBRARY_DIRS.append("/opt/local/lib")
      else:
          have_freetype = 0
  
