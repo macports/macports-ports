f2b45736c5cbc73821aedaec55b30ec3e11e19e7
Robert Shaw <rshaw@macports.org>
2004-05-20 20:51:56 +0000

    Updated to release 0.9.4
    Submitted by:   rshaw
    
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@7133 d073be05-634f-4543-b044-5fe20cf6d1d6

--- aqua/ZopeEditManager/files/patch-buildapp.py
+++ aqua/ZopeEditManager/files/patch-buildapp.py
@@ -1,27 +1,27 @@
---- buildapp.py.orig	Thu Jan  8 19:48:20 2004
-+++ buildapp.py	Thu Mar  4 23:33:59 2004
+--- ../ZopeEditManager-src-0.9.4.orig/buildapp.py	Tue May 18 12:56:30 2004
++++ buildapp.py	Thu May 20 13:47:47 2004
 @@ -7,8 +7,10 @@
  from bundlebuilder import buildapp, Plist
  
  plist = Plist( CFBundleIdentifier = "com.urbanape.zopeeditmanager"
--             , CFBundleShortVersionString = "Version 0.9.2"
--             , CFBundleGetInfoString = ("ZopeEditManager version 0.9.2, "
-+             , CFBundleShortVersionString = "Version @VERSION@"
+-             , CFBundleShortVersionString = "Version 0.9.4"
+-             , CFBundleGetInfoString = ("ZopeEditManager version 0.9.4, "
 +             , CFBundleHelpBookFolder = "ZopeEditManagerHelp"
 +             , CFBundleHelpBookName = "ZopeEditManager Help"
++             , CFBundleShortVersionString = "Version @VERSION@"
 +             , CFBundleGetInfoString = ("ZopeEditManager version @VERSION@, "
                                          "Copyright 2003 Zope Corporation")
               , NSHumanReadableCopyright = "Copyright 2003 Zope Corporation"
-              , CFBundleDocumentTypes = ( { 'CFBundleTypeExtensions' : ( 'zem', ),
-@@ -18,6 +20,7 @@
-                                            'CFBundleTypeIconFile' : 'ZEMDocument.icns', }, ) )
+              , CFBundleDocumentTypes = (
+@@ -20,6 +22,7 @@
+     }, ) )
  
  buildapp( name = "ZopeEditManager"
 +        , creator = "ZEMD"
          , mainprogram = "__main__.py"
          , resources = [ "Resources/English.lproj"
                        , "Resources/com.urbanape.zopeeditmanager.plist"
-@@ -31,6 +34,9 @@
+@@ -33,6 +36,9 @@
                        , "Resources/remove_idle.png"
                        , "Resources/remove_pressed.png"
                        , "Resources/ZEMDocument.icns"
