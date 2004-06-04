61a57fbfd560555d720a0ceb96156c426abe2d6e
Markus W. Weißmann <mww@macports.org>
2004-06-04 12:14:07 +0000

    some clean up & fixes in the server variant
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@7371 d073be05-634f-4543-b044-5fe20cf6d1d6

--- databases/postgresql/files/pgsql.sh
+++ databases/postgresql/files/pgsql.sh
@@ -1,20 +1,24 @@
 #!/bin/sh
 
-RESTART_MODE=__RSMODE
-STOP_MODE=__STMODE
+RESTART_MODE=fast
+STOP_MODE=fast
+
+DBDIR=__PREFIX__/var/db/pgsql/defaultdb
+LOGFILE=__PREFIX__/var/log/pgsql/pgsql.log
+PGCTL=__PREFIX__/bin/pg_ctl
 
 case "$1" in
 
 start)
-  su postgres -c "__PREFIX/bin/pg_ctl start -D __DBDIR -l __LOGDIR/pgsql.log"
+  su postgres -c "$PGCTL start -D $DBDIR -l $LOGFILE"
   ;;
 
 restart)
-  su postgres -c "__PREFIX/bin/pg_ctl restart -D __DBDIR -l __LOGDIR/pgsql.log -m $RESTART_MODE"
+  su postgres -c "$PGCTL restart -D $DBDIR -l $LOGFILE -m $RESTART_MODE"
   ;;
 
 stop)
-  su postgres -c "__PREFIX/bin/pg_ctl stop -D __DBDIR -m $STOP_MODE"
+  su postgres -c "$PGCTL stop -D $DBDIR -m $STOP_MODE"
   ;;
 
 esac
