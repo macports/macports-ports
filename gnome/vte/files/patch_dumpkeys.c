5b63015d91415d42affa1cab6d085cb8ec31b165
Ole Guldberg Jensen <olegb@macports.org>
2004-06-15 09:03:06 +0000

    update to 0.11.11
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@7519 d073be05-634f-4543-b044-5fe20cf6d1d6

--- gnome/vte/files/patch_dumpkeys.c
+++ gnome/vte/files/patch_dumpkeys.c
@@ -1,12 +1,12 @@
---- src/dumpkeys.c.org	Tue Oct 28 20:21:33 2003
-+++ src/dumpkeys.c	Tue Oct 28 20:21:42 2003
+--- src/dumpkeys.c.org	Tue May 11 10:29:17 2004
++++ src/dumpkeys.c	Tue May 11 10:29:11 2004
 @@ -18,9 +18,6 @@
  
- #ident "$Id: patch_dumpkeys.c,v 1.1 2004/01/10 19:25:33 olegb Exp $"
+ #ident "$Id: patch_dumpkeys.c,v 1.2 2004/06/15 09:03:06 olegb Exp $"
  #include "../config.h"
 -#ifdef HAVE_SYS_SELECT_H
 -#include <sys/select.h>
 -#endif
- #include <sys/time.h>
- #include <sys/types.h>
- #include <unistd.h>
+ #ifdef HAVE_SYS_TERMIOS_H
+ #include <sys/termios.h>
+ #endif
