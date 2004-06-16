ab87c3d9b7c1a4209801b7ce0ea135c6b4c9f52c
Ole Guldberg Jensen <olegb@macports.org>
2004-06-16 12:49:52 +0000

    update to 0.6.1
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@7531 d073be05-634f-4543-b044-5fe20cf6d1d6

--- gnome/genius/files/patch_ve-misc.c
+++ gnome/genius/files/patch_ve-misc.c
@@ -1,6 +1,57 @@
---- vicious-extensions/ve-misc.c.org	Sat May  1 11:00:37 2004
-+++ vicious-extensions/ve-misc.c	Sat May  1 11:04:06 2004
-@@ -434,12 +434,7 @@
+--- vicious-extensions/ve-misc.c.org	Wed Jun 16 07:04:49 2004
++++ vicious-extensions/ve-misc.c	Wed Jun 16 07:06:03 2004
+@@ -28,6 +28,13 @@
+ 
+ #include "ve-misc.h"
+ 
++#ifdef __APPLE__
++# include <crt_externs.h>
++# define environ (*_NSGetEnviron())
++#elif
++ extern char **environ;
++#endif
++
+ char **
+ ve_split (const char *s)
+ {
+@@ -364,9 +371,6 @@
+ int
+ ve_setenv (const char *name, const char *value, gboolean overwrite)
+ {
+-#if defined (HAVE_SETENV)
+-	return setenv (name, value != NULL ? value : "", overwrite);
+-#else
+ 	char *string;
+ 	
+ 	if (! overwrite && g_getenv (name) != NULL) {
+@@ -379,7 +383,6 @@
+ 	 */
+ 	string = g_strconcat (name, "=", value, NULL);
+ 	return putenv (string);
+-#endif
+ }
+ #endif
+ 
+@@ -397,10 +400,6 @@
+ void
+ ve_unsetenv (const char *name)
+ {
+-#if defined (HAVE_SETENV)
+-	unsetenv (name);
+-#else
+-	extern char **environ;
+ 	int i, len;
+ 
+ 	if (environ == NULL)
+@@ -421,7 +420,6 @@
+ 		environ[i] = environ[i + 1];
+ 		i++;
+ 	}
+-#endif
+ }
+ #endif
+ 
+@@ -437,13 +435,8 @@
  void
  ve_clearenv (void)
  {
@@ -8,9 +59,9 @@
 -	clearenv ();
 -#else
 -	extern char **environ;
--	environ[0] = NULL;
+ 	if (environ != NULL)
+ 		environ[0] = NULL;
 -#endif
-+	unsetenv (NULL);
  }
  
  char *
