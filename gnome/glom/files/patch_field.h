3499154614d0f9e0e924df009ba6257cab9d0f21
Ole Guldberg Jensen <olegb@macports.org>
2004-07-12 06:37:19 +0000

    new port,
    port            glom
    version         0.8.6
    
    thanks to mww@
    
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@7704 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ gnome/glom/files/patch_field.h
@@ -0,0 +1,10 @@
+--- glom/data_structure/field.h.org	Mon Jul 12 07:02:33 2004
++++ glom/data_structure/field.h	Mon Jul 12 07:02:39 2004
+@@ -22,6 +22,7 @@
+ #define GLOM_DATASTRUCTURE_FIELD_H
+ 
+ #include <libgdamm.h>
++#include <libintl.h>
+ 
+ //Predicate, for use with std::find_if():
+ 
