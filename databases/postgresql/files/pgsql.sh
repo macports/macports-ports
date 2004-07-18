f0534dc25f76219f1e8c543dfc79136f69d4fc7e
Markus W. Weißmann <mww@macports.org>
2004-07-18 18:17:31 +0000

    Bug:            https://trac.macports.org/ticket/1926 Submitted by:   decibel [at] decibel.org Reviewed by:    mww@
    integrate pg_autovacuum into postgresql startup script
    
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@7773 d073be05-634f-4543-b044-5fe20cf6d1d6

--- databases/postgresql/files/pgsql.sh
+++ databases/postgresql/files/pgsql.sh
@@ -4,21 +4,31 @@ RESTART_MODE=fast
 STOP_MODE=fast
 
 DBDIR=__PREFIX__/var/db/pgsql/defaultdb
-LOGFILE=__PREFIX__/var/log/pgsql/pgsql.log
 PGCTL=__PREFIX__/bin/pg_ctl
+PGVAC=__PREFIX__/bin/pg_autovacuum
+OPTIONS="'-c stats_start_collector=true -c stats_row_level=true'"
+
+LOGFILE=__PREFIX__/var/log/pgsql/pgsql.log
+VLOGFILE=__PREFIX__/var/log/pgsql/pg_autovacuum.log
+
+SU="su -l postgres -c"
 
 case "$1" in
 
 start)
-  su postgres -c "$PGCTL start -D $DBDIR -l $LOGFILE"
-  ;;
+	${SU} "exec $PGCTL start -D $DBDIR -l $LOGFILE -o $OPTIONS"
+	if [ -f ${PGVAC} -a -x ${PGVAC} ]; then
+		${SU} "touch ${VLOGFILE} || echo unable to write ${VLOGFILE}"
+		${SU} "exec ${PGVAC} -D -L ${VLOGFILE}"
+	fi
+	;;
 
 restart)
-  su postgres -c "$PGCTL restart -D $DBDIR -l $LOGFILE -m $RESTART_MODE"
-  ;;
+	${SU} "exec $PGCTL restart -D $DBDIR -l $LOGFILE -m $RESTART_MODE"
+	;;
 
 stop)
-  su postgres -c "$PGCTL stop -D $DBDIR -m $STOP_MODE"
-  ;;
+	${SU} "exec $PGCTL stop -D $DBDIR -m $STOP_MODE"
+	;;
 
 esac
