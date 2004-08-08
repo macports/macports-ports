9572091cf411563c9ed026ec17680d64b0fffbf6
Lawerence Sicca <lomion@macports.org>
2004-08-08 06:27:13 +0000

    updated for 1.0.8, removed glibtoolize as autogen.sh can find it now
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@7898 d073be05-634f-4543-b044-5fe20cf6d1d6

--- graphics/libmng/files/patch-autogen.sh
+++ graphics/libmng/files/patch-autogen.sh
@@ -1,15 +1,6 @@
---- autogen.sh.orig	Sat Apr  3 17:44:13 2004
-+++ autogen.sh	Sat Apr  3 17:44:32 2004
-@@ -24,7 +24,7 @@
- aclocal
- 
- echo "running libtoolize"
--libtoolize --automake
-+glibtoolize --automake
- 
- echo "running automake"
- automake --foreign --add-missing
-@@ -33,6 +33,6 @@
+--- autogen.sh.orig	Sun Aug  8 02:19:56 2004
++++ autogen.sh	Sun Aug  8 02:20:19 2004
+@@ -45,6 +45,6 @@
  autoconf
  
  # and finally invoke our new configure
