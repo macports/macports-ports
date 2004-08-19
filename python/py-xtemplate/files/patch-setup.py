c92b85dfbfec7ee18bb848db537e372d62169c8c
Markus W. Weißmann <mww@macports.org>
2004-08-19 20:06:10 +0000

    new port python/py-xtemplate
    python port of PHP XTemplate
    
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@8013 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ python/py-xtemplate/files/patch-setup.py
@@ -0,0 +1,10 @@
+--- setup.py	Mon Aug 16 18:53:30 2004
++++ setup.py.new	Thu Aug 19 15:11:26 2004
+@@ -35,7 +35,6 @@
+       url="http://pyhacks.suddenthinks.com/misc/XTemplate4Python-0.1.0.tar.gz",
+       version=VERSION,
+       py_modules=["xtpl"],   
+-      data_files=pydf,
+       classifiers=[
+         "Development Status :: 3 - Alpha",
+         "Environment :: Web Environment",
