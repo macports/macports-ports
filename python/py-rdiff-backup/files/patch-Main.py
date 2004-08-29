5d506b11fbd03a56b7bbdf9cf0eb1d0748e0c261
Robert Shaw <rshaw@macports.org>
2004-08-29 23:50:26 +0000

    Fixed +cvs variant to build more "statically" to ensure patches work when CVS code changes are made, gave it a version number that matches the CHANGELOG information for future release.  Adjusted log output levels for debug type message output from levels 3 and 4 to levels 7 and 8.  Bumped revision.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@8107 d073be05-634f-4543-b044-5fe20cf6d1d6

--- python/py-rdiff-backup/files/patch-Main.py
+++ python/py-rdiff-backup/files/patch-Main.py
@@ -1,20 +1,41 @@
---- rdiff_backup/Main.py.orig	Wed Jun 16 21:01:27 2004
-+++ rdiff_backup/Main.py	Wed Jun 16 21:00:42 2004
-@@ -502,7 +502,7 @@
+--- rdiff_backup/Main.py.orig	Sun Aug 29 15:59:41 2004
++++ rdiff_backup/Main.py	Sun Aug 29 16:02:31 2004
+@@ -397,10 +397,10 @@
+ 			rpout.conn.Globals.set_local(conn_attr, 1)
+ 
+ 	src_fsa = rpin.conn.fs_abilities.get_fsabilities_readonly('source', rpin)
+-	Log(str(src_fsa), 3)
++	Log(str(src_fsa), 7)
+ 	dest_fsa = rpout.conn.fs_abilities.get_fsabilities_readwrite(
+ 		'destination', Globals.rbdir, 1, Globals.chars_to_quote)
+-	Log(str(dest_fsa), 3)
++	Log(str(dest_fsa), 7)
+ 
+ 	update_triple(src_fsa.eas, dest_fsa.eas,
+ 				  ('eas_active', 'eas_write', 'eas_conn'))
+@@ -502,11 +502,11 @@
  		if src_support: Globals.rbdir.conn.Globals.set_local(conn_attr, 1)
  
  	target_fsa = target.conn.fs_abilities.get_fsabilities_readwrite(
 -		'destination', target, 0)
+-	Log(str(target_fsa), 3)
 +		'destination', target, 0, Globals.chars_to_quote)
- 	Log(str(target_fsa), 3)
++	Log(str(target_fsa), 7)
  	mirror_fsa = Globals.rbdir.conn.fs_abilities.get_fsabilities_restoresource(
  		Globals.rbdir)
-@@ -687,7 +687,7 @@
+-	Log(str(mirror_fsa), 3)
++	Log(str(mirror_fsa), 7)
+ 
+ 	update_triple(mirror_fsa.eas, target_fsa.eas,
+ 				  ('eas_active', 'eas_write', 'eas_conn'))
+@@ -687,8 +687,8 @@
  		SetConnections.UpdateGlobal(write_attr, 1)
  		rbdir.conn.Globals.set_local(conn_attr, 1)
  
 -	fsa = rbdir.conn.fs_abilities.get_fsabilities_readwrite('archive', rbdir)
+-	Log(str(fsa), 3)
 +	fsa = rbdir.conn.fs_abilities.get_fsabilities_readwrite('archive', rbdir, 1, Globals.chars_to_quote)
- 	Log(str(fsa), 3)
++	Log(str(fsa), 7)
  
  	update_triple(fsa.eas, ('eas_active', 'eas_write', 'eas_conn'))
+ 	update_triple(fsa.acls, ('acls_active', 'acls_write', 'acls_conn'))
--- python/py-rdiff-backup/files/patch-fs_abilities.py
+++ python/py-rdiff-backup/files/patch-fs_abilities.py
@@ -1,5 +1,5 @@
---- rdiff_backup/fs_abilities.py.orig	Wed Jun 16 22:03:30 2004
-+++ rdiff_backup/fs_abilities.py	Wed Jun 16 22:05:32 2004
+--- rdiff_backup/fs_abilities.py.orig	Sun Aug 29 16:31:30 2004
++++ rdiff_backup/fs_abilities.py	Sun Aug 29 16:33:14 2004
 @@ -174,7 +174,7 @@
  					log.Log("Warning: File system no longer needs quoting, "
  							"but will retain for backwards compatibility.", 2)
@@ -9,12 +9,49 @@
  
  This may be caused when you copy an rdiff-backup directory from a
  normal file system on to a windows one that cannot support the same
-@@ -291,7 +291,7 @@
+@@ -218,7 +218,7 @@
+ 		try: testdir.fsync()
+ 		except (IOError, OSError), exc:
+ 			log.Log("Directories on file system at %s are not fsyncable.\n"
+-					"Assuming it's unnecessary." % (testdir.path,), 4)
++					"Assuming it's unnecessary." % (testdir.path,), 8)
+ 			self.fsync_dirs = 0
+ 		else: self.fsync_dirs = 1
+ 
+@@ -274,14 +274,14 @@
+ 		except ImportError:
+ 			log.Log("Unable to import module posix1e from pylibacl "
+ 					"package.\nACLs not supported on filesystem at %s" %
+-					(rp.path,), 4)
++					(rp.path,), 8)
+ 			self.acls = 0
+ 			return
+ 
+ 		try: posix1e.ACL(file=rp.path)
+ 		except IOError, exc:
+ 			log.Log("ACLs appear not to be supported by "
+-					"filesystem at %s" % (rp.path,), 4)
++					"filesystem at %s" % (rp.path,), 8)
+ 			self.acls = 0
+ 		else: self.acls = 1
+ 		
+@@ -291,8 +291,8 @@
  		assert rp.lstat()
  		try: import xattr
  		except ImportError:
 -			log.Log("Unable to import module xattr.  EAs not "
+-					"supported on filesystem at %s" % (rp.path,), 4)
 +			log.Log("Unable to import module xattr.\nExtended attributes not "
- 					"supported on filesystem at %s" % (rp.path,), 4)
++					"supported on filesystem at %s" % (rp.path,), 8)
  			self.eas = 0
  			return
+ 
+@@ -303,7 +303,7 @@
+ 				assert xattr.getxattr(rp.path, "user.test") == "test val"
+ 		except IOError, exc:
+ 			log.Log("Extended attributes not supported by "
+-					"filesystem at %s" % (rp.path,), 4)
++					"filesystem at %s" % (rp.path,), 8)
+ 			self.eas = 0
+ 		else: self.eas = 1
+ 
