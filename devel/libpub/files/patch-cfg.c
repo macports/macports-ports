52d39df32403004630f1d18cd2f1bd88f31a1b62
Markus W. Weißmann <mww@macports.org>
2004-08-30 18:28:02 +0000

    new port devel/libpub
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@8112 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ devel/libpub/files/patch-cfg.c
@@ -0,0 +1,11 @@
+--- ../liw/cfg/cfg.c	Sun Oct 25 21:33:28 1998
++++ ../liw/cfg/cfg.c.new	Mon Aug 30 18:25:10 2004
+@@ -4,8 +4,6 @@
+  * Lars Wirzenius
+  */
+  
+-#include "publib/cfg-config.h"
+-
+ #include <assert.h>
+ #include <ctype.h>
+ #include <errno.h>
---
+++ devel/libpub/files/patch-lsm_check_entry.c
@@ -0,0 +1,11 @@
+--- ../liw/lsm/lsm_check_entry.c	Tue Apr  9 00:28:35 1996
++++ ../liw/lsm/lsm_check_entry.c.new	Mon Aug 30 18:26:12 2004
+@@ -6,8 +6,6 @@
+  * "@(#)publib-lsm:$Id: patch-lsm_check_entry.c,v 1.1 2004/08/30 18:28:02 mww Exp $"
+  */
+  
+-#include "publib/lsm-config.h"
+-
+ #include <ctype.h>
+ #include <stdlib.h>
+ #include <string.h>
