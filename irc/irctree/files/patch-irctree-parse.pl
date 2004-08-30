f4b3c6f8f8fe5e8026d08457caca4c6aa2f8578a
Markus W. Weißmann <mww@macports.org>
2004-08-30 18:24:58 +0000

    new port irc/irctree
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@8110 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ irc/irctree/files/patch-irctree-parse.pl
@@ -0,0 +1,8 @@
+--- irctree-parse.pl	Mon Dec 23 16:25:58 2002
++++ irctree-parse.pl.new	Mon Aug 30 10:41:56 2004
+@@ -1,4 +1,4 @@
+-#!/usr/bin/perl -w
++#!/usr/bin/env perl
+ # $Id: irctree-parse.pl,v 1.1 2002/12/23 15:25:58 marvin Exp $
+ 
+ $depth = 0;
