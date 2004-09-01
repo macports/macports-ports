f2941ca2d017a7f9f8431268551e51baa9d459dc
Markus W. Weißmann <mww@macports.org>
2004-09-01 20:43:47 +0000

    -move from python/py-linkchecker to more convenient www/linkchecker -update to v1.13.0
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@8146 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ www/linkchecker/files/patch-setup.py
@@ -0,0 +1,14 @@
+--- setup.py	Wed Sep  1 12:34:21 2004
++++ setup.py.new	Wed Sep  1 22:32:35 2004
+@@ -222,9 +222,9 @@
+              ['share/locale/fr/LC_MESSAGES/linkcheck.mo']),
+          ('share/locale/nl/LC_MESSAGES',
+              ['share/locale/nl/LC_MESSAGES/linkcheck.mo']),
+-         ('share/linkchecker', ['config/linkcheckerrc',
++         ('share/doc/linkchecker', ['config/linkcheckerrc',
+                                 'config/logging.conf', ]),
+-         ('share/linkchecker/examples',
++         ('share/doc/linkchecker/examples',
+              ['cgi/lconline/leer.html.en', 'cgi/lconline/leer.html.de',
+               'cgi/lconline/index.html', 'cgi/lconline/lc_cgi.html.en',
+               'cgi/lconline/lc_cgi.html.de', 'cgi/lconline/check.js',
