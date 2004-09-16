e8c59337a150438c34a0d00afa0fb18535ad9808
Gregory Wright <gwright@macports.org>
2004-09-16 16:18:03 +0000

    Version bump to 1.16. Don't build the Tcl/Tk pieces; they tickle a strange bug. Bug: Submitted by: Reviewed by: Approved by: Obtained from:
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@8321 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ graphics/graphviz/files/patch-common.h
@@ -0,0 +1,11 @@
+--- lefty/common.h.sav	Thu Sep 16 09:15:07 2004
++++ lefty/common.h	Thu Sep 16 09:15:42 2004
+@@ -17,6 +17,8 @@
+ #ifndef _COMMON_H
+ #define _COMMON_H
+ 
++#include <sys/types.h>
++
+ /* some config and conversion definitions from graphviz distribution */
+ #ifdef HAVE_CONFIG_H
+ #include "config.h"
