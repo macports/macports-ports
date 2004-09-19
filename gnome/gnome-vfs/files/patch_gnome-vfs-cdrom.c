9231d17272edb0276aeba8fa125057d4489cca4f
Ole Guldberg Jensen <olegb@macports.org>
2004-09-19 18:14:32 +0000

    gnome 2.8 !
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@8363 d073be05-634f-4543-b044-5fe20cf6d1d6

--- gnome/gnome-vfs/files/patch_gnome-vfs-cdrom.c
+++ gnome/gnome-vfs/files/patch_gnome-vfs-cdrom.c
@@ -1,11 +1,16 @@
---- libgnomevfs/gnome-vfs-cdrom.c.org	Mon Apr  5 08:17:10 2004
-+++ libgnomevfs/gnome-vfs-cdrom.c	Mon Apr  5 08:17:57 2004
-@@ -143,8 +143,10 @@
+--- libgnomevfs/gnome-vfs-cdrom.c.org	Fri Sep 17 13:59:52 2004
++++ libgnomevfs/gnome-vfs-cdrom.c	Fri Sep 17 14:00:01 2004
+@@ -143,6 +143,7 @@
  #endif /* defined(__FreeBSD__) */
  	return type;
  #else
 +	#if !defined(__APPLE__)
  	*fd = open (vol_dev_path, O_RDONLY|O_NONBLOCK);
+ 	if (*fd  < 0) {
+ 		return -1;
+@@ -153,6 +154,7 @@
+ 		return -1;
+ 	}
  	return ioctl (*fd, CDROM_DISC_STATUS, CDSL_CURRENT);
 +	#endif
  #endif
--- gnome/gnome-vfs/files/patch_pty-open.c
+++ gnome/gnome-vfs/files/patch_pty-open.c
@@ -1,15 +1,105 @@
---- modules/pty-open.c.org	Fri Jun  4 20:51:58 2004
-+++ modules/pty-open.c	Fri Jun  4 20:53:17 2004
-@@ -44,6 +44,12 @@
- #include <glib.h>
- #include "pty-open.h"
+--- modules/pty-open.c.org	Fri Sep 17 17:43:28 2004
++++ modules/pty-open.c	Fri Sep 17 17:49:03 2004
+@@ -554,51 +554,6 @@
+ static char *
+ _gnome_vfs_pty_ptsname(int master)
+ {
+-#if defined(HAVE_PTSNAME_R)
+-	gsize len = 1024;
+-	char *buf = NULL;
+-	int i;
+-	do {
+-		buf = g_malloc0(len);
+-		i = ptsname_r(master, buf, len - 1);
+-		switch (i) {
+-		case 0:
+-			/* Return the allocated buffer with the name in it. */
+-#ifdef GNOME_VFS_DEBUG
+-			if (_gnome_vfs_debug_on(GNOME_VFS_DEBUG_PTY)) {
+-				fprintf(stderr, "PTY slave is `%s'.\n", buf);
+-			}
+-#endif
+-			return buf;
+-			break;
+-		default:
+-			g_free(buf);
+-			buf = NULL;
+-			break;
+-		}
+-		len *= 2;
+-	} while ((i != 0) && (errno == ERANGE));
+-#elif defined(HAVE_PTSNAME)
+-	char *p;
+-	if ((p = ptsname(master)) != NULL) {
+-#ifdef GNOME_VFS_DEBUG
+-		if (_gnome_vfs_debug_on(GNOME_VFS_DEBUG_PTY)) {
+-			fprintf(stderr, "PTY slave is `%s'.\n", p);
+-		}
+-#endif
+-		return g_strdup(p);
+-	}
+-#elif defined(TIOCGPTN)
+-	int pty = 0;
+-	if (ioctl(master, TIOCGPTN, &pty) == 0) {
+-#ifdef GNOME_VFS_DEBUG
+-		if (_gnome_vfs_debug_on(GNOME_VFS_DEBUG_PTY)) {
+-			fprintf(stderr, "PTY slave is `/dev/pts/%d'.\n", pty);
+-		}
+-#endif
+-		return g_strdup_printf("/dev/pts/%d", pty);
+-	}
+-#endif
+ 	return NULL;
+ }
  
-+#undef HAVE_POSIX_OPENPT
-+#undef HAVE_GETPT 
-+#undef HAVE_GRANTPT 
-+#undef HAVE_PTSNAME_R 
-+#undef HAVE_UNLOCKPT 
-+#undef HAVE_PTSNAME
- int _gnome_vfs_pty_set_size(int master, int columns, int rows);
+@@ -606,18 +561,13 @@
+ _gnome_vfs_pty_getpt(void)
+ {
+ 	int fd, flags;
+-#ifdef HAVE_GETPT
+-	/* Call the system's function for allocating a pty. */
+-	fd = getpt();
+-#elif defined(HAVE_POSIX_OPENPT)
+-	fd = posix_openpt(O_RDWR | O_NOCTTY);
+-#else
+-	/* Try to allocate a pty by accessing the pty master multiplex. */
+-	fd = open("/dev/ptmx", O_RDWR | O_NOCTTY);
+-	if ((fd == -1) && (errno == ENOENT)) {
+-		fd = open("/dev/ptc", O_RDWR | O_NOCTTY); /* AIX */
+-	}
+-#endif
++
++        /* Try to allocate a pty by accessing the pty master multiplex. */
++        fd = open("/dev/ptmx", O_RDWR | O_NOCTTY);
++        if ((fd == -1) && (errno == ENOENT)) {
++                fd = open("/dev/ptc", O_RDWR | O_NOCTTY); /* AIX */
++        }
++
+ 	/* Set it to blocking. */
+ 	flags = fcntl(fd, F_GETFL);
+ 	flags &= ~(O_NONBLOCK);
+@@ -628,24 +578,13 @@
+ static int
+ _gnome_vfs_pty_grantpt(int master)
+ {
+-#ifdef HAVE_GRANTPT
+-	return grantpt(master);
+-#else
+ 	return 0;
+-#endif
+ }
  
- /* Reset the handlers for all known signals to their defaults.  The parent
+ static int
+ _gnome_vfs_pty_unlockpt(int fd)
+ {
+-#ifdef HAVE_UNLOCKPT
+-	return unlockpt(fd);
+-#elif defined(TIOCSPTLCK)
+-	int zero = 0;
+-	return ioctl(fd, TIOCSPTLCK, &zero);
+-#else
+ 	return -1;
+-#endif
+ }
+ 
+ static int
