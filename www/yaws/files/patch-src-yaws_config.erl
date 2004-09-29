497ccb3adda0022554d4e7762b96bd60dac69b25
Paul Guyot <pguyot@macports.org>
2004-09-29 11:37:15 +0000

    Update to 1.49. Fixed some destroot problem.
    Bug:
    Submitted by:
    Reviewed by:
    Approved by:
    Obtained from:
    
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@8498 d073be05-634f-4543-b044-5fe20cf6d1d6

--- www/yaws/files/patch-src-yaws_config.erl
+++ www/yaws/files/patch-src-yaws_config.erl
@@ -1,16 +1,16 @@
---- src/yaws_config.erl.orig	Sun Feb  8 12:04:16 2004
-+++ src/yaws_config.erl	Sun Feb  8 12:04:32 2004
-@@ -29,11 +29,11 @@
+--- src/yaws_config.erl.orig	Fri May 28 13:38:11 2004
++++ src/yaws_config.erl	Wed Sep 29 13:16:40 2004
+@@ -30,11 +30,11 @@
  paths() ->
      case yaws:getuid() of
  	{ok, "0"} ->    %% root 
 -	    ["/etc/yaws.conf"];
-+	    ["__PREFIX/etc/yaws.conf"];
++	    ["/Volumes/Medias/opt/local/etc/yaws.conf"];
  	_ -> %% developer
  	    [filename:join([os:getenv("HOME"), "yaws.conf"]),
  	     "./yaws.conf", 
 -	     "/etc/yaws.conf"]
-+	     "__PREFIX/etc/yaws.conf"]
++	     "/Volumes/Medias/opt/local/etc/yaws.conf"]
      end.
  
  
