e86b3a82d36f20f51e4557fff2d45927c06b7709
Markus W. Weißmann <mww@macports.org>
2004-10-05 07:27:33 +0000

    new port python/py-cfgparse
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@8577 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ python/py-cfgparse/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py	Mon Oct  4 05:39:34 2004
++++ setup.py.new	Tue Oct  5 09:22:31 2004
+@@ -29,7 +29,7 @@
+       ],
+       packages = ['cfgparse'],
+       data_files = [
+-        ('share/doc/cfgparse-%s' % VERSION, ['README', 'LICENSE-PSF',
++        ('share/doc/py-cfgparse', ['README', 'LICENSE-PSF',
+                                              'LICENSE', 'Changelog',
+                                              'html/index.html',
+                                              'html/style.css',
