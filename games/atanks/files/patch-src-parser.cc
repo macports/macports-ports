b75ea439b5c87eb94225cb32cb1cbca32620f315
Markus W. Weißmann <mww@macports.org>
2004-11-11 14:00:59 +0000

    new port games/atanks
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@9247 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ games/atanks/files/patch-src-parser.cc
@@ -0,0 +1,10 @@
+--- src/parser.cc	Fri Apr  2 17:38:09 2004
++++ parser.cc	Thu Nov 11 13:11:32 2004
+@@ -1,6 +1,7 @@
+ #include <stdio.h>
+ #include <string.h>
+ #include <stdlib.h>
++#include <sys/stat.h>
+ #include "main.h"
+ #include "parser.h"
+ 
