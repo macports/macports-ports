6e6bc4f776d8cbe754d5fe8a80c64d8f88571f68
Michael Maibaum <michaelm@macports.org>
2004-11-29 17:33:25 +0000

    updating dnstop to current version
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@9446 d073be05-634f-4543-b044-5fe20cf6d1d6

--- net/dnstop/files/patch_dnstop.c
+++ net/dnstop/files/patch_dnstop.c
@@ -1,11 +1,9 @@
---- dnstop.c.org	Sun Aug 31 07:26:12 2003
-+++ dnstop.c	Sun Aug 31 07:30:44 2003
-@@ -24,7 +24,8 @@
- #include <curses.h>
+--- dnstop.c.old	Mon Nov 29 17:24:23 2004
++++ dnstop.c	Mon Nov 29 17:23:16 2004
+@@ -25,6 +25,7 @@
  #include <assert.h>
  #include <arpa/inet.h>
--#include <arpa/nameser.h>
-+#include <arpa/nameser.h> 
+ #include <arpa/nameser.h>
 +#include <arpa/nameser_compat.h>
  
  #include <sys/socket.h>
