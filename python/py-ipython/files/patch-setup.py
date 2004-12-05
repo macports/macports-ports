01a8cfc75b27a0d598be9036cd5f5063dbd9ebfb
Markus W. Weißmann <mww@macports.org>
2004-12-05 19:54:49 +0000

    update to v0.6.5
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@9607 d073be05-634f-4543-b044-5fe20cf6d1d6

--- python/py-ipython/files/patch-setup.py
+++ python/py-ipython/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py	Thu Nov  4 08:35:11 2004
-+++ setup.py.new	Tue Nov 30 13:08:17 2004
-@@ -88,7 +88,7 @@
+--- setup.py	Wed Dec  1 00:32:32 2004
++++ setup.py.new	Sun Dec  5 20:50:27 2004
+@@ -99,7 +99,7 @@
  
  # I can't find how to make distutils create a nested dir. structure, so
  # in the meantime do it manually. Butt ugly.
--docdirbase  = 'share/doc/IPython'
+-docdirbase  = 'share/doc/ipython-%s' % version
 +docdirbase  = 'share/doc/py-ipython'
  manpagebase = 'share/man/man1'
- docfiles    = filter(isfile, glob('doc/*[!~|.lyx|.sh|.1|.1.gz]'))
- examfiles   = filter(isfile, glob('doc/examples/*.py'))
+ 
+ # We only need to exclude from this things NOT already excluded in the
