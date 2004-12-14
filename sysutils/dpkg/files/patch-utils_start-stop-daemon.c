6acf67ef2ca39a0ba8947d45ee78f82786a18892
Landon Fuller <landonf@macports.org>
2004-12-14 00:09:00 +0000

    Fix start-stop-daemon on FreeBSD 5.3
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@9755 d073be05-634f-4543-b044-5fe20cf6d1d6

--- sysutils/dpkg/files/patch-utils_start-stop-daemon.c
+++ sysutils/dpkg/files/patch-utils_start-stop-daemon.c
@@ -1,5 +1,5 @@
---- utils/start-stop-daemon.c	Sat Apr 10 11:10:57 2004
-+++ utils/start-stop-daemon.c	Sat Apr 10 11:28:43 2004
+--- utils/start-stop-daemon.c.orig	Thu Nov 11 12:10:04 2004
++++ utils/start-stop-daemon.c	Mon Dec 13 16:02:18 2004
 @@ -36,6 +36,8 @@
  #  define OSFreeBSD
  #elif defined(__NetBSD__)
@@ -18,7 +18,7 @@
  #include <sys/param.h>
  #include <sys/user.h>
  #include <sys/proc.h>
-@@ -690,11 +692,12 @@
+@@ -723,11 +725,12 @@
  {
  #if defined(OSLinux) || defined(OShpux)
  	if (execname && !pid_is_exec(pid, &exec_stat))
@@ -33,7 +33,51 @@
  	if (userspec && !pid_is_user(pid, user_id))
  		return;
  	if (cmdname && !pid_is_cmd(pid, cmdname))
-@@ -877,6 +880,78 @@
+@@ -849,7 +852,6 @@
+ {
+ 	kvm_t *kd;
+ 	int nentries;   /* Value not used */
+-	uid_t proc_uid;
+ 	struct kinfo_proc *kp;
+ 	char  errbuf[_POSIX2_LINE_MAX];
+ 
+@@ -859,34 +861,10 @@
+ 		errx(1, "%s", errbuf);
+ 	if ((kp = kvm_getprocs(kd, KERN_PROC_PID, pid, &nentries)) == 0)
+ 		errx(1, "%s", kvm_geterr(kd));
+-	if (kp->kp_proc.p_cred )
+-		kvm_read(kd, (u_long)&(kp->kp_proc.p_cred->p_ruid),
+-			&proc_uid, sizeof(uid_t));
+-	else
+-		return 0;
+-	return (proc_uid == (uid_t)uid);
+-}
+-
+-static int
+-pid_is_exec(pid_t pid, const char *name)
+-{
+-	kvm_t *kd;
+-	int nentries;
+-	struct kinfo_proc *kp;
+-	char errbuf[_POSIX2_LINE_MAX], *pidexec;
+ 
+-	kd = kvm_openfiles(NULL, NULL, NULL, O_RDONLY, errbuf);
+-	if (kd == 0)
+-		errx(1, "%s", errbuf);
+-	if ((kp = kvm_getprocs(kd, KERN_PROC_PID, pid, &nentries)) == 0)
+-		errx(1, "%s", kvm_geterr(kd));
+-	pidexec = (&kp->kp_proc)->p_comm;
+-	if (strlen(name) != strlen(pidexec))
+-		return 0;
+-	return (strcmp(name, pidexec) == 0) ? 1 : 0;
++	return (kp->ki_uid == (uid_t)uid);
+ }
+ 
+-
+ static void
+ do_procinit(void)
+ {
+@@ -895,6 +873,78 @@
  
  #endif /* OSOpenBSD */
  
