3ce7d7f841d2ed356e9bb4f4d6a40d0e3988d2d9
Markus W. Weißmann <mww@macports.org>
2004-12-20 21:57:44 +0000

    Submitted by:   mbrethen@rochester.rr.com Reviewed by:    mww@
    fix compilation on OS-X 10.2
    
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@9863 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ games/lgeneral/files/patch-slot.c
@@ -0,0 +1,10 @@
+--- src/slot.c	Mon Jul 29 03:16:19 2002
++++ src/slot.c.new	Fri Dec 17 18:20:44 2004
+@@ -16,6 +16,7 @@
+  ***************************************************************************/
+ 
+ #include <math.h>
++#include <sys/types.h>
+ #include <dirent.h>
+ #include "lgeneral.h"
+ #include "date.h"
---
+++ games/lgeneral/files/patch-strat_map.c
@@ -0,0 +1,10 @@
+--- src/strat_map.c	Fri Jul 26 03:00:48 2002
++++ src/strat_map.c.new	Fri Dec 17 18:38:12 2004
+@@ -15,6 +15,7 @@
+  *                                                                         *
+  ***************************************************************************/
+  
++#include <sys/types.h>
+ #include <dirent.h>
+ #include "lgeneral.h"
+ #include "gui.h"
