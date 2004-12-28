938ffbd43ceee0036983246f45997b392735b4fe
Toby Peterson <toby@macports.org>
2004-12-28 08:07:00 +0000

    libtorrent 0.4.5, C++ bittorrent library
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@9998 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ net/libtorrent/files/patch-src__data__file.cc
@@ -0,0 +1,10 @@
+--- src/data/file.cc.orig	Tue Dec 28 02:50:38 2004
++++ src/data/file.cc	Tue Dec 28 02:51:03 2004
+@@ -20,7 +20,6 @@
+ 		   (flags & in  ? O_RDONLY : 0) |
+ 		   (flags & out ? O_WRONLY : 0)) |
+ 		  
+-		  (flags & largefile ? O_LARGEFILE : 0) |
+ 		  (flags & create    ? O_CREAT     : 0) |
+ 		  (flags & truncate  ? O_TRUNC     : 0) |
+ 		  (flags & nonblock  ? O_NONBLOCK  : 0),
