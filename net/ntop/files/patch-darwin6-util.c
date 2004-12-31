84da2260f35727734461af0ff77ea28c0f42e6df
Markus W. Weißmann <mww@macports.org>
2004-12-31 14:22:15 +0000

    update to v3.1
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@10103 d073be05-634f-4543-b044-5fe20cf6d1d6

--- net/ntop/files/patch-darwin6-util.c
+++ net/ntop/files/patch-darwin6-util.c
@@ -1,10 +1,10 @@
---- util.old.c	Wed Nov 17 02:28:57 2004
-+++ util.c	Wed Nov 17 03:09:14 2004
-@@ -5093,6 +5093,7 @@
+--- util.c	Thu Dec 16 20:13:26 2004
++++ util.c.new	Fri Dec 31 13:31:05 2004
+@@ -5183,6 +5183,7 @@
    addr.sin_port   = htons(80);
    memcpy((char *) &addr.sin_addr.s_addr, hptr->h_addr_list[0], hptr->h_length);
  
-+#define socklen_t int /*stefan lebelt 17.11.2004*/
++#define socklen_t int
    /* Connect the socket to the remote host.  */
    rc = connect(sock, (struct sockaddr*)&addr, (socklen_t) sizeof(addr));
    if (rc != 0) {
