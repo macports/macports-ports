74825cdf0d6a852bac1204ea19be3c98a5d46f93
Toby Peterson <toby@macports.org>
2005-01-08 10:37:23 +0000

    libopendaap 0.3.0
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@10280 d073be05-634f-4543-b044-5fe20cf6d1d6

--- audio/libopendaap/files/patch-http_client.c
+++ audio/libopendaap/files/patch-http_client.c
@@ -1,10 +1,10 @@
---- http_client.c.orig	Wed Aug  4 21:55:32 2004
-+++ http_client.c	Wed Aug  4 21:55:40 2004
-@@ -49,6 +49,7 @@
+--- http_client.c.orig	Fri Dec 17 00:27:43 2004
++++ http_client.c	Sat Jan  8 05:29:01 2005
+@@ -50,6 +50,7 @@
  #include <stdio.h>
  #include <errno.h>
  #include <string.h>
 +#include <netinet/in.h>
  
  #include "portability.h"
- 
+ #include "thread.h"
