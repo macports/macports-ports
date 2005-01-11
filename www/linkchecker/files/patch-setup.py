e79f702702ab38f620ea5b74d5bf5944ba7c5d04
Markus W. Weißmann <mww@macports.org>
2005-01-11 22:26:31 +0000

    update to v2.1, upgrade to python 2.4
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@10334 d073be05-634f-4543-b044-5fe20cf6d1d6

--- www/linkchecker/files/patch-setup.py
+++ www/linkchecker/files/patch-setup.py
@@ -1,5 +1,5 @@
---- setup.py	Mon Dec  6 21:27:44 2004
-+++ setup.py.new	Tue Dec  7 22:55:33 2004
+--- setup.py	Tue Jan 11 13:04:34 2005
++++ setup.py.new	Tue Jan 11 23:20:17 2005
 @@ -242,7 +242,7 @@
               ['share/locale/nl/LC_MESSAGES/linkchecker.mo']),
           ('share/linkchecker',
@@ -9,12 +9,16 @@
               ['cgi/lconline/leer.html.en', 'cgi/lconline/leer.html.de',
                'cgi/lconline/index.html', 'cgi/lconline/lc_cgi.html.en',
                'cgi/lconline/lc_cgi.html.de', 'cgi/lconline/check.js',
-@@ -253,7 +253,7 @@
+@@ -253,10 +253,10 @@
      data_files.append(('share/man/man1', ['doc/en/linkchecker.1']))
-     data_files.append(('share/man/de/man1', ['doc/de/linkchecker.de.1']))
-     data_files.append(('share/man/fr/man1', ['doc/fr/linkchecker.fr.1']))
+     data_files.append(('share/man/de/man1', ['doc/de/linkchecker.1']))
+     data_files.append(('share/man/fr/man1', ['doc/fr/linkchecker.1']))
 -    data_files.append(('share/linkchecker/examples',
 +    data_files.append(('share/doc/linkchecker/examples',
                ['config/linkchecker-completion', 'config/linkcheck-cron.sh']))
  elif os.name == 'nt':
-     data_files.append(('share/linkchecker/doc',
+-    data_files.append(('share/linkchecker/doc',
++    data_files.append(('share/doc/linkchecker',
+              ['doc/documentation.html', 'doc/index.html',
+               'doc/install.html', 'doc/index.html', 'doc/other.html',
+               'doc/upgrading.html', 'doc/lc.css', 'doc/navigation.css',
