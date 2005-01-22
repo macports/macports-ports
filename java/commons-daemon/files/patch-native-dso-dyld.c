781b3ba9fb20f4938a8c4a775a3ae0e4fae04e5b
James D. Berry <jberry@macports.org>
2005-01-22 16:34:37 +0000

    Fix patch mangled by cvs keywords
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@10554 d073be05-634f-4543-b044-5fe20cf6d1d6

--- java/commons-daemon/files/patch-native-dso-dyld.c
+++ java/commons-daemon/files/patch-native-dso-dyld.c
@@ -1,30 +1,13 @@
---- src/native/unix/native/dso-dyld.c.orig	2005-01-22 07:15:01.000000000 -0800
-+++ src/native/unix/native/dso-dyld.c	2005-01-22 07:17:47.000000000 -0800
-@@ -14,12 +14,17 @@
-    limitations under the License.
- */
- /* @version $Id: patch-native-dso-dyld.c,v 1.1 2005/01/22 15:31:51 jberry Exp $ */
+--- src/native/unix/native/dso-dyld.c.orig	2004-02-09 07:55:21.000000000 -0800
++++ src/native/unix/native/dso-dyld.c	2005-01-22 08:27:20.000000000 -0800
+@@ -17 +16,0 @@
 -#include "jsvc.h"
- 
- #ifdef DSO_DYLD
- 
- #include <mach-o/dyld.h>
- 
+@@ -21,0 +21,5 @@
 +/* dyld.h hauls in stdbool, which jsvc doesn't expect; undo that */
 +#undef false
 +#undef true
 +#undef bool
 +#include "jsvc.h"
-+
- /* Print an error message and abort all if a specified symbol wasn't found */
- static void nosymbol(const char *s) {
-     log_error("Cannot find symbol '%s' in library",s);
-@@ -106,7 +111,7 @@
-     /* Process the correct name (add a _ before the name) */
-     while (nam[x]!='\0') x++;
-     und=(char*)malloc(sizeof(char)*(x+2));
+@@ -109 +113 @@
 -    while(x>=0) und[x+1]=nam[x--];
 +    for(; x>=0; --x) und[x+1]=nam[x];
-     und[0]='_';
- 
-     /* Find the symbol */
