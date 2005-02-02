702d6f8b07f6a469fc6c0a736852fa1d16ec8b32
Toby Peterson <toby@macports.org>
2005-02-02 09:04:40 +0000

    Fix a nasty memory leak.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@10786 d073be05-634f-4543-b044-5fe20cf6d1d6

--- audio/libopendaap/files/patch-http_client.c
+++ audio/libopendaap/files/patch-http_client.c
@@ -1,5 +1,5 @@
---- http_client.c.orig	Fri Dec 17 00:27:43 2004
-+++ http_client.c	Sat Jan  8 05:29:01 2005
+--- http_client.c.orig	Wed Feb  2 03:52:25 2005
++++ http_client.c	Wed Feb  2 03:58:25 2005
 @@ -50,6 +50,7 @@
  #include <stdio.h>
  #include <errno.h>
@@ -8,3 +8,39 @@
  
  #include "portability.h"
  #include "thread.h"
+@@ -502,6 +503,17 @@
+ 
+ }
+ 
++void HTTP_FreeHeaders(HTTP_Header **headersList) {
++    HTTP_Header *cur, *next;
++
++    cur = *headersList;
++    while (cur) {
++        next = cur->next;
++        free(cur);
++        cur = next;
++    }
++}
++
+ HTTP_GetResult *HTTP_Client_Get(HTTP_Connection *connection,
+                                 const char *path,
+                                 const char *hash,
+@@ -537,6 +549,8 @@
+     httpStatusCode = HTTP_PassStandardHeaders(headersList,
+                                               &httpContentLength);
+ 
++    HTTP_FreeHeaders(&headersList);
++
+     if (httpStatusCode == 401) /* unauthorized */
+     {
+         /* need a way to report it to the app */
+@@ -630,6 +644,8 @@
+ 
+     httpStatusCode = HTTP_PassStandardHeaders(headersList,
+                                               &httpContentLength);
++
++    HTTP_FreeHeaders(&headersList);
+ 
+     if (httpStatusCode != 200)
+     {
