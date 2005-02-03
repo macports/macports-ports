04a0a870b3204593fa2931adfbaf4cdb6e3eec40
Robert Shaw <rshaw@macports.org>
2005-02-03 03:47:25 +0000

    Added StartupItem for Darwin/MacOSX (uses rc.d on all others). Adds post-activate to properly setup initial conf file if not present. Maintainer email was invalid, also no response from his opendarwin.org account in 72hrs. Taking over maintainer of this port since it appears he has left. Reformatted portfile. Bumped revision.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@10800 d073be05-634f-4543-b044-5fe20cf6d1d6

--- sysutils/anacron/files/anacron.sh
+++ sysutils/anacron/files/anacron.sh
@@ -2,45 +2,21 @@
 
 CRONTAB=/etc/crontab
 
-checkinstall() 
-{
-	# test if anacrontab is present
-	if [ ! -f "__PREFIX/etc/anacrontab" ]; then
-		echo "Error: __PREFIX/etc/anacrontab is missing!"
-		echo "Try copying __PREFIX/etc/anacrontab.sample to __PREFIX/etc/anacrontab"
-		exit 1
-	fi
-
-	# test if default directory are present, if not we create them
-	if [ ! -d "__PREFIX/etc/cron.daily" ]; then
-		mkdir __PREFIX/etc/cron.daily
-	fi
-
-	if [ ! -d "__PREFIX/etc/cron.weekly" ]; then
-		mkdir __PREFIX/etc/cron.weekly
-	fi
-
-	if [ ! -d "__PREFIX/etc/cron.monthly" ]; then
-		mkdir __PREFIX/etc/cron.monthly
-	fi
-
-
-	# Add anacron to /etc/crontab if not already done
-	if [ -z "`grep anacron $CRONTAB`" ]; then
-		echo "10	*	*	*	*	root	__PREFIX/sbin/anacron -s" >>$CRONTAB
-	fi
-}
-
 case "$1" in
 start)
-	checkinstall
+	[ -n "`grep anacron $CRONTAB`" ] \
+		|| echo "10  *  *  *  *  root  __PREFIX/sbin/anacron -s" >>$CRONTAB
 	__PREFIX/sbin/anacron -s
 	;;
 stop)
 	/usr/bin/killall -SIGUSR1 anacron
 	;;
+restart)
+	$0 stop
+	$0 start
+	;;
 *)
-	echo "Usage: `basename $0` {start|stop}" >&2
+	echo "Usage: `basename $0` {start|stop|restart}" >&2
 	;;
 esac
 
