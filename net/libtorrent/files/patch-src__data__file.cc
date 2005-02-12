8054245c1414095d8290d69ba21d32ab83fc458d
Toby Peterson <toby@macports.org>
2005-02-12 07:25:52 +0000

    libtorrent 0.4.8
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@10941 d073be05-634f-4543-b044-5fe20cf6d1d6

--- net/libtorrent/files/patch-src__data__file.cc
+++ net/libtorrent/files/patch-src__data__file.cc
@@ -1,6 +1,6 @@
 --- src/data/file.cc.orig	Tue Dec 28 02:50:38 2004
 +++ src/data/file.cc	Tue Dec 28 02:51:03 2004
-@@ -20,7 +20,6 @@
+@@ -21,7 +21,6 @@
  		   (flags & in  ? O_RDONLY : 0) |
  		   (flags & out ? O_WRONLY : 0)) |
  		  
