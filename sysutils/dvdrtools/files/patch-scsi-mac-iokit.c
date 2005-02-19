343d1c310b35f71b1f29b17f9d5419caf79f2ac4
Markus W. Weißmann <mww@macports.org>
2005-02-19 14:36:47 +0000

    Bug:            https://trac.macports.org/ticket/2687 Submitted by:   pesco@gmx.de Reviewed by:    mww@
    new port sysutils/dvdrtools
    
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@11057 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ sysutils/dvdrtools/files/patch-scsi-mac-iokit.c
@@ -0,0 +1,18 @@
+diff -Naur libscg/scsi-mac-iokit.c libscg-patched/scsi-mac-iokit.c
+--- libscg/scsi-mac-iokit.c	Sat Feb  5 14:32:50 2005
++++ libscg-patched/scsi-mac-iokit.c	Mon Feb 14 13:48:11 2005
+@@ -55,7 +55,14 @@
+ #include <Carbon/Carbon.h>
+ #include <IOKit/IOKitLib.h>
+ #include <IOKit/IOCFPlugIn.h>
++
++/* VERSION is used as an identifier somewhere in the Apple headers. */
++#define AVOID_COLLISION_SAVE_VERSION VERSION
++#undef VERSION
+ #include <IOKit/scsi-commands/SCSITaskLib.h>
++#define VERSION AVOID_COLLISION_SAVE_VERSION
++#undef AVOID_COLLISION_SAVE_VERSION
++
+ #include <mach/mach_error.h>
+ 
+ struct scg_local {
