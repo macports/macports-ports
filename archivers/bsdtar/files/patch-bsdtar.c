1b22ebaedc0c07810fc14a7806d2c33035d73f4c
Toby Peterson <toby@macports.org>
2005-02-20 09:05:40 +0000

    Don't try to extract from /dev/nrst0 when no file is specified.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@11075 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ archivers/bsdtar/files/patch-bsdtar.c
@@ -0,0 +1,15 @@
+--- bsdtar.c.orig	Sun Feb 20 04:01:01 2005
++++ bsdtar.c	Sun Feb 20 04:02:15 2005
+@@ -68,6 +68,12 @@
+ #define	_PATH_DEFTAPE "/dev/st0"
+ #endif
+ 
++/* Alhough it's defined as such, Darwin has no /dev/nrst0. */
++#ifdef __APPLE__
++#undef _PATH_DEFTAPE
++#define _PATH_DEFTAPE "/dev/stdin"
++#endif
++
+ #ifndef _PATH_DEFTAPE
+ #define	_PATH_DEFTAPE "/dev/tape"
+ #endif
