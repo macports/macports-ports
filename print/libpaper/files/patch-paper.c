fdc6674c56fa8edf46bd6026bad55b4c8786b904
Gregory Wright <gwright@macports.org>
2005-02-22 15:29:12 +0000

    New port: libpaper version 1.1.14-3.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@11108 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ print/libpaper/files/patch-paper.c
@@ -0,0 +1,10 @@
+--- lib/paper.c.sav	Tue Feb 22 10:03:44 2005
++++ lib/paper.c	Tue Feb 22 10:03:55 2005
+@@ -14,7 +14,6 @@
+ #include <sys/stat.h>
+ 
+ #include <stdio.h>
+-#include <malloc.h>
+ #include <string.h>
+ #include <ctype.h>
+ 
