219487849ce9e5f64bc9bb5df23718daa778c8e3
Markus W. Weißmann <mww@macports.org>
2005-02-28 13:06:12 +0000

    Noticed by:		tim@ccc.de Solved by:		mww@
    add missing dependency,
    kick unused code that crashes program
    (this code is only useful for debugging purposes and might got broken in the
    transition to python 2.4)
    
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@11152 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ python/py-managesieve/files/patch-managesieve.py
@@ -0,0 +1,11 @@
+--- managesieve.py	Sat May 24 13:45:40 2003
++++ managesieve.py.new	Mon Feb 28 13:58:52 2005
+@@ -13,7 +13,7 @@
+ 
+ __all__ = [ 'MANAGESIEVE', 'SIEVE_PORT', 'OK', 'NO', 'BYE', 'Debug']
+ 
+-from imaplib import _log, _mesg
++#from imaplib import _log, _mesg
+ 
+ Debug = 0
+ CRLF = '\r\n'
