96402c62434d7f7d6c99dec6ac54e91f70ff3748
Markus W. Weißmann <mww@macports.org>
2005-03-15 19:27:16 +0000

    update to v2.6
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@11349 d073be05-634f-4543-b044-5fe20cf6d1d6

--- www/linkchecker/files/patch-setup.py
+++ www/linkchecker/files/patch-setup.py
@@ -1,7 +1,7 @@
---- setup.py	Wed Mar  2 22:14:33 2005
-+++ setup.py	Wed Mar  9 23:21:45 2005
-@@ -252,7 +252,7 @@
-              ['share/locale/es/LC_MESSAGES/linkchecker.mo']),
+--- setup.py	Tue Mar 15 01:04:57 2005
++++ ../../setup.py	Tue Mar 15 20:23:02 2005
+@@ -329,7 +329,7 @@
+ data_files = [
           ('share/linkchecker',
               ['config/linkcheckerrc', 'config/logging.conf', ]),
 -         ('share/linkchecker/examples',
@@ -9,7 +9,7 @@
               ['cgi/lconline/leer.html.en', 'cgi/lconline/leer.html.de',
                'cgi/lconline/index.html', 'cgi/lconline/lc_cgi.html.en',
                'cgi/lconline/lc_cgi.html.de', 'cgi/lconline/check.js',
-@@ -263,7 +263,7 @@
+@@ -340,7 +340,7 @@
      data_files.append(('share/man/man1', ['doc/en/linkchecker.1']))
      data_files.append(('share/man/de/man1', ['doc/de/linkchecker.1']))
      data_files.append(('share/man/fr/man1', ['doc/fr/linkchecker.1']))
