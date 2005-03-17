bd53c4a030defa8c234ce77b9131a37f86caba0b
Markus W. Weißmann <mww@macports.org>
2005-03-17 19:29:43 +0000

    Bug:            https://trac.macports.org/ticket/2710 Submitted by:   dms@freeshell.org Reviewed by:    mww@
    update to v1.1.11, add Growl support
    
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@11377 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ aqua/MacBiff/files/patch-MacBiff.m
@@ -0,0 +1,11 @@
+--- MacBiff.m	Sat Jan  1 19:53:29 2005
++++ ../../MacBiff.m	Thu Mar 17 20:24:39 2005
+@@ -27,7 +27,7 @@
+ #include <pthread.h>
+ #include <sys/types.h>
+ #ifdef USE_GROWL
+-#include <GrowlAppBridge/GrowlDefines.h>
++#include <Growl/GrowlDefines.h>
+ #endif
+ 
+ #import "MacBiff.h"
