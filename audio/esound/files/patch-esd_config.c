a906550bd836c6c7a977a54e2c131e9c385c6c82
Ole Guldberg Jensen <olegb@macports.org>
2005-03-23 07:34:02 +0000

    Bug:            https://trac.macports.org/ticket/2772 Submitted by:   danchr@ Reviewed by:    olegb@ Approved by: Obtained from:
    make esound work on OD
    
    making danchr@ maintainer.
    
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@11416 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ audio/esound/files/patch-esd_config.c
@@ -0,0 +1,11 @@
+--- ../esound-0.2.35.bak/esd_config.c	Wed Mar 23 00:04:57 2005
++++ esd_config.c	Wed Mar 23 00:05:01 2005
+@@ -9,7 +9,7 @@
+ int esd_no_spawn=1; /* If we can't even find the system config file,
+ 		       things are screwed up - don't try to make things
+ 		       worse. */
+-int esd_spawn_wait_ms=100; /* Time to wait trying to connect to an
++int esd_spawn_wait_ms=999; /* Time to wait trying to connect to an
+ 			      autospawned ESD, in milliseconds. */
+ char esd_spawn_options[LINEBUF_SIZE] = "-terminate -nobeeps -as 2";
+ 
