ad31ba2444c2f082a087407e4a6d9b5669b42ebf
Ole Guldberg Jensen <olegb@macports.org>
2005-03-24 21:22:50 +0000

    update to 2.10.0
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@11453 d073be05-634f-4543-b044-5fe20cf6d1d6

--- gnome/gnome-utils/files/patch_logrtns.c
+++ gnome/gnome-utils/files/patch_logrtns.c
@@ -1,46 +1,37 @@
---- logview/logrtns.c.org	Sun Jul 27 18:15:28 2003
-+++ logview/logrtns.c	Sun Jul 27 18:16:30 2003
-@@ -790,11 +790,11 @@
-       if (IsLeapYear (curmark->year - lastyear + thisyear))
- 	 curmark->time += 24 * 60 * 60;		/*  Add one day */
+--- logview/logrtns.c.org	2005-03-24 20:42:30.000000000 +0100
++++ logview/logrtns.c	2005-03-24 20:47:06.000000000 +0100
+@@ -34,6 +34,7 @@
+ #include "logview.h"
+ #include "logrtns.h"
+ #include <libgnomevfs/gnome-vfs-mime-utils.h>
++#include <stdlib.h>
  
+ /*
+  * -------------------
+@@ -645,7 +646,7 @@
+ 	   if (IsLeapYear (curmark->year - lastyear + thisyear))
+ 		   curmark->time += 24 * 60 * 60;		/*  Add one day */
+ 	   
 -#if defined(__NetBSD__) || defined(__FreeBSD__)
-+/* #if defined(__NetBSD__) || defined(__FreeBSD__) */
-       curmark->time += correction - tmptm->tm_gmtoff;
--#else
-+/* #else
-       curmark->time += correction - timezone;
--#endif
-+#endif */
- 
-       memcpy (&curmark->fulldate, localtime (&curmark->time), sizeof (struct tm));
- 
-@@ -816,22 +816,22 @@
++#if defined(__NetBSD__) || defined(__FreeBSD__) || defined(__APPLE__)
+ 	   curmark->time += correction - tmptm->tm_gmtoff;
+ #else
+ 	   curmark->time += correction - timezone;
+@@ -669,7 +670,7 @@
     if (IsLeapYear (thisyear))
-       log->lstats.enddate += 24 * 60 * 60;	/*  Add one day */
- 
+ 	   log->lstats.enddate += 24 * 60 * 60;	/*  Add one day */
+    
 -#if defined(__NetBSD__) || defined(__FreeBSD__)
-+/* #if defined(__NetBSD__) || defined(__FreeBSD__) */
++#if defined(__NetBSD__) || defined(__FreeBSD__) || defined(__APPLE__)
     log->lstats.enddate += correction - tmptm->tm_gmtoff;
--#else
-+/* #else
+ #else
     log->lstats.enddate += correction - timezone;
--#endif
-+#endif */
- 
-    footm.tm_year = thisyear - lastyear;
-    correction = mktime (&footm);
+@@ -680,7 +681,7 @@
     if (IsLeapYear (thisyear - lastyear))
-       log->lstats.startdate += 24 * 60 * 60;	/*  Add one day */
+ 	   log->lstats.startdate += 24 * 60 * 60;	/*  Add one day */
  
 -#if defined(__NetBSD__) || defined(__FreeBSD__)
-+/* #if defined(__NetBSD__) || defined(__FreeBSD__) */
++#if defined(__NetBSD__) || defined(__FreeBSD__) || defined(__APPLE__)
     log->lstats.startdate += correction - tmptm->tm_gmtoff;
--#else
-+/* #else
+ #else
     log->lstats.startdate += correction - timezone;
--#endif
-+#endif*/
-    log->lstats.numlines = nl;
- 
-    return;
