ab29d5ccd81ac7659a1b907f96f06311e50584c2
Ole Guldberg Jensen <olegb@macports.org>
2005-03-24 22:47:34 +0000

    add teh patch ... damnit.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@11459 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ gnome/gcalctool/files/patch-syntax_translation.c
@@ -0,0 +1,11 @@
+--- gcalctool/syntax_translation.c.org	2005-03-24 23:35:52.000000000 +0100
++++ gcalctool/syntax_translation.c	2005-03-24 23:36:12.000000000 +0100
+@@ -21,7 +21,7 @@
+ 
+ #include <stdio.h>
+ #include <libintl.h>
+-#include <malloc.h>
++#include <stdlib.h>
+ #include <assert.h>
+ #include <string.h>
+ 
