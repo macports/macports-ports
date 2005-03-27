6a6ba9515ba5769651a02b0ceaac61fcd2a4bb76
Markus W. Weißmann <mww@macports.org>
2005-03-27 09:19:40 +0000

    update to v0.11
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@11490 d073be05-634f-4543-b044-5fe20cf6d1d6

--- mail/putmail/files/patch-install.sh
+++ mail/putmail/files/patch-install.sh
@@ -1,14 +1,16 @@
---- install.sh	Tue Mar 15 14:18:19 2005
-+++ ../../install.sh	Thu Mar 17 20:34:28 2005
-@@ -1,8 +1,8 @@
+--- install.sh	Sun Mar 20 21:09:21 2005
++++ install.sh.new	Sat Mar 26 20:58:44 2005
+@@ -1,9 +1,9 @@
  #!/usr/bin/env bash
  
 -program_dest=/usr/local/bin
 -i18n_dest=/usr/share/locale
 -doc_dest=/usr/local/share/doc/putmail-`cat doc/VERSION`
+-man_dest=/usr/local/share/man
 +program_dest=__PREFIX__/bin
 +i18n_dest=__PREFIX__/share/locale
 +doc_dest=__PREFIX__/share/doc/putmail
++man_dest=__PREFIX__/share/man
  
  # Create directories if necessary
  if [ ! -e "$program_dest" ]; then
