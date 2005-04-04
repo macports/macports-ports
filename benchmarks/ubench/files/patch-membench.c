50493597a181aaa286adc7e4813f316c42505111
Toby Peterson <toby@macports.org>
2005-04-04 04:55:35 +0000

    Fix build.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@11585 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ benchmarks/ubench/files/patch-membench.c
@@ -0,0 +1,10 @@
+--- membench.c.orig	2005-04-04 00:45:43.000000000 -0400
++++ membench.c	2005-04-04 00:45:53.000000000 -0400
+@@ -33,6 +33,7 @@
+ #include <errno.h>
+ #include <string.h>
+ #include <time.h>
++#include <limits.h>
+ 
+ #if defined HPUX || defined _AIX
+ extern char *sys_errlist[];
