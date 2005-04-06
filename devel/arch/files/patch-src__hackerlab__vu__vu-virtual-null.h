61b8bcea0755b6e9ad2b502e6ff5383826df33d5
Toby Peterson <toby@macports.org>
2005-04-06 14:56:43 +0000

    Build fix.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@11656 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ devel/arch/files/patch-src__hackerlab__vu__vu-virtual-null.h
@@ -0,0 +1,11 @@
+--- src/hackerlab/vu/vu-virtual-null.h.orig	2005-04-04 15:47:21.000000000 -0400
++++ src/hackerlab/vu/vu-virtual-null.h	2005-04-04 15:49:11.000000000 -0400
+@@ -22,7 +22,7 @@
+  * An in-process emulation of /dev/null.
+  *
+  */
+-extern struct vu_fs_discipline vu_virtual_null_vtable;
++//extern struct vu_fs_discipline vu_virtual_null_vtable;
+ 
+ 
+ /* automatically generated __STDC__ prototypes */
