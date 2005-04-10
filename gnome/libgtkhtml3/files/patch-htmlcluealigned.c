496b4535f522925e00cf1c62174f21284283e1e8
Ole Guldberg Jensen <olegb@macports.org>
2005-04-10 13:44:07 +0000

    update to 3.6.1
    \o/ now without teh spellslikahorse hack !!
    
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@11731 d073be05-634f-4543-b044-5fe20cf6d1d6

--- gnome/libgtkhtml3/files/patch-htmlcluealigned.c
+++
@@ -1,11 +0,0 @@
---- src/htmlcluealigned.c.org	2005-03-25 10:37:23.000000000 +0100
-+++ src/htmlcluealigned.c	2005-03-25 10:38:21.000000000 +0100
-@@ -26,7 +26,7 @@
- #define ALIGN_BORDER 0
- 
- 
--static HTMLClueAlignedClass html_cluealigned_class;
-+/* static HTMLClueAlignedClass html_cluealigned_class; */
- static HTMLClueClass *parent_class = NULL;
- 
- 
