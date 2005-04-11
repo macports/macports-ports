15fe0af4ea355d51e48e035e42a66f87d69b833a
Toby Peterson <toby@macports.org>
2005-04-11 08:45:26 +0000

    Fix build on HFSX.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@11744 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ editors/vim/files/patch-src__os_mac.h
@@ -0,0 +1,11 @@
+--- src/os_mac.h.orig	2005-04-03 05:12:15.000000000 -0400
++++ src/os_mac.h	2005-04-03 06:12:23.000000000 -0400
+@@ -19,7 +19,7 @@
+ /*
+  * Macintosh machine-dependent things.
+  */
+-#include <QuickDraw.h>
++#include <Quickdraw.h>
+ #include <ToolUtils.h>
+ #include <LowMem.h>
+ #include <Scrap.h>
