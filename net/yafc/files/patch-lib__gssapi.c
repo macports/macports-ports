c6dd697e009bd341cd2b10da3e37ada86c49767a
Toby Peterson <toby@macports.org>
2005-04-16 19:26:53 +0000

    build fix
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@11826 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ net/yafc/files/patch-lib__gssapi.c
@@ -0,0 +1,10 @@
+--- lib/gssapi.c.orig	2005-04-16 15:16:31.000000000 -0400
++++ lib/gssapi.c	2005-04-16 15:16:35.000000000 -0400
+@@ -72,7 +72,6 @@
+ static gss_OID_desc gss_c_nt_hostbased_service_oid_desc =
+ {10, (void *)"\x2a\x86\x48\x86\xf7\x12" "\x01\x02\x01\x04"};
+ 
+-static gss_OID GSS_C_NT_HOSTBASED_SERVICE = &gss_c_nt_hostbased_service_oid_desc;
+ 
+ 
+ 
