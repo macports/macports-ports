3e180556362432d644605e288fe05c518cdabcb0
Toby Peterson <toby@macports.org>
2005-04-18 15:39:56 +0000

    libarchive 1.02.019
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@11854 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ archivers/libarchive/files/patch-archive_platform.h
@@ -0,0 +1,11 @@
+--- archive_platform.h.orig	2005-04-18 11:36:12.000000000 -0400
++++ archive_platform.h	2005-04-18 11:36:20.000000000 -0400
+@@ -119,7 +119,7 @@
+  * acl_set_file(), we assume it has the rest of the POSIX.1e draft
+  * functions used in archive_read_extract.c.
+  */
+-#if HAVE_SYS_ACL_H && HAVE_ACL_CREATE_ENTRY && HAVE_ACL_INIT && HAVE_ACL_SET_FILE
++#if HAVE_SYS_ACL_H && HAVE_ACL_CREATE_ENTRY && HAVE_ACL_INIT && HAVE_ACL_SET_FILE && !defined(__APPLE__)
+ #define	HAVE_POSIX_ACL	1
+ #endif
+ 
