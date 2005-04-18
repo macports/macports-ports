4061b46ac0b81b77f5696ac5eb2b8b78b6a79291
Toby Peterson <toby@macports.org>
2005-04-18 13:16:39 +0000

    build fix
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@11848 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ devel/indent/files/patch-src__indent.h
@@ -0,0 +1,11 @@
+--- src/indent.h.orig	2005-04-18 09:14:30.000000000 -0400
++++ src/indent.h	2005-04-18 09:14:32.000000000 -0400
+@@ -97,7 +97,7 @@
+ extern unsigned long in_prog_size;
+ 
+ /* The output file. */
+-extern FILE *output;
++//extern FILE *output;
+ 
+ 
+ 
