8bce9b3e14679ada1c9e61078571d982399faaff
Toby Peterson <toby@macports.org>
2005-04-19 13:03:05 +0000

    9e, plan9 unarchiver
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@11889 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ archivers/9e/files/patch-9e.c
@@ -0,0 +1,10 @@
+--- 9e.c.orig	2005-04-19 08:59:41.000000000 -0400
++++ 9e.c	2005-04-19 09:00:03.000000000 -0400
+@@ -4,6 +4,7 @@
+ #include <dirent.h>
+ #include <errno.h>
+ #include <stdio.h>
++#include <stdlib.h>
+ #include <string.h>
+ #include <unistd.h>
+ #include <sys/stat.h>
