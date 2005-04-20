1db55ea2fd30b2276f5114d7cdc1311a15b7e4fa
Toby Peterson <toby@macports.org>
2005-04-20 11:34:25 +0000

    sudosh 1.4.9p1
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@11917 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ shells/sudosh/files/patch-src__sudosh.c
@@ -0,0 +1,10 @@
+--- src/sudosh.c.orig	2005-04-20 07:30:13.000000000 -0400
++++ src/sudosh.c	2005-04-20 07:30:27.000000000 -0400
+@@ -24,6 +24,7 @@
+ #include <unistd.h>
+ #include <signal.h>
+ #include <pwd.h>
++#include <string.h>
+ 
+ #include "config.h"
+ 
