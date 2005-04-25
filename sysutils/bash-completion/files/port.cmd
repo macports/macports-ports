9e30f2023684d14ec865044ff44bc9d2966a8ccf
Charlie Allom <yeled@macports.org>
2005-04-25 10:27:27 +0000

    added the extra 'sync' and 'selfupdate' targets, plus restructured 	slightly to make it more self-consistent.
    	these new cmds only works for release1. toby says release1 is the new
    	HEAD. I think you're all mad.
    
    	port {sync,selfupdate} just spit out silent or stupid errors if someone
    	tries to use them - so what the hey..
    
    Bug:            https://trac.macports.org/ticket/2971
    Submitted by:   Ben Lings <ben.lings@gmail.com>
    Reviewed by:
    Approved by:
    Obtained from:
    
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@12001 d073be05-634f-4543-b044-5fe20cf6d1d6

--- sysutils/bash-completion/files/port.cmd
+++ sysutils/bash-completion/files/port.cmd
@@ -1,40 +1,7 @@
 have port && {
 # helper functions for port completion
 #
-_port_installed_ports()
-{
-        COMPREPLY=( $( port installed | sed -ne \
-        's|^  \('$cur'[^ ]*\).*$|\1|p' | uniq ) )
-}
 
-_port_active_ports()
-{
-        COMPREPLY=( $( port installed | grep '(active)' | sed -ne \
-        's|^  \('$cur'[^ ]*\).*$|\1|p' | uniq ) )
-}
-
-_port_inactive_ports()
-{
-        COMPREPLY=( $( port installed | grep -v '(active)' | sed -ne \
-        's|^  \('$cur'[^ ]*\).*$|\1|p' | uniq ) )
-}
-
-_port_all_ports()
-{
-        COMPREPLY=( $( port $portdiropt list | \
-	awk '/^'$cur'/ { print $1 }' | sort 2> /dev/null ) )
-}
-
-_port_installed_variants()
-{
-        COMPREPLY=( $( port installed | awk '/'$port'/ { print $2 }' | tr '\r' ' ' 2> /dev/null ) )
-        COMPREPLY=( $( compgen -W '${COMPREPLY[@]}' -- $cur ) )
-}
-_port_available_variants()
-{
-        COMPREPLY=( $(port variants $port | tr '\r' ' ' | grep -v "has no variants") )
-        COMPREPLY=( $( compgen -P'+' -W '${COMPREPLY[@]}' -- $cur ) )
-}
 # port(1) completion
 # 
 _port()
@@ -47,104 +14,126 @@ _port()
 
 	count=0
 	for i in ${COMP_WORDS[@]}; do
-		[ $count -eq $COMP_CWORD ] && break
-		# Last parameter was the portdir, now go back to mode selection
-		if [ "${COMP_WORDS[((count))]}" == "$portdir" -a "$mode" == "portdir" ]; then
-			mode=""
-		fi
-		if [ -z "$mode" ]; then
-			case $i in
-			-D)
-				mode=portdir
-				portdir=${COMP_WORDS[((count+1))]}
-				portdiropt="-D $portdir"
-				;;
-			activate|deactivate|install|uninstall|upgrade|clean|deps|contents|variants|info|\
-                        unarchive|fetch|extract|patch|configure|build|destroot|test|archive|\
+	    [ $count -eq $COMP_CWORD ] && break
+	    # Last parameter was the portdir, now go back to mode selection
+	    if [ "${COMP_WORDS[((count))]}" == "$portdir" -a "$mode" == "portdir" ]; then
+		mode=""
+	    fi
+	    if [ -z "$mode" ]; then
+		case $i in
+		    -D)
+			mode=portdir
+			portdir=${COMP_WORDS[((count+1))]}
+			portdiropt="-D $portdir"
+			;;
+		    install|uninstall|activate|deactivate|installed|\
+			outdated|upgrade|selfupdate|sync|\
+			clean|list|search|info|variants|deps|contents|\
+			unarchive|fetch|extract|patch|configure|build|destroot|test|archive|\
 			pkg|mpkg|dmg|rpmpackage|provides)
-				mode=$i
-				;;
-			esac
-		elif [  -z "$port" ]; then
-		    case $mode in
-		    uninstall|upgrade|contents)
-			    if [ $( port installed | awk ' !/The following ports/ {print $1}' \
-				| uniq | grep '^'$i'$') ]; then
-				port=$i
-			    fi
-			    ;;
-		    *)
-			    if [ $(port $portdiropt list | awk '{ print $1 }' | grep '^'$i'$') ]; then
-				port=$i
-			    fi
-			    ;;
-		    esac
-		fi
-		count=$((++count))
+                        mode=$i
+			;;
+		esac
+	    elif [  -z "$port" ]; then
+	    case $mode in
+		uninstall|upgrade|contents)
+		    if [ $( port installed | awk ' !/The following ports/ {print $1}' \
+			| uniq | grep '^'$i'$') ]; then
+			port=$i
+		    fi
+		    ;;
+		*)
+		    if [ $(port $portdiropt list | awk '{ print $1 }' | grep '^'$i'$') ]; then
+			port=$i
+		    fi
+		    ;;
+	    esac
+	    fi
+	    count=$((++count))
 	done
 
 	if [ -n "$port" ]; then
-		case $mode in
-		activate|uninstall)
-			_port_installed_variants
-			return 0
-			;;
-
+            # complete variants
+	    case $mode in
+		contents|uninstall)
+		    # installed variants
+		    COMPREPLY=( $( port installed \
+			| awk '/'$port'/ { print $2 }' | tr '\r' ' ' 2> /dev/null ) )
+		    COMPREPLY=( $( compgen -W '${COMPREPLY[@]}' -- $cur ) )
+		    return 0
+		    ;;
+		activate)
+		    # inactive variants
+		    COMPREPLY=( $( port installed | grep -v '(active)' \
+			| awk '/'$port'/ { print $2 }' | tr '\r' ' ' 2> /dev/null ) )
+		    COMPREPLY=( $( compgen -W '${COMPREPLY[@]}' -- $cur ) )
+		    return 0
+		    ;;
+		deactivate)
+		    # active variants
+		    COMPREPLY=( $( port installed | grep '(active)' \
+			| awk '/'$port'/ { print $2 }' | tr '\r' ' ' 2> /dev/null ) )
+		    COMPREPLY=( $( compgen -W '${COMPREPLY[@]}' -- $cur ) )
+		    return 0
+		    ;;
 		*)
-			_port_available_variants
-			return 0
-			;;
-		esac
+		    # all variants
+		    COMPREPLY=( $(port $portdiropt variants $port | tr '\r' ' ' | grep -v "has no variants") )
+		    COMPREPLY=( $( compgen -P'+' -W '${COMPREPLY[@]}' -- $cur ) )
+		    return 0
+		    ;;
+	    esac
+
 	fi
 
 	if [ -n "$mode" ]; then
-		case $mode in
-		# list installed ports
+	    # complete port names
+	    case $mode in
                 uninstall|upgrade|contents)
-		        if [ -z "$port" ]; then
-		            _port_installed_ports
-			fi
-			return 0
-			;;
+		    # installed ports
+		    COMPREPLY=( $( port installed \
+			| sed -ne 's|^  \('$cur'[^ ]*\).*$|\1|p' | uniq ) )
+		    return 0
+		    ;;
 		activate)
-			_port_inactive_ports
-			return 0
-			;;
+		    # inactive ports
+		    COMPREPLY=( $( port installed | grep -v '(active)' \
+			| sed -ne 's|^  \('$cur'[^ ]*\).*$|\1|p' | uniq ) )
+		    return 0
+		    ;;
 		deactivate)
-			_port_active_ports
-			return 0
-			;;
-		provides)
-			_filedir
-			return 0
-			;;
-		# list all ports
+		    # active ports
+		    COMPREPLY=( $( port installed | grep '(active)' \
+			| sed -ne 's|^  \('$cur'[^ ]*\).*$|\1|p' | uniq ) )
+		    return 0
+		    ;;
+		provides|portdir)
+		    _filedir
+		    return 0
+		    ;;
+		installed|outdated|list|sync|selfupdate)
+		    # no port
+		    return 0
+		    ;;
 		*)
-			if [ -z "$port" ]; then
-			    _port_all_ports
-			fi
-			return 0
-			;;
-
+    		    # all ports
+		    COMPREPLY=( $( port $portdiropt list \
+			| awk '/^'$cur'/ { print $1 }' | sort 2> /dev/null ) )
+		    return 0
+		    ;;
 		esac
 	fi
 
-	if [[ "$cur" == -* ]]; then
-
-		COMPREPLY=( $( compgen -W '-v -d -q -f -o -n -a -u -s -b -c -k' \
-				-- $cur ) )
-	else
-
-		COMPREPLY=( $( compgen -W 'install uninstall upgrade installed outdated \
-				clean list search contents deps variants info \
+	COMPREPLY=( $( compgen -W '-v -d -q -c -k -n -a -u -D -f -o -s -b \
+				install uninstall activate deactivate installed \
+                                outdated upgrade selfupdate sync \
+				clean list search info variants deps contents \
 				unarchive fetch extract patch configure build \
-                                destroot test archive activate deactivate \
+                                destroot test archive \
                                 pkg mpkg dmg rpmpackage provides' -- $cur ) )
 
-	fi
-
+	
 	return 0
 }
 complete -F _port $filenames port
 }
-
