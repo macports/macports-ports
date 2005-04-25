3cebeb41ac58d9901e9b88ecbbacd5f5519ba70b
Toby Peterson <toby@macports.org>
2005-04-25 05:01:00 +0000

    build fix
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@11999 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ aqua/dnsupdate/files/patch-DUInterface.h
@@ -0,0 +1,10 @@
+--- DUInterface.h.orig	2005-04-25 00:57:39.000000000 -0400
++++ DUInterface.h	2005-04-25 00:57:47.000000000 -0400
+@@ -8,7 +8,6 @@
+ 
+ #import <Foundation/Foundation.h>
+ 
+-extern NSString *DUInterfaceOk;
+ extern NSString *DUIPCheckError;
+ 
+ @interface DUInterface : NSObject {
