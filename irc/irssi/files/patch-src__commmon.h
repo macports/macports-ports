32f0ba6e20cecded2de9e6f018ac4e6dcaea9f6a
Toby Peterson <toby@macports.org>
2005-05-01 03:37:23 +0000

    Fix +perl w/ GCC 4.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@12108 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ irc/irssi/files/patch-src__commmon.h
@@ -0,0 +1,11 @@
+--- src/common.h.orig	2005-04-30 23:35:28.000000000 -0400
++++ src/common.h	2005-04-30 23:35:35.000000000 -0400
+@@ -14,7 +14,7 @@
+ #define DEFAULT_SERVER_ADD_PORT 6667
+ 
+ #ifdef HAVE_CONFIG_H
+-#include "config.h"
++#include "../config.h"
+ #endif
+ 
+ #include <stdio.h>
