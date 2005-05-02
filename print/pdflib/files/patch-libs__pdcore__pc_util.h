29d53d2f689979a66674b4c17476ba2917ac182d
Toby Peterson <toby@macports.org>
2005-05-02 05:35:35 +0000

    tiger fix
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@12125 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ print/pdflib/files/patch-libs__pdcore__pc_util.h
@@ -0,0 +1,10 @@
+--- libs/pdcore/pc_util.h.orig	2005-05-02 01:24:15.000000000 -0400
++++ libs/pdcore/pc_util.h	2005-05-02 01:24:21.000000000 -0400
+@@ -19,6 +19,7 @@
+ #ifndef PC_UTIL_H
+ #define PC_UTIL_H
+ 
++#include <sys/types.h>
+ #include <stdlib.h>
+ #include <string.h>
+ #include <limits.h>
