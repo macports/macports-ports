6286f90660941ace1761222d28554c8576c5581d
Toby Peterson <toby@macports.org>
2005-05-03 23:47:15 +0000

    adium 0.80
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@12151 d073be05-634f-4543-b044-5fe20cf6d1d6

--- aqua/adium/files/patch-Frameworks__AIUtilities_Framework__AIUtilities.h
+++
@@ -1,9 +0,0 @@
---- Frameworks/AIUtilities Framework/AIUtilities.h.orig	2005-04-03 19:46:22.000000000 -0400
-+++ Frameworks/AIUtilities Framework/AIUtilities.h	2005-04-03 19:46:34.000000000 -0400
-@@ -120,5 +120,5 @@
- #import <AIUtilities/QTSoundFilePlayer.h>
- #import <AIUtilities/SUSpeaker.h>
- 
--#import <AIUtilities/NDApplescriptObject.h>
-+#import <AIUtilities/NDAppleScriptObject.h>
- #import <AIUtilities/NDComponentInstance.h>
