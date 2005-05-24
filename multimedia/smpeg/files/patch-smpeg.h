4ef91747507118d5f9a4e0a3d90629b70f9ebc9f
Markus W. Weißmann <mww@macports.org>
2005-05-24 20:22:50 +0000

    build dylib on darwin8, fix patch-version in smpeg.h, inc. revision
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@12378 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ multimedia/smpeg/files/patch-smpeg.h
@@ -0,0 +1,11 @@
+--- smpeg.h	2001-04-04 23:58:18.000000000 +0200
++++ smpeg.h	2005-05-24 21:03:05.000000000 +0200
+@@ -34,7 +34,7 @@
+ 
+ #define SMPEG_MAJOR_VERSION      0
+ #define SMPEG_MINOR_VERSION      4
+-#define SMPEG_PATCHLEVEL         3
++#define SMPEG_PATCHLEVEL         4
+ 
+ typedef struct {
+         Uint8 major;
