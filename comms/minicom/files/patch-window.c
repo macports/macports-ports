011e2eeb8b5e42254dfe7e0573e71e86d3286b4f
Waqar Malik <waqar@macports.org>
2005-05-27 18:50:06 +0000

    Bug:            https://trac.macports.org/ticket/3009 Submitted by:   bahamat@digitalelf.net Approved by:    waqar@opendarwin.org Fixed compile issue with Tiger.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@12444 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ comms/minicom/files/patch-window.c
@@ -0,0 +1,11 @@
+--- src/window.c.orig	2005-05-27 13:41:43.000000000 -0500
++++ src/window.c	2005-05-27 13:41:53.000000000 -0500
+@@ -62,7 +62,7 @@
+ static char *CS, *SF, *SR, *VB, *BL;
+ static char *VE, *VI, *KS, *KE;
+ static char *CD, *CL, *IC, *DC;
+-static char *BC, *CR, *NL;
++static char *CR, *NL;
+ #if ST_LINE
+ static char *TS, *FS, *DS;
+ #endif
