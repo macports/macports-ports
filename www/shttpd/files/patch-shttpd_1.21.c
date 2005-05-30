8784c7b896c9fd7a67b1009bf812ee4320143c5c
Toby Peterson <toby@macports.org>
2005-05-30 05:14:59 +0000

    shttpd 1.21
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@12470 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ www/shttpd/files/patch-shttpd_1.21.c
@@ -0,0 +1,11 @@
+--- shttpd_1.21.c.orig	2005-05-30 01:12:32.000000000 -0400
++++ shttpd_1.21.c	2005-05-30 01:12:42.000000000 -0400
+@@ -152,7 +152,7 @@
+ #endif	/* _WIN32 */
+ 
+ /* Darwin and Win32 do not have socklen_t */
+-#if defined(__APPLE_CC__) || defined(_WIN32)
++#if defined(_WIN32)
+ typedef int socklen_t;
+ #endif
+ 
