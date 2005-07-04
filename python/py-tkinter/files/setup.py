a6db243dd5659f9d6570af2b5a95522619e9e634
Markus W. Weißmann <mww@macports.org>
2005-07-04 10:04:47 +0000

    new port python/py-tkinter
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@12935 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ python/py-tkinter/files/setup.py
@@ -0,0 +1,25 @@
+import os, string
+
+try:
+    import distutils
+    from distutils import sysconfig
+    from distutils.command.install import install
+    from distutils.core import setup, Extension
+except:
+    raise SystemExit, "Distutils problem"
+
+tkversion = "__TK-VERSION__"
+prefix = "__PREFIX__"
+inc_dirs = [prefix + "/include"]
+lib_dirs = [prefix + "/lib"]
+libs = ["tcl" + tkversion, "tk" + tkversion]
+
+setup(name = "Tkinter",
+      description = "Tk Extension to Python",
+      
+      ext_modules = [Extension('_tkinter', ['_tkinter.c', 'tkappinit.c'],
+                               define_macros=[('WITH_APPINIT', 1)],
+                               include_dirs = inc_dirs,
+                               libraries = libs,
+                               library_dirs = lib_dirs)]
+      )
