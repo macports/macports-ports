2272e8839efbc8e3759b669510da748f55a21fb6
Markus W. Weißmann <mww@macports.org>
2005-07-11 17:10:22 +0000

    catch all libs - freetype, jpeg, zlib
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@13059 d073be05-634f-4543-b044-5fe20cf6d1d6

--- python/py-pil/files/patch-setup.py
+++ python/py-pil/files/patch-setup.py
@@ -1,18 +1,19 @@
 --- setup.py	2005-03-23 19:16:40.000000000 +0100
-+++ setup.py	2005-07-11 16:02:23.000000000 +0200
-@@ -119,11 +119,10 @@
++++ setup.py	2005-07-11 18:50:55.000000000 +0200
+@@ -28,11 +28,11 @@
+ #
+ # TIFF_ROOT = libinclude("/opt/tiff")
  
-         if sys.platform == "darwin":
-             # attempt to make sure we pick freetype2 over other versions
--            add_directory(include_dirs, "/sw/include/freetype2")
--            add_directory(include_dirs, "/sw/lib/freetype2/include")
--            # fink installation directories
--            add_directory(library_dirs, "/sw/lib")
--            add_directory(include_dirs, "/sw/include")
-+            add_directory(include_dirs, "__PREFIX__/include/freetype2")
-+            # darwinports installation directories
-+            add_directory(library_dirs, "__PREFIX__/lib")
-+            add_directory(include_dirs, "__PREFIX__/include")
+-FREETYPE_ROOT = None
+-JPEG_ROOT = None
+-TIFF_ROOT = None
+-ZLIB_ROOT = None
+-TCL_ROOT = None
++FREETYPE_ROOT = "__PREFIX__/lib/", "__PREFIX__/include/freetype2/"
++JPEG_ROOT = "__PREFIX__/lib", "__PREFIX__/include"
++TIFF_ROOT = "__PREFIX__/lib", "__PREFIX__/include"
++ZLIB_ROOT = "__PREFIX__/lib", "__PREFIX__/include"
++TCL_ROOT = "__PREFIX__/lib", "__PREFIX__/include"
  
-         # FIXME: check /opt/stuff directories here?
+ # FIXME: add mechanism to explicitly *disable* the use of a library
  
