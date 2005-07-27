070bc4f162dd7f11606255de0f9b43cc25374659
Landon Fuller <landonf@macports.org>
2005-07-27 21:49:36 +0000

    Add pyrexdoc, a somewhat shakey documentation generator for Pyrex modules
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@13254 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ python/py-pyrexdoc/files/patch-pyrexdoc.py
@@ -0,0 +1,8 @@
+--- pyrexdoc.py.orig	Wed Jul 27 14:44:28 2005
++++ pyrexdoc.py	Wed Jul 27 14:44:38 2005
+@@ -1,3 +1,5 @@
++#!/usr/bin/env python
++
+ import sys,types,os,os.path
+ from pdb import set_trace as trace
+ 
