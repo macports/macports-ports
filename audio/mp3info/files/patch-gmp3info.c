4053d72dd3b484faff420ad8c76614a0fe9558fa
Yves de Champlain <yves@macports.org>
2005-07-30 12:25:26 +0000

    Add missing patch for gmp3info.c Bug: Submitted by: Reviewed by: Approved by: Obtained from:
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@13304 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ audio/mp3info/files/patch-gmp3info.c
@@ -0,0 +1,19 @@
+--- gmp3info.c.orig	2005-07-30 08:19:11.000000000 -0400
++++ gmp3info.c	2005-07-30 08:20:44.000000000 -0400
+@@ -484,11 +484,11 @@
+ }
+ 
+ void about_mp3info(void) {
+-	quick_popup(VERSION,"MP3Info
+-			An ID3 Tag Editor\n\n
+-			Original author: Ricardo Cerqueira <rmc@plug.pt>
+-			Current maintainer: Cedric Tefft <cedric@earthling.net>
+-			GTK Version: Ricardo Cerqueira <rmc@plug.pt>\n\n
++	quick_popup(VERSION,"MP3Info\
++			An ID3 Tag Editor\n\n\
++			Original author: Ricardo Cerqueira <rmc@plug.pt>\
++			Current maintainer: Cedric Tefft <cedric@earthling.net>\
++			GTK Version: Ricardo Cerqueira <rmc@plug.pt>\n\n\
+ 			Ricardo Cerqueira, Cedric Tefft   1999-2001\n\n");
+ }
+ 
