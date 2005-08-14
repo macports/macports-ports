58ea0c460cb431b624052bf2d809151f6c7f2d33
Paul Guyot <pguyot@macports.org>
2005-08-14 08:31:07 +0000

    Fix a configure detection problem with gcc-4.
    Bug:            https://trac.macports.org/ticket/3604
    Submitted by:
    Reviewed by:
    Approved by:
    Obtained from:
    
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@13504 d073be05-634f-4543-b044-5fe20cf6d1d6

--- lang/ruby/files/patch-mkmf.rb
+++ lang/ruby/files/patch-mkmf.rb
@@ -1,5 +1,5 @@
---- lib/mkmf.rb.orig	Fri Jan 21 10:59:57 2005
-+++ lib/mkmf.rb	Fri Jan 21 11:00:57 2005
+--- lib/mkmf.rb.orig	2005-08-14 17:03:02.000000000 +0900
++++ lib/mkmf.rb	2005-08-14 17:03:36.000000000 +0900
 @@ -47,6 +47,9 @@
  $sitedir = CONFIG["sitedir"]
  $sitelibdir = CONFIG["sitelibdir"]
@@ -10,7 +10,28 @@
  
  $mswin = /mswin/ =~ RUBY_PLATFORM
  $bccwin = /bccwin/ =~ RUBY_PLATFORM
-@@ -751,6 +754,7 @@
+@@ -347,7 +350,7 @@
+ 
+ def try_func(func, libs, headers = nil, &b)
+   headers = cpp_include(headers)
+-  try_link(<<"SRC", libs, &b) or try_link(<<"SRC", libs, &b)
++  try_link(<<"SRC", libs, &b) or try_link(<<"SRC", libs, &b) or try_link(<<"SRC", libs, &b)
+ #{headers}
+ /*top*/
+ int main() { return 0; }
+@@ -359,6 +362,11 @@
+ int main() { return 0; }
+ int t() { void ((*volatile p)()); p = (void ((*)()))#{func}; return 0; }
+ SRC
++int #{func}();
++/*top*/
++int main() { return 0; }
++int t() { #{func}(); return 0; }
++SRC
+ end
+ 
+ def egrep_cpp(pat, src, opt = "", &b)
+@@ -751,6 +759,7 @@
  RUBY_SO_NAME = #{CONFIG['RUBY_SO_NAME']}
  arch = #{CONFIG['arch']}
  sitearch = #{CONFIG['sitearch']}
@@ -18,7 +39,7 @@
  ruby_version = #{Config::CONFIG['ruby_version']}
  ruby = #{$ruby}
  RUBY = #{($nmake && !$extmk && !$configure_args.has_key?('--ruby')) ? '$(ruby:/=\)' : '$(ruby)'}
-@@ -962,7 +966,7 @@
+@@ -962,7 +971,7 @@
    $CFLAGS = with_config("cflags", arg_config("CFLAGS", config["CFLAGS"])).dup
    $ARCH_FLAG = with_config("arch_flag", arg_config("ARCH_FLAG", config["ARCH_FLAG"])).dup
    $CPPFLAGS = with_config("cppflags", arg_config("CPPFLAGS", config["CPPFLAGS"])).dup
