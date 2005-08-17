ad8f07d0a610826da893f79e3c24a62c6d69d107
Paul Guyot <pguyot@macports.org>
2005-08-17 03:25:40 +0000

    New port: ttf2pt1.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@13590 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ print/ttf2pt1/files/patch-ft.c
@@ -0,0 +1,10 @@
+--- ft.c	2004-01-01 06:30:50.000000000 +0900
++++ ft.c	2005-08-17 12:04:06.000000000 +0900
+@@ -12,6 +12,7 @@
+ #include <stdlib.h>
+ #include <ctype.h>
+ #include <sys/types.h>
++#include <ft2build.h>
+ #include <freetype/freetype.h>
+ #include <freetype/ftglyph.h>
+ #include <freetype/ftsnames.h>
