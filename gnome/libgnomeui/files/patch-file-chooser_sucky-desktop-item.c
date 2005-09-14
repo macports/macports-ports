faf1490780bbba679ea64a882c2b16e9d01fee4e
Ole Guldberg Jensen <olegb@macports.org>
2005-09-14 07:58:02 +0000

    add patch and remove patch.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@14100 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ gnome/libgnomeui/files/patch-file-chooser_sucky-desktop-item.c
@@ -0,0 +1,16 @@
+--- file-chooser/sucky-desktop-item.c.org	Wed Sep 14 00:52:11 2005
++++ file-chooser/sucky-desktop-item.c	Wed Sep 14 00:54:15 2005
+@@ -67,7 +67,12 @@
+ 
+ #define sure_string(s) ((s)!=NULL?(s):"")
+ 
+-extern char **environ;
++#ifdef __APPLE__
++# include <crt_externs.h
+++# define environ (*_NSGetEnviron())
++#elif
++ extern char **environ;
++#endif
+ 
+ struct _SuckyDesktopItem {
+ 	int refcount;
