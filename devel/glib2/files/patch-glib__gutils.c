530874f54d25ca2b6f77dd6c6eaa09bef3cd7144
Chris Ridd <cjr@macports.org>
2005-09-20 18:23:52 +0000

    Fix for _environ on Tiger from Mike Alexander
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@14173 d073be05-634f-4543-b044-5fe20cf6d1d6

--- devel/glib2/files/patch-glib__gutils.c
+++ devel/glib2/files/patch-glib__gutils.c
@@ -1,12 +1,14 @@
 --- glib/gutils.c.org	2005-09-08 08:58:41.000000000 +0200
 +++ glib/gutils.c	2005-09-08 08:58:58.000000000 +0200
-@@ -46,9 +46,6 @@
+@@ -46,9 +46,10 @@
  #ifdef HAVE_SYS_PARAM_H
  #include <sys/param.h>
  #endif
--#ifdef HAVE_CRT_EXTERNS_H 
+ #ifdef HAVE_CRT_EXTERNS_H 
 -#include <crt-externs.h> /* for _NSGetEnviron */
--#endif
++#include <crt_externs.h> /* for _NSGetEnviron */
++#define HAVE__NSGETENVIRON
+ #endif
  
  /* implement gutils's inline functions
   */
