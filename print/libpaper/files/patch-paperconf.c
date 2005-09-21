03019c9aa8fd0d489f504f888bd8d3df2dff579d
Gregory Wright <gwright@macports.org>
2005-09-21 02:00:46 +0000

    Patch to fix bug https://trac.macports.org/ticket/5013. Thanks to Michail Vidiassov!
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@14193 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ print/libpaper/files/patch-paperconf.c
@@ -0,0 +1,10 @@
+--- src/paperconf.c.org	2004-10-23 13:53:15.000000000 +0400
++++ src/paperconf.c	2005-09-16 23:18:59.000000000 +0400
+@@ -10,6 +10,7 @@
+ #include <stdio.h>
+ #include <string.h>
+ #include <errno.h>
++#include <stdlib.h>
+ 
+ #include <paper.h>
+ 
