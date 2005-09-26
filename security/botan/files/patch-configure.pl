7dee5a493b2d00c45b10ca2929dbb3ec19d2d8cb
Markus W. Weißmann <mww@macports.org>
2005-09-26 11:40:13 +0000

    update to 1.4.7
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@14266 d073be05-634f-4543-b044-5fe20cf6d1d6

--- security/botan/files/patch-configure.pl
+++ security/botan/files/patch-configure.pl
@@ -1,20 +1,29 @@
---- configure.pl	Mon Mar 14 01:31:05 2005
-+++ ../../configure.pl	Wed Apr 13 18:52:26 2005
-@@ -680,7 +680,7 @@
+--- work/Botan-1.4.7/configure.pl	2005-09-26 03:57:10.000000000 +0200
++++ configure.pl	2005-09-26 12:34:33.000000000 +0200
+@@ -374,7 +374,7 @@
+       'root'       => 'c:\Botan',
+       },
+    'darwin'     => {
+-      'docs'       => 'doc',
++      'docs'       => 'share/doc',
+       'group'      => 'wheel',
+       },
+    'defaults'   => {
+@@ -703,7 +703,7 @@
+    'gcc'        => {
+       'aix'        => '$(CXX) -shared -fPIC',
        'beos'       => 'ld -shared -h $(SONAME)',
+-      'darwin'     => '$(CXX) -dynamiclib -fPIC -install_name $(SONAME)',
++      'darwin'     => '$(CXX) -dynamiclib -fPIC -install_name $(LIBDIR)/$(SONAME)',
        'default'    => '$(CXX) -shared -fPIC -Wl,-soname,$(SONAME)',
        'hpux'       => '$(CXX) -shared -fPIC -Wl,+h,$(SONAME)',
--      'macosx'     => '$(CXX) -dynamiclib -fPIC -install_name $(SONAME)',
-+      'macosx'     => '$(CXX) -dynamiclib -fPIC -install_name $(LIBDIR)/$(SONAME)',
        'solaris'    => '$(CXX) -shared -fPIC -Wl,-h,$(SONAME)',
-       },
-    'hpcc'       => {
-@@ -2083,7 +2083,7 @@
+@@ -2111,7 +2111,7 @@
  BINDIR        = \$(INSTALLROOT)/bin
  LIBDIR        = \$(INSTALLROOT)/$lib_dir
  HEADERDIR     = \$(INSTALLROOT)/$header_dir/botan
 -DOCDIR        = \$(INSTALLROOT)/$doc_dir/Botan-\$(VERSION)
-+DOCDIR        = \$(INSTALLROOT)/share/doc/botan
++DOCDIR        = \$(INSTALLROOT)/$doc_dir/botan
  
  OWNER         = $install_user
  GROUP         = $install_group
