07643b6cb2e1670748d960cc439dfbed33a577aa
Toby Peterson <toby@macports.org>
2005-09-29 00:19:05 +0000

    Fix w/ openssl 0.9.8
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@14312 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ net/dsniff/files/patch-sshcrypto.c
@@ -0,0 +1,11 @@
+--- sshcrypto.c.orig	2005-09-28 17:08:59.000000000 -0700
++++ sshcrypto.c	2005-09-28 17:09:09.000000000 -0700
+@@ -14,6 +14,8 @@
+ 
+ #include <sys/types.h>
+ #include <openssl/ssl.h>
++#include <openssl/blowfish.h>
++#include <openssl/des.h>
+ 
+ #include <err.h>
+ #include <stdio.h>
