260b38b58d20128140f5cc1539334c74b8e74bdd
Toby Peterson <toby@macports.org>
2005-10-03 17:51:00 +0000

    wordplay 7.22
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@14391 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ games/wordplay/files/patch-wordplay.c
@@ -0,0 +1,10 @@
+--- wordplay.c.orig	2005-10-03 10:45:29.000000000 -0700
++++ wordplay.c	2005-10-03 10:45:50.000000000 -0700
+@@ -136,6 +136,7 @@
+ */
+ 
+ #include <stdio.h>
++#include <stdlib.h>
+ #include <string.h>
+ #include <ctype.h>
+ 
