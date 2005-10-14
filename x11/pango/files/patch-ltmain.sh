4afd7b705644b360a1fbe148fa515a4d414fb25c
Randall H. Wood <rhwood@macports.org>
2005-10-14 23:18:12 +0000

    Update pango from version 1.10.0 to 1.10.1
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@14639 d073be05-634f-4543-b044-5fe20cf6d1d6

--- x11/pango/files/patch-ltmain.sh
+++ x11/pango/files/patch-ltmain.sh
@@ -1,15 +1,15 @@
---- ltmain.sh.orig	2005-09-11 11:11:38.000000000 -0700
-+++ ltmain.sh	2005-09-11 11:12:01.000000000 -0700
-@@ -1369,7 +1369,7 @@
+--- ltmain.sh.orig	2005-09-14 11:00:12.000000000 -0400
++++ ltmain.sh	2005-10-14 19:07:04.000000000 -0400
+@@ -1518,7 +1518,7 @@
  	continue
  	;;
  
 -     -mt|-mthreads|-kthread|-Kthread|-pthread|-pthreads|--thread-safe)
 +     -mt|-mthreads|-kthread|-Kthread|-lpthread|-lpthreads|--thread-safe)
- 	deplibs="$deplibs $arg"
- 	continue
- 	;;
-@@ -1853,7 +1853,7 @@
+ 	compiler_flags="$compiler_flags $arg"
+ 	compile_command="$compile_command $arg"
+ 	finalize_command="$finalize_command $arg"
+@@ -2005,7 +2005,7 @@
  	lib=
  	found=no
  	case $deplib in
