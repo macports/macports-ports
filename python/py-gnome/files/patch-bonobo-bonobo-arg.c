95b879f5e2de24abfc0a09e463de384a2da54c33
Paul Guyot <pguyot@macports.org>
2005-10-25 15:05:29 +0000

    Bug:            https://trac.macports.org/ticket/5095, https://trac.macports.org/ticket/5470 Submitted by:   Olaf Foellinger Reviewed by: Approved by: Obtained from:
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@14810 d073be05-634f-4543-b044-5fe20cf6d1d6

--- python/py-gnome/files/patch-bonobo-bonobo-arg.c
+++
@@ -1,9 +0,0 @@
---- bonobo/bonobo-arg.c.orig	Sun Feb  1 23:20:45 2004
-+++ bonobo/bonobo-arg.c	Sun Feb  1 23:21:01 2004
-@@ -1,5 +1,6 @@
- /* -*- Mode: C; c-basic-offset: 4 -*- */
- 
-+#define NO_IMPORT_PYGOBJECT
- #include <pygobject.h>
- #include <pyorbit.h>
- #include <bonobo/bonobo-types.h>
