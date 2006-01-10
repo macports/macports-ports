bed7be7e0f08982b68bef2797f9a931268381823
Markus W. Weißmann <mww@macports.org>
2006-01-10 08:29:17 +0000

    version 0.7.0
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@15796 d073be05-634f-4543-b044-5fe20cf6d1d6

--- python/py-ipython/files/patch-setup.py
+++ python/py-ipython/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py	Wed Dec  1 00:32:32 2004
-+++ setup.py.new	Sun Dec  5 20:50:27 2004
+--- work/ipython-0.7.0/setup.py	2005-12-25 01:46:14.000000000 +0100
++++ setup.py	2006-01-10 09:27:58.000000000 +0100
 @@ -99,7 +99,7 @@
- 
- # I can't find how to make distutils create a nested dir. structure, so
- # in the meantime do it manually. Butt ugly.
+ # Note that http://www.redbrick.dcu.ie/~noel/distutils.html, ex. 2/3, contain
+ # information on how to do this more cleanly once python 2.4 can be assumed.
+ # Thanks to Noel for the tip.
 -docdirbase  = 'share/doc/ipython-%s' % version
-+docdirbase  = 'share/doc/py-ipython'
++docdirbase  = 'share/doc/ipython'
  manpagebase = 'share/man/man1'
  
  # We only need to exclude from this things NOT already excluded in the
