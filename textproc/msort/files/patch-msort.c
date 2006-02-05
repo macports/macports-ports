7cdc9a5e53dca85ee1749c8efb72bf12033eaea8
Markus W. Weißmann <mww@macports.org>
2006-02-05 10:07:49 +0000

    version 8.13
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@16242 d073be05-634f-4543-b044-5fe20cf6d1d6

--- textproc/msort/files/patch-msort.c
+++ textproc/msort/files/patch-msort.c
@@ -1,8 +1,8 @@
---- msort.c	2005-10-29 04:29:48.000000000 +0200
-+++ msort.c	2005-10-29 18:01:52.000000000 +0200
-@@ -1259,8 +1259,6 @@
-   extern int GetTimeKey(wchar_t *, double *);
-   extern int GetDateKey(wchar_t *, double *,struct ymdinfo *);
+--- msort.c	2006-02-04 21:52:58.000000000 +0100
++++ msort.c	2006-02-05 11:05:25.000000000 +0100
+@@ -1330,8 +1330,6 @@
+   extern int GetTimeKey(wchar_t *, DOUBLE *);
+   extern int GetDateKey(wchar_t *, DOUBLE *,struct ymdinfo *);
    extern wchar_t *wcCopyRange(wchar_t *, long, long);
 -  extern int fwprintf (__FILE *__restrict __stream,
 -		       __const wchar_t *__restrict __format, ...);
