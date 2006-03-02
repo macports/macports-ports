71c4bb37a7f0bbeb5b413eed3f24e1646409ea30
Markus W. Weißmann <mww@macports.org>
2006-03-02 19:06:15 +0000

    Bug:            https://trac.macports.org/ticket/7535 Submitted by:   mww@
    fix possible build errors
    
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@16729 d073be05-634f-4543-b044-5fe20cf6d1d6

--- python/py-sqlite/files/patch-setup.py
+++ python/py-sqlite/files/patch-setup.py
@@ -1,5 +1,5 @@
---- setup.py.orig	2006-02-27 13:22:00.000000000 -0800
-+++ setup.py	2006-02-27 13:22:40.000000000 -0800
+--- setup.py	2006-02-02 12:01:31.000000000 +0100
++++ setup.py	2006-03-02 19:55:53.000000000 +0100
 @@ -23,10 +23,11 @@
  
  import glob, os, sys
@@ -15,3 +15,17 @@
  
  # If you need to change anything, it should be enough to change setup.cfg.
  
+@@ -90,11 +91,11 @@
+ 
+ def main():
+     build_docs()
+-    data_files = [("pysqlite2-doc",
++    data_files = [("share/doc/py-sqlite",
+                         glob.glob("doc/*.html") \
+                       + glob.glob("doc/*.txt") \
+                       + glob.glob("doc/*.css")),
+-                   ("pysqlite2-doc/code",
++                   ("share/doc/py-sqlite/code",
+                         glob.glob("doc/code/*.py"))]
+ 
+     if sys.platform == "win32":
