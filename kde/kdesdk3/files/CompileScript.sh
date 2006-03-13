67cc505155b71ed45f54e6b610828021b0e888f8
Takanori Yamamoto <takanori@macports.org>
2006-03-13 09:48:01 +0000

    Synced with Fink, revision 1023 Submitted by:   takanori@ Reviewed by:    ben@
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@16917 d073be05-634f-4543-b044-5fe20cf6d1d6

--- kde/kdesdk3/files/CompileScript.sh
+++ kde/kdesdk3/files/CompileScript.sh
@@ -9,13 +9,16 @@
         . ./environment-helper.sh
         export lt_cv_sys_max_cmd_len=65536
 
-        export CC=gcc-3.3 CXX=g++-3.3
+        export CC=gcc CXX=g++
 
         ./build-helper.sh cvs       %N %v %r make -f admin/Makefile.common cvs
-        ./build-helper.sh configure %N %v %r ./configure %c
+        ./build-helper.sh configure %N %v %r ./configure %c $CONFIGURE_PARAMS
 
         perl -pi -e 's,(..libdir.)/kde3,$1,g' kompare/libdialogpages/Makefile
         find . -name \*.l -exec touch {} \;
         find . -name \*.ll -exec touch {} \;
 
-        ./build-helper.sh make      %N %v %r make all LIB_DBIV=-ldb-4.2 all_libraries="$ALL_LIBRARIES"
+	#fink
+        #./build-helper.sh make      %N %v %r make all LIB_DBIV=-ldb-4.2 all_libraries="$ALL_LIBRARIES"
+	#darwinports
+        ./build-helper.sh make      %N %v %r make all LIB_DBIV=-ldb-4.3 all_libraries="$ALL_LIBRARIES"
