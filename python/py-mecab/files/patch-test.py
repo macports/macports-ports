a6b91c9335d455d8399166d9939ed0350f166c01
Takanori Yamamoto <takanori@macports.org>
2006-03-30 03:05:29 +0000

    New port: py-mecab Bug:            https://trac.macports.org/ticket/8000 Submitted by:   rsky0711@gmail.com Reviewed by:    takanori@
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@17238 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ python/py-mecab/files/patch-test.py
@@ -0,0 +1,9 @@
+--- test.py.orig	2006-03-19 02:10:55.000000000 +0900
++++ test.py	2006-03-23 21:10:13.000000000 +0900
+@@ -1,4 +1,5 @@
+-#!/usr/bin/python
++#!%PREFIX%/bin/python
++# -*- coding:euc-jp -*-
+ 
+ import MeCab
+ import sys
