f8a858e12d9a5ed60362df18cd7063582ed090e0
Mark Duling <markd@macports.org>
2006-04-08 19:37:35 +0000

    Bug: NONE Submitted by: markd@opendarwin.org Reviewed by: Approved by: Obtained from: Version bump; old tarballs not available.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@17368 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ security/prelude-manager/files/patch-src-server-generic.c
@@ -0,0 +1,10 @@
+--- src/server-generic.c.org.c	2006-03-16 08:42:22.000000000 -0800
++++ src/server-generic.c	2006-04-08 12:27:25.000000000 -0700
+@@ -37,7 +37,6 @@
+ #include <sys/types.h>
+ #include <arpa/inet.h>
+ #include <syslog.h>
+-#include <sys/poll.h>
+ #include <assert.h>
+ #include <sys/stat.h>
+ #include <signal.h>
---
+++ security/prelude-manager/files/patch-src-server-logic.c
@@ -0,0 +1,10 @@
+--- src/server-logic.c.org.c	2006-03-16 08:46:07.000000000 -0800
++++ src/server-logic.c	2006-04-08 12:26:59.000000000 -0700
+@@ -25,7 +25,6 @@
+ 
+ #include <stdio.h>
+ #include <stdlib.h>
+-#include <sys/poll.h>
+ #include <pthread.h>
+ #include <assert.h>
+ #include <errno.h>
