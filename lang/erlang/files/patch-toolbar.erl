7715db02f9aa8bb2ecb85c68627b22a1b787ec9a
Gregory Wright <gwright@macports.org>
2006-05-22 22:05:56 +0000

    Still need the long timeout for the browser to start, otherwise the html help pages can't be viewed. Bug: Submitted by: Reviewed by: Approved by: Obtained from:
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@18061 d073be05-634f-4543-b044-5fe20cf6d1d6

--- lang/erlang/files/patch-toolbar.erl
+++ lang/erlang/files/patch-toolbar.erl
@@ -1,7 +1,7 @@
---- lib/toolbar/src/toolbar.erl.sav	Fri Feb 11 13:09:09 2005
-+++ lib/toolbar/src/toolbar.erl	Fri Feb 11 13:09:41 2005
-@@ -43,7 +43,7 @@
- -export([init/1,start_tool/3]). % spawn
+--- lib/toolbar/src/toolbar.erl.sav	2006-05-22 17:04:22.000000000 -0400
++++ lib/toolbar/src/toolbar.erl	2006-05-22 17:05:07.000000000 -0400
+@@ -36,7 +36,7 @@
+ -export([create_tool_file/0,add_gs_contribs/0]).
  
  %
 --define (STARTUP_TIMEOUT, 20000).
@@ -9,14 +9,12 @@
  
  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
  
-@@ -236,8 +236,8 @@
- 		
- 		%% About help
+@@ -234,7 +234,7 @@
  		about_help ->
--		    tool_utils:notify(S,["Help text is on HTML format",
--		                  "Requires Netscape to be up and running"]),
-+		    tool_utils:notify(S,["Help text is in HTML format and",
-+		                  "will be displayed in your default browser."]),
+ 		    WinObj = toolbar_graphics:get_window(Window),
+ 		    Text = ["Help text is on HTML format",
+-			    "Requires Netscape to be up and running"],
++			    "will be displayed in your default browser"],
+ 		    tool_utils:notify(WinObj, Text),
  		    loop(S,Window,LoopData,TimerRef);
  
- 		%% Window has been resized, redraw it
