c1655d841ddcdfac457b7362e64b2d82fc14abef
Gregory Wright <gwright@macports.org>
2006-05-26 01:37:51 +0000

    Fix patch file. Bug: Submitted by: Reviewed by: Approved by: Obtained from:
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@18105 d073be05-634f-4543-b044-5fe20cf6d1d6

--- lang/hugs98/files/patch-builtin.c
+++ lang/hugs98/files/patch-builtin.c
@@ -1,17 +1,6 @@
---- src/builtin.c	2005/09/16 16:03:46	1.91
-+++ src/builtin.c	2006/05/19 21:20:51	1.92
-@@ -7,8 +7,8 @@
-  * the license in the file "License", which is included in the distribution.
-  *
-  * $RCSfile: patch-builtin.c,v $
-- * $Revision: 1.1 $
-- * $Date: 2006/05/19 23:50:17 $
-+ * $Revision: 1.1 $
-+ * $Date: 2006/05/19 23:50:17 $
-  * ------------------------------------------------------------------------*/
- 
- /* We include math.h before prelude.h because SunOS 4's cpp incorrectly
-@@ -1988,6 +1988,7 @@ static struct thunk_data* foreignThunks 
+--- src/builtin.c.orig	2005-09-16 12:03:46.000000000 -0400
++++ src/builtin.c	2006-05-25 21:33:22.000000000 -0400
+@@ -1988,6 +1988,7 @@
     to return to it before tail jumping from the adjustor thunk.
  */
  static unsigned char *obscure_ccall_ret_code;	/* set by initAdjustor() */
@@ -19,7 +8,7 @@
  
  /* Heavily arch-specific, I'm afraid.. */
  
-@@ -2024,8 +2025,6 @@ static void* local mallocBytesRWX(int le
+@@ -2024,8 +2025,6 @@
      return addr;
  }
  
