c18ee9b9fbab89de8c7d93aedf387681105285dd
Jordan K. Hubbard <jkh@macports.org>
2006-08-28 03:07:50 +0000

    Update and make work properly on Darwin 9.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@19297 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ devel/mono/files/patch-interp.h
@@ -0,0 +1,13 @@
+--- ./mono/interpreter/interp.h.orig	2006-08-19 14:13:56.000000000 -0700
++++ ./mono/interpreter/interp.h	2006-08-19 14:14:02.000000000 -0700
+@@ -115,8 +115,8 @@
+ void
+ mono_interp_transform_init (void);
+ 
+-void inline stackval_from_data (MonoType *type, stackval *result, char *data, gboolean pinvoke);
+-void inline stackval_to_data (MonoType *type, stackval *val, char *data, gboolean pinvoke);
++void stackval_from_data (MonoType *type, stackval *result, char *data, gboolean pinvoke);
++void stackval_to_data (MonoType *type, stackval *val, char *data, gboolean pinvoke);
+ void ves_exec_method (MonoInvocation *frame);
+ 
+ /*
