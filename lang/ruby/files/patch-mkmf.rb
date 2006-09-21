f9b5238ebcf4b261bf7ad3a9de3b8c7080670feb
James D. Berry <jberry@macports.org>
2006-09-21 01:25:57 +0000

    Update ruby --> 1.8.5
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@19642 d073be05-634f-4543-b044-5fe20cf6d1d6

--- lang/ruby/files/patch-mkmf.rb
+++ lang/ruby/files/patch-mkmf.rb
@@ -1,6 +1,6 @@
---- lib/mkmf.rb.orig	2005-11-27 16:22:53.000000000 -0800
-+++ lib/mkmf.rb	2006-01-08 08:38:20.000000000 -0800
-@@ -50,6 +50,9 @@
+--- lib/mkmf.rb.orig	2006-08-16 22:47:50.000000000 -0700
++++ lib/mkmf.rb	2006-09-20 16:54:17.000000000 -0700
+@@ -51,6 +51,9 @@
  $sitedir = CONFIG["sitedir"]
  $sitelibdir = CONFIG["sitelibdir"]
  $sitearchdir = CONFIG["sitearchdir"]
@@ -10,7 +10,7 @@
  
  $mswin = /mswin/ =~ RUBY_PLATFORM
  $bccwin = /bccwin/ =~ RUBY_PLATFORM
-@@ -409,7 +412,7 @@
+@@ -410,7 +413,7 @@
  
  def try_func(func, libs, headers = nil, &b)
    headers = cpp_include(headers)
@@ -19,7 +19,7 @@
  #{headers}
  /*top*/
  int main() { return 0; }
-@@ -421,6 +424,11 @@
+@@ -422,6 +425,11 @@
  int main() { return 0; }
  int t() { void ((*volatile p)()); p = (void ((*)()))#{func}; return 0; }
  SRC
@@ -31,7 +31,7 @@
  end
  
  def try_var(var, headers = nil, &b)
-@@ -970,6 +978,7 @@
+@@ -1080,6 +1088,7 @@
  RUBY_SO_NAME = #{CONFIG['RUBY_SO_NAME']}
  arch = #{CONFIG['arch']}
  sitearch = #{CONFIG['sitearch']}
@@ -39,12 +39,12 @@
  ruby_version = #{Config::CONFIG['ruby_version']}
  ruby = #{$ruby}
  RUBY = $(ruby#{sep})
-@@ -1269,7 +1278,7 @@
+@@ -1400,7 +1409,7 @@
    $CFLAGS = with_config("cflags", arg_config("CFLAGS", config["CFLAGS"])).dup
    $ARCH_FLAG = with_config("arch_flag", arg_config("ARCH_FLAG", config["ARCH_FLAG"])).dup
    $CPPFLAGS = with_config("cppflags", arg_config("CPPFLAGS", config["CPPFLAGS"])).dup
 -  $LDFLAGS = (with_config("ldflags") || "").dup
 +  $LDFLAGS = with_config("ldflags", arg_config("LDFLAGS", config["LDFLAGS"])).dup
-   $INCFLAGS = "-I$(topdir)"
+   $INCFLAGS = "-I$(topdir) -I$(hdrdir) -I$(srcdir)"
    $DLDFLAGS = with_config("dldflags", arg_config("DLDFLAGS", config["DLDFLAGS"])).dup
    $LIBEXT = config['LIBEXT'].dup
--- lang/ruby/files/patch-ruby.c
+++ lang/ruby/files/patch-ruby.c
@@ -1,5 +1,5 @@
---- ruby.c.orig	2005-12-11 16:36:52.000000000 -0800
-+++ ruby.c	2006-02-02 12:38:41.000000000 -0800
+--- ruby.c.orig	2006-08-15 19:11:21.000000000 -0700
++++ ruby.c	2006-09-20 17:32:18.000000000 -0700
 @@ -298,6 +298,13 @@
      ruby_incpush(RUBY_RELATIVE(RUBY_SITE_ARCHLIB));
      ruby_incpush(RUBY_RELATIVE(RUBY_SITE_LIB));
@@ -14,71 +14,3 @@
      ruby_incpush(RUBY_RELATIVE(RUBY_LIB));
  #ifdef RUBY_THIN_ARCHLIB
      ruby_incpush(RUBY_RELATIVE(RUBY_THIN_ARCHLIB));
-@@ -1033,43 +1033,47 @@
-     rb_progname = rb_tainted_str_new(s, i);
- #else
-     if (len == 0) {
--	char *s = origargv[0];
--	int i;
-+	char *s1 = origargv[0];
-+	int j;
- 
--	s += strlen(s);
-+	s1 += strlen(s1);
- 	/* See if all the arguments are contiguous in memory */
--	for (i = 1; i < origargc; i++) {
--	    if (origargv[i] == s + 1) {
--		s++;
--		s += strlen(s);	/* this one is ok too */
-+	for (j = 1; j < origargc; j++) {
-+	    if (origargv[j] == s1 + 1) {
-+		s1++;
-+		s1 += strlen(s1);	/* this one is ok too */
- 	    }
- 	    else {
- 		break;
- 	    }
- 	}
- #ifndef DOSISH
--	if (s + 1 == envspace.begin) {
--	    s = envspace.end;
-+	if (s1 + 1 == envspace.begin) {
-+	    s1 = envspace.end;
- 	    ruby_setenv("", NULL); /* duplicate environ vars */
- 	}
- #endif
--	len = s - origargv[0];
-+	len = s1 - origargv[0];
-     }
- 
-     if (i >= len) {
- 	i = len;
--	memcpy(origargv[0], s, i);
--	origargv[0][i] = '\0';
-     }
--    else {
--	memcpy(origargv[0], s, i);
--	s = origargv[0]+i;
--	*s++ = '\0';
--	while (++i < len)
--	    *s++ = ' ';
--	for (i = 1; i < origargc; i++)
--	    origargv[i] = 0;
-+    memcpy(origargv[0], s, i);
-+    memset(origargv[0] + i, '\0', len - i + 1);
-+
-+    /* If the new program name is longer than the original one, then
-+     * have any command line arguments that were written over be
-+     * empty strings. */
-+    s = origargv[0] + i;
-+    for (i = 1; i < origargc; ++i) {
-+	if (origargv[i] < s ) {
-+	    origargv[i] = s;
-+	} else {
-+	    break;
-+	}
-     }
-+
-     rb_progname = rb_tainted_str_new2(origargv[0]);
- #endif
- }
