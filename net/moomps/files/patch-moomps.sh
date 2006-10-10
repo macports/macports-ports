261556235e3f978a9b672ffad4c685058e6a4fc0
Mark Duling <markd@macports.org>
2006-10-10 01:03:27 +0000

    Update to 5.7.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@20001 d073be05-634f-4543-b044-5fe20cf6d1d6

--- net/moomps/files/patch-moomps.sh
+++ net/moomps/files/patch-moomps.sh
@@ -1,35 +1,22 @@
---- moomps.sh.org	2006-04-10 13:00:53.000000000 -0700
-+++ moomps.sh	2006-05-04 22:37:44.000000000 -0700
-@@ -14,16 +14,16 @@
- # config: /etc/moomps/rc
- # pidfile: /var/run/moomps.pid
+--- moomps.sh.org	2006-10-01 10:13:22.000000000 -0700
++++ moomps.sh	2006-10-01 13:13:11.000000000 -0700
+@@ -14,8 +14,8 @@
+ # config: $MOOMPSRCFILE
+ # pidfile: $PIDFILE
  
 -PATH=/sbin:/bin:/usr/bin:/usr/sbin
 -. /etc/init.d/functions
 +#PATH=/sbin:/bin:/usr/bin:/usr/sbin
 +#. /etc/init.d/functions
  
--[ -f /usr/sbin/moomps ] || exit 1
--[ -d /etc/moomps ] || exit 1
--[ -d /srv/moomps ] || exit 1
-+#[ -f __PREFIX__/sbin/moomps ] || exit 1
-+#[ -d __PREFIX__/etc/moomps ] || exit 1
-+#[ -d __PREFIX__/var/moomps ] || exit 1
- 
- return=0
--lockfile=/var/lock/subsys/moomps
--pidfile=/var/run/moomps.pid
-+lockfile=__PREFIX__/var/lock/moomps
-+pidfile=__PREFIX__/var/run/moomps.pid
- 
- 
- start(){
+ [ -f $SBINDIR/moomps ] || exit 1
+ [ -d `dirname $MOOMPSRCFILE` ] || exit 1
 @@ -31,7 +31,7 @@
      touch $pidfile
      chown moomps $pidfile
      # load all configuration files from directory and use process ID file:
--    daemon --user moomps /usr/sbin/moomps --pid-file $pidfile /srv/moomps/
-+    __PREFIX__/sbin/moomps --pid-file $pidfile __PREFIX__/share/moomps/moofiles
+-    daemon --user moomps $SBINDIR/moomps --pid-file $pidfile -r $MOOMPSRCFILE $DATADIR/moomps
++    $SBINDIR/moomps --pid-file $pidfile -r $MOOMPSRCFILE $DATADIR/moomps/moofiles
      return=$?
      echo
      [ $return -eq 0 ] && touch $lockfile
