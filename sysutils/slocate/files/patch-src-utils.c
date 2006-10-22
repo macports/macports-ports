b869c7a0c20b4890b56900be2ef8c74829d79292
Paul Guyot <pguyot@macports.org>
2006-10-22 01:33:24 +0000

    slocate: fixed a bug where -i would return all entries, even those that don't match.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@20123 d073be05-634f-4543-b044-5fe20cf6d1d6

--- sysutils/slocate/files/patch-src-utils.c
+++ sysutils/slocate/files/patch-src-utils.c
@@ -1,5 +1,14 @@
---- src/utils.c.orig	2006-07-26 00:06:53.000000000 +0900
-+++ src/utils.c	2006-07-26 00:08:07.000000000 +0900
+--- src/utils.c	2006-01-06 09:16:19.000000000 +0900
++++ src/utils.c	2006-10-22 10:33:13.000000000 +0900
+@@ -130,7 +130,7 @@
+ 			}
+ 			
+ 		} else
+-		    foundit = (strcasestr(search_str, search_str) != NULL);
++		    foundit = (strcasestr(full_path, search_str) != NULL);
+ 
+ #endif /* FNM_CASEFOLD */
+ 	} 
 @@ -139,6 +139,7 @@
  	    ret = 1;		
  
