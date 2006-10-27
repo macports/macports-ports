32c0c885cb9e8cc09fbb4d571922bea5333c5f2e
Takanori Yamamoto <takanori@macports.org>
2006-10-27 18:23:39 +0000

    Version 3.5.4.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@20195 d073be05-634f-4543-b044-5fe20cf6d1d6

--- kde/kdesdk3/files/CompileScript.sh
+++ kde/kdesdk3/files/CompileScript.sh
@@ -1,18 +1,7 @@
 #!/bin/sh -e
 
-        export HOME=/tmp
-        export PREFIX="%p"
-	#fink
-        #export LEX="%p/lib/flex/bin/flex"
-        #darwinports
-        export LEX="%p/bin/flex"
+        export PREFIX="%p" USE_UNSERMAKE=1
         . ./environment-helper.sh
-        export lt_cv_sys_max_cmd_len=65536
-
-#darwinports
-	export UNSERMAKE="no"
-
-        export CC=gcc CXX=g++
 
         ./build-helper.sh cvs       %N %v %r make -f admin/Makefile.common cvs
         ./build-helper.sh configure %N %v %r ./configure %c $CONFIGURE_PARAMS
@@ -22,6 +11,6 @@
         find . -name \*.ll -exec touch {} \;
 
 	#fink
-        #./build-helper.sh make      %N %v %r make all LIB_DBIV=-ldb-4.2 all_libraries="$ALL_LIBRARIES"
+        #./build-helper.sh make      %N %v %r unsermake $UNSERMAKEFLAGS LIB_DBIV=-ldb-4.2
 	#darwinports
-        ./build-helper.sh make      %N %v %r make all LIB_DBIV=-ldb-4.3 all_libraries="$ALL_LIBRARIES"
+        ./build-helper.sh make      %N %v %r unsermake $UNSERMAKEFLAGS LIB_DBIV=-ldb-4.3
