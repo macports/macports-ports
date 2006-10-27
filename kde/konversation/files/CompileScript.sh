b516027a1c13fd467bedac47598fabb626305d1b
Takanori Yamamoto <takanori@macports.org>
2006-10-27 18:31:23 +0000

    Synced with Fink, Version 0.19-1025.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@20202 d073be05-634f-4543-b044-5fe20cf6d1d6

--- kde/konversation/files/CompileScript.sh
+++ kde/konversation/files/CompileScript.sh
@@ -1,14 +1,8 @@
 #!/bin/sh -e
 
-  export HOME=/tmp
-  export EH_PREFIX="%p"
-  . ./environment-helper.sh
+        export PREFIX="%p" USE_UNSERMAKE=1
+        . ./environment-helper.sh
 
-#darwinports
-  export UNSERMAKE="no"
-
-  export CC=gcc CXX=g++
-
-  ./build-helper.sh cvs       %N %v %r make -f admin/Makefile.common cvs
-  ./build-helper.sh configure %N %v %r ./configure %c $CONFIGURE_PARAMS
-  ./build-helper.sh make      %N %v %r make all all_libraries="$ALL_LIBRARIES"
+        ./build-helper.sh cvs       %N %v %r make -f admin/Makefile.common cvs
+        ./build-helper.sh configure %N %v %r ./configure %c $CONFIGURE_PARAMS
+        ./build-helper.sh make      %N %v %r unsermake $UNSERMAKEFLAGS
--- kde/konversation/files/InstallScript.sh
+++ kde/konversation/files/InstallScript.sh
@@ -1,15 +1,12 @@
 #!/bin/sh -e
 
-  export PREFIX="%p"
-  . ./environment-helper.sh
+        export PREFIX="%p" USE_UNSERMAKE=1
+        . ./environment-helper.sh
 
-#darwinports
-  export UNSERMAKE="no"
+        ./build-helper.sh install %N %v %r unsermake -p -j1 install DESTDIR=%d LN_S='ln -sf'
 
-  ./build-helper.sh install %N %v %r make -j1 install DESTDIR=%d
+        [ -f %i/share/services/irc.protocol  ] && mv %i/share/services/irc.protocol  %i/share/services/konvirc.protocol
+        [ -f %i/share/services/irc6.protocol ] && mv %i/share/services/irc6.protocol %i/share/services/konvirc6.protocol
 
-  [ -f %i/share/services/irc.protocol  ] && mv %i/share/services/irc.protocol  %i/share/services/konvirc.protocol
-  [ -f %i/share/services/irc6.protocol ] && mv %i/share/services/irc6.protocol %i/share/services/konvirc6.protocol
-
-  mkdir -p %i/share/doc/installed-packages
-  touch %i/share/doc/installed-packages/%N
+        mkdir -p %i/share/doc/installed-packages
+        touch %i/share/doc/installed-packages/%N
