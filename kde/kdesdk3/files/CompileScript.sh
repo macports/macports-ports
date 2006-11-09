efaee0f0aadafdb62818bcfad67a93e1a2e65886
Takanori Yamamoto <takanori@macports.org>
2006-11-09 11:40:35 +0000

    Use db44 instead of db4.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@20618 d073be05-634f-4543-b044-5fe20cf6d1d6

--- kde/kdesdk3/files/CompileScript.sh
+++ kde/kdesdk3/files/CompileScript.sh
@@ -13,4 +13,4 @@
 	#fink
         #./build-helper.sh make      %N %v %r unsermake $UNSERMAKEFLAGS LIB_DBIV=-ldb-4.2
 	#darwinports
-        ./build-helper.sh make      %N %v %r unsermake $UNSERMAKEFLAGS LIB_DBIV=-ldb-4.3
+        ./build-helper.sh make      %N %v %r unsermake $UNSERMAKEFLAGS LIB_DBIV=-ldb-4.4
