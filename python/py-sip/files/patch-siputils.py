9184ff717eb49fc8975a027b41d2173fb35fd8ce
Mark Duling <markd@macports.org>
2006-11-09 01:38:43 +0000

    Closes https://trac.macports.org/ticket/11003.  Update to 4.5.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@20613 d073be05-634f-4543-b044-5fe20cf6d1d6

--- python/py-sip/files/patch-siputils.py
+++ python/py-sip/files/patch-siputils.py
@@ -1,13 +1,19 @@
---- siputils.py.orig	2006-08-18 16:36:18.000000000 -0400
-+++ siputils.py	2006-08-18 16:52:22.000000000 -0400
-@@ -1273,8 +1273,8 @@
+--- siputils.py.org.py	2006-11-04 14:09:43.000000000 -0800
++++ siputils.py	2006-11-06 20:23:11.000000000 -0800
+@@ -1302,11 +1302,11 @@
              # This allows Apple's Python to be used even if a later python.org
              # version is also installed.
              dl = string.split(sys.exec_prefix, os.sep)
--            dl = dl[:dl.index("Python.framework")]
+-            try:
+-                dl = dl[:dl.index("Python.framework")]
+-            except ValueError:
+-                error("SIP requires Python to be built as a framework")
 -            self.LFLAGS.append("-F%s" % string.join(dl, os.sep))
-+            #dl = dl[:dl.index("Python.framework")]
-+            #self.LFLAGS.append("-F%s" % string.join(dl, os.sep))
++            # try:
++               # dl = dl[:dl.index("Python.framework")]
++            # except ValueError:
++               # error("SIP requires Python to be built as a framework")
++            # self.LFLAGS.append("-F%s" % string.join(dl, os.sep))
              self.LFLAGS.append("-framework Python")
  
          Makefile.finalise(self)
