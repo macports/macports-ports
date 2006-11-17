e4b4a1c8ca07a6cb2557853ab63202bef5d3448e
Takanori Yamamoto <takanori@macports.org>
2006-11-17 18:40:49 +0000

    Version 3.5.5.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@20688 d073be05-634f-4543-b044-5fe20cf6d1d6

--- kde/kdepim3/files/CompileScript.sh
+++ kde/kdepim3/files/CompileScript.sh
@@ -5,6 +5,12 @@
         export ac_cv_path_GPGME_CONFIG="%p/bin/gpgme-config --thread=pthread"
 
         export CC=gcc CXX=g++
+#fink
+#        export PKG_CONFIG_PATH="%p/lib/pilot-link10/lib/pkgconfig:$PKG_CONFIG_PATH"
+#        export CPPFLAGS="-I%p/lib/pilot-link10/include $CPPFLAGS"
+#        export LDFLAGS="-L%p/lib/pilot-link10/lib $LDFLAGS"
+#macports
+#
 
         ranlib %p/lib/libgpg*.a >/dev/null 2>&1
         ./build-helper.sh cvs       %N %v %r make -f admin/Makefile.common cvs
