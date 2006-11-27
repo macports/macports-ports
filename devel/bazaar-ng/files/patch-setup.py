cb85be5dff75b19d747ba0a1cc94ef58b8f2bd37
Pierre Queinnec <pmq@macports.org>
2006-11-27 19:01:38 +0000

    Closes https://trac.macports.org/ticket/10952. Version bump to 0.12. Submitted by: macports.zelusgmail@dfgh.net, nirs@freeshell.org Approved by: pmq@
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@20771 d073be05-634f-4543-b044-5fe20cf6d1d6

--- devel/bazaar-ng/files/patch-setup.py
+++ devel/bazaar-ng/files/patch-setup.py
@@ -1,10 +1,11 @@
---- setup.py	2006-05-22 13:11:12.000000000 -0700
-+++ setup.py	2006-05-16 21:01:47.000000000 -0700
-@@ -115,6 +115,6 @@
-                 ],
-       scripts=['bzr'],
-       cmdclass={'install_scripts': my_install_scripts, 'build': bzr_build},
--      data_files=[('man/man1', ['bzr.1'])],
-+      data_files=[('share/man/man1', ['bzr.1'])],
-     #   todo: install the txt files from bzrlib.doc.api.
-      )
+--- setup.py.orig	2006-10-27 18:52:02.000000000 +0200
++++ setup.py	2006-10-27 19:31:56.000000000 +0200
+@@ -207,7 +207,7 @@
+ else:
+     # std setup
+     ARGS = {'scripts': ['bzr'],
+-            'data_files': [('man/man1', ['bzr.1'])],
++            'data_files': [('share/man/man1', ['bzr.1'])],
+             'cmdclass': {'build': bzr_build,
+                          'install_scripts': my_install_scripts,
+                         },
