412853e7ffa65a41013fb4d48a1c20b5a21d8ef2
Paul Guyot <pguyot@macports.org>
2006-12-07 05:27:35 +0000

    tex/XeTeX Yet another patch for case sensitiveness.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@20878 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ tex/XeTeX/files/patch-texk-xdv2pdf-xdv2pdf.cpp
@@ -0,0 +1,11 @@
+--- texk/xdv2pdf/xdv2pdf.cpp	2006-08-23 04:41:00.000000000 +0900
++++ /Junk/src/macports/trunk/dports/tex/XeTeX/xdv2pdf.cpp	2006-12-07 14:26:40.000000000 +0900
+@@ -46,7 +46,7 @@
+ #include "config.h"
+ 
+ #include <ApplicationServices/ApplicationServices.h>
+-#include <Quicktime/Quicktime.h>
++#include <QuickTime/QuickTime.h>
+ 
+ #include <string>
+ #include <vector>
