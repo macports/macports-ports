a4cbf5417a437b90e8374dd355ea1e20df7b6660
Mark Duling <markd@macports.org>
2006-12-13 01:57:48 +0000

    https://trac.macports.org/ticket/11114.  New port.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@20947 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ security/samdump2/files/patch-samdump2.cpp
@@ -0,0 +1,10 @@
+--- samdump2.cpp	2006-10-14 07:04:54.000000000 +0200
++++ samdump2.cpp	2006-12-11 14:06:48.000000000 +0100
+@@ -39,6 +39,7 @@
+ 
+ #include <stdio.h>
+ #include <stdlib.h>
++#include <openssl/des.h>
+ #include "rc4.h"
+ #include "md5.h"
+ #include "des/des.h"
