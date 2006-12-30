7460dd0250d975ea274b9df2f9efb201ed600a59
Kevin Ballard <eridius@macports.org>
2006-12-30 15:33:14 +0000

    Delete stale patchfiles for pinfo port. Fixes https://trac.macports.org/ticket/11198
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@21077 d073be05-634f-4543-b044-5fe20cf6d1d6

--- textproc/pinfo/files/patch-utils.h
+++
@@ -1,8 +0,0 @@
---- src/utils.h.org	2002-02-06 22:16:23.000000000 +0300
-+++ src/utils.h	2005-11-07 17:22:15.000000000 +0300
-@@ -51,4 +51,5 @@
- 
- extern int curses_open;		/* is curses screen open? */
- 
-+void handlewinch ();
- #endif
