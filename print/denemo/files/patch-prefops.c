b6eb2328783ffc6a3793ad869059df3c38ceea0a
Yves de Champlain <yves@macports.org>
2007-01-12 04:25:16 +0000

    denemo 0.7.6
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@21196 d073be05-634f-4543-b044-5fe20cf6d1d6

--- print/denemo/files/patch-prefops.c
+++ print/denemo/files/patch-prefops.c
@@ -1,16 +1,16 @@
---- src/prefops.c.orig	2006-02-09 14:56:10.000000000 -0500
-+++ src/prefops.c	2006-02-10 14:12:09.000000000 -0500
-@@ -89,10 +89,10 @@
-     /* Reasonable default values */
+--- src/prefops.c.orig	2006-12-22 07:51:18.000000000 -0500
++++ src/prefops.c	2007-01-11 23:09:16.000000000 -0500
+@@ -88,10 +88,10 @@
+   /* Reasonable default values */
  
-     ret->lilypath = g_string_new ("lilypond");
--    ret->midiplayer = g_string_new ("playmidi");
--    ret->audioplayer = g_string_new ("play");
-+    ret->midiplayer = g_string_new ("qtplay");
-+    ret->audioplayer = g_string_new ("qtplay");
-     ret->csoundcommand = g_string_new("csound -dm6");
--    ret->browser = g_string_new("firefox");
-+    ret->browser = g_string_new("dillo");
-     ret->csoundorcfile = g_string_new("");
-     ret->pdfviewer = g_string_new("xpdf");
-     ret->texteditor = g_string_new("xedit");
+   ret->lilypath = g_string_new ("lilypond");
+-  ret->midiplayer = g_string_new ("playmidi");
+-  ret->audioplayer = g_string_new ("play");
++  ret->midiplayer = g_string_new ("qtplay");
++  ret->audioplayer = g_string_new ("qtplay");
+   ret->csoundcommand = g_string_new ("csound -dm6");
+-  ret->browser = g_string_new ("firefox");
++  ret->browser = g_string_new ("dillo");
+   ret->csoundorcfile = g_string_new ("");
+   ret->pdfviewer = g_string_new ("xpdf");
+   ret->texteditor = g_string_new ("xedit");
