3ca4ea75a829ff8710ff9bfccd5d9ec4d9d8d420
Mark Duling <markd@macports.org>
2007-01-19 08:47:31 +0000

    Closes https://trac.macports.org/ticket/10961.  Update to 0.8.0.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@21277 d073be05-634f-4543-b044-5fe20cf6d1d6

--- python/py-mssql/files/patch-setup.py
+++ python/py-mssql/files/patch-setup.py
@@ -1,18 +1,18 @@
---- setup.py	2006-02-23 01:37:52.000000000 +0100
-+++ setup.py	2006-03-03 09:44:38.000000000 +0100
-@@ -23,14 +23,10 @@
+--- setup.py.org.py	2006-09-22 14:03:02.000000000 -0700
++++ setup.py	2007-01-19 00:53:39.000000000 -0800
+@@ -70,14 +70,10 @@
  
  else:	# try some generic paths
  	include_dirs = [
 -		'/usr/include', '/usr/local/include',
 -		'/usr/include/freetds', '/usr/local/include/freetds',
--		'/usr/pkg/freetds/include'	# netbsd2
+-		'/usr/pkg/freetds/include'	# netbsd
 +		'__PREFIX__/include', '__PREFIX__/include/freetds'
  	]
  	library_dirs = [
 -		'/usr/lib', '/usr/local/lib',
 -		'/usr/lib/freetds', '/usr/local/lib/freetds'
--		'/usr/pkg/freetds/lib'		# netbsd2
+-		'/usr/pkg/freetds/lib'		# netbsd
 +		'__PREFIX__/lib', '__PREFIX__/lib/freetds'
  	]
  	libraries = ["sybdb"]
