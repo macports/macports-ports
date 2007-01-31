f98af3a818a69622f13687e232e497db81f4b29b
Landon Fuller <landonf@macports.org>
2007-01-31 02:26:23 +0000

    Assume ownership and update to latest release. This fixes the i386 build.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@21619 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ devel/liboil/files/patch-liboiltmp.c
@@ -0,0 +1,9 @@
+--- liboil/liboiltmp.c	2007-01-30 18:32:12.000000000 -0800
++++ liboil/liboiltmp.c	2007-01-30 18:32:32.000000000 -0800
+@@ -6,4 +6,5 @@
+ void oil_random_argb(void) {}
+ void oil_test_get_source_data(void) {}
+ void oil_test_get_value(void) {}
+-
++void oil_test_get_arg_post_n(void) {}
++void oil_test_get_arg_stride(void) {}
