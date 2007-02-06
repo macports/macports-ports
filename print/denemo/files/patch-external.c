fe43669578e3c69048602d98ec0a85cbcdf41b06
Yves de Champlain <yves@macports.org>
2007-02-06 17:39:36 +0000

    Rev 2 : fix for truncate() giga-bug
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@21780 d073be05-634f-4543-b044-5fe20cf6d1d6

--- print/denemo/files/patch-external.c
+++ print/denemo/files/patch-external.c
@@ -1,11 +1,10 @@
---- src/external.c.orig	2007-01-13 14:48:44.000000000 -0500
-+++ src/external.c	2007-01-13 14:49:11.000000000 -0500
-@@ -167,7 +167,7 @@
-       rewind (fp);
-       fprintf (fp, "%d", -1);
-       fclose (fp);
--      truncate (pidpath->str, 2);	/* '-' '1' EOF */
-+/*      truncate (pidpath->str, 2);	 '-' '1' EOF */
-       g_string_free (pidpath, TRUE);
-     }
- }
+--- src/external.c.orig	2007-02-06 12:25:59.000000000 -0500
++++ src/external.c	2007-02-06 12:27:40.000000000 -0500
+@@ -12,6 +12,7 @@
+ #include "prefops.h"
+ #include <signal.h>
+ #include "external.h"
++#include <unistd.h>
+ 
+ 
+ /* test it a given temp filename points to an 
