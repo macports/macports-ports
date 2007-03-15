30f6f5eeec4e18fb46c6371f47eaa041a83d789c
Paul Guyot <pguyot@macports.org>
2007-03-15 06:47:18 +0000

    lang/ruby: Update to 1.8.6. Ticket: https://trac.macports.org/ticket/11557
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@22808 d073be05-634f-4543-b044-5fe20cf6d1d6

--- lang/ruby/files/patch-instruby.rb
+++ lang/ruby/files/patch-instruby.rb
@@ -1,20 +1,29 @@
---- instruby.rb.orig	2005-07-05 07:45:33.000000000 -0700
-+++ instruby.rb	2006-01-08 08:29:51.000000000 -0800
-@@ -99,6 +99,8 @@
- archlibdir = with_destdir(CONFIG["archdir"])
- sitelibdir = with_destdir(CONFIG["sitelibdir"])
- sitearchlibdir = with_destdir(CONFIG["sitearchdir"])
-+vendorlibdir = with_destdir(CONFIG["vendorlibdir"])
-+vendorarchlibdir = with_destdir(CONFIG["vendorarchdir"])
- mandir = with_destdir(File.join(CONFIG["mandir"], "man"))
+--- instruby.rb.orig	2007-03-15 13:25:14.000000000 +0900
++++ instruby.rb	2007-03-15 13:31:29.000000000 +0900
+@@ -162,6 +162,8 @@
+ archlibdir = CONFIG["archdir"]
+ sitelibdir = CONFIG["sitelibdir"]
+ sitearchlibdir = CONFIG["sitearchdir"]
++vendorlibdir = CONFIG["vendorlibdir"]
++vendorarchlibdir = CONFIG["vendorarchdir"]
+ mandir = File.join(CONFIG["mandir"], "man")
  configure_args = Shellwords.shellwords(CONFIG["configure_args"])
  enable_shared = CONFIG["ENABLE_SHARED"] == 'yes'
-@@ -106,7 +108,7 @@
- lib = CONFIG["LIBRUBY"]
- arc = CONFIG["LIBRUBY_A"]
- 
--makedirs [bindir, libdir, rubylibdir, archlibdir, sitelibdir, sitearchlibdir]
-+makedirs [bindir, libdir, rubylibdir, archlibdir, sitelibdir, sitearchlibdir, vendorlibdir, vendorarchlibdir]
- 
- install?(:bin) do
-   ruby_bin = File.join(bindir, ruby_install_name)
+@@ -202,7 +204,7 @@
+   extout = "#$extout"
+   install?(:ext, :arch, :'ext-arch') do
+     puts "installing extension objects"
+-    makedirs [archlibdir, sitearchlibdir]
++    makedirs [archlibdir, sitearchlibdir, vendorarchlibdir]
+     if noinst = CONFIG["no_install_files"] and noinst.empty?
+       noinst = nil
+     end
+@@ -210,7 +212,7 @@
+   end
+   install?(:ext, :comm, :'ext-comm') do
+     puts "installing extension scripts"
+-    makedirs [rubylibdir, sitelibdir]
++    makedirs [rubylibdir, sitelibdir, vendorlibdir]
+     install_recursive("#{extout}/common", rubylibdir)
+   end
+ end
--- lang/ruby/files/patch-mkmf.rb
+++ lang/ruby/files/patch-mkmf.rb
@@ -1,5 +1,5 @@
---- lib/mkmf.rb.orig	2006-08-16 22:47:50.000000000 -0700
-+++ lib/mkmf.rb	2006-09-20 16:54:17.000000000 -0700
+--- lib/mkmf.rb.orig	2007-02-28 22:23:42.000000000 +0900
++++ lib/mkmf.rb	2007-03-15 13:39:26.000000000 +0900
 @@ -51,6 +51,9 @@
  $sitedir = CONFIG["sitedir"]
  $sitelibdir = CONFIG["sitelibdir"]
@@ -16,12 +16,12 @@
    headers = cpp_include(headers)
 -  try_link(<<"SRC", libs, &b) or try_link(<<"SRC", libs, &b)
 +  try_link(<<"SRC", libs, &b) or try_link(<<"SRC", libs, &b) or try_link(<<"SRC", libs, &b)
+ #{COMMON_HEADERS}
  #{headers}
  /*top*/
- int main() { return 0; }
 @@ -422,6 +425,11 @@
  int main() { return 0; }
- int t() { void ((*volatile p)()); p = (void ((*)()))#{func}; return 0; }
+ int t() { #{func}(); return 0; }
  SRC
 +int #{func}();
 +/*top*/
@@ -31,7 +31,7 @@
  end
  
  def try_var(var, headers = nil, &b)
-@@ -1080,6 +1088,7 @@
+@@ -1087,6 +1095,7 @@
  RUBY_SO_NAME = #{CONFIG['RUBY_SO_NAME']}
  arch = #{CONFIG['arch']}
  sitearch = #{CONFIG['sitearch']}
@@ -39,12 +39,3 @@
  ruby_version = #{Config::CONFIG['ruby_version']}
  ruby = #{$ruby}
  RUBY = $(ruby#{sep})
-@@ -1400,7 +1409,7 @@
-   $CFLAGS = with_config("cflags", arg_config("CFLAGS", config["CFLAGS"])).dup
-   $ARCH_FLAG = with_config("arch_flag", arg_config("ARCH_FLAG", config["ARCH_FLAG"])).dup
-   $CPPFLAGS = with_config("cppflags", arg_config("CPPFLAGS", config["CPPFLAGS"])).dup
--  $LDFLAGS = (with_config("ldflags") || "").dup
-+  $LDFLAGS = with_config("ldflags", arg_config("LDFLAGS", config["LDFLAGS"])).dup
-   $INCFLAGS = "-I$(topdir) -I$(hdrdir) -I$(srcdir)"
-   $DLDFLAGS = with_config("dldflags", arg_config("DLDFLAGS", config["DLDFLAGS"])).dup
-   $LIBEXT = config['LIBEXT'].dup
