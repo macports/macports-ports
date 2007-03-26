ddd16c114832921c7cd084907507d3964bb3c091
Elias Pipping <pipping@macports.org>
2007-03-26 20:16:01 +0000

    * new port
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@23164 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ devel/uriparser/files/patch-UriBase.h
@@ -0,0 +1,11 @@
+--- include/uriparser/UriBase.h.bak	2007-03-26 22:10:23.000000000 +0200
++++ include/uriparser/UriBase.h	2007-03-26 22:10:39.000000000 +0200
+@@ -116,7 +116,7 @@
+ # include <stdio.h> /* For NULL */
+ # include <ctype.h> /* For wchar_t */
+ # include <string.h> /* For strlen, memset, memcpy */
+-# include <malloc.h> /* For malloc */
++# include <malloc/malloc.h> /* For malloc */
+ #endif /* URI_DOXYGEN */
+ 
+ 
