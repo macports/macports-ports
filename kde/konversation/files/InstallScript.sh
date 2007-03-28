c30775ac531519c6409990a2cec98a245e7ebe2a
Takanori Yamamoto <takanori@macports.org>
2007-03-28 10:38:35 +0000

    Synced with Fink, Version 1.0.1-11.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@23282 d073be05-634f-4543-b044-5fe20cf6d1d6

--- kde/konversation/files/InstallScript.sh
+++ kde/konversation/files/InstallScript.sh
@@ -1,12 +1,9 @@
-#!/bin/sh -e
+#!/bin/sh -ev
 
         export PREFIX="%p" USE_UNSERMAKE=1
         . ./environment-helper.sh
 
         ./build-helper.sh install %N %v %r unsermake -p -j1 install DESTDIR=%d LN_S='ln -sf'
-
-        [ -f %i/share/services/irc.protocol  ] && mv %i/share/services/irc.protocol  %i/share/services/konvirc.protocol
-        [ -f %i/share/services/irc6.protocol ] && mv %i/share/services/irc6.protocol %i/share/services/konvirc6.protocol
 
         mkdir -p %i/share/doc/installed-packages
         touch %i/share/doc/installed-packages/%N
