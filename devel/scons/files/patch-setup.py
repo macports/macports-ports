29e97d30e0a89a128ef336b09d1c5f8cc3978120
Landon Fuller <landonf@macports.org>
2007-03-30 00:33:36 +0000

    Update to 0.96.95, required for nsis 2.24
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@23365 d073be05-634f-4543-b044-5fe20cf6d1d6

--- devel/scons/files/patch-setup.py
+++ devel/scons/files/patch-setup.py
@@ -1,20 +1,11 @@
---- setup.py.orig	2005-09-08 07:15:23.000000000 -0600
-+++ setup.py	2006-01-31 01:23:57.000000000 -0700
-@@ -330,7 +330,7 @@
+--- setup.py.orig	2007-02-12 19:55:03.000000000 -0800
++++ setup.py	2007-03-29 17:36:09.000000000 -0700
+@@ -336,7 +336,7 @@
              if is_win32:
                  dir = 'Doc'
              else:
 -                dir = os.path.join('man', 'man1')
 +                dir = os.path.join('share', 'man', 'man1')
-             self.data_files = [(dir, ["scons.1", "sconsign.1"])]
+             self.data_files = [(dir, man_pages)]
              man_dir = os.path.join(self.install_dir, dir)
              msg = "Installed SCons man pages into %s" % man_dir
-@@ -351,7 +351,7 @@
-                           "SCons.Sig",
-                           "SCons.Tool"],
-     'package_dir'      : {'' : 'engine'},
--    'data_files'       : [('man/man1', ["scons.1", "sconsign.1"])],
-+    'data_files'       : [('share/man/man1', ["scons.1", "sconsign.1"])],
-     'scripts'          : ['script/scons',
-                           'script/sconsign',
-                           'script/scons.bat'],
