3cdaacfd23657ee0064625ab10af54763bde7a40
Mark Grimes <mgrimes@macports.org>
2007-04-11 18:53:12 +0000

    Update to arc-5.21o, closes https://trac.macports.org/ticket/11627
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@23864 d073be05-634f-4543-b044-5fe20cf6d1d6

--- archivers/arc/files/patch-marc.c
+++ archivers/arc/files/patch-marc.c
@@ -1,18 +1,18 @@
---- marc.c.orig	2004-11-06 06:25:45.000000000 -0500
-+++ marc.c	2005-04-04 06:56:30.000000000 -0400
-@@ -23,6 +23,7 @@
- 	 Computer Innovations Optimizing C86
- */
- #include <stdio.h>
-+#include <string.h>
- #include "arc.h"
- 
- #if	UNIX
-@@ -48,7 +49,6 @@
+--- marc.c.orig	2007-04-11 11:52:45.000000000 -0700
++++ marc.c	2007-04-11 11:53:14.000000000 -0700
+@@ -53,7 +53,6 @@ main(nargs,arg)			       /* system entry
  int nargs;			       /* number of arguments */
  char *arg[];			       /* pointers to arguments */
  {
--    char *makefnam();		       /* filename fixup routine */
+-    char *makefnam();
      char *envfind();
  #if	!_MTS
      char *arctemp2, *mktemp();		/* temp file stuff */
+@@ -319,7 +318,6 @@ int n;				       /* number of entry to e
+     char buf[100];		       /* input buffer */
+     int x;			       /* index */
+     char *p = lst[n]+1;		       /* filename pointer */
+-    char *makefnam();
+ 
+     if(*p)			       /* use name if one was given */
+     {	 makefnam(p,".CMD",buf);
