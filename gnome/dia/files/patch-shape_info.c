39e83e7c1bd25ae3d740b1118401de07495a063e
Jann Röder <jann@macports.org>
2007-04-17 07:27:29 +0000

    dia version bump
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@24128 d073be05-634f-4543-b044-5fe20cf6d1d6

--- gnome/dia/files/patch-shape_info.c
+++ gnome/dia/files/patch-shape_info.c
@@ -1,11 +1,11 @@
---- objects/custom/shape_info.c.org	2005-05-09 08:22:06.000000000 +0200
-+++ objects/custom/shape_info.c	2005-05-09 08:22:16.000000000 +0200
-@@ -116,7 +116,7 @@
- /* routine to chomp off the start of the string */
- #define path_chomp(path) while (path[0]!='\0'&&strchr(" \t\n\r,", path[0])) path++
+--- objects/custom/shape_info.c.orig	2007-01-06 23:28:09.000000000 +0100
++++ objects/custom/shape_info.c	2007-04-13 16:38:55.000000000 +0200
+@@ -114,7 +114,7 @@
+ }
+ 
  
 -static void
 +void
- parse_path(ShapeInfo *info, const char *path_str, DiaSvgGraphicStyle *s)
+ parse_path(ShapeInfo *info, const char *path_str, DiaSvgStyle *s, const char* filename)
  {
-   enum {
+   GArray *points;
