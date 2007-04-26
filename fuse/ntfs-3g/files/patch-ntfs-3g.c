bf2dbe44d850d693742652a9ab793193c59cf446
Kevin Ballard <eridius@macports.org>
2007-04-26 06:01:07 +0000

    Fix default options for ntfs-3g, bump revision
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@24489 d073be05-634f-4543-b044-5fe20cf6d1d6

--- fuse/ntfs-3g/files/patch-ntfs-3g.c
+++ fuse/ntfs-3g/files/patch-ntfs-3g.c
@@ -1,17 +1,5 @@
 --- src/ntfs-3g.c.old	2007-04-15 15:13:04.000000000 -0400
 +++ src/ntfs-3g.c	2007-04-15 15:12:17.000000000 -0400
-@@ -134,7 +134,11 @@
- } opts;
- 
- static const char *EXEC_NAME = "ntfs-3g";
-+#if (__FreeBSD__ >= 10)
-+static char def_opts[] = "silent,nonempty,";
-+#else
- static char def_opts[] = "silent,allow_other,nonempty,";
-+#endif
- static ntfs_fuse_context_t *ctx;
- static u32 ntfs_sequence;
- 
 @@ -2124,12 +2128,16 @@
  		goto err_out;
  	}
