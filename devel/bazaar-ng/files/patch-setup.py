799280d1d609071d4df5d714b59dc0b64135f15d
Elias Pipping <pipping@macports.org>
2007-05-07 15:07:05 +0000

    maintainer update (https://trac.macports.org/ticket/11916)
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@24893 d073be05-634f-4543-b044-5fe20cf6d1d6

--- devel/bazaar-ng/files/patch-setup.py
+++ devel/bazaar-ng/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	2007-04-02 09:28:26.000000000 -0400
-+++ setup.py	2007-04-02 09:28:59.000000000 -0400
-@@ -217,7 +217,7 @@
+--- setup.py.orig	2007-05-07 14:51:05.000000000 +0100
++++ setup.py	2007-05-07 14:51:41.000000000 +0100
+@@ -236,7 +236,7 @@
  else:
      # std setup
      ARGS = {'scripts': ['bzr'],
 -            'data_files': [('man/man1', ['bzr.1'])],
 +            'data_files': [('share/man/man1', ['bzr.1'])],
-             'cmdclass': {'build': bzr_build,
-                          'install_scripts': my_install_scripts,
-                         },
+             'cmdclass': command_classes,
+             'ext_modules': ext_modules,
+            }
