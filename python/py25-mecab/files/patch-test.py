743c13dafc7607a7dccc11d644856d1a31abd172
Takanori Yamamoto <takanori@macports.org>
2007-06-12 12:42:51 +0000

    Version bump to mecab 0.96. Ticket:	     	https://trac.macports.org/ticket/12104 Submitted by:	rsky0711 at gmail dot com
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@26098 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ python/py25-mecab/files/patch-test.py
@@ -0,0 +1,9 @@
+--- test.py.orig	2007-06-10 23:32:44.000000000 +0900
++++ test.py	2007-06-11 13:35:17.000000000 +0900
+@@ -1,4 +1,5 @@
+-#!/usr/bin/python
++#!%PREFIX%/bin/python
++# -*- coding:euc-jp -*-
+ 
+ import MeCab
+ import sys
