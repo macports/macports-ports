17ffb2c7e89e5112a2da656fb77c329395ebbfa8
Boey Maun Suang <boeyms@macports.org>
2007-06-15 19:44:23 +0000

    editors/emacs:  * Update to latest stable version (22.1); partially addresses https://trac.macports.org/ticket/12075.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@26206 d073be05-634f-4543-b044-5fe20cf6d1d6

--- editors/emacs/files/patch-src-s-darwin.h
+++ editors/emacs/files/patch-src-s-darwin.h
@@ -1,5 +1,5 @@
---- emacs/src/s/darwin.h	Tue Nov 30 06:57:27 2004
-+++ emacs/src/s/darwin.h.new	Wed Jan 12 19:30:53 2005
+--- src/s/darwin.h	Tue Nov 30 06:57:27 2004
++++ src/s/darwin.h.new	Wed Jan 12 19:30:53 2005
 @@ -261,7 +261,7 @@
     end of the header for adding load commands.  Needed for dumping.
     0x690 is the total size of 30 segment load commands (at 56
