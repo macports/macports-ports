0ceb361a3112b79f06477fefa5a7415f5fa7f229
Yves de Champlain <yves@macports.org>
2007-06-17 21:53:18 +0000

    prepare for gnutep-make-2.0
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@26262 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ gnustep/ProjectManager/files/patch-Controller.m
@@ -0,0 +1,10 @@
+--- Controller.m.orig	2007-06-17 12:28:34.000000000 -0400
++++ Controller.m	2007-06-17 12:20:01.000000000 -0400
+@@ -28,6 +28,7 @@
+ #import <Foundation/NSFileManager.h>
+ #import <Foundation/NSUserDefaults.h>
+ #import <Foundation/NSNotification.h>
++#import <Foundation/NSError.h>
+ 
+ #import <AppKit/NSOpenPanel.h>
+ #import <AppKit/NSView.h>
