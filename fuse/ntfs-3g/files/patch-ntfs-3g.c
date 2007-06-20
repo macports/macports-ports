ed601a51bcdc970094b3974b8c550c43caf9878c
Kevin Ballard <eridius@macports.org>
2007-06-20 07:14:07 +0000

    Bump ntfs-3g to 1.616
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@26340 d073be05-634f-4543-b044-5fe20cf6d1d6

--- fuse/ntfs-3g/files/patch-ntfs-3g.c
+++ fuse/ntfs-3g/files/patch-ntfs-3g.c
@@ -1,19 +1,15 @@
---- src/ntfs-3g.c.old	2007-04-15 15:13:04.000000000 -0400
-+++ src/ntfs-3g.c	2007-04-15 15:12:17.000000000 -0400
-@@ -2124,12 +2128,16 @@
- 		goto err_out;
- 	}
- 
-+#if (__FreeBSD__ >= 10)
-+	fstype = FSTYPE_FUSE;
-+#else
- #ifdef linux
+--- src/ntfs-3g.c.old	2007-06-20 00:20:09.000000000 -0700
++++ src/ntfs-3g.c	2007-06-20 00:21:06.000000000 -0700
+@@ -2157,7 +2157,11 @@
  	fstype = get_fuse_fstype();
  	if (fstype == FSTYPE_NONE || fstype == FSTYPE_UNKNOWN)
  		fstype = load_fuse_module();
- #endif	
- 	create_dev_fuse();
+-#endif	
++#else
++#if (__FreeBSD__ >= 10)
++	fstype = FSTYPE_FUSE;
 +#endif
++#endif
+ 	create_dev_fuse();
  	
  	if (stat(opts.device, &sbuf)) {
- 		ntfs_log_perror("Failed to access '%s'", opts.device);
