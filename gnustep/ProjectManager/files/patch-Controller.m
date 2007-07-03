4b1cb11914f0e3d7b313f32df0a5e1a7cd38685b
Yves de Champlain <yves@macports.org>
2007-07-03 04:55:00 +0000

    Revision 1, fix runtime problem
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@26674 d073be05-634f-4543-b044-5fe20cf6d1d6

--- gnustep/ProjectManager/files/patch-Controller.m
+++ gnustep/ProjectManager/files/patch-Controller.m
@@ -1,10 +1,11 @@
---- Controller.m.orig	2007-06-17 12:28:34.000000000 -0400
-+++ Controller.m	2007-06-17 12:20:01.000000000 -0400
-@@ -28,6 +28,7 @@
+--- Controller.m.orig	2007-07-03 00:41:14.000000000 -0400
++++ Controller.m	2007-07-03 00:41:18.000000000 -0400
+@@ -29,6 +29,8 @@
  #import <Foundation/NSFileManager.h>
  #import <Foundation/NSUserDefaults.h>
  #import <Foundation/NSNotification.h>
 +#import <Foundation/NSError.h>
++#import <Foundation/NSDebug.h>
  
  #import <AppKit/NSOpenPanel.h>
  #import <AppKit/NSView.h>
