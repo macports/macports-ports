57d6cc6d3d22cb68fe1c26635f7c55b1fc9011c9
Mark Grimes <mgrimes@macports.org>
2007-07-06 21:41:49 +0000

    Update to git-core 1.5.2.3
    - Add p5-error (Fix https://trac.macports.org/ticket/12240)
    - Refresh and rename patch files to literal naming conventions instead
      of functional.
    - Add darwin_9 variant to workaround ticket https://trac.macports.org/ticket/12245.
    - whitespace fixes
    
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@26771 d073be05-634f-4543-b044-5fe20cf6d1d6

--- devel/git-core/files/patch-http.h
+++ devel/git-core/files/patch-http.h
@@ -1,12 +1,10 @@
-diff --git http.h http.h
-index 69b6b66..b33fe7b 100644
---- http.h
-+++ http.h
+--- http.h.orig	2007-07-03 02:08:49.000000000 -0700
++++ http.h	2007-07-03 02:09:00.000000000 -0700
 @@ -7,7 +7,6 @@
  #include <curl/easy.h>
-
+ 
  #if LIBCURL_VERSION_NUM >= 0x071000
 -#define USE_CURL_MULTI
  #define DEFAULT_MAX_REQUESTS 5
  #endif
-
+ 
