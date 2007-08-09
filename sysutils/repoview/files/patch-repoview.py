573dc8ca82b06ac7543ce5880aa2faf842abb4a9
Anders F. Björklund <afb@macports.org>
2007-08-09 08:40:45 +0000

    major version update, 0.6.0
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@27584 d073be05-634f-4543-b044-5fe20cf6d1d6

--- sysutils/repoview/files/patch-repoview.py
+++ sysutils/repoview/files/patch-repoview.py
@@ -1,57 +1,24 @@
-# Fedora: yum-2.9-api.patch
---- repoview.py.orig	2006-07-04 20:53:55.000000000 -0400
-+++ repoview.py	2006-07-04 21:21:56.000000000 -0400
-@@ -42,7 +42,7 @@
+--- repoview.py.orig	2007-08-09 00:14:08.000000000 +0200
++++ repoview.py	2007-08-09 00:16:14.000000000 +0200
+@@ -53,7 +53,7 @@
  try:
-     from yum.comps import Comps, CompsException
-     from yum.mdparser import MDParser
--    from repomd.repoMDObject import RepoMD
-+    from yum.repoMDObject import RepoMD
+     import sqlite3 as sqlite
  except ImportError:
-     try:
-         from noyum.comps import Comps, CompsException
-@@ -364,7 +364,7 @@
-         except IOError: 
-             return 1
-         checksum = checksum.strip()
--        if checksum != self.repodata['primary']['checksum'][1]: 
-+        if checksum != self.repodata['primary'].checksum[1]: 
-             return 1
-         _say("RepoView: Repository has not changed. Force the run with -f.\n")
-         sys.exit(0)
-@@ -374,7 +374,7 @@
-         Utility method for parsing comps.xml.
-         """
-         _say('parsing comps...', 1)
--        loc = self.repodata['group']['relativepath']
-+        loc = self.repodata['group'].location[1]
-         comps = Comps()
-         try:
-             comps.add(os.path.join(self.repodir, loc))
-@@ -436,7 +436,7 @@
-         Utility method for processing primary.xml.
-         """
-         _say('parsing primary...', 1)
--        loc = self.repodata['primary']['relativepath']
-+        loc = self.repodata['primary'].location[1]
-         mdp = MDParser(os.path.join(self.repodir, loc))
-         ignored = 0
-         for package in mdp:
-@@ -507,7 +507,7 @@
-         Utility method to get data from other.xml.
-         """
-         _say('parsing other...', 1)
--        loc = self.repodata['other']['relativepath']
-+        loc = self.repodata['other'].location[1]
-         otherxml = os.path.join(self.repodir, loc)
-         ignored = 0
-         mdp = MDParser(otherxml)
-@@ -789,7 +789,7 @@
-         _say('writing checksum...', 1)
-         chkfile = os.path.join(self.outdir, 'checksum')
-         fh = open(chkfile, 'w')
--        fh.write(self.repodata['primary']['checksum'][1])
-+        fh.write(self.repodata['primary'].checksum[1])
-         fh.close()
-         _say('done\n')
-         _say('Moving new repoview dir in place...', 1)
+-    import sqlite
++    from pysqlite2 import dbapi2 as sqlite
+ 
+ ##
+ # Some hardcoded constants
+@@ -488,7 +488,11 @@
+                      ORDER BY date DESC LIMIT 1''' % pkg_key
+             ocursor = self.oconn.cursor()
+             ocursor.execute(query)
+-            (author, time_added, changelog) = ocursor.fetchone()
++            row = ocursor.fetchone()
++            if row:
++                (author, time_added, changelog) = row
++            else:
++                (author, time_added, changelog) = (vendor, time_build, "Build")
+             # strip email and everything that follows from author
+             try:
+                 author = author[:author.index('<')].strip()
