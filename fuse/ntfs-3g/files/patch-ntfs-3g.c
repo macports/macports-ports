a190da50d80322483550e4321e2a95b426ce486f
Kevin Ballard <eridius@macports.org>
2007-08-11 06:39:23 +0000

    Bump ntfs-3g to 1.710, thanks Simon Ruderich. Fix ntfs-3g installing in /bin and /lib. Fixes ticket https://trac.macports.org/ticket/12406.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@27641 d073be05-634f-4543-b044-5fe20cf6d1d6

--- fuse/ntfs-3g/files/patch-ntfs-3g.c
+++ fuse/ntfs-3g/files/patch-ntfs-3g.c
@@ -1,15 +1,15 @@
---- src/ntfs-3g.c.old	2007-06-20 00:20:09.000000000 -0700
-+++ src/ntfs-3g.c	2007-06-20 00:21:06.000000000 -0700
-@@ -2157,7 +2157,11 @@
- 	fstype = get_fuse_fstype();
- 	if (fstype == FSTYPE_NONE || fstype == FSTYPE_UNKNOWN)
+--- src/ntfs-3g.c.old	2007-07-10 00:25:15.000000000 +0200
++++ src/ntfs-3g.c	2007-08-09 14:58:09.000000000 +0200
+@@ -2170,7 +2170,11 @@
  		fstype = load_fuse_module();
+ 	
+ 	create_dev_fuse();
 -#endif	
 +#else
 +#if (__FreeBSD__ >= 10)
 +	fstype = FSTYPE_FUSE;
 +#endif
 +#endif
- 	create_dev_fuse();
  	
  	if (stat(opts.device, &sbuf)) {
+ 		ntfs_log_perror("Failed to access '%s'", opts.device);
