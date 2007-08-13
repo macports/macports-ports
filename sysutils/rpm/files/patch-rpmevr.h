fb771bd17be438fc6a6aeaa1975f1ef8bdaa77e4
Anders F. Björklund <afb@macports.org>
2007-08-13 10:56:57 +0000

    patch cleanup, linux support
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@27736 d073be05-634f-4543-b044-5fe20cf6d1d6

--- sysutils/rpm/files/patch-rpmevr.h
+++ sysutils/rpm/files/patch-rpmevr.h
@@ -1,21 +1,24 @@
---- ./lib/rpmevr.h.orig	2007-05-16 12:55:05.000000000 +0200
-+++ ./lib/rpmevr.h	2007-06-20 10:13:14.000000000 +0200
-@@ -18,8 +18,6 @@
- /**
-  * Dependency Attributes.
+--- lib/rpmevr.h.orig	2007-05-16 12:55:05.000000000 +0200
++++ lib/rpmevr.h	2007-08-09 18:36:13.000000000 +0200
+@@ -6,6 +6,10 @@
+  * Structure(s) and routine(s) used for EVR parsing and comparison.
   */
--typedef	enum evrFlags_e rpmsenseFlags;
--typedef	enum evrFlags_e evrFlags;
  
- /*@-matchfields@*/
- enum evrFlags_e {
-@@ -67,6 +65,9 @@
- };
- /*@=matchfields@*/
- 
-+typedef	enum evrFlags_e rpmsenseFlags;
-+typedef	enum evrFlags_e evrFlags;
++#ifdef __cplusplus
++extern "C" {
++#endif
 +
- #define	RPMSENSE_SENSEMASK	0x0e	 /* Mask to get senses, ie serial, */
-                                          /* less, greater, equal.          */
- #define	RPMSENSE_NOTEQUAL	(RPMSENSE_EQUAL ^ RPMSENSE_SENSEMASK)
+ /**
+  */
+ /*@-exportlocal@*/
+@@ -117,10 +121,6 @@
+ #define	isErasePreReq(_x)	((_x) & _ERASE_ONLY_MASK)
+ #endif	/* _RPMEVR_INTERNAL */
+ 
+-#ifdef __cplusplus
+-extern "C" {
+-#endif
+-
+ /** \ingroup rpmds
+  * Segmented string compare.
+  * @param a		1st string
