b3f707a600b2f3172600f18aa9b9a061de2ed81b
Ryan Schmidt <ryandesign@macports.org>
2007-08-23 10:05:02 +0000

    duplicity: maintainer update to 0.4.3. See https://trac.macports.org/ticket/12537
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@28162 d073be05-634f-4543-b044-5fe20cf6d1d6

--- sysutils/duplicity/files/patch-setup.py
+++ sysutils/duplicity/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.orig	2006-09-23 10:16:16.000000000 -0700
-+++ setup.py	2006-09-23 10:17:08.000000000 -0700
-@@ -19,7 +19,9 @@
+--- setup.py.orig	2007-08-20 11:19:25.000000000 -0400
++++ setup.py	2007-08-20 14:32:36.000000000 -0400
+@@ -31,7 +31,9 @@
  	  package_dir = {"duplicity": "src"},
  	  ext_modules = [Extension("duplicity._librsync",
  							   ["_librsyncmodule.c"],
