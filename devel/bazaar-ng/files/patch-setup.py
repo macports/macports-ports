04de29dfcb14541175e69fd9401dfaf79941fbf1
Ryan Schmidt <ryandesign@macports.org>
2007-08-30 19:58:52 +0000

    bazaar-ng: maintainer update to 0.90. See ticket https://trac.macports.org/ticket/12572.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@28416 d073be05-634f-4543-b044-5fe20cf6d1d6

--- devel/bazaar-ng/files/patch-setup.py
+++ devel/bazaar-ng/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	2007-07-16 23:53:29.000000000 -0400
-+++ setup.py	2007-07-17 08:30:21.000000000 -0400
-@@ -245,7 +245,7 @@
- else:
+--- setup.py.orig	2007-08-28 15:51:08.000000000 -0400
++++ setup.py	2007-08-28 21:39:28.000000000 -0400
+@@ -297,7 +297,7 @@
+     DATA_FILES = []
+     if not 'bdist_egg' in sys.argv:
+         # generate and install bzr.1 only with plain install, not easy_install one
+-        DATA_FILES = [('man/man1', ['bzr.1'])]
++        DATA_FILES = [('share/man/man1', ['bzr.1'])]
+ 
      # std setup
      ARGS = {'scripts': ['bzr'],
--            'data_files': [('man/man1', ['bzr.1'])],
-+            'data_files': [('share/man/man1', ['bzr.1'])],
-             'cmdclass': command_classes,
-             'ext_modules': ext_modules,
-            }
