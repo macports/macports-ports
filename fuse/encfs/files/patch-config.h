e3eaa6256fa397ce45ad2ccd9bd17a7fab316f0f
Kevin Ballard <eridius@macports.org>
2007-10-09 02:28:16 +0000

    Fix encfs for case-sensitive systems (https://trac.macports.org/ticket/12807). Thanks raphael
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@29766 d073be05-634f-4543-b044-5fe20cf6d1d6

--- fuse/encfs/files/patch-config.h
+++ fuse/encfs/files/patch-config.h
@@ -1,5 +1,5 @@
---- encfs/config.h.old	2007-03-14 19:32:37.000000000 -0400
-+++ encfs/config.h	2007-03-14 19:32:50.000000000 -0400
+--- encfs/Config.h.old	2007-03-14 19:32:37.000000000 -0400
++++ encfs/Config.h	2007-03-14 19:32:50.000000000 -0400
 @@ -18,6 +18,7 @@
  #ifndef _Config_incl_
  #define _Config_incl_
