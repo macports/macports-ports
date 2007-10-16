70262576401caec43abd786aeafb6e3f9f82498a
Takanori Yamamoto <takanori@macports.org>
2007-10-16 23:04:05 +0000

    * Added a patch that allows command+space to be used as a conversion key.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@29961 d073be05-634f-4543-b044-5fe20cf6d1d6

--- x11/kinput2/files/patch-lib_Canna.c
+++ x11/kinput2/files/patch-lib_Canna.c
@@ -1,5 +1,5 @@
 --- lib/Canna.c.orig	2002-10-03 18:35:27.000000000 +0900
-+++ lib/Canna.c	2007-10-13 02:04:29.000000000 +0900
++++ lib/Canna.c	2007-10-17 07:58:25.000000000 +0900
 @@ -61,6 +61,8 @@
  #include "CannaP.h"
  #include "DebugPrint.h"
@@ -9,15 +9,18 @@
  #define _WCHAR_T /* この定義は jrkanji.h で wcKanjiStatus などを定義するため */
  #define wchar_t wchar
  
-@@ -246,7 +248,7 @@
+@@ -246,9 +248,9 @@
      kanabuf[0] = '\0';
      nbytes = XKanaLookup(event, kanabuf, 20, &ks, &compose_status);
  
 -    buf[0] = (wchar)kanabuf[0]; /* きたない */
 +    buf[0] = (wchar)(unsigned char)kanabuf[0]; /* きたない */
  
-     if (ks == XK_space && (event->xkey.state & ShiftMask)) {
+-    if (ks == XK_space && (event->xkey.state & ShiftMask)) {
++    if (ks == XK_space && ((event->xkey.state & ShiftMask) || (event->xkey.state & Mod2Mask))) {
        void convend();
+ 
+       convend(obj);
 @@ -271,7 +273,7 @@
  
      /* かな漢字変換する */
