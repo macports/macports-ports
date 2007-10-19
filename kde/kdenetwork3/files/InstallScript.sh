777030f34d7bb13a1658c1595f5255dafb370bed
Takanori Yamamoto <takanori@macports.org>
2007-10-19 07:14:38 +0000

    KDE 3.5.8.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@30063 d073be05-634f-4543-b044-5fe20cf6d1d6

--- kde/kdenetwork3/files/InstallScript.sh
+++ kde/kdenetwork3/files/InstallScript.sh
@@ -1,17 +1,17 @@
 #!/bin/sh -ev
 
-        export PREFIX="%p" USE_UNSERMAKE=1
-        . ./environment-helper.sh
+	export PREFIX="%p" USE_UNSERMAKE=1
+	. ./environment-helper.sh
 
-        ./build-helper.sh install %N %v %r unsermake -p -j1 install DESTDIR=%d
+	./build-helper.sh install %N %v %r unsermake -p -j1 install DESTDIR=%d
 
-        mkdir -p %i/share/doc/installed-packages
-        touch %i/share/doc/installed-packages/%N
-        touch %i/share/doc/installed-packages/%N-base
-        touch %i/share/doc/installed-packages/%N-common
+	mkdir -p %i/share/doc/installed-packages
+	touch %i/share/doc/installed-packages/%N
+	touch %i/share/doc/installed-packages/%N-base
+	touch %i/share/doc/installed-packages/%N-common
 
-        rm -rf %i/share/doc/kde/en/kcontrol/kxmlrpcd
-        rm -rf %i/share/doc/kde/en/kppp
-        rm -rf %i/share/doc/kde/en/kwifimanager
+	rm -rf %i/share/doc/kde/en/kcontrol/kxmlrpcd
+	rm -rf %i/share/doc/kde/en/kppp
+	rm -rf %i/share/doc/kde/en/kwifimanager
 
-        find %i/share/apps/kopete -type d -name CVS -exec rm -rf {} \; || :
+	find %i/share/apps/kopete -type d -name CVS -exec rm -rf {} \; || :
