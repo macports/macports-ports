2666254678d83a9154c1c6628ff5a7ab05ee8658
Takanori Yamamoto <takanori@macports.org>
2007-10-19 07:11:48 +0000

    KDE 3.5.8.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@30058 d073be05-634f-4543-b044-5fe20cf6d1d6

--- kde/kdelibs3/files/InstallScript.sh
+++ kde/kdelibs3/files/InstallScript.sh
@@ -1,21 +1,38 @@
 #!/bin/sh -ev
 
-        export PREFIX="%p" USE_UNSERMAKE=1
-        . ./environment-helper.sh
+	export PREFIX="%p" USE_UNSERMAKE=1
+	. ./environment-helper.sh
 
-        ./build-helper.sh install        %N %v %r unsermake -p -j1 install DESTDIR=%d
-#apidox#./build-helper.sh install-apidox %N %v %r make -j1 install-apidox install-apidox-recurse DESTDIR=%d
+	./build-helper.sh install        %N %v %r unsermake -p -j1 install DESTDIR=%d
 
-        rm -rf %i/share/icons/hicolor/index.theme
+#fink
+#	cat <<END >%i/bin/kde-update-caches.sh
+##!/bin/sh
+#
+#	if [ \`id -un\` != 'root' ]; then
+#		echo "you must run this script as root!"
+#		echo ""
+#		exit 1
+#	fi
+#
+#	ENV="env KDEDIR= KDEDIRS= HOME=/tmp XDG_CACHE_HOME=/tmp/kb/cache XDG_CONFIG_HOME=/tmp/kb/config XDG_DATA_HOME=/tmp/kb/share HISTFILE=/tmp/kb/.bash_history USER=root LOGNAME=root"
+#	\$ENV %p/bin/update-mime-database %p/share/mime
+#	\$ENV %p/bin/kbuildsycoca --global --noincremental --nosignal
+#END
+#	chmod 755 %i/bin/kde-update-caches.sh
+#macports
+#
 
-        install -c -m 644 darwin/* %i/share/config
+	rm -rf %i/share/icons/hicolor/index.theme
 
-        mkdir -p %i/share/doc/installed-packages
-        touch %i/share/doc/installed-packages/%N
+	install -c -m 644 darwin/* %i/share/config
 
-        touch %i/share/doc/installed-packages/kdelibs3
-        touch %i/share/doc/installed-packages/kdelibs3-shlibs
-        touch %i/share/doc/installed-packages/kdelibs3-dev
-        touch %i/share/doc/installed-packages/kdelibs3-ssl
-        touch %i/share/doc/installed-packages/kdelibs3-ssl-shlibs
-        touch %i/share/doc/installed-packages/kdelibs3-ssl-dev
+	mkdir -p %i/share/doc/installed-packages
+	touch %i/share/doc/installed-packages/%N
+
+	touch %i/share/doc/installed-packages/kdelibs3
+	touch %i/share/doc/installed-packages/kdelibs3-shlibs
+	touch %i/share/doc/installed-packages/kdelibs3-dev
+	touch %i/share/doc/installed-packages/kdelibs3-ssl
+	touch %i/share/doc/installed-packages/kdelibs3-ssl-shlibs
+	touch %i/share/doc/installed-packages/kdelibs3-ssl-dev
