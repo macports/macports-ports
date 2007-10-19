01589270f4e05077d02b417497eebe09fb54e5b1
Takanori Yamamoto <takanori@macports.org>
2007-10-19 07:15:17 +0000

    KDE 3.5.8.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@30066 d073be05-634f-4543-b044-5fe20cf6d1d6

--- kde/kdeedu3/files/CompileScript.sh
+++ kde/kdeedu3/files/CompileScript.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -ev
 
-        export PREFIX="%p" USE_UNSERMAKE=1
-        . ./environment-helper.sh
-        export LDFLAGS="-L%p/lib/python2.4/config"
+	export PREFIX="%p" USE_UNSERMAKE=1
+	. ./environment-helper.sh
+	export LDFLAGS="-L%p/lib/python2.4/config"
 
-        ./build-helper.sh cvs       %N %v %r make -f admin/Makefile.common cvs
-        ./build-helper.sh configure %N %v %r ./configure %c $CONFIGURE_PARAMS
-        ./build-helper.sh make      %N %v %r unsermake $UNSERMAKEFLAGS kvoctrain/kvoctrain/common-dialogs/prefs.h all
+	./build-helper.sh cvs       %N %v %r make -f admin/Makefile.common cvs
+	./build-helper.sh configure %N %v %r ./configure %c $CONFIGURE_PARAMS
+	./build-helper.sh make      %N %v %r unsermake $UNSERMAKEFLAGS kvoctrain/kvoctrain/common-dialogs/prefs.h all
--- kde/kdeedu3/files/InstallScript.sh
+++ kde/kdeedu3/files/InstallScript.sh
@@ -1,20 +1,22 @@
 #!/bin/sh -ev
 
-        export PREFIX="%p" USE_UNSERMAKE=1
-        . ./environment-helper.sh
+	export PREFIX="%p" USE_UNSERMAKE=1
+	. ./environment-helper.sh
 
-        ./build-helper.sh install %N %v %r unsermake -p -j1 install DESTDIR=%d
+	./build-helper.sh install %N %v %r unsermake -p -j1 install DESTDIR=%d
 
-        mkdir -p %i/share/doc/installed-packages
-        touch %i/share/doc/installed-packages/%N
-        touch %i/share/doc/installed-packages/%N-base
+	mkdir -p %i/share/doc/installed-packages
+	touch %i/share/doc/installed-packages/%N
+	touch %i/share/doc/installed-packages/%N-base
 
-        # FIXME: this works around an initialization error in kstars, this should
-        # get fixed for real!
-        install -d -m 755 %i/share/config
-        touch %i/share/config/kstarsrc
+	# FIXME: this works around an initialization error in kstars, this should
+	# get fixed for real!
+	install -d -m 755 %i/share/config
+	touch %i/share/config/kstarsrc
 
-        # they changed the library name, but not the compat version??
-        pushd %i/lib
-        ln -s libkdeeduui.3.dylib libkdeeduui.1.dylib
-        popd
+	# they changed the library name, but not the compat version??
+	pushd %i/lib
+	ln -s libkdeeduui.3.dylib libkdeeduui.1.dylib
+	popd
+
+	perl -pi -e 's,-L%b,-L%p,g' %i/lib/*.la %i/lib/kde3/*.la
