4bc298ce8401cb404f6fd75dc897b174810811b3
Takanori Yamamoto <takanori@macports.org>
2007-10-19 07:16:20 +0000

    KDE 3.5.8.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@30071 d073be05-634f-4543-b044-5fe20cf6d1d6

--- kde/kdesdk3/files/CompileScript.sh
+++ kde/kdesdk3/files/CompileScript.sh
@@ -1,16 +1,16 @@
 #!/bin/sh -ev
 
-        export PREFIX="%p" USE_UNSERMAKE=1
-        . ./environment-helper.sh
+	export PREFIX="%p" USE_UNSERMAKE=1
+	. ./environment-helper.sh
 
-        ./build-helper.sh cvs       %N %v %r make -f admin/Makefile.common cvs
-        ./build-helper.sh configure %N %v %r ./configure %c $CONFIGURE_PARAMS
+	./build-helper.sh cvs       %N %v %r make -f admin/Makefile.common cvs
+	./build-helper.sh configure %N %v %r ./configure %c $CONFIGURE_PARAMS
 
-        perl -pi -e 's,(..libdir.)/kde3,$1,g' kompare/libdialogpages/Makefile
-        find . -name \*.l -exec touch {} \;
-        find . -name \*.ll -exec touch {} \;
+	perl -pi -e 's,(..libdir.)/kde3,$1,g' kompare/libdialogpages/Makefile
+	find . -name \*.l -exec touch {} \;
+	find . -name \*.ll -exec touch {} \;
 
-	#fink
-        #./build-helper.sh make      %N %v %r unsermake $UNSERMAKEFLAGS LIB_DBIV=-ldb-4.2
-	#darwinports
-        ./build-helper.sh make      %N %v %r unsermake $UNSERMAKEFLAGS LIB_DBIV=-ldb-4.4
+#fink
+#	./build-helper.sh make      %N %v %r unsermake $UNSERMAKEFLAGS LIB_DBIV=-ldb-4.2
+#macports
+	./build-helper.sh make      %N %v %r unsermake $UNSERMAKEFLAGS LIB_DBIV=-ldb-4.4
