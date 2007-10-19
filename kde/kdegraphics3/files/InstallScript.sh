f003b69f1963b74be998a43f10ca1974ff3d0fc7
Takanori Yamamoto <takanori@macports.org>
2007-10-19 07:14:54 +0000

    KDE 3.5.8.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@30064 d073be05-634f-4543-b044-5fe20cf6d1d6

--- kde/kdegraphics3/files/InstallScript.sh
+++ kde/kdegraphics3/files/InstallScript.sh
@@ -1,13 +1,13 @@
 #!/bin/sh -ev
 
-        export PREFIX="%p" USE_UNSERMAKE=1
-        . ./environment-helper.sh
+	export PREFIX="%p" USE_UNSERMAKE=1
+	. ./environment-helper.sh
 
-        ./build-helper.sh install %N %v %r unsermake -p -j1 install DESTDIR=%d
+	./build-helper.sh install %N %v %r unsermake -p -j1 install DESTDIR=%d
 
-        mkdir -p %i/share/doc/installed-packages
-        touch %i/share/doc/installed-packages/%N
-        touch %i/share/doc/installed-packages/%N-base
+	mkdir -p %i/share/doc/installed-packages
+	touch %i/share/doc/installed-packages/%N
+	touch %i/share/doc/installed-packages/%N-base
 
-        rm -rf %i/share/doc/kde/en/kamera
-        rm -rf %i/share/doc/kde/en/kooka
+	rm -rf %i/share/doc/kde/en/kamera
+	rm -rf %i/share/doc/kde/en/kooka
