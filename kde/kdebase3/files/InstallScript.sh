1327874b75c696ab8674e4b540bd3e0b3adf1b50
Takanori Yamamoto <takanori@macports.org>
2007-10-19 07:12:01 +0000

    KDE 3.5.8.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@30059 d073be05-634f-4543-b044-5fe20cf6d1d6

--- kde/kdebase3/files/InstallScript.sh
+++ kde/kdebase3/files/InstallScript.sh
@@ -1,30 +1,30 @@
-#!/bin/sh -e
+#!/bin/sh -ev
 
-        export PREFIX="%p" USE_UNSERMAKE=1
-        . ./environment-helper.sh
+	export PREFIX="%p" USE_UNSERMAKE=1
+	. ./environment-helper.sh
 
-        ./build-helper.sh install        %N %v %r unsermake -p -j1 install DESTDIR=%d
+	./build-helper.sh install        %N %v %r unsermake -p -j1 install DESTDIR=%d
 #apidox#./build-helper.sh install-apidox %N %v %r make -j1 install-apidox install-apidox-recurse DESTDIR=%d
 
-        mkdir -p %i/share/doc/installed-packages
-        touch %i/share/doc/installed-packages/%N
+	mkdir -p %i/share/doc/installed-packages
+	touch %i/share/doc/installed-packages/%N
 
-        touch %i/share/doc/installed-packages/kdebase3
-        touch %i/share/doc/installed-packages/kdebase3-shlibs
-        touch %i/share/doc/installed-packages/kdebase3-dev
-        touch %i/share/doc/installed-packages/kdebase3-ssl
-        touch %i/share/doc/installed-packages/kdebase3-ssl-shlibs
-        touch %i/share/doc/installed-packages/kdebase3-ssl-dev
+	touch %i/share/doc/installed-packages/kdebase3
+	touch %i/share/doc/installed-packages/kdebase3-shlibs
+	touch %i/share/doc/installed-packages/kdebase3-dev
+	touch %i/share/doc/installed-packages/kdebase3-ssl
+	touch %i/share/doc/installed-packages/kdebase3-ssl-shlibs
+	touch %i/share/doc/installed-packages/kdebase3-ssl-dev
 
-        install -d -m 755 %i/etc/pam.d
-        install -c -m 444 /etc/pam.d/login %i/etc/pam.d/kde
-        install -c -m 444 /etc/pam.d/login %i/etc/pam.d/kdm
-        install -c -m 444 /etc/pam.d/login %i/etc/pam.d/kcheckpass
-        install -c -m 444 /etc/pam.d/login %i/etc/pam.d/kscreensaver
-        rm -rf %i/share/icons/crystalsvg/scalable/apps/artsbuilder.*
+	install -d -m 755 %i/etc/pam.d
+	install -c -m 444 /etc/pam.d/login %i/etc/pam.d/kde
+	install -c -m 444 /etc/pam.d/login %i/etc/pam.d/kdm
+	install -c -m 444 /etc/pam.d/login %i/etc/pam.d/kcheckpass
+	install -c -m 444 /etc/pam.d/login %i/etc/pam.d/kscreensaver
+	rm -rf %i/share/icons/crystalsvg/scalable/apps/artsbuilder.*
 
-        install -d -m 755 "%d`%p/bin/xfontpath basedir`"
+	install -d -m 755 "%d`%p/bin/xfontpath basedir`"
 #fink
-#       mv "%i/share/apps/konsole/fonts" "%d`%p/bin/xfontpath basedir`/konsole"
+#	mv "%i/share/apps/konsole/fonts" "%d`%p/bin/xfontpath basedir`/konsole"
 #macports
-        mv "%i/share/apps/konsole/fonts" "%d%p/share/fonts/konsole"
+	mv "%i/share/apps/konsole/fonts" "%d%p/share/fonts/konsole"
