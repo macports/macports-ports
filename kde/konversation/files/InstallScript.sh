5f2084c739b4bce5f4b585e6d56d853268a50a8a
Takanori Yamamoto <takanori@macports.org>
2007-10-19 07:21:13 +0000

    Synced with Fink, Version 1.0.1-1023.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@30075 d073be05-634f-4543-b044-5fe20cf6d1d6

--- kde/konversation/files/InstallScript.sh
+++ kde/konversation/files/InstallScript.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -ev
 
-        export PREFIX="%p" USE_UNSERMAKE=1
-        . ./environment-helper.sh
+	export PREFIX="%p" USE_UNSERMAKE=1
+	. ./environment-helper.sh
 
-        ./build-helper.sh install %N %v %r unsermake -p -j1 install DESTDIR=%d LN_S='ln -sf'
+	./build-helper.sh install %N %v %r unsermake -p -j1 install DESTDIR=%d LN_S='ln -sf'
 
-        mkdir -p %i/share/doc/installed-packages
-        touch %i/share/doc/installed-packages/%N
+	mkdir -p %i/share/doc/installed-packages
+	touch %i/share/doc/installed-packages/%N
