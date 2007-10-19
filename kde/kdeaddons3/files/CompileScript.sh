8ad0db49be525be31bf6664534871aa36b8e9abf
Takanori Yamamoto <takanori@macports.org>
2007-10-19 07:15:42 +0000

    KDE 3.5.8.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@30068 d073be05-634f-4543-b044-5fe20cf6d1d6

--- kde/kdeaddons3/files/CompileScript.sh
+++ kde/kdeaddons3/files/CompileScript.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -ev
 
-        export PREFIX="%p" USE_UNSERMAKE=1
-        . ./environment-helper.sh
+	export PREFIX="%p" USE_UNSERMAKE=1
+	. ./environment-helper.sh
 
-        export CC=gcc CXX=g++
+	export CC=gcc CXX=g++
 
-        ./build-helper.sh cvs       %N %v %r make -f admin/Makefile.common cvs
-        ./build-helper.sh configure %N %v %r ./configure %c $CONFIGURE_PARAMS
-        ./build-helper.sh make      %N %v %r unsermake $UNSERMAKEFLAGS
+	./build-helper.sh cvs       %N %v %r make -f admin/Makefile.common cvs
+	./build-helper.sh configure %N %v %r ./configure %c $CONFIGURE_PARAMS
+	./build-helper.sh make      %N %v %r unsermake $UNSERMAKEFLAGS
