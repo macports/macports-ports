74885738480915dd96a4addccdb8826eec1e4e35
Mark Grimes <mgrimes@macports.org>
2007-11-03 20:14:58 +0000

    Update to 1.5.3.5.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@30675 d073be05-634f-4543-b044-5fe20cf6d1d6

--- devel/git-core/files/patch-git-instaweb.sh
+++ devel/git-core/files/patch-git-instaweb.sh
@@ -1,18 +1,6 @@
-diff --git git-instaweb.sh.orig git-instaweb.sh
-index b79c6b6..9e9253a 100755
---- git-instaweb.sh.orig
-+++ git-instaweb.sh
-@@ -30,8 +30,7 @@ test -z "$port" && port=1234
- 
- start_httpd () {
- 	httpd_only="`echo $httpd | cut -f1 -d' '`"
--	if test "`expr index $httpd_only /`" -eq '1' || \
--				which $httpd_only >/dev/null
-+	if test echo $http_only | grep ^/ || which $httpd_only >/dev/null
- 	then
- 		$httpd $fqgitdir/gitweb/httpd.conf
- 	else
-@@ -207,10 +206,14 @@ EOF
+--- git-instaweb.sh.orig	2007-11-02 12:50:47.000000000 -0700
++++ git-instaweb.sh	2007-11-02 13:03:42.000000000 -0700
+@@ -206,10 +206,14 @@ EOF
  }
  
  script='
@@ -20,14 +8,14 @@ index b79c6b6..9e9253a 100755
 -s#\(my\|our\) $gitbin =.*#\1 $gitbin = "'$GIT_EXEC_PATH'";#
 -s#\(my\|our\) $projects_list =.*#\1 $projects_list = $projectroot;#
 -s#\(my\|our\) $git_temp =.*#\1 $git_temp = "'$fqgitdir/gitweb/tmp'";#'
-+s#^my $projectroot =.*#my $projectroot = "'`dirname $fqgitdir`'";#
-+s#^our $projectroot =.*#our $projectroot = "'`dirname $fqgitdir`'";#
-+s#my $gitbin =.*#my $gitbin = "'$GIT_EXEC_PATH'";#
-+s#our $gitbin =.*#our $gitbin = "'$GIT_EXEC_PATH'";#
-+s#my $projects_list =.*#my $projects_list = $projectroot;#
-+s#our $projects_list =.*#our $projects_list = $projectroot;#
-+s#my $git_temp =.*#my $git_temp = "'$fqgitdir/gitweb/tmp'";#
-+s#our $git_temp =.*#our $git_temp = "'$fqgitdir/gitweb/tmp'";#'
+++s#^my $projectroot =.*#my $projectroot = "'`dirname $fqgitdir`'";#
+++s#^our $projectroot =.*#our $projectroot = "'`dirname $fqgitdir`'";#
+++s#my $gitbin =.*#my $gitbin = "'$GIT_EXEC_PATH'";#
+++s#our $gitbin =.*#our $gitbin = "'$GIT_EXEC_PATH'";#
+++s#my $projects_list =.*#my $projects_list = $projectroot;#
+++s#our $projects_list =.*#our $projects_list = $projectroot;#
+++s#my $git_temp =.*#my $git_temp = "'$fqgitdir/gitweb/tmp'";#
+++s#our $git_temp =.*#our $git_temp = "'$fqgitdir/gitweb/tmp'";#'
  
  gitweb_cgi () {
  	cat > "$1.tmp" <<\EOFGITWEB
