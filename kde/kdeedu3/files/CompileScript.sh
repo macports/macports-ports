ab2e221ff1b8af978df2d1e7866302d330b138ee
Takanori Yamamoto <takanori@macports.org>
2007-11-04 00:41:39 +0000

    Allow building on Leopard.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@30679 d073be05-634f-4543-b044-5fe20cf6d1d6

--- kde/kdeedu3/files/CompileScript.sh
+++ kde/kdeedu3/files/CompileScript.sh
@@ -2,7 +2,10 @@
 
 	export PREFIX="%p" USE_UNSERMAKE=1
 	. ./environment-helper.sh
-	export LDFLAGS="-L%p/lib/python2.4/config"
+#fink
+#	export LDFLAGS="-L%p/lib/python2.4/config"
+#macports
+#
 
 	./build-helper.sh cvs       %N %v %r make -f admin/Makefile.common cvs
 	./build-helper.sh configure %N %v %r ./configure %c $CONFIGURE_PARAMS
