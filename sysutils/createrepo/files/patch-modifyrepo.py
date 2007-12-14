d5bc3f870c9517970824941d82bb0aa46d1062d1
Anders F. Björklund <afb@macports.org>
2007-12-14 08:30:51 +0000

    port upgrade: createrepo 0.4.11 + avoid using the ill-defined python program
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@32030 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ sysutils/createrepo/files/patch-modifyrepo.py
@@ -0,0 +1,8 @@
+--- modifyrepo.py.orig	2007-11-26 21:23:16.000000000 +0100
++++ modifyrepo.py	2007-12-14 09:17:43.000000000 +0100
+@@ -1,4 +1,4 @@
+-#!/usr/bin/env python
++#!__PYTHON__
+ # $Id$
+ #
+ # This tools is used to insert arbitrary metadata into an RPM repository.
