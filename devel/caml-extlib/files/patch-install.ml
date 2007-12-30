8ba9cbf605f3378dec87a827416c4311a10a6361
Kevin Ballard <eridius@macports.org>
2007-12-30 21:38:59 +0000

    Bump caml-extlib to 1.5.1, move to new Google Code project page
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@32423 d073be05-634f-4543-b044-5fe20cf6d1d6

--- devel/caml-extlib/files/patch-install.ml
+++ devel/caml-extlib/files/patch-install.ml
@@ -1,16 +1,6 @@
---- install.ml.bak	2006-12-06 16:22:11.000000000 -0500
-+++ install.ml	2006-12-06 16:22:58.000000000 -0500
-@@ -173,9 +173,6 @@
- 	end;
- 	if !autodoc then begin
- 		run (sprintf "ocamldoc -sort -html -d %s %s" doc_dir (m_list ".mli"));
--		run ((match path_type with
--				| PathDos -> sprintf "%s odoc_style.css %s\\style.css";
--				| PathUnix -> sprintf "%s odoc_style.css %s/style.css") cp_cmd doc_dir);
- 	end;
- 	match install_dir with
- 	  Findlib ->
-@@ -194,7 +191,7 @@
+--- old-install.ml	2007-12-30 16:32:31.000000000 -0500
++++ install.ml	2007-12-30 16:33:29.000000000 -0500
+@@ -194,7 +194,7 @@
  	    end;
  	    run (sprintf "%s META.txt META" cp_cmd);
  	    let files = Buffer.contents files in
