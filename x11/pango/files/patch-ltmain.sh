11a8cba418cc47313a61b86e71ddf99b1adaf13d
Ryan Schmidt <ryandesign@macports.org>
2008-01-12 19:34:36 +0000

    pango: update to 1.18.4
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@32729 d073be05-634f-4543-b044-5fe20cf6d1d6

--- x11/pango/files/patch-ltmain.sh
+++ x11/pango/files/patch-ltmain.sh
@@ -1,20 +1,20 @@
---- ltmain.sh.orig	2005-09-14 11:00:12.000000000 -0400
-+++ ltmain.sh	2005-10-14 19:07:04.000000000 -0400
-@@ -1518,7 +1518,7 @@
+--- ltmain.sh.orig	2008-01-09 03:50:47.000000000 -0600
++++ ltmain.sh	2008-01-09 03:52:08.000000000 -0600
+@@ -1639,7 +1639,7 @@
  	continue
  	;;
  
--     -mt|-mthreads|-kthread|-Kthread|-pthread|-pthreads|--thread-safe)
-+     -mt|-mthreads|-kthread|-Kthread|-lpthread|-lpthreads|--thread-safe)
+-     -mt|-mthreads|-kthread|-Kthread|-pthread|-pthreads|--thread-safe|-threads)
++     -mt|-mthreads|-kthread|-Kthread|-lpthread|-pthreads|--thread-safe|-threads)
  	compiler_flags="$compiler_flags $arg"
  	compile_command="$compile_command $arg"
  	finalize_command="$finalize_command $arg"
-@@ -2005,7 +2005,7 @@
+@@ -2134,7 +2134,7 @@
  	lib=
  	found=no
  	case $deplib in
--	-mt|-mthreads|-kthread|-Kthread|-pthread|-pthreads|--thread-safe)
-+	-mt|-mthreads|-kthread|-Kthread|-lpthread|-lpthreads|--thread-safe)
+-	-mt|-mthreads|-kthread|-Kthread|-pthread|-pthreads|--thread-safe|-threads)
++	-mt|-mthreads|-kthread|-Kthread|-lpthread|-pthreads|--thread-safe|-threads)
  	  if test "$linkmode,$pass" = "prog,link"; then
  	    compile_deplibs="$deplib $compile_deplibs"
  	    finalize_deplibs="$deplib $finalize_deplibs"
