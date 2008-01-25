9f871cf002e04395ce17a3d3ed52d62944527ef7
Guido Soranzio <gui_dos@macports.org>
2008-01-25 13:46:30 +0000

    gnumeric:  * Update to version 1.8.1  * Removes files not needed
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@33367 d073be05-634f-4543-b044-5fe20cf6d1d6

--- gnome/gnumeric/files/patch-commonlib.c
+++
@@ -1,9 +0,0 @@
---- src/tools/solver/lp_solve/commonlib.c.sav	Fri Feb 25 16:34:52 2005
-+++ src/tools/solver/lp_solve/commonlib.c	Fri Feb 25 16:39:18 2005
-@@ -1,5 +1,6 @@
- 
- #include <sys/types.h>
-+#include <gnumeric-config.h>
- 
- #ifdef INTEGERTIME
- # include <time.h>
