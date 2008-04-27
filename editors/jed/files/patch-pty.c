e396136b0a6fbf529712b3385f771c62bf20a60a
Ryan Schmidt <ryandesign@macports.org>
2008-04-27 10:47:30 +0000

    jed: update to 0.99-18; closes https://trac.macports.org/ticket/14872
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@36333 d073be05-634f-4543-b044-5fe20cf6d1d6

--- editors/jed/files/patch-pty.c
+++ editors/jed/files/patch-pty.c
@@ -1,11 +1,11 @@
---- src/pty.c.org	Sun Sep  5 08:55:33 2004
-+++ src/pty.c	Sun Sep  5 08:56:08 2004
-@@ -18,7 +18,7 @@
- 
+--- src/pty.c.orig	2008-03-31 00:47:37.000000000 +0200
++++ src/pty.c	2008-03-31 00:48:54.000000000 +0200
+@@ -19,7 +19,7 @@
  #include <errno.h>
  
--#if !defined (__linux__) && !defined(__CYGWIN__) && defined(HAVE_GRANTPT)
-+#if !defined (__linux__) && !defined(__CYGWIN__) && !defined (__APPLE__) && defined(HAVE_GRANTPT) 
- # define USE_SYSV_PTYS
- # include <sys/types.h>
- # include <stropts.h>
+ #ifdef HAVE_GRANTPT
+-# if !defined (__linux__) && !defined(__CYGWIN__) && !defined(__FreeBSD__)
++# if !defined (__linux__) && !defined(__CYGWIN__) && !defined(__APPLE__) && !defined(__FreeBSD__)
+ #  define USE_SYSV_PTYS
+ #  include <sys/types.h>
+ #  include <stropts.h>
