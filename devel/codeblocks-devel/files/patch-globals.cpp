4160394a4fd538d586120c5838f28b3b290dfcb3
Anders F. Björklund <afb@macports.org>
2008-05-02 09:00:48 +0000

    increase nightly build revision, change autotools depends style
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@36445 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ devel/codeblocks-devel/files/patch-globals.cpp
@@ -0,0 +1,12 @@
+Index: src/sdk/globals.cpp
+===================================================================
+--- src/sdk/globals.cpp	(revision 5020)
++++ src/sdk/globals.cpp	(arbetskopia)
+@@ -29,6 +29,7 @@
+ #include <wx/msgdlg.h>
+ #include <wx/fontmap.h>
+ #include <algorithm>
++#include <string>
+ #include "filefilters.h"
+ #include "tinyxml/tinywxuni.h"
+ 
