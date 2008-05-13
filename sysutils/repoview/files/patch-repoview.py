b2e8b07281c41c547813425322007b4a4d2c7c24
Anders F. Björklund <afb@macports.org>
2008-05-13 21:09:26 +0000

    revert back to 0.5.2 (without sqlite) for now, but don't worry about epoch as it'll return to 0.6.2 later
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@36741 d073be05-634f-4543-b044-5fe20cf6d1d6

--- sysutils/repoview/files/patch-repoview.py
+++ sysutils/repoview/files/patch-repoview.py
@@ -1,11 +1,57 @@
---- repoview.py.orig	2007-08-09 00:14:08.000000000 +0200
-+++ repoview.py	2007-08-09 00:16:14.000000000 +0200
-@@ -53,7 +53,7 @@
+# Fedora: yum-2.9-api.patch
+--- repoview.py.orig	2006-07-04 20:53:55.000000000 -0400
++++ repoview.py	2006-07-04 21:21:56.000000000 -0400
+@@ -42,7 +42,7 @@
  try:
-     import sqlite3 as sqlite
+     from yum.comps import Comps, CompsException
+     from yum.mdparser import MDParser
+-    from repomd.repoMDObject import RepoMD
++    from yum.repoMDObject import RepoMD
  except ImportError:
--    import sqlite
-+    from pysqlite2 import dbapi2 as sqlite
- 
- ##
- # Some hardcoded constants
+     try:
+         from noyum.comps import Comps, CompsException
+@@ -364,7 +364,7 @@
+         except IOError: 
+             return 1
+         checksum = checksum.strip()
+-        if checksum != self.repodata['primary']['checksum'][1]: 
++        if checksum != self.repodata['primary'].checksum[1]: 
+             return 1
+         _say("RepoView: Repository has not changed. Force the run with -f.\n")
+         sys.exit(0)
+@@ -374,7 +374,7 @@
+         Utility method for parsing comps.xml.
+         """
+         _say('parsing comps...', 1)
+-        loc = self.repodata['group']['relativepath']
++        loc = self.repodata['group'].location[1]
+         comps = Comps()
+         try:
+             comps.add(os.path.join(self.repodir, loc))
+@@ -436,7 +436,7 @@
+         Utility method for processing primary.xml.
+         """
+         _say('parsing primary...', 1)
+-        loc = self.repodata['primary']['relativepath']
++        loc = self.repodata['primary'].location[1]
+         mdp = MDParser(os.path.join(self.repodir, loc))
+         ignored = 0
+         for package in mdp:
+@@ -507,7 +507,7 @@
+         Utility method to get data from other.xml.
+         """
+         _say('parsing other...', 1)
+-        loc = self.repodata['other']['relativepath']
++        loc = self.repodata['other'].location[1]
+         otherxml = os.path.join(self.repodir, loc)
+         ignored = 0
+         mdp = MDParser(otherxml)
+@@ -789,7 +789,7 @@
+         _say('writing checksum...', 1)
+         chkfile = os.path.join(self.outdir, 'checksum')
+         fh = open(chkfile, 'w')
+-        fh.write(self.repodata['primary']['checksum'][1])
++        fh.write(self.repodata['primary'].checksum[1])
+         fh.close()
+         _say('done\n')
+         _say('Moving new repoview dir in place...', 1)
