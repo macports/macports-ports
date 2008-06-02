2a94807890304e36ab16abf5e0454202bc313ae0
Rainer Müller <raimue@macports.org>
2008-06-02 00:34:32 +0000

    perl/p5-sdl_perl: Maintainer update, closes https://trac.macports.org/ticket/13854
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@37273 d073be05-634f-4543-b044-5fe20cf6d1d6

--- perl/p5-sdl_perl/files/patch-make-lib-SDL-Build-Darwin.pm
+++ perl/p5-sdl_perl/files/patch-make-lib-SDL-Build-Darwin.pm
@@ -1,8 +1,21 @@
---- make/lib/SDL/Build/Darwin.pm.orig	2006-11-19 21:47:14.000000000 -0800
-+++ make/lib/SDL/Build/Darwin.pm	2006-11-19 21:48:58.000000000 -0800
-@@ -16,17 +16,30 @@
- 	'/usr/include/GL'          => '/usr/lib', 
- 	'/usr/include/gl'          => '/usr/lib', 
+--- make/lib/SDL/Build/Darwin.pm.orig	2008-05-03 13:58:14.000000000 -0700
++++ make/lib/SDL/Build/Darwin.pm	2008-05-03 14:00:52.000000000 -0700
+@@ -5,28 +5,25 @@
+ sub fetch_includes
+ {
+ 	return (
+-	'/usr/local/include/SDL'   => '/usr/local/lib',
++	"$ENV{'PREFIX'}/include"   => "$ENV{'PREFIX'}/lib",
++	"$ENV{'PREFIX'}/include/SDL" => "$ENV{'PREFIX'}/lib",
+ 	'/usr/local/include'       => '/usr/local/lib',
+-	'/usr/local/include/smpeg' => '/usr/local/lib',
+-	'/usr/include/SDL'         => '/usr/lib',
+ 	'/usr/include'             => '/usr/lib',
+-	'/usr/include/smpeg'       => '/usr/lib',
+-	'/usr/local/include/GL'    => '/usr/local/lib',
+-	'/usr/local/include/gl'    => '/usr/local/lib',
+-	'/usr/include/GL'          => '/usr/lib', 
+-	'/usr/include/gl'          => '/usr/lib', 
  
 -	'/System/Library/Frameworks/SDL_mixer.framework/Headers'     => '../../lib',
 -	'/System/Library/Frameworks/SDL_image.framework/Headers'     => '../../lib',
@@ -12,19 +25,8 @@
 -	'/System/Library/Frameworks/libvorbisfile.framework/Headers' => '../../lib',
 -	'/System/Library/Frameworks/libvorbisenc.framework/Headers'  => '../../lib',
 -	'../../include'                                              => '../../lib',
--	'/System/Library/Frameworks/OpenGL.framework/Headers'        =>
-+	'/Library/Frameworks/SDL.framework/Headers'	      => '../../lib',
-+	'/Library/Frameworks/SDL_mixer.framework/Headers'     => '../../lib',
-+	'/Library/Frameworks/SDL_image.framework/Headers'     => '../../lib',
-+	'/Library/Frameworks/SDL_net.framework/Headers'       => '../../lib',
-+	'/Library/Frameworks/SDL_ttf.framework/Headers'       => '../../lib',
-+	'/Library/Frameworks/SDL_gfx.framework/Headers'       => '../../lib',
-+	'/Library/Frameworks/libogg.framework/Headers'        => '../../lib',
-+	'/Library/Frameworks/libvorbis.framework/Headers'     => '../../lib',
-+	'/Frameworks/libvorbisfile.framework/Headers'	      => '../../lib',
-+	'/Library/Frameworks/libvorbisenc.framework/Headers'  => '../../lib',
 +	'../../include'                                       => '../../lib',
-+	'/System/Library/Frameworks/OpenGL.framework/Headers' =>
+ 	'/System/Library/Frameworks/OpenGL.framework/Headers'        =>
  		'/System/Library/Frameworks/OpenGL.framework/Libraries',
  	);
  }
