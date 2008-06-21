d9111e2863254712f3bdce3b5c46aa023523424b
Wataru Kimura <kimuraw@macports.org>
2008-06-21 09:28:12 +0000

    update lang/ruby-1.8.7-p22 to rev2 - add missing Config::CONFIG['vendorarch'] - correct files/patch-mkconfig.rb
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@37739 d073be05-634f-4543-b044-5fe20cf6d1d6

--- lang/ruby/files/patch-mkconfig.rb
+++ lang/ruby/files/patch-mkconfig.rb
@@ -1,16 +1,16 @@
---- lib/mkmf.rb.orig	2008-05-29 20:23:36.000000000 +0900
-+++ lib/mkmf.rb	2008-06-21 14:00:40.000000000 +0900
-@@ -54,6 +54,13 @@
- $vendordir = CONFIG["vendordir"]
- $vendorlibdir = CONFIG["vendorlibdir"]
- $vendorarchdir = CONFIG["vendorarchdir"]
-+# macports -rvendor-specific support
-+if defined?(VENDOR_SPECIFIC) && VENDOR_SPECIFIC
-+  CONFIG["sitearch"] = CONFIG["vendorarch"]
-+  CONFIG["sitedir"] = CONFIG["vendordir"]
-+  CONFIG["sitelibdir"] = CONFIG["vendorlibdir"]
-+  CONFIG["sitearchdir"] = CONFIG["vendorarchdir"]
-+end
- 
- $mswin = /mswin/ =~ RUBY_PLATFORM
- $bccwin = /bccwin/ =~ RUBY_PLATFORM
+--- mkconfig.rb.orig	2008-06-21 17:22:11.000000000 +0900
++++ mkconfig.rb	2008-06-21 17:23:00.000000000 +0900
+@@ -147,6 +147,13 @@
+   CONFIG["sitearchdir"] = "$(sitelibdir)/$(sitearch)"
+   CONFIG["vendorlibdir"] = "$(vendordir)/$(ruby_version)"
+   CONFIG["vendorarchdir"] = "$(vendorlibdir)/$(sitearch)"
++  # macports -rvendor-specific support
++  if defined?(VENDOR_SPECIFIC) && VENDOR_SPECIFIC
++    CONFIG["sitearch"] = CONFIG["vendorarch"]
++    CONFIG["sitedir"] = CONFIG["vendordir"]
++    CONFIG["sitelibdir"] = CONFIG["vendorlibdir"]
++    CONFIG["sitearchdir"] = CONFIG["vendorarchdir"]
++  end
+   CONFIG["topdir"] = File.dirname(__FILE__)
+   MAKEFILE_CONFIG = {}
+   CONFIG.each{|k,v| MAKEFILE_CONFIG[k] = v.dup}
