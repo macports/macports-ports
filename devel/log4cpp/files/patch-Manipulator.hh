9a942e9dafc3d581c3c38ed5dc5487e5804d41eb
Joshua Root <jmr@macports.org>
2008-07-30 11:58:13 +0000

    log4cpp: update to version 1.0 (https://trac.macports.org/ticket/15872, maintainer timeout)
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@38758 d073be05-634f-4543-b044-5fe20cf6d1d6

--- devel/log4cpp/files/patch-Manipulator.hh
+++
@@ -1,9 +0,0 @@
---- include/log4cpp/Manipulator.hh	2005-06-01 12:07:48.000000000 -0700
-+++ Manipulator.hh	2006-01-30 14:24:43.000000000 -0800
-@@ -26,5 +26,5 @@
- inline	tab(unsigned int i) : size(i) {}
- friend LOG4CPP_EXPORT std::ostream& operator<< (std::ostream& os, const tab& w);
- 	};
--};
-+}
- #endif
