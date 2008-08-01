cf2f0f4c6e8e938c2b86851bf4bc6349b0bc4576
Landon Fuller <landonf@macports.org>
2008-08-01 06:05:30 +0000

    Update to lock to GEOS 2 -- requires fairly small changes. Test procedure:   - Verified that the py-matplotlib-basemap _geoslib.so linked against the geos2 port geos dylib.   - Ran test.py and sanity-checked the output. Please let me know if there are any issues and I'll do my best to fix them rapidly.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@38826 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ python/py-matplotlib-basemap/files/patch-src_geoslib.c
@@ -0,0 +1,11 @@
+--- src/_geoslib.c.orig	2008-07-31 22:26:20.000000000 -0700
++++ src/_geoslib.c	2008-07-31 22:26:30.000000000 -0700
+@@ -31,7 +31,7 @@
+ #endif
+ #include <math.h>
+ #include "numpy/arrayobject.h"
+-#include "geos_c.h"
++#include "geos2_c.h"
+ 
+ 
+ #ifdef __GNUC__
