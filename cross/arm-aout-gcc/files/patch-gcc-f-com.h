4a45ce2c0a7a81468972eb5cc33fa222d45df16b
Paul Guyot <pguyot@macports.org>
2008-09-17 08:42:55 +0000

    cross/arm-aout-gcc: Fix patchs paths (https://trac.macports.org/ticket/15677)
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@40007 d073be05-634f-4543-b044-5fe20cf6d1d6

--- cross/arm-aout-gcc/files/patch-gcc-f-com.h
+++ cross/arm-aout-gcc/files/patch-gcc-f-com.h
@@ -1,5 +1,5 @@
---- com.h	2006-11-05 08:54:21.000000000 +0900
-+++ /opt/local/var/db/dports/build/_Junk_src_macports-trunk_dports_cross_arm-aout-gcc/work/gcc-3.3.6/gcc/f/com.h	2006-11-05 08:54:00.000000000 +0900
+--- gcc/f/com.h	2006-11-05 08:54:21.000000000 +0900
++++ gcc/f/com.h	2006-11-05 08:54:00.000000000 +0900
 @@ -233,7 +233,7 @@
  void ffecom_finish_progunit (void);
  tree ffecom_get_invented_identifier (const char *pattern, ...)
--- cross/arm-aout-gcc/files/patch-gcc-java-gjavah.c
+++ cross/arm-aout-gcc/files/patch-gcc-java-gjavah.c
@@ -1,5 +1,5 @@
---- gjavah.c	2006-11-05 09:01:19.000000000 +0900
-+++ /opt/local/var/db/dports/build/_Junk_src_macports-trunk_dports_cross_arm-aout-gcc/work/gcc-3.3.6/gcc/java/gjavah.c	2006-11-05 09:01:01.000000000 +0900
+--- gcc/java/gjavah.c	2006-11-05 09:01:19.000000000 +0900
++++ gcc/java/gjavah.c	2006-11-05 09:01:01.000000000 +0900
 @@ -46,7 +46,7 @@
  static int found_error = 0;
  
