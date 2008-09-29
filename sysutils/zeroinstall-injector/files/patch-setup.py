314be5564563ab43f09ad50d9e466214002b13bf
Anders F. Björklund <afb@macports.org>
2008-09-29 07:25:18 +0000

    port upgrade: 0launch 0.36
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@40310 d073be05-634f-4543-b044-5fe20cf6d1d6

--- sysutils/zeroinstall-injector/files/patch-setup.py
+++ sysutils/zeroinstall-injector/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	2007-07-21 12:31:32.000000000 +0200
-+++ setup.py	2007-10-15 14:32:24.000000000 +0200
-@@ -38,7 +38,7 @@
+--- setup.py.orig	2008-09-06 12:55:07.000000000 +0200
++++ setup.py	2008-09-29 08:45:11.000000000 +0200
+@@ -63,7 +63,7 @@
        author_email="zero-install-devel@lists.sourceforge.net",
        url="http://0install.net",
-       scripts=['0launch', '0alias', '0store', '0store-secure-add'],
--      data_files = [('man/man1', ['0launch.1', '0alias.1', '0store-secure-add.1', '0store.1']),
-+      data_files = [('share/man/man1', ['0launch.1', '0alias.1', '0store-secure-add.1', '0store.1']),
-       		    ('share/applications', ['applications/zeroinstall-zero2desktop.desktop']),
-       		    ('share/pixmaps', ['applications/zeroinstall-zero2desktop.png'])],
-       license='LGPL',
+       scripts=['0launch', '0alias', '0store', '0store-secure-add', '0desktop'],
+-      data_files = [('man/man1', ['0launch.1', '0alias.1', '0store-secure-add.1', '0store.1', '0desktop.1']),
++      data_files = [('share/man/man1', ['0launch.1', '0alias.1', '0store-secure-add.1', '0store.1', '0desktop.1']),
+ 		    ('share/applications', ['applications/zeroinstall-add.desktop', 'applications/zeroinstall-manage.desktop']),
+ 		    ('share/desktop-directories', ['applications/zeroinstall.directory']),
+ 		    ('share/pixmaps', ['applications/zeroinstall-zero2desktop.png'])],
