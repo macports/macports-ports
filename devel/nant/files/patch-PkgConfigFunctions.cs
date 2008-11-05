356ce5f2ec57214d272c5a284a6988946af5b643
Joshua Root <jmr@macports.org>
2008-11-05 10:30:42 +0000

    nant: fix building with current mono (https://trac.macports.org/ticket/15137)
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@41516 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ devel/nant/files/patch-PkgConfigFunctions.cs
@@ -0,0 +1,11 @@
+--- src/NAnt.Core/Functions/PkgConfigFunctions.cs.orig	2008-04-26 23:08:21.000000000 -0400
++++ src/NAnt.Core/Functions/PkgConfigFunctions.cs	2008-04-26 23:09:10.000000000 -0400
+@@ -260,7 +260,7 @@
+                 execTask.Execute();
+                 ms.Position = 0;
+                 StreamReader sr = new StreamReader(ms);
+-                string output = sr.ReadLine();
++                string output = sr.ReadLine().Replace("\x00", "");
+                 sr.Close();
+                 return output;
+             } catch (Exception ex) {
---
+++ devel/nant/files/patch-ScriptTask.cs
@@ -0,0 +1,11 @@
+--- src/NAnt.DotNet/Tasks/ScriptTask.cs.orig	2008-04-26 23:06:17.000000000 -0400
++++ src/NAnt.DotNet/Tasks/ScriptTask.cs	2008-04-26 23:07:28.000000000 -0400
+@@ -516,7 +516,7 @@
+         #region Private Static Methods
+ 
+         private static CodeDomProvider CreateCodeDomProvider(string typeName, string assemblyName) {
+-            Assembly providerAssembly = Assembly.LoadWithPartialName(assemblyName);
++            Assembly providerAssembly = Assembly.Load(assemblyName);
+             if (providerAssembly == null) {
+                 throw new ArgumentException(string.Format(CultureInfo.InvariantCulture,
+                     ResourceUtils.GetString("NA2037"), assemblyName));
