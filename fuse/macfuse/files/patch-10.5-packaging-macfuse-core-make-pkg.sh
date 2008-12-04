a3822c4e7c3e80e4650fed91102ea3b5bf0ba3e7
Kevin Ballard <eridius@macports.org>
2008-12-04 00:01:43 +0000

    Bump macfuse to 1.7_1.
    It turns out the previous version was actually a beta version 1.6.
    
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@43046 d073be05-634f-4543-b044-5fe20cf6d1d6

--- fuse/macfuse/files/patch-10.5-packaging-macfuse-core-make-pkg.sh
+++ fuse/macfuse/files/patch-10.5-packaging-macfuse-core-make-pkg.sh
@@ -1,5 +1,14 @@
---- 10.5/packaging/macfuse-core/make-pkg.sh.orig	2007-11-04 12:15:47.000000000 -0700
-+++ 10.5/packaging/macfuse-core/make-pkg.sh	2008-07-05 15:31:40.000000000 -0600
+--- 10.5/packaging/macfuse-core/make-pkg.sh.old	2008-12-03 15:25:16.000000000 -0800
++++ 10.5/packaging/macfuse-core/make-pkg.sh	2008-12-03 15:25:43.000000000 -0800
+@@ -88,7 +88,7 @@
+ 
+ # Copy the uninstall script
+ UNINSTALL_DST="$DISTRIBUTION_FOLDER/Library/Filesystems/fusefs.fs/Support/uninstall-macfuse-core.sh"
+-sudo cp "$UNINSTALL_SCRIPT" "$UNINSTALL_DST"
++sudo /bin/cp "$UNINSTALL_SCRIPT" "$UNINSTALL_DST"
+ sudo chmod 755 "$UNINSTALL_DST"
+ sudo chown root:wheel "$UNINSTALL_DST"
+ 
 @@ -131,7 +131,7 @@
  VOLUME_PATH="/Volumes/$VOLUME_NAME"
  
@@ -9,7 +18,7 @@
  if [ $? -ne 0 ]
  then
      hdiutil detach "$VOLUME_PATH"
-@@ -139,7 +139,7 @@
+@@ -139,14 +139,14 @@
  fi
  
  # Set the custom icon.
@@ -18,4 +27,12 @@
  sudo /Developer/Tools/SetFile -a C "$VOLUME_PATH"
  
  # Copy over the license file.
-
+-sudo cp "$INSTALL_RESOURCES/License.rtf" "$VOLUME_PATH"/License.rtf
++sudo /bin/cp "$INSTALL_RESOURCES/License.rtf" "$VOLUME_PATH"/License.rtf
+ 
+ # Copy over the CHANGELOG.txt.
+-sudo cp "../../../../CHANGELOG.txt" "$VOLUME_PATH"/CHANGELOG.txt
++sudo /bin/cp "../../../../CHANGELOG.txt" "$VOLUME_PATH"/CHANGELOG.txt
+ 
+ # Detach the volume.
+ hdiutil detach "$VOLUME_PATH"
