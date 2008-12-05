cf8399a06e64f5f0210e2a54112ebf447f52413e
Joshua Root <jmr@macports.org>
2008-12-05 14:37:45 +0000

    Add python26 portgroup.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@43113 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ _resources/port1.0/group/python26-1.0.tcl
@@ -0,0 +1,59 @@
+# et:ts=4
+# python26-1.0.tcl
+#
+# $Id$
+#
+# Copyright (c) 2008 The MacPorts Project,
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. Neither the name of Apple Computer, Inc. nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+set prefix ${frameworks_dir}/Python.framework/Versions/2.6
+
+set python.bin	${prefix}/bin/python2.6
+set python.lib	${prefix}/lib/libpython2.6.dylib
+set python.libdir ${prefix}/Python
+set python.pkgd	${prefix}/lib/python2.6/site-packages
+set python.include	${prefix}/include/python2.6
+
+categories		python
+
+dist_subdir		python
+
+depends_lib		port:python26
+
+use_configure	no
+
+build.cmd		${python.bin} setup.py
+build.target	build
+
+destroot.cmd	${python.bin} setup.py
+destroot.destdir	--prefix=${prefix} --root=${destroot}
+
+pre-destroot	{
+	xinstall -d -m 755 ${destroot}${prefix}/share/doc/${name}/examples
+}
