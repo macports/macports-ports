0ffedda1a405e1780e645f87f0c665969aeed8ad
Yves de Champlain <yves@macports.org>
2009-02-01 21:43:29 +0000

    Update denemo to 0.8.2, become maintainer
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@46261 d073be05-634f-4543-b044-5fe20cf6d1d6

--- print/denemo/files/patch-prefops.c
+++ print/denemo/files/patch-prefops.c
@@ -1,6 +1,6 @@
---- src/prefops.c.orig	2006-12-22 07:51:18.000000000 -0500
-+++ src/prefops.c	2007-01-11 23:09:16.000000000 -0500
-@@ -88,10 +88,10 @@
+--- src/prefops.c.orig	2009-02-01 11:40:50.000000000 -0500
++++ src/prefops.c	2009-02-01 11:41:28.000000000 -0500
+@@ -72,18 +72,18 @@
    /* Reasonable default values */
  
    ret->lilypath = g_string_new ("lilypond");
@@ -10,7 +10,18 @@
 +  ret->audioplayer = g_string_new ("qtplay");
    ret->csoundcommand = g_string_new ("csound -dm6");
 -  ret->browser = g_string_new ("firefox");
-+  ret->browser = g_string_new ("dillo");
++  ret->browser = g_string_new ("open");
    ret->csoundorcfile = g_string_new ("");
-   ret->pdfviewer = g_string_new ("xpdf");
-   ret->texteditor = g_string_new ("xedit");
+-  ret->pdfviewer = g_string_new ("xpdf");
++  ret->pdfviewer = g_string_new ("open");
+   ret->sequencer = g_string_new ("/dev/sequencer");
+   ret->midi_in = g_string_new ("/dev/midi");
+ 
+ 
+-  ret->imageviewer = g_string_new ("eog");
+-  ret->texteditor = g_string_new ("xedit");
++  ret->imageviewer = g_string_new ("open");
++  ret->texteditor = g_string_new ("smultron");
+   ret->denemopath = g_string_new (g_get_home_dir());
+   ret->lilyversion = g_string_new (LILYPOND_VERSION);
+   ret->temperament = g_string_new("Equal");
