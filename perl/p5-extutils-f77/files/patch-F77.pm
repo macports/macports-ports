c8984d4c8e79493093858d10be1983d7376d8589
Bryan Blackburn <blb@macports.org>
2009-03-05 22:43:25 +0000

    perl/p5-extutils-f77 - version update to 1.16, switch to gcc43 for its fortran needs (ticket https://trac.macports.org/ticket/15838)
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@47769 d073be05-634f-4543-b044-5fe20cf6d1d6

--- perl/p5-extutils-f77/files/patch-F77.pm
+++ perl/p5-extutils-f77/files/patch-F77.pm
@@ -1,18 +1,16 @@
---- F77.pm.orig	Fri Jun 15 14:04:17 2001
-+++ F77.pm	Mon May 17 00:18:45 2004
-@@ -265,6 +265,15 @@
- $F77config{VMS}{DEFAULT}     = 'Fortran';
- $F77config{VMS}{Fortran}{Compiler} = 'Fortran';
+--- F77.pm.orig	2007-04-01 22:40:23.000000000 -0600
++++ F77.pm	2009-03-05 15:32:52.000000000 -0700
+@@ -341,10 +341,10 @@
  
-+### Darwin ###
-+
-+# Note, this is MacPorts-specific, as it relies on MacPorts to make
-+# sure g77 is installed prior to this module being installed
-+
-+$F77config{Darwin}{G77} = $F77config{Generic}{G77};
-+$F77config{Darwin}{F2c} = $F77config{Generic}{G77};
-+$F77config{Darwin}{DEFAULT} = 'G77';
-+
+ $F77config{Darwin}{GFortran}{Trail_} = 1;
+ $F77config{Darwin}{GFortran}{Cflags} = ' ';        # <---need this space!
+-$F77config{Darwin}{GFortran}{Link}   = '-L/usr/local/lib -lgfortran';    
+-$F77config{Darwin}{GFortran}{Compiler} = 'gfortran';
++$F77config{Darwin}{GFortran}{Link}   = '-L@@PREFIX@@/lib/gcc43 -lgfortran';    
++$F77config{Darwin}{GFortran}{Compiler} = 'gfortran-mp-4.3';
+ 
+-$F77config{Darwin}{DEFAULT}     = 'G77';
++$F77config{Darwin}{DEFAULT}     = 'GFortran';
+ 
  ############ End of database is here ############ 
  
- =head1 SYNOPSIS
