c1fdffa673a9287f945b23afc9ca1788eea2eae0
Bryan Blackburn <blb@macports.org>
2009-03-22 00:27:31 +0000

    fetch/mirror_sites.tcl - add trolltech mirrors
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@48436 d073be05-634f-4543-b044-5fe20cf6d1d6

--- _resources/port1.0/fetch/mirror_sites.tcl
+++ _resources/port1.0/fetch/mirror_sites.tcl
@@ -363,6 +363,15 @@ set portfetch::mirror_sites::sites(tex_ctan) {
 	ftp://ctan.tug.org/tex-archive/
 }
 
+set portfetch::mirror_sites::sites(trolltech) {
+    ftp://ftp.trolltech.com/qt/source/:nosubdir
+    http://wftp.tu-chemnitz.de/pub/Qt/qt/source/:nosubdir
+    http://ftp.heanet.ie/mirrors/ftp.trolltech.com/pub/qt/source/:nosubdir
+    http://mirror.aarnet.edu.au/pub/qt/source/:nosubdir
+    http://ftp.ntua.gr/pub/X11/Qt/qt/source/:nosubdir
+    ftp://ftp.informatik.hu-berlin.de/pub1/Mirrors/ftp.troll.no/QT/qt/source/:nosubdir
+}
+
 set portfetch::mirror_sites::sites(xcontrib) {
     ftp://ftp.net.ohio-state.edu/pub/X11/contrib/
     http://www.mirrorservice.org/sites/ftp.x.org/contrib/
