734316cac33cbdf01b46daaae1649a1dfe562126
Bryan Blackburn <blb@macports.org>
2009-05-31 07:46:29 +0000

    python/py2[56]-keyczar - semi-stealth update for security issue, not messing with dist_subdir since they also changed the distname; security issue is documented at <http://groups.google.com/group/keyczar-discuss/browse_thread/thread/5571eca0948b2a13>
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@51672 d073be05-634f-4543-b044-5fe20cf6d1d6

--- python/py26-keyczar/files/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup( name='keyczar',
-       version='@@VERSION@@',
-       packages=['keyczar'],
-       url='@@HOME@@',
-       description='@@DESCRIPTION@@',
-     )
-
