140c52365502e3df789d2c977864dbad3e2095b2
Anthony Ramine <nox@macports.org>
2009-06-13 13:46:37 +0000

    _resources: Use livecheck.distname in freshmeat livecheck.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@52275 d073be05-634f-4543-b044-5fe20cf6d1d6

--- _resources/port1.0/livecheck/freshmeat.tcl
+++ _resources/port1.0/livecheck/freshmeat.tcl
@@ -6,6 +6,6 @@ if {!$has_homepage || ${livecheck.url} eq ${homepage}} {
     set livecheck.url "http://freshmeat.net/projects/${livecheck.name}/releases.atom"
 }
 if {${livecheck.regex} eq ""} {
-    set livecheck.regex [list "(?i)<title>${livecheck.name} (.*)</title>"]
+    set livecheck.regex [list "(?i)<title>${livecheck.distname} (.*)</title>"]
 }
 set livecheck.type "regex"
