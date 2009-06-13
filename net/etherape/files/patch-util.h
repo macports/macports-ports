10bdaee53e851dab5fac5355a7f186f73987db13
Frank Schima <mf2k@macports.org>
2009-06-13 16:22:35 +0000

    Update to version 0.9.7. Removed unnecessary patchfiles. (https://trac.macports.org/ticket/19848)
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@52287 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ net/etherape/files/patch-util.h
@@ -0,0 +1,11 @@
+--- src/util.h.orig	2009-05-17 20:58:55.000000000 +0200
++++ src/util.h	2009-05-17 20:59:54.000000000 +0200
+@@ -26,6 +26,8 @@
+ #ifndef __UTIL_H__
+ #define __UTIL_H__
+ 
++#include <glib.h>
++
+ #ifdef __cplusplus
+ extern "C"
+ {
