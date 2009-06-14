5e9dc3932f0532ffc8e5c7e2369d62b3303b9e31
Toby Peterson <toby@macports.org>
2009-06-14 10:00:34 +0000

    fix a 64-bit warning, clean up Portfile a bit
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@52334 d073be05-634f-4543-b044-5fe20cf6d1d6

--- archivers/9e/files/patch-9e.c
+++ archivers/9e/files/patch-9e.c
@@ -1,5 +1,5 @@
---- 9e.c.orig	2005-04-19 08:59:41.000000000 -0400
-+++ 9e.c	2005-04-19 09:00:03.000000000 -0400
+--- 9e.c.orig	2009-06-14 02:51:47.000000000 -0700
++++ 9e.c	2009-06-14 02:52:33.000000000 -0700
 @@ -4,6 +4,7 @@
  #include <dirent.h>
  #include <errno.h>
@@ -8,3 +8,12 @@
  #include <string.h>
  #include <unistd.h>
  #include <sys/stat.h>
+@@ -144,7 +145,7 @@
+       fout = NULL;
+     } else {
+       if (Verbose)
+-	fprintf(stderr, "%s %d\n", namebuf, size);
++	fprintf(stderr, "%s %ld\n", namebuf, size);
+       if(mode & CHDIR) {
+         assert(size == 0);
+ 	/* Give ourselves read, write, and execute permission */
