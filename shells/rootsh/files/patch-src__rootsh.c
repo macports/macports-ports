80ece35927c5eccca8ed521b995bf4cc31f159d3
Toby Peterson <toby@macports.org>
2009-06-28 10:26:21 +0000

    rootsh 1.5.3
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@53026 d073be05-634f-4543-b044-5fe20cf6d1d6

--- shells/rootsh/files/patch-src__rootsh.c
+++ shells/rootsh/files/patch-src__rootsh.c
@@ -1,24 +1,23 @@
---- src/rootsh.c.orig	Thu Mar 24 08:08:20 2005
-+++ src/rootsh.c	Sun Mar 27 00:38:09 2005
-@@ -42,6 +42,9 @@
- #include <sys/stat.h>
+--- src/rootsh.c.orig	2009-06-28 03:19:08.000000000 -0700
++++ src/rootsh.c	2009-06-28 03:24:41.000000000 -0700
+@@ -43,6 +43,8 @@
  #include <dirent.h>
  #include <regex.h>
-+#include <sys/aio.h>
+ #include <wordexp.h>
 +#include <sys/ioctl.h>
 +#include <util.h>
  #if HAVE_SYS_SELECT_H
  #  include <sys/select.h>
  #endif
-@@ -177,7 +180,6 @@
+@@ -180,7 +182,6 @@
  //  
  */
  extern char **environ;
 -extern int errno;
  
- static char progName[MAXPATHLEN];
- static char sessionId[MAXPATHLEN + 11];
-@@ -235,7 +237,7 @@
+ volatile sig_atomic_t sigwinch_received;
+ 
+@@ -242,7 +243,7 @@
    //  
    //  long_options	Used by getopt_long.
    */
@@ -27,3 +26,35 @@
    char *sucmd = SUCMD;
    static char sessionIdEnv[sizeof(sessionId) + 17];
    fd_set readmask;
+@@ -296,7 +297,9 @@
+         version();
+         break;
+       case 'c':
++	/*
+ 	// if they supply a -c somewhere and it isn't the first arg, then the command is no good
++	*/
+ 	if ( strcmp("-c", argv[1]) ) usage ();
+ 	doscp = 1;
+ 	break;
+@@ -339,10 +342,10 @@
+   // The user requested a SCP command
+   */
+   if (doscp) {
++	  int i;
+ 	  if (! beginlogging()) {
+ 		  exit(EXIT_FAILURE);
+ 	  }
+-	  int i;
+ 	  for ( i = 0; i < argc; i++ ) {
+ 		  dologging(argv[i], sizeof(argv[i]));
+ 		  dologging("\n", sizeof("\n"));
+@@ -834,7 +837,8 @@
+         */
+         msglen = snprintf(msgbuf, (sizeof(msgbuf) - 1),
+             "*** USER TRIED TO DELETE AND RECREATE THIS FILE ***\r\n");
+-        unlink(logFileName) && rmdir(logFileName);
++        if (unlink(logFileName))
++          rmdir(logFileName);
+       } else {
+         if (fstat(logFile, &statBuf) == -1) {
+           /*
