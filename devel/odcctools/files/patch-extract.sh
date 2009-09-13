addbab29e51589710364db95ad8d3339122b4195
Toby Peterson <toby@macports.org>
2009-09-13 18:29:57 +0000

    odcctools: simplify excessive fix for https://trac.macports.org/ticket/21204, should also resolve https://trac.macports.org/ticket/21346
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@57583 d073be05-634f-4543-b044-5fe20cf6d1d6

--- devel/odcctools/files/patch-extract.sh
+++ devel/odcctools/files/patch-extract.sh
@@ -1,50 +1,15 @@
---- extract.sh.orig	2007-05-28 09:32:38.000000000 -0400
-+++ extract.sh	2007-05-28 09:35:10.000000000 -0400
-@@ -44,7 +44,7 @@
+--- extract.sh.orig	2009-09-13 11:24:01.000000000 -0700
++++ extract.sh	2009-09-13 11:24:14.000000000 -0700
+@@ -44,11 +44,7 @@
  
  
  
 -if [ "`tar --help | grep -- --strip-components 2> /dev/null`" ]; then
-+if [ "`gnutar --help | grep -- --strip-components 2> /dev/null`" ]; then
-     TARSTRIP=--strip-components
- else
-     TARSTRIP=--strip-path
-@@ -62,9 +62,9 @@
- fi
+-    TARSTRIP=--strip-components
+-else
+-    TARSTRIP=--strip-path
+-fi
++TARSTRIP=--strip-components
  
- mkdir -p ${DISTDIR}
--tar ${TARSTRIP}=1 -jxf ${CCTOOLSDISTFILE} -C ${DISTDIR}
-+gnutar ${TARSTRIP}=1 -jxf ${CCTOOLSDISTFILE} -C ${DISTDIR}
- mkdir -p ${DISTDIR}/ld64
--tar ${TARSTRIP}=1 -jxf ${LD64DISTFILE} -C ${DISTDIR}/ld64
-+gnutar ${TARSTRIP}=1 -jxf ${LD64DISTFILE} -C ${DISTDIR}/ld64
- find ${DISTDIR}/ld64/doc/ -type f -exec cp "{}" ${DISTDIR}/man \;
+ PATCHFILESDIR=${TOPSRCDIR}/patches
  
- # Clean the source a bit
-@@ -81,7 +81,7 @@
- 
-     mv ${DISTDIR}/include/mach/machine.h ${DISTDIR}/include/mach/machine.h.new;
-     for i in mach architecture i386 libkern; do
--	tar cf - -C "$SDKROOT/usr/include" $i | tar xf - -C ${DISTDIR}/include
-+	gnutar cf - -C "$SDKROOT/usr/include" $i | gnutar xf - -C ${DISTDIR}/include
-     done
-     mv ${DISTDIR}/include/mach/machine.h.new ${DISTDIR}/include/mach/machine.h;
- 
-@@ -135,7 +135,7 @@
- set -e
- 
- echo "Adding new files"
--tar cf - --exclude=CVS --exclude=.svn -C ${ADDEDFILESDIR} . | tar xvf - -C ${DISTDIR}
-+gnutar cf - --exclude=CVS --exclude=.svn -C ${ADDEDFILESDIR} . | gnutar xvf - -C ${DISTDIR}
- 
- echo "Deleting cruft"
- find ${DISTDIR} -name Makefile -exec rm -f "{}" \;
-@@ -151,7 +151,7 @@
- if [ $MAKEDISTFILE -eq 1 ]; then
-     DATE=$(date +%Y%m%d)
-     mv ${DISTDIR} ${DISTDIR}-$DATE
--    tar jcf ${DISTDIR}-$DATE.tar.bz2 ${DISTDIR}-$DATE
-+    gnutar jcf ${DISTDIR}-$DATE.tar.bz2 ${DISTDIR}-$DATE
- fi
- 
- exit 0
