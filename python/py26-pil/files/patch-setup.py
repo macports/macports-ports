07ed1ef91e7fb1c9233425d052059636f903d9f7
Bryan Blackburn <blb@macports.org>
2009-09-19 06:59:09 +0000

    python/py26-pil - make sure to link against MacPorts Tcl and Tk, not system ones
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@57935 d073be05-634f-4543-b044-5fe20cf6d1d6

--- python/py26-pil/files/patch-setup.py
+++ python/py26-pil/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py	2005-03-23 19:16:40.000000000 +0100
-+++ setup.py	2005-07-11 18:50:55.000000000 +0200
-@@ -28,11 +28,11 @@
+--- setup.py.orig	2006-12-03 04:37:29.000000000 -0700
++++ setup.py	2009-09-18 19:58:45.000000000 -0600
+@@ -34,11 +34,11 @@
  #
  # TIFF_ROOT = libinclude("/opt/tiff")
  
@@ -17,3 +17,35 @@
  
  # FIXME: add mechanism to explicitly *disable* the use of a library
  
+@@ -302,30 +302,7 @@
+                 "_imagingtiff", ["_imagingtiff.c"], libraries=["tiff"]
+                 ))
+ 
+-        if sys.platform == "darwin":
+-            # locate Tcl/Tk frameworks
+-            frameworks = []
+-            framework_roots = [
+-                "/Library/Frameworks",
+-                "/System/Library/Frameworks"
+-                ]
+-            for root in framework_roots:
+-                if (os.path.exists(os.path.join(root, "Tcl.framework")) and
+-                    os.path.exists(os.path.join(root, "Tk.framework"))):
+-                    print "--- using frameworks at", root
+-                    frameworks = ["-framework", "Tcl", "-framework", "Tk"]
+-                    dir = os.path.join(root, "Tcl.framework", "Headers")
+-                    add_directory(self.compiler.include_dirs, dir, 0)
+-                    dir = os.path.join(root, "Tk.framework", "Headers")
+-                    add_directory(self.compiler.include_dirs, dir, 1)
+-                    break
+-            if frameworks:
+-                exts.append(Extension(
+-                    "_imagingtk", ["_imagingtk.c", "Tk/tkImaging.c"],
+-                    extra_compile_args=frameworks, extra_link_args=frameworks
+-                    ))
+-                feature.tcl = feature.tk = 1 # mark as present
+-        elif feature.tcl and feature.tk:
++        if feature.tcl and feature.tk:
+             exts.append(Extension(
+                 "_imagingtk", ["_imagingtk.c", "Tk/tkImaging.c"],
+                 libraries=[feature.tcl, feature.tk]
