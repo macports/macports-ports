d913aa74c011a2ac2a453d9fb78b41b21294848f
Toby Peterson <toby@macports.org>
2009-11-16 09:04:53 +0000

    libusb 1.0.5
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@60586 d073be05-634f-4543-b044-5fe20cf6d1d6

--- devel/libusb/files/patch-libusb__io.c
+++ devel/libusb/files/patch-libusb__io.c
@@ -1,6 +1,6 @@
 --- libusb/io.c.orig	2009-09-02 17:20:20.000000000 -0700
 +++ libusb/io.c	2009-09-02 17:21:01.000000000 -0700
-@@ -1485,6 +1485,7 @@
+@@ -1663,6 +1663,7 @@
  	return (r == ETIMEDOUT);
  }
  
@@ -8,11 +8,11 @@
  static void handle_timeout(struct usbi_transfer *itransfer)
  {
  	struct libusb_transfer *transfer =
-@@ -1497,6 +1498,7 @@
+@@ -1675,6 +1676,7 @@
  		usbi_warn(TRANSFER_CTX(transfer),
  			"async cancel failed %d errno=%d", r, errno);
  }
 +#endif
  
- static int handle_timeouts(struct libusb_context *ctx)
- {
+ #ifdef USBI_OS_HANDLES_TIMEOUT
+ static int handle_timeouts_locked(struct libusb_context *ctx)
