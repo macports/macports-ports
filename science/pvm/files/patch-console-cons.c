2eda9ebde5cf3fbc5df5afdedad71a351346364b
Ryan Schmidt <ryandesign@macports.org>
2009-11-22 15:23:07 +0000

    pvm: update to 3.4.6, simply DARWIN.def patch, make sure we're always UsingTheRightCompiler; see https://trac.macports.org/ticket/22541 (maintainer timeout)
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@60758 d073be05-634f-4543-b044-5fe20cf6d1d6

--- science/pvm/files/patch-console-cons.c
+++ science/pvm/files/patch-console-cons.c
@@ -1,20 +1,20 @@
---- console/cons.c	2001-02-08 00:10:09.000000000 +0100
-+++ console/cons.c	2006-08-18 10:28:46.000000000 +0200
-@@ -788,7 +788,7 @@
+--- console/cons.c.orig	2007-04-19 18:14:46.000000000 -0500
++++ console/cons.c	2009-11-15 02:07:52.000000000 -0600
+@@ -792,7 +792,7 @@
  			last_line = (char *) NULL;
  		}
  
--		matches = completion_matches( text, command_generator );
-+		matches = rl_completion_matches( text, command_generator );
+-		matches = (char **) completion_matches( text, command_generator );
++		matches = (char **) rl_completion_matches( text, command_generator );
  
  	}
  
-@@ -797,7 +797,7 @@
+@@ -801,7 +801,7 @@
  
  		if ( !last_line || strcmp( last_line, rl_line_buffer ) ) {
  
--			matches = completion_matches( text,
-+			matches = rl_completion_matches( text,
+-			matches = (char **) completion_matches( text,
++			matches = (char **) rl_completion_matches( text,
  				command_help_generator );
  
  			if ( last_line ) free( last_line );
