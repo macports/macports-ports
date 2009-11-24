935f746e8fe7f9980bbbad47b30f200acedfb190
Toby Peterson <toby@macports.org>
2009-11-24 01:34:00 +0000

    libusb 1.0.6
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@60800 d073be05-634f-4543-b044-5fe20cf6d1d6

--- devel/libusb/files/patch-libusb__os__darwin_usb.c
+++ devel/libusb/files/patch-libusb__os__darwin_usb.c
@@ -1,6 +1,6 @@
 --- libusb/os/darwin_usb.c.orig	2009-09-02 17:18:57.000000000 -0700
 +++ libusb/os/darwin_usb.c	2009-09-02 17:19:24.000000000 -0700
-@@ -281,7 +281,7 @@
+@@ -279,7 +279,7 @@
    if (kresult != kIOReturnSuccess) {
      _usbi_log (ctx, LOG_LEVEL_ERROR, "could not add hotplug event source: %s", darwin_error_str (kresult));
  
