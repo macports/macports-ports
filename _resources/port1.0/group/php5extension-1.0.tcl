451763ff1450f917c02741a41408266a02b1476a
Ryan Schmidt <ryandesign@macports.org>
2009-12-13 12:48:05 +0000

    php5 update:
     * update php5extension forgotten in https://trac.macports.org/changeset/61490
     * revision-bump all php5extension ports to get this change
     * fix .ini file name in php5-oracle and php5-postgresql (https://trac.macports.org/ticket/22868)
    
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@61501 d073be05-634f-4543-b044-5fe20cf6d1d6

--- _resources/port1.0/group/php5extension-1.0.tcl
+++ _resources/port1.0/group/php5extension-1.0.tcl
@@ -46,78 +46,111 @@
 #   php5extension.type      zend
 
 
-default build.dir                   {[php5extension.build_dir_proc]}
-default build.target                {[php5extension.build_target_proc]}
-default configure.args              {[php5extension.configure_args_proc]}
-default configure.dir               {[php5extension.build_dir_proc]}
-options php5extension.extension
+default build.dir                   {[lindex ${php5extension.build_dirs} 0]}
+default configure.dir               {[lindex ${php5extension.build_dirs} 0]}
+default destroot.dir                {[lindex ${php5extension.build_dirs} 0]}
+options php5extension.build_dirs
+default php5extension.build_dirs    {[php5extension.build_dirs_proc]}
+options php5extension.extensions
 options php5extension.extension_dir
-default php5extension.extension_dir {[php5extension.extension_dir_proc]}
-options php5extension.extract_dirs
-default php5extension.extract_dirs  {ext/${php5extension.extension}}
+default php5extension.extension_dir {[exec ${prefix}/bin/php-config --extension-dir]}
 options php5extension.ini
-default php5extension.ini           {${php5extension.extension}.ini}
+default php5extension.ini           {[lindex ${php5extension.extensions} 0].ini}
 options php5extension.inidir
 default php5extension.inidir        {${prefix}/var/db/php5}
+options php5extension.php_ini
+default php5extension.php_ini       {${prefix}/etc/php5/php.ini}
 options php5extension.type
 default php5extension.type      php
 options php5extension.source
 default php5extension.source        standalone
-options php5extension.use_phpize
-default php5extension.use_phpize    yes
 
-proc php5extension.setup {extension version {source ""}} {
-    global php5extension.extension php5extension.ini php5extension.inidir php5extension.source
-    global build.dir destroot prefix
+proc php5extension.setup {extensions version {source ""}} {
+    global php5extension.build_dirs php5extension.extensions php5extension.ini php5extension.inidir php5extension.source
+    global destroot
     
-    php5extension.extension     ${extension}
+    # Use "set" to preserve the list structure.
+    set php5extension.extensions ${extensions}
+    
     php5extension.source        ${source}
     
-    name                        php5-${php5extension.extension}
+    name                        php5-[lindex ${php5extension.extensions} 0]
     version                     ${version}
     categories                  php
-    distname                    ${php5extension.extension}-${version}
+    distname                    [lindex ${php5extension.extensions} 0]-${version}
     
     depends_lib                 path:bin/phpize:php5
     
     configure.universal_args-delete --disable-dependency-tracking
     
+    variant debug description {Enable debug support (useful to analyze a PHP-related core dump)} {}
+    
     pre-configure {
-        if {"yes" == ${php5extension.use_phpize}} {
-            system "cd ${configure.dir} && ${prefix}/bin/phpize"
+        set php_debug_variant [regexp {/debug-[^/]+$} ${php5extension.extension_dir}]
+        if {${php_debug_variant} && ![variant_isset debug]} {
+            ui_error "${name} cannot be installed without the debug variant because PHP is installed with the debug variant."
+            return -code error "incompatible variant selection"
+        } elseif {[variant_isset debug] && !${php_debug_variant}} {
+            ui_error "${name} cannot be installed with the debug variant because PHP is installed without the debug variant."
+            return -code error "incompatible variant selection"
+        }
+        foreach dir ${php5extension.build_dirs} {
+            ui_debug "Generating configure script in [file tail ${dir}]"
+            system "cd ${dir} && ${prefix}/bin/phpize"
+        }
+    }
+    
+    configure {
+        foreach configure.dir ${php5extension.build_dirs} {
+            ui_debug "Configuring in [file tail ${configure.dir}]"
+            portconfigure::configure_main
+        }
+    }
+    
+    build {
+        foreach build.dir ${php5extension.build_dirs} {
+            ui_debug "Building in [file tail ${build.dir}]"
+            portbuild::build_main
         }
     }
     
     destroot.destdir            INSTALL_ROOT=${destroot}
     
-    post-destroot {
+    destroot {
+        foreach destroot.dir ${php5extension.build_dirs} {
+            ui_debug "Staging in [file tail ${destroot.dir}]"
+            portdestroot::destroot_main
+        }
         xinstall -m 755 -d ${destroot}${php5extension.inidir}
+        if {"zend" == ${php5extension.type}} {
+            set extension_prefix "zend_extension=${php5extension.extension_dir}/"
+        } else {
+            set extension_prefix "extension="
+        }
         set fp [open ${destroot}${php5extension.inidir}/${php5extension.ini} w]
-        foreach extensionfile [glob -tails -directory ${destroot}${php5extension.extension_dir} *.so] {
-            if {"zend" == ${php5extension.type}} {
-                puts $fp "zend_extension=${php5extension.extension_dir}/${extensionfile}"
-            } else {
-                puts $fp "extension=${extensionfile}"
-            }
+        puts $fp "; Do not edit this file; it is automatically generated by MacPorts."
+        puts $fp "; Any changes you make will be lost if you upgrade or uninstall ${name}."
+        puts $fp "; To configure PHP, edit ${php5extension.php_ini}."
+        foreach extension ${php5extension.extensions} {
+            puts $fp "${extension_prefix}${extension}.so"
         }
         close $fp
     }
     
     post-install {
-        set phpini ${prefix}/etc/php5/php.ini
-        if {[file exists ${phpini}]} {
+        if {[file exists ${php5extension.php_ini}]} {
             set count 0
-            set fp [open ${phpini} r]
+            set fp [open ${php5extension.php_ini} r]
             while {![eof $fp]} {
                 set line [gets $fp]
                 regexp {^extension_dir *= *"?([^\"]*)"?} $line -> phpiniextensiondir
                 if {[info exists phpiniextensiondir]} {
-                    ui_debug "Found extension_dir ${phpiniextensiondir} in ${phpini}"
+                    ui_debug "Found extension_dir ${phpiniextensiondir} in ${php5extension.php_ini}"
                     if {${phpiniextensiondir} != ${php5extension.extension_dir}} {
                         if {0 == ${count}} {
                             ui_msg "Your php.ini contains a line that will prevent ${name}"
                             ui_msg "and other PHP extensions from working. To fix this,"
-                            ui_msg "edit ${phpini} and delete this line:"
+                            ui_msg "edit ${php5extension.php_ini} and delete this line:"
                             ui_msg ""
                         }
                         ui_msg ${line}
@@ -132,7 +165,7 @@ proc php5extension.setup {extension version {source ""}} {
     
     if {"pecl" == ${source}} {
         global php5extension.homepage
-        set php5extension.homepage  http://pecl.php.net/package/${php5extension.extension}/
+        set php5extension.homepage  http://pecl.php.net/package/[lindex ${php5extension.extensions} 0]/
         
         homepage                    ${php5extension.homepage}
         master_sites                http://pecl.php.net/get/
@@ -142,7 +175,7 @@ proc php5extension.setup {extension version {source ""}} {
         livecheck.url               ${php5extension.homepage}
         livecheck.regex             {>([0-9.]+)</a></th>\s*<[^>]+>stable<}
     } elseif {"bundled" == ${source}} {
-        homepage                    http://www.php.net/${php5extension.extension}
+        homepage                    http://www.php.net/[lindex ${php5extension.extensions} 0]
         master_sites                php
         
         dist_subdir                 php5
@@ -150,17 +183,20 @@ proc php5extension.setup {extension version {source ""}} {
         use_bzip2                   yes
         
         pre-extract {
-            if {"yes" == ${php5extension.use_phpize}} {
-                foreach extract_dir ${php5extension.extract_dirs} {
-                    extract.post_args-append ${worksrcdir}/${extract_dir}
-                }
+            foreach extension ${php5extension.extensions} {
+                extract.post_args-append ${worksrcdir}/ext/${extension}
             }
         }
         
-        destroot {
-            xinstall -d ${destroot}${php5extension.extension_dir}
-            eval xinstall -m 644 [glob ${build.dir}/modules/*.so] ${destroot}${php5extension.extension_dir}
+        pre-configure {
+            set php_version [exec ${prefix}/bin/php-config --version]
+            if {${version} != ${php_version}} {
+                ui_error "${name} ${version} requires PHP ${version} but you have PHP ${php_version}."
+                return -code error "incompatible PHP installation"
+            }
         }
+        
+        destroot.target             install-modules install-headers
         
         livecheck.type              regex
         livecheck.url               http://www.php.net/downloads.php
@@ -168,41 +204,14 @@ proc php5extension.setup {extension version {source ""}} {
     }
 }
 
-proc php5extension.build_dir_proc {} {
-    global php5extension.extension php5extension.source php5extension.use_phpize worksrcpath
+proc php5extension.build_dirs_proc {} {
+    global php5extension.extensions php5extension.source worksrcpath
     if {"bundled" == ${php5extension.source}} {
-        if {"yes" == ${php5extension.use_phpize}} {
-            return ${worksrcpath}/ext/${php5extension.extension}
+        set dirs {}
+        foreach extension ${php5extension.extensions} {
+            lappend dirs ${worksrcpath}/ext/${extension}
         }
+        return ${dirs}
     }
     return ${worksrcpath}
-}
-
-proc php5extension.build_target_proc {} {
-    global php5extension.source php5extension.use_phpize
-    if {"bundled" == ${php5extension.source}} {
-        if {"yes" != ${php5extension.use_phpize}} {
-            return build-modules
-        }
-    }
-    return all
-}
-
-proc php5extension.configure_args_proc {} {
-    global php5extension.source php5extension.use_phpize
-    if {"bundled" == ${php5extension.source}} {
-        if {"yes" != ${php5extension.use_phpize}} {
-            return {
-                --disable-all
-                --disable-cgi
-                --without-pear
-            }
-        }
-    }
-    return {}
-}
-
-proc php5extension.extension_dir_proc {} {
-    global prefix
-    return [exec ${prefix}/bin/php-config --extension-dir]
 }
