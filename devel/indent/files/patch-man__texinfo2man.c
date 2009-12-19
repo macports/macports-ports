baf7b824eada4a5dcd3836647c3daaa2e0edf49c
Toby Peterson <toby@macports.org>
2009-12-19 22:14:59 +0000

    indent: Fix parallel build and a 64-bit related crash. See https://trac.macports.org/ticket/22949
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@61716 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ devel/indent/files/patch-man__texinfo2man.c
@@ -0,0 +1,9 @@
+--- man/texinfo2man.c.orig	2009-12-19 14:10:34.000000000 -0800
++++ man/texinfo2man.c	2009-12-19 14:10:39.000000000 -0800
+@@ -1,5 +1,5 @@
+ #include <stdio.h>
+-#include <malloc.h>
++#include <stdlib.h>
+ #include <string.h>
+ #include <ctype.h>
+ 
