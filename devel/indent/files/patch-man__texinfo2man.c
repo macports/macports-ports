cde2d56c7a39fe707aa71cda40ef99911716f05b
Toby Peterson <toby@macports.org>
2009-12-19 22:24:32 +0000

    indent: update to 2.2.10
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@61717 d073be05-634f-4543-b044-5fe20cf6d1d6

--- devel/indent/files/patch-man__texinfo2man.c
+++ devel/indent/files/patch-man__texinfo2man.c
@@ -1,9 +1,9 @@
---- man/texinfo2man.c.orig	2009-12-19 14:10:34.000000000 -0800
-+++ man/texinfo2man.c	2009-12-19 14:10:39.000000000 -0800
-@@ -1,5 +1,5 @@
+--- man/texinfo2man.c.orig	2009-12-19 14:18:43.000000000 -0800
++++ man/texinfo2man.c	2009-12-19 14:18:47.000000000 -0800
+@@ -1,6 +1,5 @@
  #include <stdio.h>
+ #include <stdlib.h>
 -#include <malloc.h>
-+#include <stdlib.h>
  #include <string.h>
  #include <ctype.h>
  
--- devel/indent/files/patch-src__output.c
+++ devel/indent/files/patch-src__output.c
@@ -1,24 +1,16 @@
---- src/output.c.orig	2009-12-19 14:11:52.000000000 -0800
-+++ src/output.c	2009-12-19 14:12:43.000000000 -0800
-@@ -12,6 +12,7 @@
-  */
+--- src/output.c.orig	2008-03-11 11:50:42.000000000 -0700
++++ src/output.c	2009-12-19 14:22:32.000000000 -0800
+@@ -749,11 +749,11 @@
  
- #include <stdio.h>
-+#include <stdlib.h>
- #include <sys/types.h>
- #include <utime.h>
- #include <sys/stat.h>
-@@ -737,11 +738,11 @@
-                     
-                     if (s[0] == '/' && (s[1] == '*' || s[1] == '/'))
-                     {
--                        fprintf (output, "%.*s", e_lab - s, s);
-+                        fprintf (output, "%.*s", (int) (e_lab - s), s);
-                     }
-                     else
-                     {
--                        fprintf (output, "/* %.*s */", e_lab - s, s);
-+                        fprintf (output, "/* %.*s */", (int) (e_lab - s), s);
-                     }
-                     
-                     /* no need to update cur_col: the very next thing will
+          if (s[0] == '/' && (s[1] == '*' || s[1] == '/'))
+          {
+-            fprintf (output, "%.*s", e_lab - s, s);
++            fprintf (output, "%.*s", (int) (e_lab - s), s);
+          }
+          else
+          {
+-            fprintf (output, "/* %.*s */", e_lab - s, s);
++            fprintf (output, "/* %.*s */", (int) (e_lab - s), s);
+          }
+ 
+         /* no need to update cur_col: the very next thing will
