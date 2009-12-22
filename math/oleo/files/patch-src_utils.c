3274d2df4c691515142658abfc8f5dd7ec71bd86
Jeremy Huddleston Sequoia <jeremyhu@macports.org>
2009-12-22 20:05:28 +0000

    oleo: Removed.  This has failed to build for over 10 months and is abandoned upstream.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@61863 d073be05-634f-4543-b044-5fe20cf6d1d6

--- math/oleo/files/patch-src_utils.c
+++
@@ -1,12 +0,0 @@
---- src/utils.c.orig	Tue Feb 13 16:38:06 2001
-+++ src/utils.c	Thu Feb 19 16:30:39 2004
-@@ -66,7 +66,9 @@
- #define _IOSTRG 0
- #endif
- 
-+#ifndef HAVE_SYS_ERRLIST
- extern int sys_nerr;
-+#endif
- 
- struct id
-   {
