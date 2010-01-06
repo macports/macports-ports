1bfaa3a57b32f21fa46fea3fb9232b5ac2f2fcf0
Joshua Root <jmr@macports.org>
2010-01-06 21:56:01 +0000

    cryptlib: stealth update (https://trac.macports.org/ticket/23162)
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@62406 d073be05-634f-4543-b044-5fe20cf6d1d6

--- devel/cryptlib/files/patch-crypt_osconfig.h
+++ devel/cryptlib/files/patch-crypt_osconfig.h
@@ -1,10 +1,9 @@
 --- crypt/osconfig.h.orig	2009-05-07 02:07:36.000000000 +1000
 +++ crypt/osconfig.h	2009-09-22 05:53:36.000000000 +1000
-@@ -231,7 +231,10 @@
+@@ -231,6 +231,10 @@
    #else
  	#define L_ENDIAN
    #endif
--  #define B_ENDIAN
 +  #if defined( __LP64__ )
 +    #undef SIXTY_FOUR_BIT
 +    #define SIXTY_FOUR_BIT_LONG
