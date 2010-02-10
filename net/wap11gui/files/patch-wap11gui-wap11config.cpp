809d2d4c365afdf997d7abb4c239c4727a811a7e
Jeremy Huddleston Sequoia <jeremyhu@macports.org>
2010-02-10 06:50:34 +0000

    wap11gui: Remove redundant dependency on xorg-libs, fix netsnmp API usage to build correctly
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@63607 d073be05-634f-4543-b044-5fe20cf6d1d6

--- net/wap11gui/files/patch-wap11gui-wap11config.cpp
+++ net/wap11gui/files/patch-wap11gui-wap11config.cpp
@@ -1,5 +1,17 @@
 --- wap11gui/wap11config.cpp.orig	Fri Feb  6 20:10:58 2004
 +++ wap11gui/wap11config.cpp	Fri Feb  6 20:16:43 2004
+@@ -36,9 +36,9 @@ WAP11Config::WAP11Config()
+ {
+   init_snmp("WAP11-SNMP");
+   add_mibdir(MIBSDIR);
+-  init_mib_internals();
++  netsnmp_init_mib_internals();
+   snmp_sess_init(&session);
+-  if(read_module("ATMEL-MIB")==0)
++  if(netsnmp_read_module("ATMEL-MIB")==0)
+   {
+     THROW_WAP11_EXCEPTION( WAP11SNMPAuthError );
+   }
 @@ -549,10 +549,10 @@
  {
    char s[16];
