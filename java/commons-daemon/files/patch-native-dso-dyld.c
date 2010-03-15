80491ef042126fa480969cb0cef09872238c0b01
Anthony Ramine <nox@macports.org>
2010-03-15 13:13:19 +0000

    Update commons-daemon to 1.0.2 and fix universal variant
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@64734 d073be05-634f-4543-b044-5fe20cf6d1d6

--- java/commons-daemon/files/patch-native-dso-dyld.c
+++ java/commons-daemon/files/patch-native-dso-dyld.c
@@ -1,13 +1,5 @@
 --- src/native/unix/native/dso-dyld.c.orig	2004-02-09 07:55:21.000000000 -0800
 +++ src/native/unix/native/dso-dyld.c	2005-01-22 08:27:20.000000000 -0800
-@@ -17 +16,0 @@
--#include "jsvc.h"
-@@ -21,0 +21,5 @@
-+/* dyld.h hauls in stdbool, which jsvc doesn't expect; undo that */
-+#undef false
-+#undef true
-+#undef bool
-+#include "jsvc.h"
-@@ -109 +113 @@
+@@ -118 +118 @@
 -    while(x>=0) und[x+1]=nam[x--];
 +    for(; x>=0; --x) und[x+1]=nam[x];
