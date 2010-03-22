121852c49d760b4f2866966fd1cfc454d40fbb02
Toby Peterson <toby@macports.org>
2010-03-22 17:30:41 +0000

    libusb: supposed big-endian fix
    http://www.libusb.org/ticket/30
    
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@65136 d073be05-634f-4543-b044-5fe20cf6d1d6

--- devel/libusb/files/patch-libusb__os__darwin_usb.c
+++ devel/libusb/files/patch-libusb__os__darwin_usb.c
@@ -1,5 +1,5 @@
---- libusb/os/darwin_usb.c.orig	2009-09-02 17:18:57.000000000 -0700
-+++ libusb/os/darwin_usb.c	2009-09-02 17:19:24.000000000 -0700
+--- libusb/os/darwin_usb.c.orig	2009-11-21 09:10:21.000000000 -0800
++++ libusb/os/darwin_usb.c	2010-03-22 10:29:38.000000000 -0700
 @@ -279,7 +279,7 @@
    if (kresult != kIOReturnSuccess) {
      _usbi_log (ctx, LOG_LEVEL_ERROR, "could not add hotplug event source: %s", darwin_error_str (kresult));
@@ -9,3 +9,16 @@
    }
  
    /* arm notifiers */
+@@ -1191,9 +1191,9 @@
+   tpriv->req.bmRequestType     = setup->bmRequestType;
+   tpriv->req.bRequest          = setup->bRequest;
+   /* these values should already be in bus order */
+-  tpriv->req.wValue            = setup->wValue;
+-  tpriv->req.wIndex            = setup->wIndex;
+-  tpriv->req.wLength           = setup->wLength;
++  tpriv->req.wValue            = libusb_le16_to_cpu(setup->wValue);
++  tpriv->req.wIndex            = libusb_le16_to_cpu(setup->wIndex);
++  tpriv->req.wLength           = libusb_le16_to_cpu(setup->wLength);
+   /* data is stored after the libusb control block */
+   tpriv->req.pData             = transfer->buffer + LIBUSB_CONTROL_SETUP_SIZE;
+   tpriv->req.completionTimeout = transfer->timeout;
