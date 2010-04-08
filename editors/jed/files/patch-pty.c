db30ce8579407d09c5acf0e3055a0d0e59c0b58e
Ryan Schmidt <ryandesign@macports.org>
2010-04-08 04:52:02 +0000

    jed: update to 0.99-19
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@66265 d073be05-634f-4543-b044-5fe20cf6d1d6

--- editors/jed/files/patch-pty.c
+++ editors/jed/files/patch-pty.c
@@ -1,11 +1,11 @@
---- src/pty.c.orig	2008-03-31 00:47:37.000000000 +0200
-+++ src/pty.c	2008-03-31 00:48:54.000000000 +0200
+--- src/pty.c.orig	2009-12-13 20:12:55.000000000 -0600
++++ src/pty.c	2010-04-07 23:45:26.000000000 -0500
 @@ -19,7 +19,7 @@
  #include <errno.h>
  
  #ifdef HAVE_GRANTPT
--# if !defined (__linux__) && !defined(__CYGWIN__) && !defined(__FreeBSD__)
-+# if !defined (__linux__) && !defined(__CYGWIN__) && !defined(__APPLE__) && !defined(__FreeBSD__)
+-# if !defined (__linux__) && !defined(__CYGWIN__) && !defined(__FreeBSD__) && !defined(_AIX)
++# if !defined (__linux__) && !defined(__CYGWIN__) && !defined(__APPLE__) && !defined(__FreeBSD__) && !defined(_AIX)
  #  define USE_SYSV_PTYS
  #  include <sys/types.h>
  #  include <stropts.h>
