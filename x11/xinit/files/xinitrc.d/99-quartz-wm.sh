84cd559aee1356fa6944328f40ac78a4d0c73d21
Jeremy Huddleston Sequoia <jeremyhu@macports.org>
2010-04-20 20:30:49 +0000

    xinit: Fix fontpath.  https://trac.macports.org/ticket/24534
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@66705 d073be05-634f-4543-b044-5fe20cf6d1d6

--- x11/xinit/files/xinitrc.d/99-quartz-wm.sh
+++ x11/xinit/files/xinitrc.d/99-quartz-wm.sh
@@ -1,5 +1,5 @@
 [ -n "${USERWM}" -a -x "${USERWM}" ] && exec "${USERWM}"
-[ -x @PREFIX@/bin/quartz-wm ] && exec @PREFIX@/bin/quartz-wm
+[ -x __PREFIX__/bin/quartz-wm ] && exec __PREFIX__/bin/quartz-wm
 [ -x /usr/bin/quartz-wm ] && exec /usr/bin/quartz-wm
 [ -x /usr/X11/bin/quartz-wm ] && exec /usr/X11/bin/quartz-wm
 [ -x /usr/X11R6/bin/quartz-wm ] && exec /usr/X11R6/bin/quartz-wm
