91564776ae20a3bd3351c3510c72f671c03a797f
Jeremy Huddleston Sequoia <jeremyhu@macports.org>
2010-04-21 15:05:38 +0000

    xinit: Adjust font setup again.
    Changed base dir to share/fonts instead of the lib/X11/fonts symlink
    Added directories where other non-standard font ports install to
    
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@66728 d073be05-634f-4543-b044-5fe20cf6d1d6

--- x11/xinit/files/xinitrc.d/10-fontdir.sh
+++ x11/xinit/files/xinitrc.d/10-fontdir.sh
@@ -1,5 +1,5 @@
 if [ -x __PREFIX__/bin/xset ] ; then
-        fontpath="__PREFIX__/lib/X11/fonts/misc/,__PREFIX__/lib/X11/fonts/TTF/,__PREFIX__/lib/X11/fonts/OTF,__PREFIX__/lib/X11/fonts/Type1/,__PREFIX__/lib/X11/fonts/75dpi/:unscaled,__PREFIX__/lib/X11/fonts/100dpi/:unscaled,__PREFIX__/lib/X11/fonts/75dpi/,__PREFIX__/lib/X11/fonts/100dpi/"
+        fontpath="__PREFIX__/share/fonts/misc/,__PREFIX__/share/fonts/TTF/,__PREFIX__/share/fonts/OTF,__PREFIX__/share/fonts/Type1/,__PREFIX__/share/fonts/75dpi/:unscaled,__PREFIX__/share/fonts/100dpi/:unscaled,__PREFIX__/share/fonts/75dpi/,__PREFIX__/share/fonts/100dpi/,__PREFIX__/share/fonts/libwmf,__PREFIX__/share/fonts/urw-fonts"
 
         [ -e "$HOME"/.fonts/fonts.dir ] && fontpath="$fontpath,$HOME/.fonts"
         [ -e "$HOME"/Library/Fonts/fonts.dir ] && fontpath="$fontpath,$HOME/Library/Fonts"
