3458433e6245393f2193340735397fccd1642662
Joshua Root <jmr@macports.org>
2010-05-15 11:14:57 +0000

    xcodeversion portgroup: remove developer_dir fallback
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@67639 d073be05-634f-4543-b044-5fe20cf6d1d6

--- _resources/port1.0/group/xcodeversion-1.0.tcl
+++ _resources/port1.0/group/xcodeversion-1.0.tcl
@@ -45,13 +45,6 @@ default minimum_xcodeversions {}
 
 pre-extract {
     if {"darwin" == ${os.platform}} {
-        if {![info exists developer_dir]} {
-            if {${os.major} < 9} {
-                set developer_dir /Developer
-            } else {
-                set developer_dir [exec xcode-select -print-path]
-            }
-        }
         set current_xcodeversion [exec defaults read ${developer_dir}/Applications/Xcode.app/Contents/Info CFBundleShortVersionString]
         foreach {darwin_major minimum_xcodeversion} [join ${minimum_xcodeversions}] {
             if {${darwin_major} == ${os.major}} {
