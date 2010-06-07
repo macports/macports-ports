8ce2695f287e22492002a98b6a2acd4e4ce09c7e
Joshua Root <jmr@macports.org>
2010-06-07 15:46:05 +0000

    muniversal: use ui_info for per-arch phase messages
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@68597 d073be05-634f-4543-b044-5fe20cf6d1d6

--- _resources/port1.0/group/muniversal-1.0.tcl
+++ _resources/port1.0/group/muniversal-1.0.tcl
@@ -165,7 +165,7 @@ variant universal {
 
     configure {
         foreach arch ${universal_archs_to_use} {
-            ui_msg "$UI_PREFIX [format [msgcat::mc "Configuring %1\$s for architecture %2\$s"] $name ${arch}]"
+            ui_info "$UI_PREFIX [format [msgcat::mc "Configuring %1\$s for architecture %2\$s"] $name ${arch}]"
 
             copy ${worksrcpath} ${worksrcpath}-${arch}
 
@@ -326,7 +326,7 @@ variant universal {
 
     build {
         foreach arch ${universal_archs_to_use} {
-            ui_msg "$UI_PREFIX [format [msgcat::mc "Building %1\$s for architecture %2\$s"] $name ${arch}]"
+            ui_info "$UI_PREFIX [format [msgcat::mc "Building %1\$s for architecture %2\$s"] $name ${arch}]"
             
             if { [info exists merger_build_env(${arch})] } {
                 build.env-append  $merger_build_env(${arch})
@@ -362,7 +362,7 @@ variant universal {
 
     destroot {
         foreach arch ${universal_archs_to_use} {
-            ui_msg "$UI_PREFIX [format [msgcat::mc "Staging %1\$s into destroot for architecture %2\$s"] $name ${arch}]"
+            ui_info "$UI_PREFIX [format [msgcat::mc "Staging %1\$s into destroot for architecture %2\$s"] $name ${arch}]"
             copy ${destroot} ${workpath}/destroot-${arch}
             set destdirSave ${destroot.destdir}
             eval destroot.destdir  [string map "${destroot} ${workpath}/destroot-${arch}" ${destroot.destdir}]
@@ -607,7 +607,7 @@ variant universal {
             # Rosetta does not translate G5 instructions
             # PowerPC systems can't translate Intel instructions
             if { (${os.arch}=="i386" && ${arch}!="ppc64") || (${os.arch}=="powerpc" && ${arch}!="i386" && ${arch}!="x86_64") } {
-                ui_msg "$UI_PREFIX [format [msgcat::mc "Testing %1\$s for architecture %2\$s"] $name ${arch}]"
+                ui_info "$UI_PREFIX [format [msgcat::mc "Testing %1\$s for architecture %2\$s"] $name ${arch}]"
                 set test_dir_save ${test.dir}
                 if { [string match "${worksrcpath}/*" ${test.dir}] } {
                     # The test directory is inside the source directory, so put in the new source directory name.
