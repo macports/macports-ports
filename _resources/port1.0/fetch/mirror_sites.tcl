fbf4205ecedf53ec08bdac93c69728cd4b799ca1
Joshua Root <jmr@macports.org>
2010-06-21 19:14:45 +0000

    new distfiles mirror courtesy of PNL
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@69037 d073be05-634f-4543-b044-5fe20cf6d1d6

--- _resources/port1.0/fetch/mirror_sites.tcl
+++ _resources/port1.0/fetch/mirror_sites.tcl
@@ -263,6 +263,7 @@ set portfetch::mirror_sites::sites(macports_distfiles) {
     http://distfiles.macports.org/:mirror
     http://aarnet.au.distfiles.macports.org/pub/macports/mpdistfiles/:mirror
     http://lil.fr.distfiles.macports.org/:mirror
+    http://mirror.pnl.gov/macports/mpdistfiles/:mirror
 }
 
 set portfetch::mirror_sites::sites(openbsd) {
