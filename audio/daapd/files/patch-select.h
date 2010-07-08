d14dd774e0649d3f325bf8ea698fc8d72a73e39f
Ryan Schmidt <ryandesign@macports.org>
2010-07-08 04:22:52 +0000

    daapd: update to 0.2.4b, fix compilation of and also install libdaaplib.a
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@69503 d073be05-634f-4543-b044-5fe20cf6d1d6

--- audio/daapd/files/patch-select.h
+++ audio/daapd/files/patch-select.h
@@ -1,11 +1,11 @@
---- libhttpd/src/select.h.orig	2005-01-05 10:18:47.000000000 +1100
-+++ libhttpd/src/select.h	2006-01-05 19:41:28.000000000 +1100
-@@ -37,7 +37,7 @@
- #include "httpd_priv.h"
+--- libhttpd/src/select.h.orig	2006-03-05 08:00:38.000000000 -0600
++++ libhttpd/src/select.h	2010-07-07 22:45:33.000000000 -0500
+@@ -38,7 +38,7 @@
  
  #ifdef __APPLE__
+ #ifndef _SOCKLEN_T
 -	typedef int socklen_t;
 +	typedef __uint32_t socklen_t;
+ #define _SOCKLEN_T
  #endif
- 
- #ifdef __sgi__
+ #endif
