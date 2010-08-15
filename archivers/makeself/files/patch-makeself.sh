eb323ddebace6843535dc8caaa7c96af1b760dcc
Ryan Schmidt <ryandesign@macports.org>
2010-08-15 02:20:19 +0000

    makeself: update to 2.1.5 and use 'supported_archs noarch'
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@70601 d073be05-634f-4543-b044-5fe20cf6d1d6

--- archivers/makeself/files/patch-makeself.sh
+++ archivers/makeself/files/patch-makeself.sh
@@ -1,7 +1,6 @@
-diff -rwub makeself.orig/makeself.sh makeself/makeself.sh
---- makeself.sh	Wed Oct 20 20:15:41 2004
-+++ makeself.sh	Wed Oct 20 20:16:30 2004
-@@ -106,7 +106,7 @@
+--- makeself.sh.orig	2008-01-04 17:53:49.000000000 -0600
++++ makeself.sh	2010-08-14 21:17:27.000000000 -0500
+@@ -126,7 +126,7 @@
  APPEND=n
  COPY=none
  TAR_ARGS=cvf
