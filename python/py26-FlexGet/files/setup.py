2d94eefd81eae6c5619a8411ba36c08514a48497
Anthony Ramine <nox@macports.org>
2010-08-16 17:24:43 +0000

    Oops, remove garbage files
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@70663 d073be05-634f-4543-b044-5fe20cf6d1d6

--- python/py26-FlexGet/files/setup.py
+++
@@ -1,6 +0,0 @@
-from distutils.core import setup
-
-setup(name = 'FlexGet',
-      version = '@VERSION@',
-      packages = ['flexget', 'flexget.plugins', 'flexget.utils'],
-      scripts = ['scripts/flexget'])
