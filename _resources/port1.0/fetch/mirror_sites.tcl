2de900a6bb321b74172a476f48024eb2c9dd1d55
Joshua Root <jmr@macports.org>
2010-11-15 03:00:56 +0000

    remove seattle distfiles mirror which is currently down
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@73482 d073be05-634f-4543-b044-5fe20cf6d1d6

--- _resources/port1.0/fetch/mirror_sites.tcl
+++ _resources/port1.0/fetch/mirror_sites.tcl
@@ -273,7 +273,6 @@ set portfetch::mirror_sites::sites(macports_distfiles) {
     http://distfiles.macports.org/:mirror
     http://aarnet.au.distfiles.macports.org/pub/macports/mpdistfiles/:mirror
     http://lil.fr.distfiles.macports.org/:mirror
-    http://sea.us.distfiles.macports.org/macports/mpdistfiles/:mirror
 }
 
 set portfetch::mirror_sites::sites(netbsd) {
