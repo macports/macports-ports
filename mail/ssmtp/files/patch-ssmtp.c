dfcd6156ab3954a8e028c83de1bacdaffbf6b042
Joshua Root <jmr@macports.org>
2010-11-18 08:00:56 +0000

    ssmtp: update to 2.62 (fixes https://trac.macports.org/ticket/20735)
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@73562 d073be05-634f-4543-b044-5fe20cf6d1d6

--- mail/ssmtp/files/patch-ssmtp.c
+++ mail/ssmtp/files/patch-ssmtp.c
@@ -1,8 +1,8 @@
 --- ssmtp.c.old	Sun Feb  1 22:28:40 2004
 +++ ssmtp.c	Sun Feb  1 22:28:55 2004
-@@ -14,6 +14,7 @@
- */
- #define VERSION "2.60.4"
+@@ -13,6 +13,7 @@
+ #define VERSION "2.62"
+ #define _GNU_SOURCE
  
 +#include <sys/types.h>
  #include <sys/socket.h>
