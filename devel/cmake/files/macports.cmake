93ef511851e85caacbda5fe7136a00826ad1a0db
Chris Scharver <css@macports.org>
2010-11-23 04:02:24 +0000

    Bump CMake 2.8.3 to revision 1 based on some changes to locating Qt libraries and and modifying ccmake's use of ncurses. refs https://trac.macports.org/ticket/27383
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@73737 d073be05-634f-4543-b044-5fe20cf6d1d6

--- devel/cmake/files/macports.cmake
+++ devel/cmake/files/macports.cmake
@@ -1,4 +1,3 @@
-# Use ncurses to use the MacPorts ncurses
-set(CURSES_USE_NCURSES TRUE)
-set(CURSES_NEED_NCURSES TRUE)
-set(CURSES_CURSES_LIBRARY ${CURSES_NCURSES_LIBRARY})
+# Coerce CMake to use the MacPorts ncurses library
+# patch __PREFIX__ in the Portfile post-patch stage
+set(CURSES_CURSES_LIBRARY "__PREFIX__/lib/libncurses.dylib" CACHE FILEPATH "The Curses Library" FORCE)
