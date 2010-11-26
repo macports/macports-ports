c4d79417bfeed148ed8f4dbcc2e0e71b1d9f18a0
Anders F. Björklund <afb@macports.org>
2010-11-26 11:05:54 +0000

    port upgrade: yum 3.2.28
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@73822 d073be05-634f-4543-b044-5fe20cf6d1d6

--- sysutils/yum/files/patch-packages.py
+++ sysutils/yum/files/patch-packages.py
@@ -1,11 +1,20 @@
---- yum/packages.py.orig	2008-11-06 11:57:14.000000000 +0100
-+++ yum/packages.py	2008-11-06 12:09:20.000000000 +0100
-@@ -1121,6 +1121,8 @@ class YumHeaderPackage(YumAvailablePacka
+--- yum/packages.py.orig	2010-07-09 19:28:16.000000000 +0200
++++ yum/packages.py	2010-08-03 16:43:16.000000000 +0100
+@@ -1353,11 +1353,14 @@
+            is a pre-requires or a not"""
+         # FIXME this should probably be put in rpmUtils.miscutils since 
          # - that's what it is
-         newflag = flag
++        RPMSENSE_PREREQ = (1 << 6)
++        RPMSENSE_SCRIPT_PRE = (1 << 9)
++        RPMSENSE_SCRIPT_POST = (1 << 10)
          if flag is not None:
-+            if not hasattr(rpm, 'RPMSENSE_PREREQ'):
-+                return 0
-             newflag = flag & rpm.RPMSENSE_PREREQ
-             if newflag == rpm.RPMSENSE_PREREQ:
+             # Note: RPMSENSE_PREREQ == 0 since rpm-4.4'ish
+-            if flag & (rpm.RPMSENSE_PREREQ |
+-                       rpm.RPMSENSE_SCRIPT_PRE |
+-                       rpm.RPMSENSE_SCRIPT_POST):
++            if flag & (RPMSENSE_PREREQ |
++                       RPMSENSE_SCRIPT_PRE |
++                       RPMSENSE_SCRIPT_POST):
                  return 1
+         return 0
+ 
