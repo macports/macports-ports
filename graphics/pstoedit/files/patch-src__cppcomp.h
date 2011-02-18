bc079746632647b52a97b36bad39612ecc2e7639
Ryan Schmidt <ryandesign@macports.org>
2011-02-18 02:00:11 +0000

    pstoedit: update to 3.50; see https://trac.macports.org/ticket/28442
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@76083 d073be05-634f-4543-b044-5fe20cf6d1d6

--- graphics/pstoedit/files/patch-src__cppcomp.h
+++ graphics/pstoedit/files/patch-src__cppcomp.h
@@ -1,6 +1,6 @@
---- src/cppcomp.h.orig	2007-07-22 04:27:40.000000000 -0700
-+++ src/cppcomp.h	2009-07-22 19:43:48.000000000 -0700
-@@ -235,7 +235,7 @@
+--- src/cppcomp.h.orig	2009-06-21 10:10:39.000000000 -0500
++++ src/cppcomp.h	2011-02-17 17:11:47.000000000 -0600
+@@ -244,7 +244,7 @@
  static inline void strncpy_s(char * de, size_t de_size, const char *  so, size_t count)   {
  	const size_t sourcelen = strlen(so);
  	size_t tobecopied = sourcelen < count ? sourcelen : count;
