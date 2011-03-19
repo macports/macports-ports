2cc5e69d14b749427030b3e21e4399de38e3dcd3
Ryan Schmidt <ryandesign@macports.org>
2011-03-19 09:17:33 +0000

    mirror_sites.tcl: use fr2 php mirror instead of fr, which doesn't seem to have distfiles; see https://trac.macports.org/ticket/28828
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@77062 d073be05-634f-4543-b044-5fe20cf6d1d6

--- _resources/port1.0/fetch/mirror_sites.tcl
+++ _resources/port1.0/fetch/mirror_sites.tcl
@@ -317,7 +317,7 @@ set portfetch::mirror_sites::sites(php) {
     http://de.php.net/distributions/:nosubdir
     http://es.php.net/distributions/:nosubdir
     http://fi.php.net/distributions/:nosubdir
-    http://fr.php.net/distributions/:nosubdir
+    http://fr2.php.net/distributions/:nosubdir
     http://jp2.php.net/distributions/:nosubdir
     http://uk2.php.net/distributions/:nosubdir
     http://us.php.net/distributions/:nosubdir
