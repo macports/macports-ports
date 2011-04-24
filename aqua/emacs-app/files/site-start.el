0c71dfbaec7a44896932609c5f39fd54e2823d57
Chris Scharver <css@macports.org>
2011-04-24 16:24:12 +0000

    emacs-app: bump to 23.3, fix site-start, and remove old patch. refs https://trac.macports.org/ticket/29232
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@78120 d073be05-634f-4543-b044-5fe20cf6d1d6

--- aqua/emacs-app/files/site-start.el
+++ aqua/emacs-app/files/site-start.el
@@ -1,1 +1,4 @@
 (setq load-path (cons "__PREFIX__/share/emacs/site-lisp" load-path))
+(if (file-readable-p "__PREFIX__/share/emacs/site-lisp/subdirs.el")
+    (let ((default-directory "__PREFIX__/share/emacs/site-lisp"))
+      (load "__PREFIX__/share/emacs/site-lisp/subdirs.el")))
