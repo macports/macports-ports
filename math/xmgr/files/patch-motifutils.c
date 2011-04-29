3b939b4abc1109ce40fb99b33d2214bb36b725ce
Joshua Root <jmr@macports.org>
2011-04-29 14:34:48 +0000

    xmgr: fix another 64-bit crash (https://trac.macports.org/ticket/29317)
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@78231 d073be05-634f-4543-b044-5fe20cf6d1d6

--- math/xmgr/files/patch-motifutils.c
+++ math/xmgr/files/patch-motifutils.c
@@ -1,8 +1,17 @@
---- src/motifutils.c	2011-01-30 19:39:41.000000000 +0100
-+++ src/motifutils.c	2011-01-30 19:39:47.000000000 +0100
+--- src/motifutils.c.orig	2011-01-30 19:39:41.000000000 +0100
++++ src/motifutils.c	2011-04-29 14:00:06.000000000 +0200
 @@ -121,7 +121,7 @@
  
      i = 0;
+     va_start(var, nchoices);
+-    while ((s = va_arg(var, char *)) != NULL) {
++    while ((s = va_arg(var, char *)) != NULL && i<nchoices) {
+ 	retval[i + 2] = XmCreatePushButton(retval[1], s, NULL, 0);
+ 	i++;
+     }
+@@ -169,7 +169,7 @@
+     i = 0;
+ 
      va_start(var, nchoices);
 -    while ((s = va_arg(var, char *)) != NULL) {
 +    while ((s = va_arg(var, char *)) != NULL && i<nchoices) {
