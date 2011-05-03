65094a60f0a59270799e49a7639c510b475dfb00
Frank Schima <mf2k@macports.org>
2011-05-03 15:24:40 +0000

    py26-pyqt4: Update to version 4.8.4.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@78328 d073be05-634f-4543-b044-5fe20cf6d1d6

--- python/py26-pyqt4/files/patch-configure.py
+++ python/py26-pyqt4/files/patch-configure.py
@@ -1,11 +1,11 @@
---- configure.py.orig	2009-07-15 01:37:10.000000000 +1200
-+++ configure.py	2009-07-19 18:28:19.000000000 +1200
-@@ -840,7 +840,7 @@
+--- configure.py.orig	2011-05-03 09:10:13.000000000 -0600
++++ configure.py	2011-05-03 09:10:47.000000000 -0600
+@@ -949,7 +949,7 @@
                  if sys.platform == "darwin":
                      # We need to work out how to specify the right framework
                      # version.
 -                    link = "-framework Python"
 +                    link = "%s @@MACPORTS_PYTHON_FRAMEWORK@@" % sipcfg.build_macros().get('LFLAGS', '')
-                 elif ("--enable-shared" in ducfg.get("CONFIG_ARGS", "") and
-                       glob.glob("%s/lib/libpython%d.%d*" % (ducfg["exec_prefix"], py_major, py_minor))):
-                     lib_dir_flag = quote("-L%s/lib" % ducfg["exec_prefix"])
+                 elif "--enable-shared" in ducfg.get("CONFIG_ARGS", ""):
+                     if glob.glob("%s/lib/libpython%d.%d*" % (ducfg["exec_prefix"], py_major, py_minor)):
+                         lib_dir_flag = quote("-L%s/lib" % ducfg["exec_prefix"])
