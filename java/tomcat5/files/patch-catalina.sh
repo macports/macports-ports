cf0ba8bb3c773530567de791dd48f2c6abd0d4cd
Ryan Schmidt <ryandesign@macports.org>
2011-07-31 01:33:28 +0000

    tomcat5: don't use .. components in patchfile; see https://trac.macports.org/ticket/30451
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@81450 d073be05-634f-4543-b044-5fe20cf6d1d6

--- java/tomcat5/files/patch-catalina.sh
+++ java/tomcat5/files/patch-catalina.sh
@@ -1,5 +1,5 @@
---- ../container/catalina/src/bin/catalina.sh.orig	2005-09-23 06:45:08.000000000 -0700
-+++ ../container/catalina/src/bin/catalina.sh	2005-10-25 12:54:10.000000000 -0700
+--- container/catalina/src/bin/catalina.sh.orig	2005-09-23 06:45:08.000000000 -0700
++++ container/catalina/src/bin/catalina.sh	2005-10-25 12:54:10.000000000 -0700
 @@ -146,6 +146,11 @@
    JAVA_OPTS="$JAVA_OPTS "-Djava.util.logging.manager=org.apache.juli.ClassLoaderLogManager" "-Djava.util.logging.config.file="$CATALINA_BASE/conf/logging.properties"
  fi
