3af6d39c2cbc94faa03730e39e63d3b9d821bac7
Joshua Root <jmr@macports.org>
2011-08-01 12:45:58 +0000

    add packages.macports.org to archive mirror list, since there are a number of archives there now
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@81538 d073be05-634f-4543-b044-5fe20cf6d1d6

--- _resources/port1.0/fetch/archive_sites.tcl
+++ _resources/port1.0/fetch/archive_sites.tcl
@@ -3,6 +3,7 @@
 namespace eval portfetch::mirror_sites { }
 
 set portfetch::mirror_sites::sites(macports_archives) {
+    http://packages.macports.org/:nosubdir
 }
 
 set portfetch::mirror_sites::archive_prefix(macports_archives) /opt/local
