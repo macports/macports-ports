1696b888476d24a189acd6fad2bf8ddb44e9003a
Joshua Root <jmr@macports.org>
2011-08-21 03:28:43 +0000

    libpub: remove .. path components from patches
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@82860 d073be05-634f-4543-b044-5fe20cf6d1d6

--- devel/libpub/files/patch-cfg.c
+++ devel/libpub/files/patch-cfg.c
@@ -1,5 +1,5 @@
---- ../liw/cfg/cfg.c	Sun Oct 25 21:33:28 1998
-+++ ../liw/cfg/cfg.c.new	Mon Aug 30 18:25:10 2004
+--- liw/cfg/cfg.c	Sun Oct 25 21:33:28 1998
++++ liw/cfg/cfg.c.new	Mon Aug 30 18:25:10 2004
 @@ -4,8 +4,6 @@
   * Lars Wirzenius
   */
