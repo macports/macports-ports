334af36da7556356b52cfbdc09f6f924cac455cb
Ryan Schmidt <ryandesign@macports.org>
2011-08-30 01:44:45 +0000

    supertux: use app portgroup, add app icon, indicate license; see https://trac.macports.org/ticket/31015
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@83337 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ games/supertux/files/patch-src_setup.cpp
@@ -0,0 +1,10 @@
+--- src/setup.cpp.orig	2005-07-08 07:19:17.000000000 -0500
++++ src/setup.cpp	2011-08-29 04:08:20.000000000 -0500
+@@ -578,7 +578,6 @@
+ 
+   /* Set icon image: */
+ 
+-  seticon();
+ 
+   /* Unicode needed for input handling: */
+ 
