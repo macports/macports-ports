f32b6bc0470f78ca99b68b69997972561651826b
Ryan Schmidt <ryandesign@macports.org>
2011-09-14 08:39:20 +0000

    cutecom-qt4-mac: update to 0.22.0, use cmake portgroup; see https://trac.macports.org/ticket/30779 (maintainer timeout)
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@83987 d073be05-634f-4543-b044-5fe20cf6d1d6

--- comms/cutecom-qt4-mac/files/patch-qcppdialogimpl.cpp
+++ comms/cutecom-qt4-mac/files/patch-qcppdialogimpl.cpp
@@ -1,41 +1,17 @@
---- qcppdialogimpl.cpp.orig	2008-03-12 16:09:50.000000000 -0500
-+++ qcppdialogimpl.cpp	2009-03-28 10:13:13.000000000 -0500
-@@ -243,11 +243,10 @@
-    bool entryFound=false;
-    QStringList devices=settings.readListEntry("/cutecom/AllDevices", &entryFound);
+--- qcppdialogimpl.cpp.orig	2009-06-25 15:10:49.000000000 -0500
++++ qcppdialogimpl.cpp	2011-09-13 23:58:27.000000000 -0500
+@@ -351,12 +351,12 @@
+    QStringList devices=settings.value("/cutecom/AllDevices").toStringList();
     if (!entryFound)
+    {
 -      devices<<"/dev/ttyS0"<<"/dev/ttyS1"<<"/dev/ttyS2"<<"/dev/ttyS3";
 +      devices<<DEVLIST;
+    }
  
-    m_deviceCb->insertStringList(devices);
--
--   m_deviceCb->setCurrentText(settings.readEntry("/cutecom/CurrentDevice", "/dev/ttyS0"));
-+   m_deviceCb->setCurrentText(settings.readEntry("/cutecom/CurrentDevice", DEFAULT_DEV));
+    m_deviceCb->insertItems(0, devices);
  
-    QStringList history=settings.readListEntry("/cutecom/History");
- 
-@@ -929,15 +928,15 @@
-    case 230400:
-       _baud=B230400;
-       break;
--   case 460800:
--      _baud=B460800;
--      break;
--   case 576000:
--      _baud=B576000;
--      break;
--   case 921600:
--      _baud=B921600;
--      break;
-+//   case 460800:
-+//      _baud=B460800;
-+//      break;
-+//   case 576000:
-+//      _baud=B576000;
-+//      break;
-+//   case 921600:
-+//      _baud=B921600;
-+//      break;
- //   case 128000:
- //      _baud=B128000;
- //      break;
+-   int indexOfCurrentDevice = devices.indexOf(settings.value("/cutecom/CurrentDevice", "/dev/ttyS0").toString());
++   int indexOfCurrentDevice = devices.indexOf(settings.value("/cutecom/CurrentDevice", DEFAULT_DEV).toString());
+    // fprintf(stderr, "currentDEev: -%s - index: %d\n", settings.value("/cutecom/CurrentDevice", "/dev/ttyS0").toString().toLatin1().constData(), indexOfCurrentDevice);
+    if (indexOfCurrentDevice!=-1)
+    {
