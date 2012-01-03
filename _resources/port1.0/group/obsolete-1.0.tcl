62bbc899a72432d9f9c3885e7e21bfa2162be7f5
Markus W. Weißmann <mww@macports.org>
2012-01-03 19:12:14 +0000

    add port-group obsolete to avoid copy/pasting the installation-warning over and over again
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@88508 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ _resources/port1.0/group/obsolete-1.0.tcl
@@ -0,0 +1,55 @@
+# -*- coding: utf-8; mode: tcl; c-basic-offset: 4; indent-tabs-mode: nil; tab-width: 4; truncate-lines: t -*- vim:fenc=utf-8:et:sw=4:ts=4:sts=4
+# $Id$
+
+# Copyright (c) 2012 Markus Weissmann <mww@macports.org>
+# Copyright (c) 2012 The MacPorts Project
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. Neither the name of Apple Computer, Inc. nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+#
+# Usage:
+# replaced_by   name-of-port-that-deprecated-this-port
+# PortGroup     obsolete 1.0
+
+# set a number of reasonable defaults for a port that is only there to
+# inform users that they should uninstall it and install something else
+# instead; you might want to override some of the defaults though.
+description      this port is only a stub and has been made obsolete by ${replaced_by}
+long_description ${description}
+livecheck.type   none
+maintainers      nomaintainer
+homepage         http://www.macports.org/
+license          BSD
+platforms        darwin
+supported_archs	 noarch
+distfiles
+
+pre-configure {
+   ui_error "${name} has been made obsolete by the port ${replaced_by}. Please install ${replaced_by} instead."
+   return -code error "obsolete port"
+}
+
