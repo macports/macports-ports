1f6a3f4651a46988e97e2d28361f5251703f5f24
James D. Berry <jberry@macports.org>
2012-02-07 04:49:06 +0000

    Update commons-daemon 1.02 -> 1.0.8
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@89702 d073be05-634f-4543-b044-5fe20cf6d1d6

--- java/commons-daemon/files/patch-native-dso-dyld.c
+++ java/commons-daemon/files/patch-native-dso-dyld.c
@@ -1,5 +1,13 @@
---- src/native/unix/native/dso-dyld.c.orig	2004-02-09 07:55:21.000000000 -0800
-+++ src/native/unix/native/dso-dyld.c	2005-01-22 08:27:20.000000000 -0800
-@@ -118 +118 @@
--    while(x>=0) und[x+1]=nam[x--];
-+    for(; x>=0; --x) und[x+1]=nam[x];
+--- src/native/unix/native/dso-dyld.c.orig	2010-03-11 02:03:32.000000000 -0800
++++ src/native/unix/native/dso-dyld.c	2012-02-06 07:59:47.000000000 -0800
+@@ -124,8 +124,8 @@
+     while (nam[x] != '\0')
+         x++;
+     und = (char *)malloc(sizeof(char) * (x + 2));
+-    while (x >= 0)
+-        und[x + 1] = nam[x--];
++    for (; x >= 0; --x)
++    	und[x + 1] = nam[x];
+     und[0] = '_';
+ 
+     /* Find the symbol */
