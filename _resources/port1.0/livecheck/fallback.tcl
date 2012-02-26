a1e3bbb78a65b56bdbb3ab54d99a481979ad24e5
Joshua Root <jmr@macports.org>
2012-02-26 17:25:01 +0000

    change freshmeat livecheck to freecode (https://trac.macports.org/ticket/32026), still allowing freshmeat as an alias
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@90212 d073be05-634f-4543-b044-5fe20cf6d1d6

--- _resources/port1.0/livecheck/fallback.tcl
+++ _resources/port1.0/livecheck/fallback.tcl
@@ -3,4 +3,4 @@
 # If the default type cannot be determined from the mirror, this file is
 # sourced.
 
-source [getdefaultportresourcepath "port1.0/livecheck"]/freshmeat.tcl
+source [getdefaultportresourcepath "port1.0/livecheck"]/freecode.tcl
--- _resources/port1.0/livecheck/freshmeat.tcl
+++ _resources/port1.0/livecheck/freshmeat.tcl
@@ -1,17 +1,5 @@
 # $Id$
 #
-# This file contains the defaults for freshmeat.
+# Provide freshmeat as an alias for freecode
 
-if {${livecheck.name} eq "default"} {
-    set livecheck.name ${name}
-}
-if {${livecheck.distname} eq "default"} {
-    set livecheck.distname ${livecheck.name}
-}
-if {!$has_homepage || ${livecheck.url} eq ${homepage}} {
-    set livecheck.url "http://freshmeat.net/projects/${livecheck.name}/releases.atom"
-}
-if {${livecheck.regex} eq ""} {
-    set livecheck.regex [list "(?i)<title>[quotemeta ${livecheck.distname}] (.*)</title>"]
-}
-set livecheck.type "regex"
+source [getdefaultportresourcepath "port1.0/livecheck"]/freecode.tcl
