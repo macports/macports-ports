9b68cbb70d922e847ea1bf9f66c1b3e10d6fef97
Ryan Schmidt <ryandesign@macports.org>
2012-03-04 02:44:28 +0000

    pserv: update to 3.4; use rmd160 and sha256 checksum types; GPL-2+ license; rewrite master_sites to avoid redirects; don't clobber conf files or index.html in case user had modified them; install a launchd plist
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@90388 d073be05-634f-4543-b044-5fe20cf6d1d6

--- www/pserv/files/patch-main.h
+++ www/pserv/files/patch-main.h
@@ -1,37 +1,26 @@
---- sources/main.h	Mon Aug 30 13:19:03 2004
-+++ sources/main.h.new	Mon Aug 30 16:48:33 2004
-@@ -15,7 +15,9 @@
- 
- You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- */
--
-+#define CONCAT(x,y)    x "" y
-+#define STRING(z)      #z
-+#define FUNCTION(a,b)  CONCAT( STRING(a), STRING(b) )
- 
- /* --- CPP parsing options --- */
- #define PRINTF_DEBUG            /* enable this to print some debugging messages */
-@@ -37,18 +39,18 @@
+--- sources/main.h.orig	2005-06-01 05:36:18.000000000 -0500
++++ sources/main.h	2012-03-03 20:03:48.000000000 -0600
+@@ -37,18 +37,18 @@
  #define MIME_TYPE_DEFAULT   "application/octet-stream"
  
  /* configuration file location */
--#define DEFAULT_CONFIG_LOCATION "/usr/people/multix/pserv/defaults/"
-+#define DEFAULT_CONFIG_LOCATION FUNCTION( PREFIX, /etc/pserv/ )
+-#define DEFAULT_CONFIG_LOCATION "/usr/local/etc/pserv/"
++#define DEFAULT_CONFIG_LOCATION "@PREFIX@/etc/pserv/"
  
  /* hard-wired defaults, if loading of config file fails */
  #define DEFAULT_PORT	    	2000
  #define DEFAULT_MAX_CHILDREN	5
--#define DEFAULT_DOCS_LOCATION	"/home/multix/public_html"
-+#define DEFAULT_DOCS_LOCATION	FUNCTION( PREFIX, /var/www )
+-#define DEFAULT_DOCS_LOCATION	"/usr/local/var/www"
++#define DEFAULT_DOCS_LOCATION	"@PREFIX@/var/www"
  #define DEFAULT_FILE_NAME   	"index.html"
  #define DEFAULT_SEC_TO	    	1
  #define DEFAULT_USEC_TO     	100
--#define DEFAULT_LOG_FILE    	"/home/multix/pserv/pserv.log"
--#define DEFAULT_MIME_FILE   	"/home/multix/pserv/mime_types.dat"
--#define DEFAULT_CGI_ROOT    	"/home/multix/public_html/cgi-bin"
-+#define DEFAULT_LOG_FILE    	FUNCTION( PREFIX, /var/log/pserv/pserv.log )
-+#define DEFAULT_MIME_FILE   	FUNCTION( PREFIX, /share/pserv/mime_types.dat )
-+#define DEFAULT_CGI_ROOT    	FUNCTION( PREFIX, /var/www/cgi-bin )
+-#define DEFAULT_LOG_FILE    	"/usr/local/var/log/pserv.log"
+-#define DEFAULT_MIME_FILE   	"/usr/local/etc/pserv/mime_types.dat"
+-#define DEFAULT_CGI_ROOT    	"/usr/local/var/www/cgi-bin"
++#define DEFAULT_LOG_FILE    	"@PREFIX@/var/log/pserv/pserv.log"
++#define DEFAULT_MIME_FILE   	"@PREFIX@/etc/pserv/mime_types.dat"
++#define DEFAULT_CGI_ROOT    	"@PREFIX@/var/www/cgi-bin"
  #define DEFAULT_SERVER_NAME 	"localhost"
  
  /* amount of connections queued in listening */
