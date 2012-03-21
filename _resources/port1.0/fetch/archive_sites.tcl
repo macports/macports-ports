f8e5aed925f1956395bd926064ba121986947133
Joshua Root <jmr@macports.org>
2012-03-21 15:44:17 +0000

    archive_sites.tcl: trunk support
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@91010 d073be05-634f-4543-b044-5fe20cf6d1d6

--- _resources/port1.0/fetch/archive_sites.tcl
+++ _resources/port1.0/fetch/archive_sites.tcl
@@ -6,4 +6,7 @@ set portfetch::mirror_sites::sites(macports_archives) {
     http://packages.macports.org/:nosubdir
 }
 
+set portfetch::mirror_sites::archive_type(macports_archives) tbz2
 set portfetch::mirror_sites::archive_prefix(macports_archives) /opt/local
+set portfetch::mirror_sites::archive_frameworks_dir(macports_archives) /opt/local/Library/Frameworks
+set portfetch::mirror_sites::archive_applications_dir(macports_archives) /Applications/MacPorts
