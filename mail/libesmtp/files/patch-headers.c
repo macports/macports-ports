fdea44959dac19f33b86c74dec85b075d3240f24
Ryan Schmidt <ryandesign@macports.org>
2012-05-17 23:44:02 +0000

    libesmtp: update to 1.0.6; https://trac.macports.org/ticket/34440 (maintainer timeout)
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@93219 d073be05-634f-4543-b044-5fe20cf6d1d6

--- mail/libesmtp/files/patch-headers.c
+++ mail/libesmtp/files/patch-headers.c
@@ -1,32 +1,19 @@
-*** headers.c.orig	2006-03-30 19:30:35.000000000 +0200
---- headers.c	2006-03-30 19:57:05.000000000 +0200
-***************
-*** 29,36 ****
---- 29,38 ----
-  #include <stdio.h>
-  #include <stdarg.h>
-  #include <string.h>
-+ #include <strings.h>
-  #include <stdlib.h>
-  #include <unistd.h>
-+ #include <sys/time.h>
-  #include <time.h>
-  #include <errno.h>
-  
-***************
-*** 168,174 ****
-      {
-  #ifdef HAVE_GETTIMEOFDAY
-        if (gettimeofday (&tv, NULL) == -1) /* This shouldn't fail ... */
-! 	snprintf (buf, sizeof buf, "%ld.%ld.%d@%s", tv.tv_sec, tv.tv_usec,
-  		  getpid (), message->session->localhost);
-        else /* ... but if it does fall back to using time() */
-  #endif
---- 170,176 ----
-      {
-  #ifdef HAVE_GETTIMEOFDAY
-        if (gettimeofday (&tv, NULL) == -1) /* This shouldn't fail ... */
-! 	snprintf (buf, sizeof buf, "%ld.%d.%d@%s", tv.tv_sec, tv.tv_usec,
-  		  getpid (), message->session->localhost);
-        else /* ... but if it does fall back to using time() */
-  #endif
+--- headers.c.orig	2010-08-09 15:35:19.000000000 -0500
++++ headers.c	2012-05-13 21:33:41.000000000 -0500
+@@ -29,6 +29,7 @@
+ #include <stdio.h>
+ #include <stdarg.h>
+ #include <string.h>
++#include <strings.h>
+ #include <stdlib.h>
+ #include <unistd.h>
+ #include <time.h>
+@@ -171,7 +172,7 @@
+     {
+ #ifdef HAVE_GETTIMEOFDAY
+       if (gettimeofday (&tv, NULL) != -1) /* This shouldn't fail ... */
+-	snprintf (buf, sizeof buf, "%ld.%ld.%d@%s", tv.tv_sec, tv.tv_usec,
++	snprintf (buf, sizeof buf, "%ld.%d.%d@%s", tv.tv_sec, tv.tv_usec,
+ 		  getpid (), message->session->localhost);
+       else /* ... but if it does fall back to using time() */
+ #endif
