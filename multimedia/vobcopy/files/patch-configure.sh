d2b387042998f26021624b7cf6b8551346b314b7
Ryan Schmidt <ryandesign@macports.org>
2012-06-30 10:31:46 +0000

    vobcopy: build for the right architectures (https://trac.macports.org/ticket/32679) and fix build error when using clang and 64-bit kernel (https://trac.macports.org/ticket/32672)
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@94810 d073be05-634f-4543-b044-5fe20cf6d1d6

--- multimedia/vobcopy/files/patch-configure.sh
+++ multimedia/vobcopy/files/patch-configure.sh
@@ -1,5 +1,14 @@
---- configure.sh.orig	2009-07-22 13:10:44.000000000 -0700
-+++ configure.sh	2009-07-22 13:10:52.000000000 -0700
+--- configure.sh.orig	2009-06-08 14:58:28.000000000 -0500
++++ configure.sh	2011-12-27 15:16:26.000000000 -0600
+@@ -153,7 +153,7 @@
+   else
+ 	  LDFLAGS="LDFLAGS += -ldvdread -L$libs_dir/lib"
+   fi	
+-elif [ `uname -m` = x86_64 ]; then #for ia64/AMD64 libraries
++elif [ `uname -m` = x86_64 -a `uname -s` != Darwin ]; then #for ia64/AMD64 libraries
+ 	LDFLAGS="LDFLAGS += -ldvdread -L$libs_dir/lib64"
+ else
+ 	LDFLAGS="LDFLAGS += -ldvdread -L$libs_dir/lib"
 @@ -228,11 +228,9 @@
  #	cp vobcopy.1 \$(MANDIR)/man1/vobcopy.1
  	install -d -m 755 \$(DESTDIR)/\$(BINDIR)
