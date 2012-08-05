f7dc2d9180793f38cae5402a07dbd46ebebe09a4
Joshua Root <jmr@macports.org>
2012-08-05 21:20:11 +0000

    remove non-working sea.us mirror (see https://trac.macports.org/ticket/33666)
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@96236 d073be05-634f-4543-b044-5fe20cf6d1d6

--- _resources/port1.0/fetch/mirror_sites.tcl
+++ _resources/port1.0/fetch/mirror_sites.tcl
@@ -294,7 +294,6 @@ set portfetch::mirror_sites::sites(macports_distfiles) {
     http://her.gr.distfiles.macports.org/mirrors/macports/mpdistfiles/:mirror
     http://jog.id.distfiles.macports.org/macports/mpdistfiles/:mirror
     http://lil.fr.distfiles.macports.org/:mirror
-    http://sea.us.distfiles.macports.org/macports/mpdistfiles/:mirror
     http://ykf.ca.distfiles.macports.org/MacPorts/mpdistfiles/:mirror
 }
 
