126cba46218a8b1641f4fb5e6a0221f598228f2f
Joshua Root <jmr@macports.org>
2012-08-23 18:12:44 +0000

    update hostname for lil.fr packages mirror
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@96977 d073be05-634f-4543-b044-5fe20cf6d1d6

--- _resources/port1.0/fetch/archive_sites.tcl
+++ _resources/port1.0/fetch/archive_sites.tcl
@@ -4,7 +4,7 @@ namespace eval portfetch::mirror_sites { }
 
 set portfetch::mirror_sites::sites(macports_archives) {
     http://packages.macports.org/:nosubdir
-    http://macports.packages.ionic.de/:nosubdir
+    http://lil.fr.packages.macports.org/:nosubdir
 }
 
 set portfetch::mirror_sites::archive_type(macports_archives) tbz2
