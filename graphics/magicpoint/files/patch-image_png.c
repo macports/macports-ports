6b731827d202d0ecce1a26b8519aa5d4ae2e85c0
Joshua Root <jmr@macports.org>
2012-09-05 20:19:58 +0000

    magicpoint: fix build with giflib 4.2 (https://trac.macports.org/ticket/34972) and libpng 1.5
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@97425 d073be05-634f-4543-b044-5fe20cf6d1d6

--- graphics/magicpoint/files/patch-image_png.c
+++ graphics/magicpoint/files/patch-image_png.c
@@ -1,5 +1,23 @@
 --- image/png.c.orig	2001-04-11 18:37:00.000000000 +1000
-+++ image/png.c	2011-08-21 10:56:49.000000000 +1000
++++ image/png.c	2012-09-06 06:01:22.000000000 +1000
+@@ -44,7 +44,7 @@
+ #define PNG_CHECK_BYTES 4
+ 
+ int 
+-pngIdent(char *fullname, char *name) {}
++pngIdent(char *fullname, char *name) {return 1;}
+     
+ Image *
+ pngLoad(fullname, name, verbose)
+@@ -86,7 +86,7 @@ pngLoad(fullname, name, verbose)
+ 		return NULL;
+ 	}
+ 
+-	if (setjmp(png_ptr->jmpbuf)) {
++	if (setjmp(png_jmpbuf(png_ptr))) {
+ 		png_destroy_read_struct(&png_ptr, &info_ptr, (png_infopp)NULL);
+ 		fclose(fp);
+ 		return NULL;
 @@ -134,7 +134,7 @@ pngLoad(fullname, name, verbose)
  	} 
  
