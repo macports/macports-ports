5b106dafe8f138a436ef95ecb8ba4c64aaefe734
Ryan Schmidt <ryandesign@macports.org>
2012-10-03 10:09:01 +0000

    wordplay: fix finding included words file; install readme in usual docdir
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@98349 d073be05-634f-4543-b044-5fe20cf6d1d6

--- games/wordplay/files/patch-wordplay.c
+++ games/wordplay/files/patch-wordplay.c
@@ -1,6 +1,6 @@
---- wordplay.c.orig	2005-10-03 10:45:29.000000000 -0700
-+++ wordplay.c	2005-10-03 10:45:50.000000000 -0700
-@@ -136,6 +136,7 @@
+--- wordplay.c.orig	1996-03-20 09:34:00.000000000 -0600
++++ wordplay.c	2012-10-03 05:07:15.000000000 -0500
+@@ -136,13 +136,14 @@
  */
  
  #include <stdio.h>
@@ -8,3 +8,11 @@
  #include <string.h>
  #include <ctype.h>
  
+ #define max(A, B) ((A) > (B) ? (A) : (B))
+ #define min(A, B) ((A) < (B) ? (A) : (B))
+ 
+-#define DEFAULT_WORD_FILE "words721.txt"
++#define DEFAULT_WORD_FILE "@PREFIX@/share/wordplay/words721.txt"
+ #define WORDBLOCKSIZE 4096
+ #define MAX_WORD_LENGTH 128
+ #define SAFETY_ZONE MAX_WORD_LENGTH + 1
