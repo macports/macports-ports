f58b14723457ed147ec49ebd77094da842346bb8
Ryan Schmidt <ryandesign@macports.org>
2012-11-28 08:07:45 +0000

    compiler_blacklist_versions-1.0.tcl: add some hopefully helpful debug statements
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@100105 d073be05-634f-4543-b044-5fe20cf6d1d6

--- _resources/port1.0/group/compiler_blacklist_versions-1.0.tcl
+++ _resources/port1.0/group/compiler_blacklist_versions-1.0.tcl
@@ -67,8 +67,12 @@ proc compiler_blacklist_versions._set_compiler_blacklist {option action args} {
             compiler.blacklist-delete ${blacklist}
             set compiler [lindex ${blacklist} 0]
             set comparisons [lrange ${blacklist} 1 end]
+            set compiler_version [compiler_blacklist_versions._get_compiler_version ${compiler}]
             if {[compiler_blacklist_versions._matches_all_versions ${compiler} ${comparisons}]} {
+                ui_debug "${compiler} ${compiler_version} blacklisted because matches ${blacklist}"
                 compiler.blacklist-append ${compiler}
+            } else {
+                ui_debug "${compiler} ${compiler_version} not blacklisted because does not match {${blacklist}}"
             }
         }
     }
