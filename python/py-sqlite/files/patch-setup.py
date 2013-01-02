118267fb73f68d5c8417ad4a95988c4b53ec2af7
Joshua Root <jmr@macports.org>
2013-01-02 09:45:31 +0000

    py*-sqlite: unify
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@100964 d073be05-634f-4543-b044-5fe20cf6d1d6

--- python/py-sqlite/files/patch-setup.py
+++ python/py-sqlite/files/patch-setup.py
@@ -5,12 +5,12 @@
          sys.exit(1)
  
 -    data_files = [("pysqlite2-doc",
-+    data_files = [("share/doc/py-sqlite",
++    data_files = [("share/doc/__SUBPORT__",
                          glob.glob("doc/*.html") \
                        + glob.glob("doc/*.txt") \
                        + glob.glob("doc/*.css")),
 -                   ("pysqlite2-doc/code",
-+                   ("share/doc/py-sqlite/code",
++                   ("share/doc/__SUBPORT__/code",
                          glob.glob("doc/code/*.py"))]
  
      py_modules = ["sqlite"]
