bf987e51ae11f31cc80f5931d3724794658a3f5c
Jeremy Huddleston Sequoia <jeremyhu@macports.org>
2013-01-05 07:21:56 +0000

    cmake: Ensure that CMAKE_OSX_DEPLOYMENT_TARGET is unset if CMAKE_OSX_SYSROOT is /
    This will avoid errors like the following when we don't use an sdkroot:
    
    CMake Error at /opt/local/share/cmake-2.8/Modules/Platform/Darwin.cmake:190 (message):
      CMAKE_OSX_DEPLOYMENT_TARGET is '10.8' but CMAKE_OSX_SYSROOT:
    
       "/"
    
      is not set to a MacOSX SDK with a recognized version.  Either set
      CMAKE_OSX_SYSROOT to a valid SDK or set CMAKE_OSX_DEPLOYMENT_TARGET to
      empty.
    
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@101141 d073be05-634f-4543-b044-5fe20cf6d1d6

--- _resources/port1.0/group/cmake-1.0.tcl
+++ _resources/port1.0/group/cmake-1.0.tcl
@@ -74,7 +74,7 @@ platform darwin {
         if {${configure.sdkroot} != ""} {
             configure.args-append -DCMAKE_OSX_SYSROOT="${configure.sdkroot}"
         } else {
-            configure.args-append -DCMAKE_OSX_SYSROOT=/
+            configure.args-append -DCMAKE_OSX_SYSROOT=/ -DCMAKE_OSX_DEPLOYMENT_TARGET=""
         }
     }
 }
