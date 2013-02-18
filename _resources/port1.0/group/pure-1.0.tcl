09955c386493aae4e84a74e14b8ce4662c4fd423
Ryan Schmidt <ryandesign@macports.org>
2013-02-18 11:41:01 +0000

    pure: update to 0.57 and switch to llvm 3.2 pure-docs: update to 0.57 all pure ports: switch to bitbucket hosting
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@103201 d073be05-634f-4543-b044-5fe20cf6d1d6

--- _resources/port1.0/group/pure-1.0.tcl
+++ _resources/port1.0/group/pure-1.0.tcl
@@ -1,7 +1,7 @@
 # -*- coding: utf-8; mode: tcl; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- vim:fenc=utf-8:ft=tcl:et:sw=4:ts=4:sts=4
 # $Id$
 #
-# Copyright (c) 2009 The MacPorts Project
+# Copyright (c) 2009-2013 The MacPorts Project
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -47,8 +47,8 @@ proc pure.setup {module version} {
 
     name                        pure-${module}
     version                     ${version}
-    homepage                    http://code.google.com/p/pure-lang/wiki/Addons#${name}
-    master_sites                googlecode:pure-lang
+    homepage                    https://bitbucket.org/purelang/pure-lang/wiki/Addons#${name}
+    master_sites                http://cdn.bitbucket.org/purelang/pure-lang/downloads/
 
     depends_lib                 path:lib/libpure.dylib:pure
 
@@ -78,6 +78,6 @@ proc pure.setup {module version} {
     }
 
     livecheck.type              regex
-    default livecheck.url       {http://code.google.com/p/pure-lang/downloads/list?q=${name}&sort=filename}
+    default livecheck.url       {https://bitbucket.org/purelang/pure-lang/downloads}
     default livecheck.regex     {${name}-(\[0-9.\]+)\\.tar}
 }
