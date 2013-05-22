c03c26f5bff1a529391f2231a72e81adde17fb7a
Ryan Schmidt <ryandesign@macports.org>
2013-05-22 12:28:15 +0000

    minivmac, minivmac-devel: make demacbinary.sh write into the current working directory
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@106318 d073be05-634f-4543-b044-5fe20cf6d1d6

--- emulators/minivmac-devel/files/demacbinary.sh
+++ emulators/minivmac-devel/files/demacbinary.sh
@@ -3,10 +3,9 @@
 # $Id$
 
 INFILE="$1"
-OUT="$2"
 
-if [ $# -ne 2 ]; then
-	echo "usage: $0 <infile> <outfile or outdir>" 1>&2
+if [ $# -ne 1 ]; then
+	echo "usage: $0 <infile>" 1>&2
 	exit 1
 fi
 
@@ -15,22 +14,16 @@ if [ ! -f "$INFILE" ]; then
 	exit 1
 fi
 
-if [ -d "$OUT" ]; then
-	OUTDIR="$OUT"
-	OUTFILE="$OUTDIR/`basename "$INFILE" .bin`"
-else
-	OUTFILE="$OUT"
-	OUTDIR="`dirname "$OUTFILE"`"
-fi
+OUTFILE="$(basename "$INFILE" .bin)"
 
-if [ ! -d "$OUTDIR" ]; then
-	echo "$0: $OUTDIR: No such directory" 1>&2
+if [ "$INFILE" == "$OUTFILE" ]; then
+	echo "$0: $INFILE: Filename doesn't end with .bin" 1>&2
 	exit 1
 fi
 
 MACBINARY="/usr/bin/macbinary"
 if [ -x "$MACBINARY" ]; then
-	"$MACBINARY" decode -C "$OUTDIR" -o "$OUTFILE" "$INFILE"
+	"$MACBINARY" decode -o "$OUTFILE" "$INFILE"
 	exit $?
 fi
 
@@ -38,7 +31,7 @@ DATAFORKFILE="$OUTFILE.data"
 RSRCFORKFILE="$OUTFILE.rsrc"
 INFOFILE="$OUTFILE.info"
 
-(cd "$OUTDIR" && macunpack -3 "$INFILE" || exit $?)
+macunpack -3 "$INFILE" || exit $?
 cp "$DATAFORKFILE" "$OUTFILE" || exit $?
 cp "$RSRCFORKFILE" "$OUTFILE/rsrc" || exit $?
 rm -f "$DATAFORKFILE" "$RSRCFORKFILE" "$INFOFILE"
