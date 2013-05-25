6e72b3ba0fed83dbe310163d128dca6fc5ac8cbd
Markus W. Weißmann <mww@macports.org>
2013-05-25 15:58:56 +0000

    ocaml-extlib: version 1.5.4
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@106417 d073be05-634f-4543-b044-5fe20cf6d1d6

--- devel/ocaml-extlib/files/patch-install.ml
+++ devel/ocaml-extlib/files/patch-install.ml
@@ -1,22 +1,11 @@
---- install.ml	2011-08-06 15:01:32.000000000 +0200
-+++ install.ml	2012-08-13 22:26:14.000000000 +0200
-@@ -174,8 +174,8 @@
- 	if !autodoc then begin
- 		run (sprintf "ocamldoc -sort -html -d %s %s" doc_dir (m_list ".mli"));
- 		run ((match path_type with
--				| PathDos -> sprintf "%s odoc_style.css %s\\style.css";
--				| PathUnix -> sprintf "%s odoc_style.css %s/style.css") cp_cmd doc_dir);
-+				| PathDos -> sprintf "%s doc/style.css %s\\style.css";
-+				| PathUnix -> sprintf "%s doc/style.css %s/style.css") cp_cmd doc_dir);
- 	end;
- 	match install_dir with
- 	  Findlib ->
-@@ -193,7 +193,7 @@
+--- work/extlib-1.5.4/install.ml	2013-05-08 12:40:59.000000000 +0200
++++ install.ml	2013-05-25 17:50:35.000000000 +0200
+@@ -210,7 +210,7 @@
  	      Buffer.add_string files ("extLib" ^ lib_ext^ " ");
  	    end;
  	    let files = Buffer.contents files in
--	    run (sprintf "ocamlfind install extlib %s META" files);
-+	    run (sprintf "##PREFIX##/bin/ocamlfind install -destdir '##DESTDIR##' -metadir '' extlib %s META" files);
+-	    run (sprintf "ocamlfind install -patch-version %s extlib META %s" version files);
++ 	    run (sprintf "##PREFIX##/bin/ocamlfind install -destdir '##DESTDIR##' -metadir '' -patch-version %s extlib META %s" version files);
  	| Dir install_dir ->
  	    List.iter (fun m ->
  			copy (m ^ ".cmi") install_dir;
