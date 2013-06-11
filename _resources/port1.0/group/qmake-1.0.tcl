32570c181059ba2592556d4a78f9683cca61686a
Nicolas Pavillon <nicos@macports.org>
2013-06-11 15:39:22 +0000

    new portgroup qmake (ticket https://trac.macports.org/ticket/38280)
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@106912 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ _resources/port1.0/group/qmake-1.0.tcl
@@ -0,0 +1,59 @@
+# -*- coding: utf-8; mode: tcl; c-basic-offset: 4; indent-tabs-mode: nil; tab-width: 4; truncate-lines: t -*- vim:fenc=utf-8:et:sw=4:ts=4:sts=4
+# $Id$
+
+#
+# Copyright (c) 2013 The MacPorts Project
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. Neither the name of Apple Computer, Inc. nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+#
+# This portgroup defines standard settings when using qmake.
+#
+# Usage:
+# PortGroup                     qmake 1.0
+
+PortGroup                       qt4 1.0
+
+pre-configure {
+    configure.cmd                   ${qt_qmake_cmd}
+    configure.pre_args-delete       --prefix=${prefix}
+    configure.pre_args-append       PREFIX=${prefix} \
+                                    "QMAKE_CC=${configure.cc} QMAKE_CXX=${configure.cxx}" \
+                                    "QMAKE_OBJC=${configure.objc}"
+    configure.args-append           "CFLAGS=\"${configure.cflags} [get_canonical_archflags cc]\""
+    configure.universal_args-delete --disable-dependency-tracking
+}
+
+if {[variant_isset universal]} {
+    pre-configure {
+        configure.pre_args-append   "CONFIG+=\"${qt_arch_types}\""
+    }
+}
+
+variant debug description "Enable debug binaries" {
+    configure.pre_args-append   "CONFIG+=debug"	
+}
