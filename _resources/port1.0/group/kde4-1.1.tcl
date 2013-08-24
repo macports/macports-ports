0449ea3cd828b99657c482548e5a64d580272927
Ryan Schmidt <ryandesign@macports.org>
2013-08-24 12:46:33 +0000

    kde4-1.1.tcl: allow MacPorts to word-wrap the notes instead of inserting a newline manually
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@110013 d073be05-634f-4543-b044-5fe20cf6d1d6

--- _resources/port1.0/group/kde4-1.1.tcl
+++ _resources/port1.0/group/kde4-1.1.tcl
@@ -147,7 +147,9 @@ variant docs description "Build documentation" {
     configure.args-delete   -DBUILD_doc=OFF -DBUILD_docs=OFF
 }
 
-notes "Don't forget that dbus needs to be started as the local 
-user (not with sudo) before any KDE programs will launch
-To start it run the following command:                  
- launchctl load -w /Library/LaunchAgents/org.freedesktop.dbus-session.plist"
+notes "
+Don't forget that dbus needs to be started as the local\
+user (not with sudo) before any KDE programs will launch.
+To start it run the following command:
+ launchctl load -w /Library/LaunchAgents/org.freedesktop.dbus-session.plist
+"
