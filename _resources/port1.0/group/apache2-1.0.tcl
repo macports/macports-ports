8e827f3589e260c8172bf5e80105fd09b3a16a2c
Bradley Giesbrecht <pixilla@macports.org>
2013-09-11 15:59:19 +0000

    _resources/port1.0/group: - Match namespace to portgroup name. www/apache24-devel: - Update portgroup namespace and revbump.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@110959 d073be05-634f-4543-b044-5fe20cf6d1d6

--- _resources/port1.0/group/apache2-1.0.tcl
+++ _resources/port1.0/group/apache2-1.0.tcl
@@ -6,106 +6,109 @@
 #
 # values to be used in eg configuration files
 #
-options apache.prefix
-default apache.prefix           {"${prefix}"}
-options apache.exec_prefix
-default apache.exec_prefix      {"${apache.prefix}"}
-options apache.name
-default apache.name             {"${name}"}
-options apache.exec_name
-default apache.exec_name        {"httpd"}
-options apache.bindir
-default apache.bindir           {"${apache.exec_prefix}/bin"}
-options apache.sbindir
-default apache.sbindir          {"${apache.exec_prefix}/sbin"}
-options apache.libdir
-default apache.libdir           {"${apache.exec_prefix}/lib/${apache.name}"}
-options apache.libexecdir
-default apache.libexecdir       {"${apache.libdir}/modules"}
-options apache.mandir
-default apache.mandir           {"${apache.prefix}/share/${apache.name}/man"}
-options apache.sysconfdir
-default apache.sysconfdir       {"${apache.prefix}/etc/${apache.name}"}
-options apache.datadir
-default apache.datadir          {"${apache.prefix}/www/${apache.name}"}
-options apache.installbuilddir
-default apache.installbuilddir  {"${apache.datadir}/build"}
-options apache.errordir
-default apache.errordir         {"${apache.datadir}/error"}
-options apache.iconsdir
-default apache.iconsdir         {"${apache.datadir}/icons"}
-options apache.htdocsdir
-default apache.htdocsdir        {"${apache.datadir}/html"}
-options apache.manualdir
-default apache.manualdir        {"${apache.datadir}/manual"}
-options apache.cgidir
-default apache.cgidir           {"${apache.datadir}/cgi-bin"}
-options apache.includedir
-default apache.includedir       {"${apache.prefix}/include/${apache.name}"}
-options apache.localstatedir
-default apache.localstatedir    {"${apache.prefix}/var"}
-options apache.runtimedir
-default apache.runtimedir       {"${apache.localstatedir}/run/${apache.name}"}
-options apache.logfiledir
-default apache.logfiledir       {"${apache.localstatedir}/log/${apache.name}"}
-options apache.proxycachedir
-default apache.proxycachedir    {"${apache.localstatedir}/tmp/${apache.name}"}
+options apache2.prefix
+default apache2.prefix          {"${prefix}"}
+options apache2.exec_prefix
+default apache2.exec_prefix     {"${apache2.prefix}"}
+options apache2.name
+default apache2.name            {"${name}"}
+options apache2.exec_name
+default apache2.exec_name       {"httpd"}
+options apache2.bindir
+default apache2.bindir          {"${apache2.exec_prefix}/bin"}
+options apache2.sbindir
+default apache2.sbindir         {"${apache2.exec_prefix}/sbin"}
+options apache2.libdir
+default apache2.libdir          {"${apache2.exec_prefix}/lib/${apache2.name}"}
+options apache2.libexecdir
+default apache2.libexecdir      {"${apache2.libdir}/modules"}
+options apache2.mandir
+default apache2.mandir          {"${apache2.prefix}/share/${apache2.name}/man"}
+options apache2.docdir
+default apache2.docdir          {"${apache2.prefix}/share/doc/${apache2.name}"}
+options apache2.sysconfdir
+default apache2.sysconfdir      {"${apache2.prefix}/etc/${apache2.name}"}
+options apache2.datadir
+default apache2.datadir         {"${apache2.prefix}/www/${apache2.name}"}
+options apache2.installbuilddir
+default apache2.installbuilddir {"${apache2.datadir}/build"}
+options apache2.errordir
+default apache2.errordir        {"${apache2.datadir}/error"}
+options apache2.iconsdir
+default apache2.iconsdir        {"${apache2.datadir}/icons"}
+options apache2.htdocsdir
+default apache2.htdocsdir       {"${apache2.datadir}/html"}
+options apache2.manualdir
+default apache2.manualdir       {"${apache2.datadir}/manual"}
+options apache2.cgidir
+default apache2.cgidir          {"${apache2.datadir}/cgi-bin"}
+options apache2.includedir
+default apache2.includedir      {"${apache2.prefix}/include/${apache2.name}"}
+options apache2.localstatedir
+default apache2.localstatedir   {"${apache2.prefix}/var"}
+options apache2.runtimedir
+default apache2.runtimedir      {"${apache2.localstatedir}/run/${apache2.name}"}
+options apache2.logfiledir
+default apache2.logfiledir      {"${apache2.localstatedir}/log/${apache2.name}"}
+options apache2.proxycachedir
+default apache2.proxycachedir   {"${apache2.localstatedir}/tmp/${apache2.name}"}
 
-options apache.user
-default apache.user             {"_www"}
-options apache.group
-default apache.group            {"_www"}
+options apache2.user
+default apache2.user            {"_www"}
+options apache2.group
+default apache2.group           {"_www"}
 
 # General settings
-options apache.listen_ports
-default apache.listen_ports     {{80 443}}
-options apache.contact
-default apache.contact          {"ops@example.com"}
-options apache.timeout
-default apache.timeout          {300}
-options apache.keepalive
-default apache.keepalive        {"On"}
-set apache(keepaliverequests)   100
-options apache.keepalivetimeout
-default apache.keepalivetimeout {5}
+options apache2.listen_ports
+default apache2.listen_ports    {{80 443}}
+options apache2.contact
+default apache2.contact         {"ops@example.com"}
+options apache2.timeout
+default apache2.timeout         {300}
+options apache2.keepalive
+default apache2.keepalive       {"On"}
+options apache2.keepaliverequests
+default apache2.keepaliverequests   {100}
+options apache2.keepalivetimeout
+default apache2.keepalivetimeout    {5}
 
 # Security
-options apache.servertokens
-default apache.servertokens     {"Prod"}
-options apache.serversignature
-default apache.serversignature  {"On"}
-options apache.traceenable
-default apache.traceenable      {"On"}
+options apache2.servertokens
+default apache2.servertokens    {"Prod"}
+options apache2.serversignature
+default apache2.serversignature {"On"}
+options apache2.traceenable
+default apache2.traceenable     {"On"}
 
 # mod_auth_openids
-options apache.allowed_openids
-default apache.allowed_openids  {""}
+options apache2.allowed_openids
+default apache2.allowed_openids {""}
 
 # Prefork Attributes
-options apache.prefork.startservers
-default apache.prefork.startservers     {16}
-options apache.prefork.minspareservers
-default apache.prefork.minspareservers  {16}
-options apache.prefork.maxspareservers
-default apache.prefork.maxspareservers  {32}
-options apache.prefork.serverlimit
-default apache.prefork.serverlimit      {400}
-options apache.prefork.maxclients
-default apache.prefork.maxclients       {400}
-options apache.prefork.threadsperchild
-default apache.prefork.threadsperchild  {10000}
+options apache2.prefork.startservers
+default apache2.prefork.startservers    {16}
+options apache2.prefork.minspareservers
+default apache2.prefork.minspareservers {16}
+options apache2.prefork.maxspareservers
+default apache2.prefork.maxspareservers {32}
+options apache2.prefork.serverlimit
+default apache2.prefork.serverlimit     {400}
+options apache2.prefork.maxclients
+default apache2.prefork.maxclients      {400}
+options apache2.prefork.threadsperchild
+default apache2.prefork.threadsperchild {10000}
 
 # Worker Attributes
-options apache.worker.startservers
-default apache.worker.startservers     {4}
-options apache.worker.maxclients
-default apache.worker.maxclients       {1024}
-options apache.worker.minsparethreads
-default apache.worker.minsparethreads  {64}
-options apache.worker.maxsparethreads
-default apache.worker.maxsparethreads  {192}
-options apache.worker.threadsperchild
-default apache.worker.threadsperchild  {64}
+options apache2.worker.startservers
+default apache2.worker.startservers    {4}
+options apache2.worker.maxclients
+default apache2.worker.maxclients      {1024}
+options apache2.worker.minsparethreads
+default apache2.worker.minsparethreads {64}
+options apache2.worker.maxsparethreads
+default apache2.worker.maxsparethreads {192}
+options apache2.worker.threadsperchild
+default apache2.worker.threadsperchild {64}
 
 # Default modules to enable via include_recipe
 
@@ -121,16 +124,16 @@ set apache_default_modules(dismod)  {}
 #   ++ optionally installs dylibs
 #
 proc apxsInstall { moduleName activate dylibs } {
-    global apache destroot prefix
+    global apache2 destroot prefix
 
-    xinstall -m 0755 -d -W ${destroot}${apache.sysconfdir} \
-        mods-available  mods-enabled
+    xinstall -m 0755 -d -W ${destroot}${apache2.sysconfdir} \
+    mods-available  mods-enabled
 
     # strip pre-/suffix from moduleName
     set moduleName [regsub {_module} ${moduleName} "" ]
     set moduleName [regsub {mod_}    ${moduleName} "" ]
 
-    set  loadFileName   ${apache.sysconfdir}/mods-available/${moduleName}.load
+    set  loadFileName   ${apache2.sysconfdir}/mods-available/${moduleName}.load
     set  loadFile       [open ${loadFileName} w 644]
 
     foreach libName ${dylibs}  {
@@ -138,12 +141,11 @@ proc apxsInstall { moduleName activate dylibs } {
         puts ${loadFile} "LoadFile ${dylibFile}"
     }
 
-    puts  ${loadFile} "LoadModule ${moduleName}_module ${apache.libexecdir}/mod_${moduleName}.so"
+    puts  ${loadFile} "LoadModule ${moduleName}_module ${apache2.libexecdir}/mod_${moduleName}.so"
 
     close ${loadFile}
 
     if { ${activate} == "yes" } {
-            exec "${apache.sbindir}/a2enmod" ${moduleName}
+        exec "${apache2.sbindir}/a2enmod" ${moduleName}
     }
-
 }
