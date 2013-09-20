603ff849a412394828589e821f0af0998e132f49
Ryan Schmidt <ryandesign@macports.org>
2013-09-20 07:24:36 +0000

    pure modules: set CC and CXX as environment variables instead of make arguments, to try to fix pure-gen build failure
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@111394 d073be05-634f-4543-b044-5fe20cf6d1d6

--- _resources/port1.0/group/pure-1.0.tcl
+++ _resources/port1.0/group/pure-1.0.tcl
@@ -56,12 +56,12 @@ proc pure.setup {module version} {
 
     use_parallel_build          yes
     pre-build {
-        build.args-append       CC=${configure.cc} \
-                                CFLAGS="${configure.cflags} ${configure.cc_archflags}" \
+        build.args-append       CFLAGS="${configure.cflags} ${configure.cc_archflags}" \
                                 CPPFLAGS="${configure.cppflags}" \
-                                CXX=${configure.cxx} \
                                 CXXFLAGS="${configure.cxxflags} ${configure.cxx_archflags}" \
                                 LDFLAGS="${configure.ldflags} ${configure.ld_archflags}"
+        build.env-append        CC=${configure.cc} \
+                                CXX=${configure.cxx}
     }
 
     post-destroot {
