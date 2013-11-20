4c82ebd1eadad5053c4ce0da3f3dc4ed1a72ac29
Ryan Schmidt <ryandesign@macports.org>
2013-11-20 22:05:50 +0000

    mirror_sites.tcl: add more php mirrors
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@113617 d073be05-634f-4543-b044-5fe20cf6d1d6

--- _resources/port1.0/fetch/mirror_sites.tcl
+++ _resources/port1.0/fetch/mirror_sites.tcl
@@ -347,32 +347,53 @@ set portfetch::mirror_sites::sites(perl_cpan) {
 # an available mirror in that country. To update this list use:
 # curl -s http://php.net/mirrors.php | sed -E -n 's,^.*http://([a-z]{2})[0-9]*(\.php\.net)/.*$,\1\2,p' | sort -u | xargs -n 1 -I % sh -c '{ ping -o -c 4 -t 15 % >/dev/null 2>&1 && echo "    http://%/:nosubdir"; }' | tee /dev/tty | pbcopy
 set portfetch::mirror_sites::sites(php) {
+    http://am.php.net/:nosubdir
+    http://ar.php.net/:nosubdir
     http://at.php.net/:nosubdir
+    http://au.php.net/:nosubdir
     http://be.php.net/:nosubdir
+    http://bg.php.net/:nosubdir
     http://br.php.net/:nosubdir
     http://ca.php.net/:nosubdir
     http://ch.php.net/:nosubdir
+    http://cz.php.net/:nosubdir
     http://de.php.net/:nosubdir
     http://dk.php.net/:nosubdir
     http://ee.php.net/:nosubdir
+    http://es.php.net/:nosubdir
     http://fi.php.net/:nosubdir
     http://fr.php.net/:nosubdir
+    http://gr.php.net/:nosubdir
     http://hk.php.net/:nosubdir
     http://ie.php.net/:nosubdir
+    http://il.php.net/:nosubdir
     http://in.php.net/:nosubdir
+    http://ir.php.net/:nosubdir
     http://is.php.net/:nosubdir
+    http://it.php.net/:nosubdir
+    http://jm.php.net/:nosubdir
     http://jp.php.net/:nosubdir
+    http://li.php.net/:nosubdir
+    http://lt.php.net/:nosubdir
+    http://lu.php.net/:nosubdir
+    http://md.php.net/:nosubdir
     http://mx.php.net/:nosubdir
+    http://my.php.net/:nosubdir
     http://nc.php.net/:nosubdir
+    http://nl.php.net/:nosubdir
     http://no.php.net/:nosubdir
     http://nz.php.net/:nosubdir
     http://pa.php.net/:nosubdir
+    http://pk.php.net/:nosubdir
     http://pl.php.net/:nosubdir
     http://pt.php.net/:nosubdir
     http://ro.php.net/:nosubdir
     http://se.php.net/:nosubdir
     http://sg.php.net/:nosubdir
+    http://si.php.net/:nosubdir
+    http://th.php.net/:nosubdir
     http://tr.php.net/:nosubdir
+    http://tw.php.net/:nosubdir
     http://tz.php.net/:nosubdir
     http://ua.php.net/:nosubdir
     http://uk.php.net/:nosubdir
