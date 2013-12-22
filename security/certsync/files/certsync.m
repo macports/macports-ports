f93e15d7982ad25171d69f403548f98b86a83ab7
Landon Fuller <landonf@macports.org>
2013-12-22 00:40:04 +0000

    Add support for Mac OS X 10.4.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@115009 d073be05-634f-4543-b044-5fe20cf6d1d6

--- security/certsync/files/certsync.m
+++ security/certsync/files/certsync.m
@@ -26,30 +26,12 @@
  */
 
 #import <Foundation/Foundation.h>
-#import <AvailabilityMacros.h>
+#import <Security/Security.h>
 
 #import <unistd.h>
 #import <stdio.h>
 
-#import <objc/message.h>
-
-/* Allow building with SDKs < 10.6 */
-#ifndef MAC_OS_X_VERSION_10_6
-#define MAC_OS_X_VERSION_10_6 1060
-#endif /* !MAC_OS_X_VERSION_10_6 */
-
-/* Allow building with SDKs < 10.5 */
-#ifndef MAC_OS_X_VERSION_10_5
-#define MAC_OS_X_VERSION_10_5 1050
-#endif /* !MAC_OS_X_VERSION_10_5 */
-
-#if MAC_OS_X_VERSION_MAX_ALLOWED <= MAC_OS_X_VERSION_10_5
-/* errSecSuccess was not defined until 10.6 */
-#define errSecSuccess noErr
-
-/* NSDataWritingAtomic was not defined until 10.6 */
-#define NSDataWritingAtomic NSAtomicWrite
-#endif
+#import "compat.h"
 
 /* A wrapper class that may be used to pass configuration through the
  * FSEvent callback API */
@@ -123,68 +105,99 @@ int nsprintf (NSString *format, ...) {
  */
 static NSArray *certificatesForTrustDomain (SecTrustSettingsDomain domain, NSError **outError) {
     NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    NSArray *trusted = nil;
     CFArrayRef certs = nil;
     OSStatus err;
     
-    /* Fetch all certificates in the given domain */
-    err = SecTrustSettingsCopyCertificates(domain, &certs);
-    if (err == errSecSuccess) {
-        PLCFAutorelease(certs);
-    } else if (err == errSecNoTrustSettings ) {
-        /* No data */
+    /* Mac OS X >= 10.5 provides SecTrustSettingsCopyCertificates() */
+    if (SecTrustSettingsCopyCertificates != NULL) {
+        /* Fetch all certificates in the given domain */
+        err = SecTrustSettingsCopyCertificates(domain, &certs);
+        if (err == errSecSuccess) {
+            PLCFAutorelease(certs);
+        } else if (err == errSecNoTrustSettings ) {
+            /* No data */
         
-        [pool release];
-        return [NSArray array];
-    } else if (err != errSecSuccess) {
-        /* Lookup failed */
-        if (outError != NULL)
-            *outError = [[NSError errorWithDomain: NSOSStatusErrorDomain code: err userInfo:nil] retain];
+            [pool release];
+            return [NSArray array];
+        } else if (err != errSecSuccess) {
+            /* Lookup failed */
+            if (outError != NULL)
+                *outError = [[NSError errorWithDomain: NSOSStatusErrorDomain code: err userInfo:nil] retain];
         
-        [pool release];
-        [*outError autorelease];
-        return nil;
-    }
-    
-    /* Extract trusted roots */
-    NSMutableArray *results = [NSMutableArray arrayWithCapacity: CFArrayGetCount(certs)];
-    for (id certObj in (NSArray *) certs) {
-        SecCertificateRef cert = (SecCertificateRef) certObj;
-        
-        /* Fetch the trust settings */
-        CFArrayRef trustSettings = nil;
-        err = SecTrustSettingsCopyTrustSettings(cert, domain, &trustSettings);
-        if (err != errSecSuccess) {
-            /* Shouldn't happen */
-            nsfprintf(stderr, @"Failed to fetch trust settings\n");
-            continue;
-        } else {
-            PLCFAutorelease(trustSettings);
+            [pool release];
+            [*outError autorelease];
+            return nil;
         }
+    
+        /* Extract trusted roots */
+        NSMutableArray *results = [NSMutableArray arrayWithCapacity: CFArrayGetCount(certs)];
+        trusted = results;
         
-        /* If empty, trust for everything (as per the Security Framework documentation) */
-        if (CFArrayGetCount(trustSettings) == 0) {
-            [results addObject: certObj];
-        } else {
-            /* Otherwise, walk the properties and evaluate the trust settings result */
-            for (NSDictionary *trustProps in (NSArray *) trustSettings) {
-                CFNumberRef settingsResultNum;
-                SInt32 settingsResult;
+        NSEnumerator *resultEnumerator = [(NSArray *)certs objectEnumerator];
+        id certObj;
+        while ((certObj = [resultEnumerator nextObject]) != nil) {
+            SecCertificateRef cert = (SecCertificateRef) certObj;
+        
+            /* Fetch the trust settings */
+            CFArrayRef trustSettings = nil;
+            err = SecTrustSettingsCopyTrustSettings(cert, domain, &trustSettings);
+            if (err != errSecSuccess) {
+                /* Shouldn't happen */
+                nsfprintf(stderr, @"Failed to fetch trust settings\n");
+                continue;
+            } else {
+                PLCFAutorelease(trustSettings);
+            }
+        
+            /* If empty, trust for everything (as per the Security Framework documentation) */
+            if (CFArrayGetCount(trustSettings) == 0) {
+                [results addObject: certObj];
+            } else {
+                /* Otherwise, walk the properties and evaluate the trust settings result */
+                NSEnumerator *trustEnumerator = [(NSArray *)trustSettings objectEnumerator];
+                NSDictionary *trustProps;
+                while ((trustProps = [trustEnumerator nextObject]) != nil) {
+                    CFNumberRef settingsResultNum;
+                    SInt32 settingsResult;
                 
-                settingsResultNum = (CFNumberRef) [trustProps objectForKey: (id) kSecTrustSettingsResult];
-                CFNumberGetValue(settingsResultNum, kCFNumberSInt32Type, &settingsResult);
+                    settingsResultNum = (CFNumberRef) [trustProps objectForKey: (id) kSecTrustSettingsResult];
+                    CFNumberGetValue(settingsResultNum, kCFNumberSInt32Type, &settingsResult);
                 
-                /* If a root, add to the result set */
-                if (settingsResult == kSecTrustSettingsResultTrustRoot || settingsResult == kSecTrustSettingsResultTrustAsRoot) {
-                    [results addObject: certObj];
-                    break;
+                    /* If a root, add to the result set */
+                    if (settingsResult == kSecTrustSettingsResultTrustRoot || settingsResult == kSecTrustSettingsResultTrustAsRoot) {
+                        [results addObject: certObj];
+                        break;
+                    }
                 }
             }
         }
-    }
+    } else {
+        /* Fetch all certificates in the given domain */
+        err = SecTrustCopyAnchorCertificates(&certs);
+        if (err == noErr) {
+            PLCFAutorelease(certs);
+        } else if (err == errSecTrustNotAvailable) {
+            /* No data */
+            [pool release];
+            return [NSArray array];
+        } else if (err != noErr) {
+            /* Lookup failed */
+            if (outError != NULL)
+                *outError = [[NSError errorWithDomain: NSOSStatusErrorDomain code: err userInfo:nil] retain];
 
-    [results retain];
+            [pool release];
+            [*outError autorelease];
+            return nil;
+        }
+
+        /* All certs are trusted */
+        trusted = (NSArray *) certs;
+    }
+    
+    [trusted retain];
     [pool release];
-    return [results autorelease];
+    return [trusted autorelease];
 }
 
 static int exportCertificates (BOOL userAnchors, NSString *outputFile) {
@@ -231,34 +244,39 @@ static int exportCertificates (BOOL userAnchors, NSString *outputFile) {
         return EXIT_FAILURE;
     }
     
-    for (id certObj in result) {
-        CFErrorRef cferror = NULL;
-        CFStringRef subject;
+    NSEnumerator *resultEnumerator = [result objectEnumerator];
+    id certObj;
+    while ((certObj = [resultEnumerator nextObject]) != nil) {
+        NSError *subjectError = NULL;
+        CFStringRef subject = NULL;
+        BOOL subjectUnsupported = NO;
 
-#if MAC_OS_X_VERSION_MAX_ALLOWED > MAC_OS_X_VERSION_10_6
-        if (SecCertificateCopyShortDescription != NULL) {
-            subject = PLCFAutorelease(SecCertificateCopyShortDescription(NULL, (SecCertificateRef) certObj, &cferror));
-        } else {
+        if (SecCertificateCopyShortDescription != NULL /* 10.7 */) {
+            subject = PLCFAutorelease(SecCertificateCopyShortDescription(NULL, (SecCertificateRef) certObj, (CFErrorRef *) &subjectError));
+            
+        } else if (SecCertificateCopySubjectSummary != NULL /* 10.6 */) {
             subject = PLCFAutorelease(SecCertificateCopySubjectSummary((SecCertificateRef) certObj));
+            
+        } else if (SecCertificateCopyCommonName != NULL /* 10.5 */) {
+            if ((err = SecCertificateCopyCommonName((SecCertificateRef) certObj, &subject)) == errSecSuccess && subject != NULL) {
+                PLCFAutorelease(subject);
+            } else {
+                /* In the case that the CN is simply unavailable, provide a more useful error code */
+                if (err == errSecSuccess)
+                    err = errSecNoSuchAttr;
+
+                NSDictionary *userInfo = [NSDictionary dictionaryWithObjectsAndKeys: @"SecCertificateCopyCommonName() failed", NSLocalizedDescriptionKey, nil];
+                subjectError = [NSError errorWithDomain: NSOSStatusErrorDomain code: err userInfo: userInfo];
+                subject = NULL;
+            }
+        } else /* <= 10.4 */ {
+            subjectUnsupported = YES;
         }
-#elif MAC_OS_X_VERSION_MAX_ALLOWED == MAC_OS_X_VERSION_10_6
-        subject = PLCFAutorelease(SecCertificateCopySubjectSummary((SecCertificateRef) certObj));
-#elif MAC_OS_X_VERSION_MAX_ALLOWED <= MAC_OS_X_VERSION_10_5
-        if ((err = SecCertificateCopyCommonName((SecCertificateRef) certObj, &subject)) == errSecSuccess && subject != NULL) {
-            PLCFAutorelease(subject);
-        } else {
-            /* In the case that the CN is simply unavailable, provide a more useful error code */
-            if (err == errSecSuccess)
-                err = errSecNoSuchAttr;
-    
-            NSDictionary *userInfo = [NSDictionary dictionaryWithObjectsAndKeys: @"SecCertificateCopyCommonName() failed", NSLocalizedDescriptionKey, nil];
-            cferror = (CFErrorRef) [NSError errorWithDomain: NSOSStatusErrorDomain code: err userInfo: userInfo];
-            subject = NULL;
-        }
-#endif
 
         if (subject == NULL) {
-            nsfprintf(stderr, @"Failed to extract certificate description: %@\n", cferror);
+            /* Don't print an error if fetching the subject is unsupported on the platform (eg, <= 10.4) */
+            if (!subjectUnsupported)
+                nsfprintf(stderr, @"Failed to extract certificate description: %@\n", subjectError);
         } else {
             nsfprintf(stderr, @"Found %@\n", subject);
         }
@@ -271,15 +289,11 @@ static int exportCertificates (BOOL userAnchors, NSString *outputFile) {
     
     /* Prefer the non-deprecated SecItemExport on Mac OS X >= 10.7. We use an ifdef to keep the code buildable with earlier SDKs, too. */
     nsfprintf(stderr, @"Exporting certificates from the keychain\n");
-#if MAC_OS_X_VERSION_MAX_ALLOWED > MAC_OS_X_VERSION_10_6
     if (SecItemExport != NULL) {
         err = SecItemExport((CFArrayRef) anchors, kSecFormatPEMSequence, kSecItemPemArmour, NULL, &pemData);
     } else {
         err = SecKeychainItemExport((CFArrayRef) anchors, kSecFormatPEMSequence, kSecItemPemArmour, NULL, &pemData);
     }
-#else
-    err = SecKeychainItemExport((CFArrayRef) anchors, kSecFormatPEMSequence, kSecItemPemArmour, NULL, &pemData);
-#endif
     PLCFAutorelease(pemData);
 
     if (err != errSecSuccess) {
