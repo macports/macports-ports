a77acd793d3987afbe1685c2074fdfef34771892
Rainer Müller <raimue@macports.org>
2014-01-28 23:44:52 +0000

    port1.0/group: New port group for the waf build system, see also https://trac.macports.org/ticket/42303
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@116567 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ _resources/port1.0/group/waf-1.0.tcl
@@ -0,0 +1,49 @@
+# et:ts=4
+# waf-1.0.tcl
+#
+# $Id$
+#
+# Copyright (c) 2014 The MacPorts Project
+# Copyright (c) 2014 Rainer Mueller <raimue@macports.org>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. Neither the name of Apple Computer, Inc. nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+options waf.python
+default waf.python ${prefix}/bin/python2.7
+
+depends_build-append    port:python27
+
+configure.cmd           ${waf.python} ./waf configure
+configure.args          --prefix=${prefix} --nocache
+
+configure.universal_args-delete --disable-dependency-tracking
+
+build.cmd               ${waf.python} ./waf
+build.target            build
+
+destroot.destdir        --destdir=${destroot}
