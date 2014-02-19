6bc6aba41c32ed41d3912535de8006c2f9b50254
David B. Evans <devans@macports.org>
2014-02-19 17:08:33 +0000

    upower: reconfigure with our intltool.m4 using autogen.sh from upstream git, build deps.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@117216 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ devel/upower/files/autogen.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+# Run this to generate all the initial makefiles, etc.
+
+test -n "$srcdir" || srcdir=`dirname "$0"`
+test -n "$srcdir" || srcdir=.
+
+olddir=`pwd`
+cd $srcdir
+
+AUTORECONF=`which autoreconf`
+if test -z $AUTORECONF; then
+        echo "*** No autoreconf found, please intall it ***"
+        exit 1
+fi
+
+INTLTOOLIZE=`which intltoolize`
+if test -z $INTLTOOLIZE; then
+        echo "*** No intltoolize found, please install the intltool package ***"
+        exit 1
+fi
+
+GTKDOCIZE=`which gtkdocize`
+if test -z $GTKDOCIZE; then
+        echo "*** No GTK-Doc found, please install it ***"
+        exit 1
+fi
+
+gtkdocize
+autopoint --force
+AUTOPOINT='intltoolize --automake --copy' autoreconf --force --install --verbose
+
+cd $olddir
+test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
+
