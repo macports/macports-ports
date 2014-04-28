05caef227596beeda17a579cce408c920d497325
David B. Evans <devans@macports.org>
2014-04-28 17:58:04 +0000

    gnome-online-accounts: update to version 3.12.1 for darwin >= 13, version 3.8.5 for darwin < 13 due to webkit-gtk3 version limitations.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@119515 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ gnome/gnome-online-accounts/files/autogen.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# Run this to generate all the initial makefiles, etc.
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+PKG_NAME="goa"
+
+(test -f $srcdir/src/Makefile.am) || {
+    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
+    echo " top-level $PKG_NAME directory"
+    exit 1
+}
+
+which gnome-autogen.sh || {
+    echo "You need to install gnome-common"
+    exit 1
+}
+
+cd telepathy-account-widgets
+sh autogen.sh --no-configure
+cd ..
+
+. gnome-autogen.sh --enable-gtk-doc "$@"
