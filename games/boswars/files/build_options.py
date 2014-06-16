993a4130d7b97417c548815e35464ec0d2226298
David B. Evans <devans@macports.org>
2014-06-16 21:08:37 +0000

    boswars: update to version 2.7, depends on lua51, install fixed images for libpng16 (https://trac.macports.org/ticket/42197).
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@121069 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ games/boswars/files/build_options.py
@@ -0,0 +1,10 @@
+# put lua51 include path before macports in case lua 5.2 is installed
+CPPPATH = ['__PREFIX__/include/lua-5.1', '__PREFIX__/include']
+CPPDEFINES = []
+LIBPATH = ['__PREFIX__/lib']
+CCFLAGS = ['-fsigned-char']
+LINKFLAGS = ['$__RPATH']
+CC = ['__CC__']
+CXX = ['__CXX__']
+MINGWCPPPATH = []
+MINGWLIBPATH = []
