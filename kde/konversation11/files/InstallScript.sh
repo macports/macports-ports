38af361829f4604ac8eab813949cfd02428d6ebc
Nicolas Pavillon <nicos@macports.org>
2014-09-26 15:23:13 +0000

    konversation11: making port obsolete
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@125794 d073be05-634f-4543-b044-5fe20cf6d1d6

--- kde/konversation11/files/InstallScript.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -ev
-
-	export PREFIX="%p" USE_UNSERMAKE=1
-	. ./environment-helper.sh
-
-	./build-helper.sh install %N %v %r unsermake -p -j1 install DESTDIR=%d LN_S='ln -sf'
-
-	mkdir -p %i/share/doc/installed-packages
-	touch %i/share/doc/installed-packages/%N
