26a8992a1c46666fc0a150ec4070989b7e3c3913
Nicolas Pavillon <nicos@macports.org>
2014-10-04 02:15:23 +0000

    kde3 ports suite: make obsolete
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@126114 d073be05-634f-4543-b044-5fe20cf6d1d6

--- kde/kdenetwork3/files/CompileScript.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -ev
-
-	export PREFIX="%p" USE_UNSERMAKE=1 CC=gcc DO_NOT_COMPILE="ktalkd"
-	. ./environment-helper.sh
-
-	./build-helper.sh cvs       %N %v %r make -f admin/Makefile.common cvs
-	./build-helper.sh configure %N %v %r ./configure %c $CONFIGURE_PARAMS
-	./build-helper.sh make      %N %v %r unsermake $UNSERMAKEFLAGS
