0accd01493ee73505d87a523acd8006dc7e872e9
Joshua Root <jmr@macports.org>
2014-10-09 19:39:54 +0000

    ttf2pt1: fix inclusion of freetype headers (https://trac.macports.org/ticket/45311)
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@126416 d073be05-634f-4543-b044-5fe20cf6d1d6

--- print/ttf2pt1/files/patch-ft.c
+++ print/ttf2pt1/files/patch-ft.c
@@ -1,10 +1,20 @@
---- ft.c	2004-01-01 06:30:50.000000000 +0900
-+++ ft.c	2005-08-17 12:04:06.000000000 +0900
-@@ -12,6 +12,7 @@
+--- ft.c.orig	2004-01-01 08:30:50.000000000 +1100
++++ ft.c	2014-10-10 06:38:57.000000000 +1100
+@@ -12,11 +12,12 @@
  #include <stdlib.h>
  #include <ctype.h>
  #include <sys/types.h>
+-#include <freetype/freetype.h>
+-#include <freetype/ftglyph.h>
+-#include <freetype/ftsnames.h>
+-#include <freetype/ttnameid.h>
+-#include <freetype/ftoutln.h>
 +#include <ft2build.h>
- #include <freetype/freetype.h>
- #include <freetype/ftglyph.h>
- #include <freetype/ftsnames.h>
++#include FT_FREETYPE_H
++#include FT_GLYPH_H
++#include FT_SFNT_NAMES_H
++#include FT_TRUETYPE_IDS_H
++#include FT_OUTLINE_H
+ #include "pt1.h"
+ #include "global.h"
+ 
