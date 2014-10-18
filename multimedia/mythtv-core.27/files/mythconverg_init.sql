6dab0b5a090084974d8b299f741508159e37362c
Craig Treleaven <ctreleaven@macports.org>
2014-10-18 17:47:36 +0000

    mythtv-core.27: update to Fixes-20141017, add config files re MariaDB
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@126985 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ multimedia/mythtv-core.27/files/mythconverg_init.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS mythconverg;
+GRANT ALL ON mythconverg.* TO mythtv@localhost IDENTIFIED BY "mythtv";
+FLUSH PRIVILEGES;
+grant all on mythconverg.* to mythtv@"%" identified by "mythtv";
+FLUSH PRIVILEGES;
+GRANT CREATE TEMPORARY TABLES ON mythconverg.* TO mythtv@localhost IDENTIFIED BY "mythtv";
+FLUSH PRIVILEGES;
+GRANT CREATE TEMPORARY TABLES ON mythconverg.* TO mythtv@"&" IDENTIFIED BY "mythtv";
+FLUSH PRIVILEGES;
+ALTER DATABASE mythconverg DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+FLUSH PRIVILEGES;
