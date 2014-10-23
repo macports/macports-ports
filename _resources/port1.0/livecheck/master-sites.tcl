49361b797edf936253f004654a42fea3113ef504
Kurt Hindenburg <khindenburg@macports.org>
2014-10-23 04:02:43 +0000

    livecheck: set type to "none" when $master_sites is not set or empty https://trac.macports.org/ticket/45572
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@127212 d073be05-634f-4543-b044-5fe20cf6d1d6

--- _resources/port1.0/livecheck/master-sites.tcl
+++ _resources/port1.0/livecheck/master-sites.tcl
@@ -2,6 +2,8 @@
 #
 # This file contains the livecheck defaults for using port's master_sites.
 
+set livecheck.type "regex"
+
 if {${livecheck.name} eq "default"} {
     set livecheck.name ${name}
 }
@@ -9,9 +11,12 @@ if {${livecheck.distname} eq "default"} {
     set livecheck.distname ${livecheck.name}
 }
 if {!$has_homepage || ${livecheck.url} eq ${homepage}} {
-    set livecheck.url [lindex ${master_sites} 0]
+    if {![info exists master_sites] || [llength ${master_sites}] == 0} {
+        set livecheck.type "none"
+    } else {
+        set livecheck.url [lindex ${master_sites} 0]
+    }
 }
 if {${livecheck.regex} eq ""} {
     set livecheck.regex [list "[quotemeta ${livecheck.distname}]-(\\d+(?:\\.\\d+)*)"]
 }
-set livecheck.type "regex"
