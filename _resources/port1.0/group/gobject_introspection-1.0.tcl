73614158cd17e741705f3d6e721921bb6fc65c5f
David B. Evans <devans@macports.org>
2015-01-19 23:43:32 +0000

    gobject_introspection-1.0.tcl: add support for ports that may rebuild gir during destroot.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@131847 d073be05-634f-4543-b044-5fe20cf6d1d6

--- _resources/port1.0/group/gobject_introspection-1.0.tcl
+++ _resources/port1.0/group/gobject_introspection-1.0.tcl
@@ -79,10 +79,12 @@ pre-build {
         if {[info exists universal_archs_to_use]} {
             global merger_build_args
             foreach arch ${universal_archs_to_use} {
-                lappend merger_build_args(${arch})  CC='${configure.cc} -arch ${arch}'
+                lappend merger_build_args(${arch})     CC='${configure.cc} -arch ${arch}'
+                lappend merger_destroot_args(${arch})  CC='${configure.cc} -arch ${arch}'
             }
         } else {
-            build.args-append   CC="${configure.cc} ${configure.cc_archflags}"
+            build.args-append      CC="${configure.cc} ${configure.cc_archflags}"
+            destroot.args-append   CC="${configure.cc} ${configure.cc_archflags}"
         }
 
         # The rules enabled by gobject-introspection require GNU make 3.81+
