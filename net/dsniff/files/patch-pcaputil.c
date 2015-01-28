0f87f18a016767a3dd08cdafe823a7db7ff6c9e9
Clemens Lang <cal@macports.org>
2015-01-28 23:18:10 +0000

    dsniff: replace with dsniff-devel, mark dsniff-devel as replaced_by dsniff, fix FTBFS, closes https://trac.macports.org/ticket/45466
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@132319 d073be05-634f-4543-b044-5fe20cf6d1d6

--- net/dsniff/files/patch-pcaputil.c
+++ net/dsniff/files/patch-pcaputil.c
@@ -1,6 +1,6 @@
 --- pcaputil.c.old	Thu Mar 25 11:14:26 2004
 +++ pcaputil.c	Thu Mar 25 11:14:33 2004
-@@ -14,6 +14,7 @@
+@@ -17,6 +17,7 @@
  #include <string.h>
  #include <err.h>
  #include <pcap.h>
