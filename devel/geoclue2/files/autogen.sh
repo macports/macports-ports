7b4e11a99c7561a2ee871f3e031fb72e3a5113c4
David B. Evans <devans@macports.org>
2015-03-24 06:32:14 +0000

    geoclue2: reconfigure using autogen.sh for intltool 0.51 compatibility.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@134383 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ devel/geoclue2/files/autogen.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e # exit on errors
+
+REQUIRED_AUTOMAKE_VERSION=1.9
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+mkdir -p m4
+PKG_NAME=GeoClue
+
+gtkdocize
+autoreconf -v --force --install
+intltoolize -f
+
+if [ -z "$NOCONFIGURE" ]; then
+    "$srcdir"/configure --enable-maintainer-mode --enable-debug ${1+"$@"}
+fi
