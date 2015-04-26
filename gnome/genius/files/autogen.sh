380290272dea08e9d7e7c91bd742459e0ae82484
David B. Evans <devans@macports.org>
2015-04-26 14:36:17 +0000

    genius: reconfigure using upstream autogen.sh for intltool 0.51 compatibility.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@135566 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ gnome/genius/files/autogen.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# Run this to generate all the initial makefiles, etc.
+
+REQUIRED_AUTOMAKE_VERSION=1.9
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+PKG_NAME="Genius"
+
+(test -f $srcdir/configure.in \
+  && test -d $srcdir/src \
+  && test -f $srcdir/src/calc.h) || {
+    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
+    echo " top-level Genius directory"
+    exit 1
+}
+
+which gnome-autogen.sh || {
+    echo "Missing gnome-autogen.sh"
+    echo "You need to install gnome-common from the GNOME git,"
+    echo "or possibly the \"gnome-common\" distribution package"
+    exit 1
+}
+USE_GNOME2_MACROS=1 . gnome-autogen.sh
