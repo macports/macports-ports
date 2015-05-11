802ee0667e5275903102930e041e7511921c5217
David B. Evans <devans@macports.org>
2015-05-11 22:36:48 +0000

    evince: update to version 3.16.0, reconfigure using upstream autogen.sh for intltool 0.51 compatibility.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@136152 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ gnome/evince/files/autogen.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# Run this to generate all the initial makefiles, etc.
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+PKG_NAME="evince"
+
+(test -f $srcdir/configure.ac) || {
+    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
+    echo " top-level $PKG_NAME directory"
+    exit 1
+}
+
+which gnome-autogen.sh || {
+    echo "You need to install gnome-common from the GNOME CVS"
+    exit 1
+}
+
+REQUIRED_AUTOMAKE_VERSION=1.10
+REQUIRED_GTK_DOC_VERSION=1.13
+USE_GNOME2_MACROS=1
+. gnome-autogen.sh
