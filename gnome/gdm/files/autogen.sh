46e6119d491fa4a5a362c869f2be0a82de83aae5
David B. Evans <devans@macports.org>
2015-05-11 23:06:18 +0000

    gdm: update to version 3.16.1.1, reconfigure using upstream autogen.sh for intltool 0.51 compatibility.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@136194 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ gnome/gdm/files/autogen.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# Run this to generate all the initial makefiles, etc.
+
+test -n "$srcdir" || srcdir=`dirname "$0"`
+test -n "$srcdir" || srcdir=.
+
+olddir=`pwd`
+cd $srcdir
+
+AUTORECONF=`which autoreconf`
+if test -z $AUTORECONF; then
+        echo "*** No autoreconf found, please intall it ***"
+        exit 1
+fi
+
+INTLTOOLIZE=`which intltoolize`
+if test -z $INTLTOOLIZE; then
+        echo "*** No intltoolize found, please install the intltool package ***"
+        exit 1
+fi
+
+set -e
+
+intltoolize --force --copy --automake 
+autoreconf --force --install --verbose
+
+cd $olddir
+test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
