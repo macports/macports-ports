d3b6a660179c699322cc20fabf225b231b887208
David B. Evans <devans@macports.org>
2015-05-11 23:06:19 +0000

    gnome-system-monitor: update to version 3.16.0, reconfigure using upstream autogen.sh for intltool 0.51 compatibility.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@136195 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ gnome/gnome-system-monitor/files/autogen.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# Run this to generate all the initial makefiles, etc.
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+PKG_NAME="gnome-system-monitor"
+
+(test -f $srcdir/configure.ac) || {
+    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
+    echo " top-level directory"
+    exit 1
+}
+
+which gnome-autogen.sh || {
+    echo "You need to install gnome-common package"
+    exit 1
+}
+
+USE_COMMON_DOC_BUILD=yes
+
+. gnome-autogen.sh
