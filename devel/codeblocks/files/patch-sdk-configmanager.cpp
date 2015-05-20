0d17e6ff656450d8303b9395ae5c52d8ca785076
Mojca Miklavec <mojca@macports.org>
2015-05-20 06:03:24 +0000

    codeblocks: upgrade to the latest version from SVN (patches integrated upstream)
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@136514 d073be05-634f-4543-b044-5fe20cf6d1d6

--- devel/codeblocks/files/patch-sdk-configmanager.cpp
+++ devel/codeblocks/files/patch-sdk-configmanager.cpp
@@ -1,16 +1,18 @@
 written by afb
 
+properly sets the directory where CB looks for plugins
+
 --- src/sdk/configmanager.cpp.orig
 +++ src/sdk/configmanager.cpp
-@@ -1442,8 +1442,10 @@ void ConfigManager::InitPaths()
+@@ -1504,8 +1504,10 @@ void ConfigManager::InitPaths()
  #ifdef CB_AUTOCONF
      if (plugin_path_global.IsEmpty())
      {
--        if(platform::windows || platform::macosx)
-+        if(platform::windows)
+-        if (platform::windows || platform::macosx)
++        if (platform::windows)
              ConfigManager::plugin_path_global = data_path_global;
-+        else if(platform::macosx)
++        else if (platform::macosx)
 +            ConfigManager::plugin_path_global = res_path + _T("/lib/codeblocks/plugins");
          else
          {
-             ConfigManager::plugin_path_global = wxStandardPathsBase::Get().GetPluginsDir() + _T("/plugins");
+             // It seems we can not longer rely on wxStandardPathsBase::Get().GetPluginsDir(),
