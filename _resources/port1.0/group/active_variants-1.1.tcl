b1d07df29f4b5607855aec6cfe129ed1653ce6c5
Ryan Schmidt <ryandesign@macports.org>
2015-10-14 03:51:27 +0000

    active_variants-1.1.tcl: update copyright date
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@141272 d073be05-634f-4543-b044-5fe20cf6d1d6

--- _resources/port1.0/group/active_variants-1.1.tcl
+++ _resources/port1.0/group/active_variants-1.1.tcl
@@ -1,6 +1,6 @@
 # $Id$
 #
-# Copyright (c) 2012-2014 The MacPorts Project
+# Copyright (c) 2012-2015 The MacPorts Project
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
