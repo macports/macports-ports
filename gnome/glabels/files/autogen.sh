389ca3e592f22e096cfa92eaa3d94e213f1e7a1b
David B. Evans <devans@macports.org>
2015-10-16 03:15:16 +0000

    glabels: update to version 3.2.1, dependencies, reconfigure using upstream autogen.sh for intltool 0.51 compatibility, post-activate processing, minor white space (https://trac.macports.org/ticket/49172, maintainer timeout).
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@141356 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ gnome/glabels/files/autogen.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# Run this to generate all the initial makefiles, etc.
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+PKG_NAME="glabels"
+
+(test -f $srcdir/configure.ac \
+  && test -f $srcdir/README \
+  && test -d $srcdir/src) || {
+    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
+    echo " top-level $PKG_NAME directory"
+    exit 1
+}
+
+which gnome-autogen.sh || {
+    echo "You need to install gnome-common from the GNOME CVS"
+    exit 1
+}
+
+USE_GNOME2_MACROS=1
+USE_COMMON_DOC_BUILD=yes
+
+. gnome-autogen.sh
