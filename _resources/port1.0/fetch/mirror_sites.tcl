23495961789f322ae964e28ce5d8d4d8d83b3814
Ryan Schmidt <ryandesign@macports.org>
2015-10-27 04:33:16 +0000

    mirror_sites.tcl: temporarily remove aarnet mirror from gnu group (https://trac.macports.org/ticket/49416)
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@141733 d073be05-634f-4543-b044-5fe20cf6d1d6

--- _resources/port1.0/fetch/mirror_sites.tcl
+++ _resources/port1.0/fetch/mirror_sites.tcl
@@ -205,7 +205,6 @@ set portfetch::mirror_sites::sites(gnu) {
     ftp://ftp.informatik.hu-berlin.de/pub/gnu/gnu/
     ftp://ftp.lip6.fr/pub/gnu/
     http://mirror.internode.on.net/pub/gnu/
-    http://mirror.aarnet.edu.au/pub/gnu/
     ftp://ftp.unicamp.br/pub/gnu/
     ftp://ftp.gnu.org/gnu/
     http://ftp.gnu.org/gnu/
