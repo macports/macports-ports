2df3d94ca74290d6be3401358d1bb6a835218057
Ryan Schmidt <ryandesign@macports.org>
2016-01-24 23:26:35 +0000

    archive_sites.tcl: add jnb.za.packages.macports.org
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@145056 d073be05-634f-4543-b044-5fe20cf6d1d6

--- _resources/port1.0/fetch/archive_sites.tcl
+++ _resources/port1.0/fetch/archive_sites.tcl
@@ -4,6 +4,7 @@ namespace eval portfetch::mirror_sites { }
 
 set portfetch::mirror_sites::sites(macports_archives) {
     http://packages.macports.org/:nosubdir
+    http://jnb.za.packages.macports.org/packages/:nosubdir
     http://lil.fr.packages.macports.org/:nosubdir
     http://nue.de.packages.macports.org/macports/packages/:nosubdir
     http://mse.uk.packages.macports.org/sites/packages.macports.org/:nosubdir
