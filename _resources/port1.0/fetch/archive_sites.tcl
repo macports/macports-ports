54d4fabeaf2a3ccec235951e53bd25080218478a
Ryan Schmidt <ryandesign@macports.org>
2016-02-24 07:34:48 +0000

    archive_sites.tcl: remove sjc.us.packages.macports.org mirror_sites.tcl: remove sjc.us.distfiles.macports.org
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@145998 d073be05-634f-4543-b044-5fe20cf6d1d6

--- _resources/port1.0/fetch/archive_sites.tcl
+++ _resources/port1.0/fetch/archive_sites.tcl
@@ -12,7 +12,6 @@ set portfetch::mirror_sites::sites(macports_archives) {
     http://mse.uk.packages.macports.org/sites/packages.macports.org/:nosubdir
     http://osl.no.packages.macports.org/:nosubdir
     http://sea.us.packages.macports.org/macports/packages/:nosubdir
-    http://sjc.us.packages.macports.org/:nosubdir
     http://jog.id.packages.macports.org/macports/packages/:nosubdir
 }
 
