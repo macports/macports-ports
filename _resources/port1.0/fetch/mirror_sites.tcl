4a5316df7454d387e1d0fb41195f25a8780e3163
Ryan Schmidt <ryandesign@macports.org>
2016-03-14 08:06:40 +0000

    mirror_sites.tcl: fix URL for jog.id.distfiles.macports.org (https://trac.macports.org/ticket/50885)
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@146625 d073be05-634f-4543-b044-5fe20cf6d1d6

--- _resources/port1.0/fetch/mirror_sites.tcl
+++ _resources/port1.0/fetch/mirror_sites.tcl
@@ -281,7 +281,7 @@ set portfetch::mirror_sites::sites(macports_distfiles) {
     http://fco.it.distfiles.macports.org/mirrors/macports-distfiles/:mirror
     http://her.gr.distfiles.macports.org/mirrors/macports/mpdistfiles/:mirror
     http://jnb.za.distfiles.macports.org/distfiles/:mirror
-    http://jog.id.distfiles.macports.org/macports/mpdistfiles/:mirror
+    http://jog.id.distfiles.macports.org/macports/distfiles/:mirror
     http://lil.fr.distfiles.macports.org/:mirror
     http://mse.uk.distfiles.macports.org/sites/distfiles.macports.org/:mirror
     http://nou.nc.distfiles.macports.org/pub/macports/distfiles.macports.org/:mirror
