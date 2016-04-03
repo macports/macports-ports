30494322ef6d14e21853fa06fe38b7cc1c5c3886
David B. Evans <devans@macports.org>
2016-04-03 00:30:55 +0000

    evolution-data-server: update to version 3.20.0, dependencies, update autogen.sh from upstream git master.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@147398 d073be05-634f-4543-b044-5fe20cf6d1d6

--- gnome/evolution-data-server/files/autogen.sh
+++ gnome/evolution-data-server/files/autogen.sh
@@ -4,23 +4,40 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-PKG_NAME="evolution-data-server"
-REQUIRED_AUTOCONF_VERSION=2.58
-REQUIRED_AUTOMAKE_VERSION=1.10
-REQUIRED_LIBTOOL_VERSION=2.2
-REQUIRED_INTLTOOL_VERSION=0.35.5
-
 (test -f $srcdir/configure.ac \
   && test -f $srcdir/ChangeLog \
   && test -d $srcdir/calendar) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
-    echo " top-level $PKG_NAME directory"
+    echo " top-level evolution-data-server directory"
     exit 1
 }
 
-which gnome-autogen.sh || {
-    echo "You need to install gnome-common from the GNOME CVS"
-    exit 1
+olddir=`pwd`
+cd $srcdir
+
+check_exists() {
+	variable=`which $1`
+
+	if test -z $variable; then
+		echo "*** No $1 found, please intall it ***" >&2
+		exit 1
+	fi
 }
 
-USE_GNOME2_MACROS=1 . gnome-autogen.sh
+check_exists aclocal
+check_exists autoreconf
+check_exists gtkdocize
+check_exists intltoolize
+
+m4dir=`autoconf --trace 'AC_CONFIG_MACRO_DIR:$1'`
+if [ -n "$m4dir" ]; then
+	mkdir -p $m4dir
+fi
+
+aclocal -I m4 || exit $?
+gtkdocize --copy || exit $?
+intltoolize --force --copy --automake || exit $?
+autoreconf --verbose --force --install -Wno-portability || exit $?
+
+cd $olddir
+test -n "$NOCONFIGURE" || "$srcdir/configure" --disable-maintainer-mode "$@"
