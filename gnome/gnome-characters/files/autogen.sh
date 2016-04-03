2df298afcb0a67808d4c117dc9ec542016596915
David B. Evans <devans@macports.org>
2016-04-03 06:01:18 +0000

    gnome-characters: update to version 3.20.0, update autogen.sh from upsgtream git master, no longer requires gnome-common to build.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@147413 d073be05-634f-4543-b044-5fe20cf6d1d6

--- gnome/gnome-characters/files/autogen.sh
+++ gnome/gnome-characters/files/autogen.sh
@@ -1,33 +1,30 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
+test -n "$srcdir" || srcdir=`dirname "$0"`
+test -n "$srcdir" || srcdir=.
 
-srcdir=`dirname $0`
-test -z "$srcdir" && srcdir=.
+olddir=`pwd`
 
-ACLOCAL_FLAGS="${ACLOCAL_FLAGS}"
+cd $srcdir
 
-test -f $srcdir/configure.ac || {
-    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
-    echo " top-level gnome-characters directory"
-    exit 1
+(test -f configure.ac) || {
+        echo "*** ERROR: Directory "\`$srcdir\'" does not look like the top-level project directory ***"
+        exit 1
 }
 
-which gnome-autogen.sh || {
-    echo "You need to install gnome-common from GNOME Git (or from"
-    echo "your OS vendor's package manager)."
-    exit 1
-}
+PKG_NAME=`autoconf --trace 'AC_INIT:$1' configure.ac`
 
-# gllib/unictype contains gperf generated source code
-: ${GPERF=gperf}
-"$GPERF" --version 2>&1 >/dev/null || {
-    echo "You need to install gperf."
-    exit 1
-}
+if [ "$#" = 0 -a "x$NOCONFIGURE" = "x" ]; then
+        echo "*** WARNING: I am going to run \`configure' with no arguments." >&2
+        echo "*** If you wish to pass any to it, please specify them on the" >&2
+        echo "*** \`$0\' command line." >&2
+        echo "" >&2
+fi
 
-(cd "$srcdir" ;
-test -d m4 || mkdir m4/ ;
-# git submodule update --init --recursive ;
+aclocal --install || exit 1
+intltoolize --force --copy --automake || exit 1
+autoreconf --verbose --force --install -Wno-portability || exit 1
+
 test -z "$GNULIB_SRCDIR" || \
     "$GNULIB_SRCDIR"/gnulib-tool --import \
 		    --source-base=gllib --m4-base=glm4 --tests-base=gltests \
@@ -45,7 +42,16 @@ test -z "$GNULIB_SRCDIR" || \
 		    uninorm/canonical-decomposition \
 		    unistr/u32-to-u8 \
 		    unitypes \
-		    uniname/uniname ;
-)
-. gnome-autogen.sh
+		    uniname/uniname \
+		    uniwidth/width
 
+cd $olddir
+if [ "$NOCONFIGURE" = "" ]; then
+        $srcdir/configure "$@" || exit 1
+
+        if [ "$1" = "--help" ]; then exit 0 else
+                echo "Now type \`make\' to compile $PKG_NAME" || exit 1
+        fi
+else
+        echo "Skipping configure process."
+fi
