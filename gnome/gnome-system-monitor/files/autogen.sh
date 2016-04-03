7a4981f06a50aa593f9dd9784d755f93c97da6c9
David B. Evans <devans@macports.org>
2016-04-03 17:05:42 +0000

    gnome-system-monitor: update to version 3.20.0, update autogen.sh from upstream git master.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@147437 d073be05-634f-4543-b044-5fe20cf6d1d6

--- gnome/gnome-system-monitor/files/autogen.sh
+++ gnome/gnome-system-monitor/files/autogen.sh
@@ -4,8 +4,6 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-PKG_NAME="gnome-system-monitor"
-
 (test -f $srcdir/configure.ac) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
     echo " top-level directory"
@@ -16,7 +14,5 @@ which gnome-autogen.sh || {
     echo "You need to install gnome-common package"
     exit 1
 }
-
-USE_COMMON_DOC_BUILD=yes
 
 . gnome-autogen.sh
