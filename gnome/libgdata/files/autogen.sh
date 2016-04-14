0b61ed68a15a9310d574cd859c3b737e6a80a322
David B. Evans <devans@macports.org>
2016-04-14 12:08:22 +0000

    libgdata: update autogen.sh from upstream git master, add goa-1.0 to vapi dependencies if goa support is enabled, increment revision (https://trac.macports.org/ticket/51144).
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@147746 d073be05-634f-4543-b044-5fe20cf6d1d6

--- gnome/libgdata/files/autogen.sh
+++ gnome/libgdata/files/autogen.sh
@@ -1,41 +1,39 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
+test -n "$srcdir" || srcdir=$(dirname "$0")
+test -n "$srcdir" || srcdir=.
 
-srcdir=`dirname $0`
-test -z "$srcdir" && srcdir=.
+olddir=$(pwd)
 
-PKG_NAME=libgdata
+cd $srcdir
 
-(test -f $srcdir/configure.ac) || {
-	echo "**Error**: Directory "\`$srcdir\'" does not look like the top-level $PKG_NAME directory"
+(test -f configure.ac) || {
+	echo "*** ERROR: Directory '$srcdir' does not look like the top-level project directory ***"
 	exit 1
 }
 
+# shellcheck disable=SC2016
+PKG_NAME=$(autoconf --trace 'AC_INIT:$1' configure.ac)
+
 if [ "$#" = 0 -a "x$NOCONFIGURE" = "x" ]; then
-	echo "**Warning**: I am going to run \`configure' with no arguments." >&2
-	echo "If you wish to pass any to it, please specify them on the" >&2
-	echo \`$0\'" command line." >&2
+	echo "*** WARNING: I am going to run 'configure' with no arguments." >&2
+	echo "*** If you wish to pass any to it, please specify them on the" >&2
+	echo "*** '$0' command line." >&2
 	echo "" >&2
 fi
 
-# if the AC_CONFIG_MACRO_DIR() macro is used, create that directory
-# This is a automake bug fixed in automake 1.13.2
-# See http://debbugs.gnu.org/cgi/bugreport.cgi?bug=13514
-if [ -n "m4" ]; then
-	mkdir -p m4
-fi
-
-set -x
-
+aclocal --install || exit 1
 gtkdocize --copy || exit 1
 intltoolize --force --copy --automake || exit 1
-autoreconf --verbose --force --install -Wno-portability || exit 1
+autoreconf --verbose --force --install || exit 1
 
-if test x$NOCONFIGURE = x; then
-	$srcdir/configure "$@" && \
-	echo "Now type \`make\' to compile $PKG_NAME" || exit 1
+cd "$olddir"
+if [ "$NOCONFIGURE" = "" ]; then
+	$srcdir/configure "$@" || exit 1
+
+	if [ "$1" = "--help" ]; then exit 0 else
+		echo "Now type 'make' to compile $PKG_NAME" || exit 1
+	fi
 else
 	echo "Skipping configure process."
 fi
-
-set +x
