89e6cd4ccd061470a46d454ca7213f27fd60c437
David B. Evans <devans@macports.org>
2016-08-21 18:39:16 +0000

    goffice: update to version 0.10.32, autogen.sh, no longer requires gnome-common (but goffice08 still does).
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@151751 d073be05-634f-4543-b044-5fe20cf6d1d6

--- gnome/goffice/files/autogen.sh
+++ gnome/goffice/files/autogen.sh
@@ -1,37 +1,12 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
-PKG_NAME="goffice"
+PKG_NAME="Goffice"
 
-REQUIRED_AUTOMAKE_VERSION=1.9.0
-REQUIRED_LIBTOOL_VERSION=1.4.3
+test -n "$srcdir" || srcdir=$(dirname "$0")
+test -n "$srcdir" || srcdir=.
 
-# We need intltool >= 0.27.2 to extract the UTF-8 chars from source code:
-REQUIRED_INTLTOOL_VERSION=0.27.2
-
-# We require Automake 1.7.2, which requires Autoconf 2.54.
-# (It needs _AC_AM_CONFIG_HEADER_HOOK, for example.)
-REQUIRED_AUTOCONF_VERSION=2.54
-
-# This enforces the version for the machine where the tarball is built
-# from CVS.  This requirement might be stronger than the one in configure.in.
-#
-# An example: we might require pkg-config binary >= 0.18, because it
-# implements Libs.private.  But we might require macro PKG_PROG_PKG_CONFIG
-# from pkg-config >= 0.29 because this macro is able to detect old versions
-# of pkg-config conrrectly and reports a sane error message.
-#
-# In other words, the distribution tarball can be created only with 0.29 or
-# later, but it can be then compiled on any host with pkg-config >= 0.18.
-#
-# We would then use REQUIRED_PKG_CONFIG_VERSION=0.29 here and
-# PKG_PROG_PKG_CONFIG([0.18]) in configure.in.
-
-USE_GNOME2_MACROS=1
-USE_COMMON_DOC_BUILD=yes
-
-srcdir=`dirname $0`
-test -z "$srcdir" && srcdir=.
+olddir=$(pwd)
 
 (test -f $srcdir/configure.ac	\
   && test -d $srcdir/goffice	\
@@ -41,32 +16,20 @@ test -z "$srcdir" && srcdir=.
     exit 1
 }
 
-ifs_save="$IFS"; IFS=":"
-for dir in $PATH ; do
-  IFS="$ifs_save"
-  test -z "$dir" && dir=.
-  if test -f "$dir/gnome-autogen.sh" ; then
-    gnome_autogen="$dir/gnome-autogen.sh"
-    gnome_datadir=`echo $dir | sed -e 's,/bin$,/share,'`
-    break
-  fi
-done
+cd $srcdir
+aclocal --install || exit 1
+glib-gettextize --force --copy || exit 1
+gtkdocize --copy || exit 1
+intltoolize --force --copy --automake || exit 1
+autoreconf --verbose --force --install || exit 1
+cd $olddir
 
-if test -z "$gnome_autogen" ; then
-  echo "You need to install the gnome-common module and make" 1>&2
-  echo "sure the gnome-autogen.sh script is in your \$PATH." 1>&2
-  exit 1
-fi
+if [ "$NOCONFIGURE" = "" ]; then
+        $srcdir/configure "$@" || exit 1
 
-GNOME_DATADIR="$gnome_datadir"
-
-. $gnome_autogen
-
-if grep 'which gtkdoc-rebase >/dev/null &&' $srcdir/gtk-doc.make >/dev/null 2>&1 &&	\
-   grep '[^-]installfiles=`echo $(srcdir)/html/*`;' $srcdir/gtk-doc.make >/dev/null 2>&1; then
-    echo '----------------------------------------------------' 1>&2
-    echo "Your gtk-doc has a dependency problem.  Upgrade." 1>&2
-    echo "See http://bugzilla.gnome.org/show_bug.cgi?id=506506" 1>&2
-    echo '----------------------------------------------------' 1>&2
-    exit 1
+        if [ "$1" = "--help" ]; then exit 0 else
+                echo "Now type 'make' to compile $PKG_NAME" || exit 1
+        fi
+else
+        echo "Skipping configure process."
 fi
