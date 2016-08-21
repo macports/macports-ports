0fe858a2b91035de0c7bae90915267bbab0d2ac7
David B. Evans <devans@macports.org>
2016-08-21 22:23:39 +0000

    libgsf: update to version 1.14.40, no longer uses gnome-common.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@151774 d073be05-634f-4543-b044-5fe20cf6d1d6

--- gnome/libgsf/files/autogen.sh
+++ gnome/libgsf/files/autogen.sh
@@ -1,19 +1,12 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
-PKG_NAME="libgsf"
+PKG_NAME="Libgsf"
 
-REQUIRED_AUTOMAKE_VERSION=1.8
-# We require Automake 1.7.1, which requires Autoconf 2.54.
-REQUIRED_AUTOCONF_VERSION=2.54
+test -n "$srcdir" || srcdir=$(dirname "$0")
+test -n "$srcdir" || srcdir=.
 
-USE_GNOME2_MACROS=1
-
-srcdir=`dirname $0`
-test -z "$srcdir" && srcdir=.
-
-abs_srcdir=`cd $srcdir && pwd`
-ACLOCAL_FLAGS="-I $abs_srcdir/m4 $ACLOCAL_FLAGS"
+olddir=$(pwd)
 
 (test -f $srcdir/configure.ac \
   && test -d $srcdir/gsf \
@@ -23,22 +16,20 @@ ACLOCAL_FLAGS="-I $abs_srcdir/m4 $ACLOCAL_FLAGS"
     exit 1
 }
 
-ifs_save="$IFS"; IFS=":"
-for dir in $PATH ; do
-  IFS="$ifs_save"
-  test -z "$dir" && dir=.
-  if test -f "$dir/gnome-autogen.sh" ; then
-    gnome_autogen="$dir/gnome-autogen.sh"
-    gnome_datadir=`echo $dir | sed -e 's,/bin$,/share,'`
-    break
-  fi
-done
+cd $srcdir
+aclocal --install || exit 1
+glib-gettextize --force --copy || exit 1
+gtkdocize --copy || exit 1
+intltoolize --force --copy --automake || exit 1
+autoreconf --verbose --force --install || exit 1
+cd $olddir
 
-if test -z "$gnome_autogen" ; then
-  echo "You need to install the gnome-common module and make"
-  echo "sure the gnome-autogen.sh script is in your \$PATH."
-  exit 1
+if [ "$NOCONFIGURE" = "" ]; then
+        $srcdir/configure "$@" || exit 1
+
+        if [ "$1" = "--help" ]; then exit 0 else
+                echo "Now type 'make' to compile $PKG_NAME" || exit 1
+        fi
+else
+        echo "Skipping configure process."
 fi
-
-GNOME_DATADIR="$gnome_datadir"
-. $gnome_autogen
