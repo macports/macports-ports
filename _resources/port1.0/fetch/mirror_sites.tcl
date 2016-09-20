e450eb0a989e58400f57e45225843952af3e9176
Lawrence Velázquez <larryv@macports.org>
2016-09-20 05:44:52 +0000

    Alphabetize GNU mirrors by domain
    git-svn-id: https://svn.macports.org/repository/macports/trunk/dports@152874 d073be05-634f-4543-b044-5fe20cf6d1d6

--- _resources/port1.0/fetch/mirror_sites.tcl
+++ _resources/port1.0/fetch/mirror_sites.tcl
@@ -238,19 +238,19 @@ set portfetch::mirror_sites::sites(gnome) {
 }
 
 set portfetch::mirror_sites::sites(gnu) {
-    http://mirrors.ibiblio.org/gnu/ftp/gnu/
-    http://www.mirrorservice.org/sites/ftp.gnu.org/gnu/
     http://mirror.facebook.net/gnu/
     ftp://ftp.funet.fi/pub/gnu/prep/
+    http://ftp.gnu.org/gnu/
+    ftp://ftp.gnu.org/gnu/
+    ftp://ftp.gnu.org/old-gnu/
+    ftp://ftp.informatik.hu-berlin.de/pub/gnu/gnu/
+    http://mirrors.ibiblio.org/gnu/ftp/gnu/
+    http://mirror.internode.on.net/pub/gnu/
     ftp://ftp.kddlabs.co.jp/pub/gnu/gnu/
     ftp://ftp.kddlabs.co.jp/pub/gnu/old-gnu/
-    ftp://ftp.informatik.hu-berlin.de/pub/gnu/gnu/
     ftp://ftp.lip6.fr/pub/gnu/
-    http://mirror.internode.on.net/pub/gnu/
+    http://www.mirrorservice.org/sites/ftp.gnu.org/gnu/
     ftp://ftp.unicamp.br/pub/gnu/
-    ftp://ftp.gnu.org/gnu/
-    http://ftp.gnu.org/gnu/
-    ftp://ftp.gnu.org/old-gnu/
 }
 
 set portfetch::mirror_sites::sites(gnupg) {
