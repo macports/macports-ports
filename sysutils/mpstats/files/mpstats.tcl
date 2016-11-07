ce95fb1aca99b0cc4c4f8957da39f86556d68808
Lawrence Velázquez <larryv@macports.org>
2016-11-07 15:33:31 -0500

    mpstats: Remove $Id$ lines

--- sysutils/mpstats/files/mpstats.tcl
+++ sysutils/mpstats/files/mpstats.tcl
@@ -2,7 +2,6 @@
 # -*- coding: utf-8; mode: tcl; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- vim:fenc=utf-8:ft=tcl:et:sw=4:ts=4:sts=4
 # \
 if type -fp port-tclsh >/dev/null; then exec port-tclsh "$0" "$@"; else exec /usr/bin/tclsh "$0" "$@"; fi
-# $Id$
 #
 # Copyright (c) 2011,2013 The MacPorts Project
 #
