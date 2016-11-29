80da2129e8485ae1f2f71421ef80668a19c89454
Lawrence Velázquez <larryv@macports.org>
2016-11-29 18:28:42 -0500

    cmake-1.1: Remove $Id$ lines

--- _resources/port1.0/group/cmake-1.1.tcl
+++ _resources/port1.0/group/cmake-1.1.tcl
@@ -1,6 +1,4 @@
 # -*- coding: utf-8; mode: tcl; c-basic-offset: 4; indent-tabs-mode: nil; tab-width: 4; truncate-lines: t -*- vim:fenc=utf-8:et:sw=4:ts=4:sts=4
-# $Id$
-# $Id$
 #
 # Copyright (c) 2009 Orville Bennett <illogical1 at gmail.com>
 # Copyright (c) 2010-2015 The MacPorts Project
