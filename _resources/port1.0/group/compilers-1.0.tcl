26b684a2204797fc8a0f5a7c29e50b42adc30c24
David Strubbe <dstrubbe@macports.org>
2017-03-27 14:42:08 -0400

    compilers portgroup: Add clang 4.0.

--- _resources/port1.0/group/compilers-1.0.tcl
+++ _resources/port1.0/group/compilers-1.0.tcl
@@ -134,7 +134,7 @@ foreach v ${gcc_versions} {
     set cdb(gcc$v,f90)      ${prefix}/bin/gfortran-mp-$version
 }
 
-set clang_versions {33 34 35 36 37 38 39}
+set clang_versions {33 34 35 36 37 38 39 40}
 foreach v ${clang_versions} {
     # if the string is more than one character insert a '.' into it: e.g 33 -> 3.3
     set version $v
