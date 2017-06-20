7fd2d4691b2c16471067e9bf9107874201e930c3
Dan R. K. Ports <dports@macports.org>
2017-06-19 23:48:14 -0700

    texlive: switch back from test repo

--- _resources/port1.0/group/texlive-1.0.tcl
+++ _resources/port1.0/group/texlive-1.0.tcl
@@ -163,7 +163,7 @@ proc texlive.texmfport {} {
     supported_archs noarch
     installs_libs   no
 
-    master_sites    http://giraffe.cs.washington.edu/texlive/test/
+    master_sites    http://giraffe.cs.washington.edu/texlive/
     use_xz          yes
 
     global name master_sites distname extract.suffix
