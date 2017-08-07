34c4416d0262bfee4658432f406042a7a93fc248
kimura wataru <kimuraw@macports.org>
2017-08-07 18:29:02 +0900

    ruby/rb-rjab-connection: delete, destroot fails with ruby-1.8.7

--- ruby/rb-rjab-connection/files/patch-rjab_connection.rb
+++
@@ -1,6 +0,0 @@
---- lib/rjab/connection.rb	Tue Jan 22 08:21:53 2002
-+++ ../rjab-connection.jdp/src/lib/rjab/connection.rb	Mon Jan 19 15:20:38 2004
-@@ -1,2 +1,2 @@
--require "rjab/rexml/document"
-+require "rexml/document"
- require "rjab/conn/connection"
