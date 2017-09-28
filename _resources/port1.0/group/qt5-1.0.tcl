ae218d74e16d8ab8a53e5ea0ee9fb39bf3226a2e
Marcus Calhoun-Lopez <marcuscalhounlopez@gmail.com>
2017-09-28 02:56:24 -0700

    qt5 PG: whitespace changes

--- _resources/port1.0/group/qt5-1.0.tcl
+++ _resources/port1.0/group/qt5-1.0.tcl
@@ -618,48 +618,48 @@ pre-destroot {
 }
 
 if {![info exists building_qt5]} {
-pre-configure {
-    set qt_installed_name ""
+    pre-configure {
+        set qt_installed_name ""
 
-    foreach qt_test_name ${available_qt_versions} {
+        foreach qt_test_name ${available_qt_versions} {
 
-        if { [string range ${qt_test_name} end-3 end] eq "-kde" } {
-            set qt_test_port_name ${qt_test_name}
+            if { [string range ${qt_test_name} end-3 end] eq "-kde" } {
+                set qt_test_port_name ${qt_test_name}
+            } else {
+                set qt_test_port_name ${qt_test_name}-qtbase
+            }
+
+            if {![catch {set installed [lindex [registry_active ${qt_test_port_name}] 0]}]} {
+                set qt_installed_name ${qt_test_name}
+            }
+        }
+
+        if { ${qt_installed_name} eq "" } {
+            ui_error "at least one Qt must be installed"
+            return -code error "insufficient dependencies"
+        }
+
+        ui_debug "qt5 PortGroup: Qt is provided by ${qt_installed_name}"
+
+        if { [variant_exists qt5kde] && [variant_isset qt5kde] } {
+            if { [string range ${qt_installed_name} end-3 end] ne "-kde" } {
+                ui_error "qt5 PortGroup: Qt is installed but not qt5-kde, as is required by this variant"
+                ui_error "qt5 PortGroup: please run `sudo port uninstall --follow-dependents ${qt_installed_name}-qtbase and try again"
+                return -code error "improper Qt installed"
+            }
         } else {
-            set qt_test_port_name ${qt_test_name}-qtbase
-        }
-
-        if {![catch {set installed [lindex [registry_active ${qt_test_port_name}] 0]}]} {
-            set qt_installed_name ${qt_test_name}
-        }
-    }
-
-    if { ${qt_installed_name} eq "" } {
-        ui_error "at least one Qt must be installed"
-        return -code error "insufficient dependencies"
-    }
-
-    ui_debug "qt5 PortGroup: Qt is provided by ${qt_installed_name}"
-
-    if { [variant_exists qt5kde] && [variant_isset qt5kde] } {
-        if { [string range ${qt_installed_name} end-3 end] ne "-kde" } {
-            ui_error "qt5 PortGroup: Qt is installed but not qt5-kde, as is required by this variant"
-            ui_error "qt5 PortGroup: please run `sudo port uninstall --follow-dependents ${qt_installed_name}-qtbase and try again"
-            return -code error "improper Qt installed"
-        }
-    } else {
-        if { ${qt_installed_name} ne [qt5.get_default_name] } {
-            # see https://wiki.qt.io/Qt-Version-Compatibility
-            ui_warn "qt5 PortGroup: default Qt for this platform is [qt5.get_default_name] but ${qt_installed_name} is installed"
-        }
-        if { ${qt_installed_name} ne "qt5" } {
-            ui_warn "Qt dependency is not the latest version but may be the latest supported on your OS"
-        }
-        if { ${os.major} < 11 } {
-            ui_warn "Qt dependency is not supported on this platform and may not build"
+            if { ${qt_installed_name} ne [qt5.get_default_name] } {
+                # see https://wiki.qt.io/Qt-Version-Compatibility
+                ui_warn "qt5 PortGroup: default Qt for this platform is [qt5.get_default_name] but ${qt_installed_name} is installed"
+            }
+            if { ${qt_installed_name} ne "qt5" } {
+                ui_warn "Qt dependency is not the latest version but may be the latest supported on your OS"
+            }
+            if { ${os.major} < 11 } {
+                ui_warn "Qt dependency is not supported on this platform and may not build"
+            }
         }
     }
-}
 }
 
 # add qt5kde variant if one does not exist and one is requested via qt5.using_kde
@@ -748,5 +748,5 @@ namespace eval qt5pg {
 }
 
 if {![info exists building_qt5]} {
-port::register_callback qt5pg::register_dependents
+    port::register_callback qt5pg::register_dependents
 }
