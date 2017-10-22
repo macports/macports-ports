2728cab4a1ce13cf6dacd0b1831bba66dc6815ed
Marcus Calhoun-Lopez <mcalhoun@macports.org>
2017-10-22 08:42:09 -0700

    languages PG: dragonegg cannot build universal

--- _resources/port1.0/group/languages-1.0.tcl
+++ _resources/port1.0/group/languages-1.0.tcl
@@ -373,7 +373,11 @@ proc universal_setup {args} {
         ui_debug "OS doesn't support universal builds, so not adding the default universal variant"
     } elseif {[llength [option supported_archs]] == 1} {
         ui_debug "only one arch supported, so not adding the default universal variant"
-    } elseif {[regexp {^macports-gcc-(\d+(?:\.\d+)?)?$} [option configure.compiler] -> gcc_version]} {
+    } elseif {
+              [regexp {^macports-gcc-(\d+(?:\.\d+)?)?$} [option configure.compiler] -> gcc_version]
+              ||
+              [regexp {^macports-dragonegg-(\d+\.\d+)(?:-gcc-(\d+\.\d+))?$} [option configure.compiler] -> llvm_version gcc_version]
+          } {
         ui_debug "Compiler doesn't support universal builds, so not adding the default universal variant"
     } else {
         ui_debug "adding the default universal variant"
