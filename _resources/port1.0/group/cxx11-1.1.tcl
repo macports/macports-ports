0778c62a87e7fd61f0b627c8d5038b75e5987371
Ryan Schmidt <ryandesign@macports.org>
2017-11-30 14:27:10 -0600

    Fix typo: compatable => compatible

--- _resources/port1.0/group/cxx11-1.1.tcl
+++ _resources/port1.0/group/cxx11-1.1.tcl
@@ -75,7 +75,7 @@ if {${cxx_stdlib} eq "libstdc++" } {
 
     if { ${os.major} < 13 } {
         # prior to OS X Mavericks, libstdc++ was the default C++ runtime, so
-        #    assume MacPorts libstdc++ must be ABI compatable with system libstdc++
+        #    assume MacPorts libstdc++ must be ABI compatible with system libstdc++
         # for OS X Maverick and above, users must select libstdc++, so
         #    assume they want default ABI compatibility
         # see https://gcc.gnu.org/onlinedocs/gcc-5.2.0/libstdc++/manual/manual/using_dual_abi.html
