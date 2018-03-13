9ebf8f61b0a26cb0e1559df1710a50b793462f68
Rainer Müller <raimue@macports.org>
2018-03-13 13:04:38 +0100

    github portgroup: Remove docs moved to the guide
    Remove the documentation from the file to avoid duplication as it was
    moved to the guide.

--- _resources/port1.0/group/github-1.0.tcl
+++ _resources/port1.0/group/github-1.0.tcl
@@ -31,81 +31,11 @@
 #
 # This PortGroup accommodates projects hosted at GitHub.
 #
-# Usage:
-#
-# After the PortSystem 1.0 line, put:
-#
-#   PortGroup           github 1.0
-#
-# Then, replace the name and version lines with:
-#
-#   github.setup        author project version [tag_prefix]
-#
-# The port's name will be set to the GitHub project name. If that's not correct,
-# override it by setting the port name as usual, for example:
-#
-#   github.setup        someone someproject 1.2.3
-#   name                myportname
-#
-# The tag_prefix is optional, and refers to any characters that may appear
-# before the actual version number when you view the list of tags for this
-# project on GitHub. For example, if the tags are named like "v1.2.3" then
-# tag_prefix should be specified as "v":
-#
-#   github.setup        someone someproject 1.2.3 v
-#
-# Some projects use GitHub's "releases" service to offer distfiles:
+# Documentation:
+# https://guide.macports.org/#reference.portgroup.github
 # 
-# https://github.com/blog/1547-release-your-software
-#
-# If the project offers a "release", that's probably the best distfile to use
-# for the port. To do this, use:
-#
-#   github.tarball_from releases
-#
-# GitHub imposes no naming convention for "release" distfiles, so you may need
-# to set distname as you would for other ports.
-#
-# Older projects use the discontinued "downloads" service. New "downloads" can
-# no longer be created, but old ones are still available:
-#
-# https://github.com/blog/1302-goodbye-uploads
-#
-# If the project doesn't have "releases" but does have "downloads", use them
-# this way (and set distname if needed):
-#
-#   github.tarball_from downloads
-#
-# If neither "releases" nor "downloads" are available, GitHub can automatically
-# generate a distfile from a git tag or commit. This is the GitHub portgroup's
-# default behavior; to use this, simply don't set github.tarball_from. The
-# distname is irrelevant when fetching from a tag or commit, so don't set it
-# either.
-#
-# If the project's developers do not tag their releases, encourage them to do
-# so. Until they do, or if you need to use a development version that's not
-# tagged, you can use a git commit hash and set the version field. If the
-# project does not assign version numbers (or for development versions) you can
-# invent one, typically in the YYYYMMDD format corresponding to the date of the
-# commit you picked. For example, if you want to use a commit with the hash
-# 0ff25277c3842598d919cd3c73d60768 that was committed on April 1, 2014, then
-# you would use:
-#
-#   github.setup        someone someproject 0ff25277c3842598d919cd3c73d60768
-#   version             20140401
-#
-# Some projects' tag- or commit-based distfiles will not contain all the
-# necessary files, if the project uses git submodules. If available, use a
-# distfile from "releases" or "downloads" instead, as described above. If the
-# project does not provide those, encourage the project's developers to provide
-# releases. Until they do, fetch from git instead of from a distfile, and add a
-# post-fetch block to fetch the submodules:
-#
-#   fetch.type          git
-#
-#   post-fetch {
-#       system -W ${worksrcpath} "git submodule update --init"
-#   }
+# Documentation (sources):
+# https://github.com/macports/macports-guide/blob/master/guide/xml/portgroup-github.xml
 
 options github.author github.project github.version github.tag_prefix
 options github.homepage github.raw github.master_sites github.tarball_from
