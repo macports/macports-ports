1f42e7286ed736cd200fdb991220fbd4ff2fcbca
Christopher Chavez <chrischavez@gmx.us>
2018-06-18 13:40:58 -0500

    use https for some gnu.org, nongnu.org urls
    leave links in */*/files/* (patches etc.) alone
    
    known working https gnu.org subdomains:
    download.savannah, ftp, gcc, lists, savannah, www
    
    known working https nongnu.org subdomains:
    download.savannah, savannah, www

--- _resources/port1.0/livecheck/gnu.tcl
+++ _resources/port1.0/livecheck/gnu.tcl
@@ -1,14 +1,14 @@
 # This file contains the defaults for gnu.
 
 if {${livecheck.name} eq "default"} {
-    if {$has_homepage && [regexp {^http://www.gnu.org/software/([^/]+)} $homepage _ tag]} {
+    if {$has_homepage && [regexp {^https?://www\.gnu\.org/software/([^/]+)} $homepage _ tag]} {
         set livecheck.name $tag
     } else {
         set livecheck.name ${name}
     }
 }
 if {!$has_homepage || ${livecheck.url} eq ${homepage}} {
-    set livecheck.url "http://ftp.gnu.org/gnu/${livecheck.name}/?C=M&O=D"
+    set livecheck.url "https://ftp.gnu.org/gnu/${livecheck.name}/?C=M&O=D"
 }
 if {${livecheck.distname} eq "default"} {
     set livecheck.distname ${livecheck.name}
