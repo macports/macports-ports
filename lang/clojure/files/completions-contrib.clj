47047c8214469c0954737b174a8253f93331337f
alvinfrancis <alvin.francis.dumalus@gmail.com>
2018-07-26 17:30:17 +0800

    clojure: Update to version 1.9.0
    * Updates to version 1.9.0 which includes CLI tools
    * Mirrors the clojure tools linux installer
    * Drops unnecessary files in favor of clojure CLI
    * Drop rlwrap dependency -- warning included in clj script
    
    Closes: https://trac.macports.org/ticket/55848

--- lang/clojure/files/completions-contrib.clj
+++
@@ -1,6 +0,0 @@
-(def completions
-    (reduce concat (map (fn [p] (keys (ns-publics (find-ns p))))
-                        '(clojure.core clojure.set clojure.xml clojure.zip))))
- 
-(with-open [f (java.io.BufferedWriter. (java.io.FileWriter. ".clj_completions"))]
-    (.write f (apply str (interleave completions (repeat "\n")))))
