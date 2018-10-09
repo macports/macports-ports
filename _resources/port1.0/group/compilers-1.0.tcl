7915c46e3db431d8862e961ebf92a0af334d3fc3
Marcus Calhoun-Lopez <mcalhoun@macports.org>
2018-10-09 13:19:26 -0700

    compilers PG: add support for Clang 7.0

--- _resources/port1.0/group/compilers-1.0.tcl
+++ _resources/port1.0/group/compilers-1.0.tcl
@@ -116,7 +116,7 @@ foreach v ${gcc_versions} {
     set cdb(gcc$v,f90)      ${prefix}/bin/gfortran-mp-$version
 }
 
-set clang_versions {33 34 37 38 39 40 50 60}
+set clang_versions {33 34 37 38 39 40 50 60 70}
 foreach v ${clang_versions} {
     # if the string is more than one character insert a '.' into it: e.g 33 -> 3.3
     set version $v
