3979ad28f9f0c9203982c930b8b611c41724dfb3
Ryan Schmidt <ryandesign@macports.org>
2018-10-11 06:26:54 -0500

    nawk: Update to 20180827

--- lang/nawk/files/patch-run.c
+++ lang/nawk/files/patch-run.c
@@ -1,7 +1,7 @@
---- run.c.orig	Tue Dec 28 23:53:45 2004
-+++ run.c	Tue Dec 28 23:53:57 2004
-@@ -903,7 +903,7 @@
- 			break;
+--- run.c.orig	2018-08-28 15:05:48.000000000 -0500
++++ run.c	2018-10-11 06:24:36.000000000 -0500
+@@ -930,7 +930,7 @@
+ 		case 'A':
  		case 'f':	sprintf(p, fmt, getfval(x)); break;
  		case 'd':	sprintf(p, fmt, (long) getfval(x)); break;
 -		case 'u':	sprintf(p, fmt, (int) getfval(x)); break;
