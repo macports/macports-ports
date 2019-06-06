2d0b319ba48e536090533facbc655d41d16e652f
Christopher Chavez <chrischavez@gmx.us>
2019-06-06 10:14:23 -0500

    irctree, xserve-raid-tools: delete ports
    Ports depended on netcat, which was obsoleted in de0704d57c
    over 2 years ago and removed in 0ce1dc2c96.
    
    Closes: https://trac.macports.org/ticket/58477

--- irc/irctree/files/patch-irctree-parse.pl
+++
@@ -1,8 +0,0 @@
---- irctree-parse.pl.orig	Sun Oct 17 01:52:53 2004
-+++ irctree-parse.pl	Sun Oct 17 01:53:17 2004
-@@ -1,4 +1,4 @@
--#!/usr/bin/perl -w
-+#!/usr/bin/env perl
- # $Id: irctree-parse.pl,v 1.1 2002/12/23 15:25:58 marvin Exp $
- 
- $depth = 0;
