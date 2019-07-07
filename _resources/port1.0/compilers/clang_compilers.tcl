8470ccfcdae5c31638c4c5bc2b24ee05e4a2a95b
Jeremy Huddleston Sequoia <jeremyhu@macports.org>
2019-07-07 15:31:50 -0700

    llvm-3.9: Obsolete port
    Signed-off-by: Jeremy Huddleston Sequoia <jeremyhu@macports.org>

--- _resources/port1.0/compilers/clang_compilers.tcl
+++ _resources/port1.0/compilers/clang_compilers.tcl
@@ -10,16 +10,12 @@ lappend compilers macports-clang-5.0
 if {${os.major} < 18} {
     # see https://github.com/macports/macports-ports/commit/d387f4e4a47b298b1775ea8bf61772e2c2e6cd8b
     lappend compilers macports-clang-4.0
-    if {${os.major} < 17} {
-	# The High Sierra SDK requires a toolchain that can apply nullability to uuid_t
-	lappend compilers macports-clang-3.9
-	if {${os.major} < 16} {
-	    # The Sierra SDK requires a toolchain that supports class properties
-	    lappend compilers macports-clang-3.7
-	    lappend compilers macports-clang-3.4
-	    if {${os.major} < 9} {
-		lappend compilers macports-clang-3.3
-	    }
+    if {${os.major} < 16} {
+	# The Sierra SDK requires a toolchain that supports class properties
+	lappend compilers macports-clang-3.7
+	lappend compilers macports-clang-3.4
+	if {${os.major} < 9} {
+	    lappend compilers macports-clang-3.3
 	}
     }
 }
