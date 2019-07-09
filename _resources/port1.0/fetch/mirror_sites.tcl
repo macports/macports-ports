5524df31365e64a6b06325557d398728df57bafa
Zero King <l2dy@macports.org>
2019-07-09 17:27:08 +0000

    mirror_sites.tcl: add official https gnupg mirror

--- _resources/port1.0/fetch/mirror_sites.tcl
+++ _resources/port1.0/fetch/mirror_sites.tcl
@@ -443,6 +443,7 @@ set portfetch::mirror_sites::sites(gnu) {
 }
 
 set portfetch::mirror_sites::sites(gnupg) {
+    https://gnupg.org/ftp/gcrypt/
     http://mirror.cc.columbia.edu/pub/software/gnupg/
     http://ftp.freenet.de/pub/ftp.gnupg.org/gcrypt/
     ftp://ftp.gnupg.org/gcrypt/
