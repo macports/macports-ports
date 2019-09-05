27ba4db8e5db40ca29f45cd895b37c9d0527e0b2
Joshua Root <jmr@macports.org>
2019-09-06 00:19:24 +1000

    clang_compilers.tcl: add note about new versions

--- _resources/port1.0/compilers/clang_compilers.tcl
+++ _resources/port1.0/compilers/clang_compilers.tcl
@@ -1,5 +1,9 @@
 # add all working Clang compilers to the variable compilers based on ${os.major}
 
+# When adding a new clang version here, make sure to update the
+# clang_dependency PortGroup, add it to any new dependencies of the
+# new version, and update the blacklisting in the other clang ports.
+
 # does Clang work on all i386 and x86_64 systems?
 # according to https://packages.macports.org/clang-7.0/,
 #    clang builds back to Mac OS X 10.6
