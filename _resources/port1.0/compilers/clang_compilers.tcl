3be8a3454053f0ae7ea98aabf8f75eb50a5fb46e
Joshua Root <jmr@macports.org>
2019-09-06 00:12:15 +1000

    *_compilers.tcl: merge lappend calls

--- _resources/port1.0/compilers/clang_compilers.tcl
+++ _resources/port1.0/compilers/clang_compilers.tcl
@@ -3,14 +3,14 @@
 # does Clang work on all i386 and x86_64 systems?
 # according to https://packages.macports.org/clang-7.0/,
 #    clang builds back to Mac OS X 10.6
-lappend compilers macports-clang-8.0
-lappend compilers macports-clang-7.0
-lappend compilers macports-clang-6.0
-lappend compilers macports-clang-5.0
+lappend compilers macports-clang-8.0 \
+                  macports-clang-7.0 \
+                  macports-clang-6.0 \
+                  macports-clang-5.0
 if {${os.major} < 16} {
     # The Sierra SDK requires a toolchain that supports class properties
-    lappend compilers macports-clang-3.7
-    lappend compilers macports-clang-3.4
+    lappend compilers macports-clang-3.7 \
+                      macports-clang-3.4
     if {${os.major} < 9} {
         lappend compilers macports-clang-3.3
     }
