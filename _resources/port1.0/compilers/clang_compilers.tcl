693363f84ad9a4f07c5bd98570aec09eadf835a9
Joshua Root <jmr@macports.org>
2019-10-24 22:48:50 +1100

    clang_compilers.tcl: update versions for pre-10.6
    Adapted from https://github.com/macports/macports-ports/pull/5590

--- _resources/port1.0/compilers/clang_compilers.tcl
+++ _resources/port1.0/compilers/clang_compilers.tcl
@@ -1,21 +1,29 @@
 # add all working Clang compilers to the variable compilers based on ${os.major}
 
 # When adding a new clang version here, make sure to update the
-# clang_dependency PortGroup, add it to any new dependencies of the
-# new version, and update the blacklisting in the other clang ports.
+# clang_dependency PortGroup, and add it to any new dependencies of the
+# new version.
 
-# does Clang work on all i386 and x86_64 systems?
-# according to https://packages.macports.org/clang-7.0/,
-# clang builds back to Mac OS X 10.6
-lappend compilers macports-clang-9.0 \
-                  macports-clang-8.0 \
-                  macports-clang-7.0 \
-                  macports-clang-6.0 \
-                  macports-clang-5.0
+# clang 9.0 and older build on 10.6+ (darwin 10)
+# clang 7.0 and older build on 10.5+ (darwin 9)
+# clang 3.4 and older build on 10.4+ (darwin 8)
+if {${os.major} >= 10} {
+    lappend compilers macports-clang-9.0 \
+                      macports-clang-8.0
+}
+
+if {${os.major} >= 9} {
+    lappend compilers macports-clang-7.0 \
+                      macports-clang-6.0 \
+                      macports-clang-5.0
+}
+
 if {${os.major} < 16} {
     # The Sierra SDK requires a toolchain that supports class properties
-    lappend compilers macports-clang-3.7 \
-                      macports-clang-3.4
+    if {${os.major} >= 9} {
+        lappend compilers macports-clang-3.7
+    }
+    lappend compilers macports-clang-3.4
     if {${os.major} < 9} {
         lappend compilers macports-clang-3.3
     }
