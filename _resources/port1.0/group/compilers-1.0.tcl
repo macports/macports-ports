ce343c54ebb9403bdef823d65d9714a19c0d676d
Chris Jones <jonesc@macports.org>
2020-05-02 18:09:22 +0100

    compilers PG: Add gcc10

--- _resources/port1.0/group/compilers-1.0.tcl
+++ _resources/port1.0/group/compilers-1.0.tcl
@@ -75,13 +75,13 @@ if {${os.major} < 10} {
 } elseif {${os.major} < 11} {
     set compilers.gcc_default gcc8
 } else {
-    set compilers.gcc_default gcc9
+    set compilers.gcc_default gcc10
 }
 
 set compilers.list {cc cxx cpp objc fc f77 f90}
 
 # build database of gcc compiler attributes
-set gcc_versions {4.4 4.5 4.6 4.7 4.8 4.9 5 6 7 8 9}
+set gcc_versions {4.4 4.5 4.6 4.7 4.8 4.9 5 6 7 8 9 10}
 foreach ver ${gcc_versions} {
     # Remove dot from version if present
     set ver_nodot [string map {. {}} ${ver}]
@@ -91,13 +91,11 @@ foreach ver ${gcc_versions} {
     set cdb(gcc$ver_nodot,descrip)  "MacPorts gcc $ver"
     set cdb(gcc$ver_nodot,depends)  port:gcc$ver_nodot
     if {[vercmp ${ver} 4.6] < 0} {
-        set cdb(gcc$ver_nodot,dependsl) "path:lib/libgcc/libgcc_s.1.dylib:libgcc port:libgcc7 port:libgcc6 port:libgcc45"
+        set cdb(gcc$ver_nodot,dependsl) "path:lib/libgcc/libgcc_s.1.dylib:libgcc port:libgcc45"
     } elseif {[vercmp ${ver} 7] < 0} {
-        set cdb(gcc$ver_nodot,dependsl) "path:lib/libgcc/libgcc_s.1.dylib:libgcc port:libgcc7 port:libgcc6"
-    } elseif {[vercmp ${ver} 8] < 0} {
-        set cdb(gcc$ver_nodot,dependsl) "path:lib/libgcc/libgcc_s.1.dylib:libgcc port:libgcc7"
+        set cdb(gcc$ver_nodot,dependsl) "path:lib/libgcc/libgcc_s.1.dylib:libgcc port:libgcc6"
     } else {
-        set cdb(gcc$ver_nodot,dependsl) "path:lib/libgcc/libgcc_s.1.dylib:libgcc"
+        set cdb(gcc$ver_nodot,dependsl) "path:lib/libgcc/libgcc_s.1.dylib:libgcc port:libgcc${ver_nodot}"
     }
     set cdb(gcc$ver_nodot,libfortran) ${prefix}/lib/gcc$ver_nodot/libgfortran.dylib
     # note: above is ultimately a symlink to ${prefix}/lib/libgcc/libgfortran.3.dylib
