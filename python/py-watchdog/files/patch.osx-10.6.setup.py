74ee0814b24188d3e48bbc7d5c3db5feff0e04d3
Renee Otten <reneeotten@macports.org>
2020-06-29 17:39:05 -0400

    py-watchdog: update patch for 10.6

--- python/py-watchdog/files/patch.osx-10.6.setup.py
+++ python/py-watchdog/files/patch.osx-10.6.setup.py
@@ -1,12 +1,12 @@
---- setup.orig.py	2015-02-11 10:54:50.000000000 +0100
-+++ setup.py	2017-12-21 19:09:58.000000000 +0100
-@@ -56,9 +56,6 @@
-                 '-Wall',
-                 '-Wextra',
-                 '-fPIC',
+--- setup.py.orig	2020-06-29 17:37:32.000000000 -0400
++++ setup.py	2020-06-29 17:38:06.000000000 -0400
+@@ -68,9 +68,6 @@
+                 # Issue #628
+                 '-Wno-nullability-extension',
+                 '-Wno-newline-eof',
 -
 -                # required w/Xcode 5.1+ and above because of '-mno-fused-madd'
--                '-Wno-error=unused-command-line-argument-hard-error-in-future'
+-                '-Wno-error=unused-command-line-argument'
              ]
          ),
      ]
