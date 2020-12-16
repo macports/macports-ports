a73c7b593f79925536c1a2c1cef36dc5390576e1
Aaron Madlon-Kay <amake@macports.org>
2020-12-16 13:17:32 +0900

    java-1.0 portgroup: wildcards not supported on Big Sur
    See https://trac.macports.org/ticket/61445

--- _resources/port1.0/group/java-1.0.tcl
+++ _resources/port1.0/group/java-1.0.tcl
@@ -57,12 +57,17 @@ proc find_java_home {} {
         #
         # Temporarily unset and stash the value here.
         #
+        # Also it seems that wildcard syntax does not work, so we remove them.
+        #
         # See https://trac.macports.org/ticket/61445
         global os.platform os.major
-        set big_sur_workaround [expr {${os.platform} eq "darwin" && ${os.major} == 20 && [info exists ::env(JAVA_HOME)]}]
+        set big_sur_workaround [expr {${os.platform} eq "darwin" && ${os.major} == 20}]
         if {${big_sur_workaround}} {
-            set env_java_home $::env(JAVA_HOME)
-            unset $::env(JAVA_HOME)
+            set java.version [string map {+ "" * ""} ${java.version}]
+            if {[info exists ::env(JAVA_HOME)]} {
+                set env_java_home $::env(JAVA_HOME)
+                unset $::env(JAVA_HOME)
+            }
         }
 
         if { [catch {set val [exec "/usr/libexec/java_home" "-f" "-v" ${java.version}]}] } {
@@ -76,7 +81,7 @@ proc find_java_home {} {
         }
 
         # Restore original JAVA_HOME value stashed above
-        if {${big_sur_workaround}} {
+        if {${big_sur_workaround} && [info exists env_java_home]} {
             set $::env(JAVA_HOME) ${env_java_home}
         }
     }
