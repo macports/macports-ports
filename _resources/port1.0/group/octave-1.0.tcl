22442dcfcad4daaa81316967c2ae93a9d0acbd18
Marius Schamschula <mps@macports.org>
2020-12-19 08:03:50 -0600

    octave PG: use compiler.cxx_standard  2011

--- _resources/port1.0/group/octave-1.0.tcl
+++ _resources/port1.0/group/octave-1.0.tcl
@@ -18,9 +18,10 @@ options octave.module octave.config_h
 default octave.config_h {no}
 
 # some header files from Octave require C++-11
-PortGroup cxx11 1.1
+compiler.cxx_standard  2011
 # overrule cxx11 PortGroup because octave can use GCC compilers for Fortran
 #    even if configure.cxx_stdlib is libc++
+PortGroup compiler_blacklist_versions 1.0
 compiler.blacklist-delete *gcc*
 
 # override universal_setup found in portutil.tcl so it uses muniversal PortGroup
@@ -105,6 +106,7 @@ post-extract {
         delete ${workpath}/${octave.module}
         move [glob ${workpath}/*-${version}] ${workpath}/${octave.module}
     }
+
 #     set worksrcdir_name [exec /bin/ls ${workpath} | grep -v -E "^\\."]
 #     if {[string equal ${worksrcdir_name} ${octave.module}] == 0} {
 #         # work-around for case-insensitive file systems when the
