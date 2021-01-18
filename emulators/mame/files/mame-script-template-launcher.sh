2c8ae5455008a641418699f7ebbaae082f792329
Christopher Nielsen <mascguy@github.com>
2021-01-18 16:43:30 -0500

    mame: update notes to include link to our tutorial; minor portfile cleanup; update launcher to specify pluginspath, artpath, hashpath, ctrlrpath

--- emulators/mame/files/mame-script-template-launcher.sh
+++ emulators/mame/files/mame-script-template-launcher.sh
@@ -12,11 +12,19 @@ script_dir=$(dirname "${script_file}")
 cmd="${script_dir}/@@MACPORTS_MAME_EXECUTABLE@@"
 bgfx_path="${script_dir}/bgfx"
 languagepath="${script_dir}/language"
+pluginspath="${script_dir}/plugins"
+artpath="${script_dir}/artwork"
+hashpath="${script_dir}/hash"
+ctrlrpath="${script_dir}/ctrlr"
 
 cmdline=()
 cmdline+=( "${cmd}" )
 cmdline+=( -bgfx_path "${bgfx_path}" )
 cmdline+=( -languagepath "${languagepath}" )
+cmdline+=( -pluginspath "${pluginspath}" )
+cmdline+=( -artpath "${artpath}" )
+cmdline+=( -hashpath "${hashpath}" )
+cmdline+=( -ctrlrpath "${ctrlrpath}" )
 
 # Note that Mame handles duplicate arguments, using the last one found.
 # Net-Net, there's no need to check whether the user specified the same
