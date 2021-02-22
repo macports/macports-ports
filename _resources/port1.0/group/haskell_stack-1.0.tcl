228dad0f175782a7d6862fdeccd2b03c60cdb755
Steven Thomas Smith <s.t.smith@ieee.org>
2021-02-22 17:13:09 -0500

    haskell_stack: Bugfix of stack_root
    Fixes: https://trac.macports.org/ticket/62228

--- _resources/port1.0/group/haskell_stack-1.0.tcl
+++ _resources/port1.0/group/haskell_stack-1.0.tcl
@@ -21,7 +21,7 @@
 #
 # haskell_stack.stack_root
 #   The root directory for stack, passed as STACK_ROOT in haskell_stack.env.
-#   Defaults to ${workpath}/.stack.
+#   Defaults to ${workpath}/.home/.stack.
 #
 # haskell_stack.yaml
 #   The location of the stack.yaml config file, passed as STACK_YAML in
@@ -64,7 +64,7 @@ proc haskell_stack.add_dependencies {} {
 port::register_callback haskell_stack.add_dependencies
 
 options haskell_stack.stack_root
-default haskell_stack.stack_root {${workpath}/.stack}
+default haskell_stack.stack_root {${workpath}/.home/.stack}
 
 post-extract {
     xinstall -m 0755 -d "[option haskell_stack.stack_root]"
