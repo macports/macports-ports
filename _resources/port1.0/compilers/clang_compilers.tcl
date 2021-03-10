2f37547eddee14389286364929d2b2f2b57b321c
Chris Jones <jonesc@macports.org>
2021-03-10 12:32:35 +0000

    Add MP clang 10 and 11 at *end* of default fallback list

--- _resources/port1.0/compilers/clang_compilers.tcl
+++ _resources/port1.0/compilers/clang_compilers.tcl
@@ -9,7 +9,7 @@
 # clang 3.4 and older build on 10.4+  (darwin 8)
 
 if {${os.major} >= 10} {
-    # TODO: evaluate compatibility of clang-{10,11}
+    # TODO: evaluate compatibility of clang-{10,11} as defaults instead of 9.0
     #lappend compilers macports-clang-11
     #lappend compilers macports-clang-10
     lappend compilers macports-clang-9.0 \
@@ -20,6 +20,12 @@ if {${os.major} >= 9} {
     lappend compilers macports-clang-7.0 \
                       macports-clang-6.0 \
                       macports-clang-5.0
+}
+
+# Add 10 and 11 as defaults *after* 9.0 - 5.0
+if {${os.major} >= 10} {
+    lappend compilers macports-clang-11 \
+                      macports-clang-10
 }
 
 if {${os.major} < 16} {
