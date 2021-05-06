62721de0394d8a52da9922b85e4fde1420c1f172
Chris Jones <jonesc@macports.org>
2021-05-06 11:46:35 +0100

    janet PG: Use compiler_wrapper PG to generate compiler wraps

--- _resources/port1.0/group/janet-1.0.tcl
+++ _resources/port1.0/group/janet-1.0.tcl
@@ -7,13 +7,12 @@
 #     (see https://janet-lang.org/docs/jpm.html).
 #
 
+PortGroup compiler_wrapper      1.0
+compwrap.print_compiler_command no
+
 # search in ${prefix}/lib/janet/bin and create link in ${prefix}/bin
 options janet.link_bin
 default janet.link_bin          yes
-
-# program to create and maintain library archives
-options janet.compiler.ar
-default janet.compiler.ar       {[janetpg::get_default_ar]}
 
 namespace eval janetpg {
 }
@@ -27,29 +26,24 @@ proc janetpg::get_default_ar {} {
 # You can set the compilers, but no spaces seem to be allowed.
 # Hence, we cannot simply append the CFLAGS/CXXFLAGS to the compiler name.
 # As a workaround, create custom compilers with CFLAGS/CXXFLAGS baked in.
-pre-configure {
-    # create directory
-    xinstall -d -m 0755 ${worksrcpath}/macports_bin
-    # create empty files with the correct permissions
-    foreach bin {cc cxx ar} {
-        close [open ${worksrcpath}/macports_bin/${bin} w 0755]
+# Use compiler_wrapper PG to do this.
+
+use_configure                   no
+
+pre-build {
+    # Generate the wrappers
+    foreach bin {cc cxx} {
+        compwrap::create_wrapper ${bin}
     }
 }
-default configure.pre_args      {}
-default configure.cmd           {
-    /bin/echo \\\\#!/bin/bash >> ${worksrcpath}/macports_bin/cc  && echo exec \\\$CC  \\\$CFLAGS   \\\\\"\\\\$@\\\\\" >> ${worksrcpath}/macports_bin/cc  &&
-    /bin/echo \\\\#!/bin/bash >> ${worksrcpath}/macports_bin/cxx && echo exec \\\$CXX \\\$CXXFLAGS \\\\\"\\\\$@\\\\\" >> ${worksrcpath}/macports_bin/cxx &&
-    /bin/echo \\\\#!/bin/bash >> ${worksrcpath}/macports_bin/ar  && echo exec ${janet.compiler.ar} \\\\\"\\\\$@\\\\\" >> ${worksrcpath}/macports_bin/ar
-}
-default configure.post_args     {}
 
 # Build with jpm.
 default build.cmd               {${prefix}/bin/jpm}
 default build.args              {--verbose
                                  --offline
-                                 --compiler=${worksrcpath}/macports_bin/cc
-                                 --cpp-compiler=${worksrcpath}/macports_bin/cxx
-                                 --archiver=${worksrcpath}/macports_bin/ar
+                                 --compiler=[compwrap::wrapper_path cc]
+                                 --cpp-compiler=[compwrap::wrapper_path cxx]
+                                 --archiver=[janetpg::get_default_ar]
                                  build
                                 }
 default build.target            {}
