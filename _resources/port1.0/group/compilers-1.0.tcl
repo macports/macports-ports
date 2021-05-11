8759dbd93fb71e89a59cd58be4ca2d854db24535
Chris Jones <jonesc@macports.org>
2021-05-11 08:28:55 +0100

    {lib}gcc11:        Add new GCC major version libgcc,{lib}gcc10: Update to use gcc11 as primary runtime on Darwin>10

--- _resources/port1.0/group/compilers-1.0.tcl
+++ _resources/port1.0/group/compilers-1.0.tcl
@@ -84,7 +84,7 @@ if {${os.major} < 10} {
 } elseif {${os.major} < 11} {
     set compilers.gcc_default gcc8
 } else {
-    set compilers.gcc_default gcc10
+    set compilers.gcc_default gcc11
 }
 
 set compilers.list {cc cxx cpp objc fc f77 f90}
@@ -92,7 +92,7 @@ set compilers.list {cc cxx cpp objc fc f77 f90}
 # build database of gcc compiler attributes
 # Should match those in compilers/gcc_compilers.tcl
 if { ${os.arch} eq "arm" } {
-    set gcc_versions {10 devel}
+    set gcc_versions {10 11 devel}
 } else {
     set gcc_versions {}
     if { ${os.major} < 20 } {
@@ -102,7 +102,7 @@ if { ${os.arch} eq "arm" } {
         lappend gcc_versions 8
     }
     if { ${os.major} >= 11 } {
-        lappend gcc_versions 9 10 devel
+        lappend gcc_versions 9 10 11 devel
     }
 }
 # GCC version providing the primary runtime
