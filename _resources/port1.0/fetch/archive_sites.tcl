macports/macports-ports

commit 7b4411401c0a23f440e2ed4beb1483bb426abfe2

Temporarily limit CDN access to Big Sur

_resources/port1.0/fetch/archive_sites.tcl
@@ -10,6 +10,8 @@ namespace eval portfetch::mirror_sites { }
 # https with them yet.
 global os.platform os.major
 set fastly      [expr {${os.platform} eq "darwin" && ${os.major} < 13 ? "http" : "https"}]
+# Temporarily limit CDN access to Big Sur until end of month.
+set fastly      [expr {${os.platform} eq "darwin" && ${os.major} < 20 ? "" : "https"}]
 # cert doesn't have macports.org SANs; admin notified
 #set aarnet.au   [expr {${os.platform} eq "darwin" && ${os.major} < 13 ? "http" : "https"}]
 set aarnet.au   http

_resources/port1.0/fetch/mirror_sites.tcl
@@ -422,6 +422,8 @@ set portfetch::mirror_sites::sites(macports) {
 # https with them yet.
 global os.platform os.major
 set fastly      [expr {${os.platform} eq "darwin" && ${os.major} < 13 ? "http" : "https"}]
+# Temporarily limit CDN access to Big Sur until end of month.
+set fastly      [expr {${os.platform} eq "darwin" && ${os.major} < 20 ? "" : "https"}]
 # cert doesn't have macports.org SANs; admin notified
 #set aarnet.au   [expr {${os.platform} eq "darwin" && ${os.major} < 13 ? "http" : "https"}]
 set aarnet.au   http

commit 52dad7d3d0363141f1acae70c435aaec0b5a3583

Put primary packages server back in the right spot
As the comment says, the primary packages server needs to be kept
first in the list. 7b44114 caused atl.us to be treated as the primary
server in most cases, but nue.de is the one that should be.

_resources/port1.0/fetch/archive_sites.tcl
@@ -40,6 +40,7 @@ set ywg.ca      [expr {${os.platform} eq "darwin" && ${os.major} < 10 ? "http" :
 # Keep the primary packages server first in the list
 set portfetch::mirror_sites::sites(macports_archives) [lsearch -all -glob -inline -not "
     ${fastly}://packages.macports.org/:nosubdir
+    ${nue.de}://nue.de.packages.macports.org/:nosubdir
     ${atl.us}://atl.us.packages.macports.org/:nosubdir
     ${cph.dk}://cph.dk.packages.macports.org/:nosubdir
     ${fco.it}://fco.it.packages.macports.org/:nosubdir
@@ -49,7 +50,6 @@ set portfetch::mirror_sites::sites(macports_archives) [lsearch -all -glob -inlin
     ${kmq.jp}://kmq.jp.packages.macports.org/:nosubdir
     ${mse.uk}://mse.uk.packages.macports.org/:nosubdir
     ${nou.nc}://nou.nc.packages.macports.org/pub/macports/packages.macports.org/:nosubdir
-    ${nue.de}://nue.de.packages.macports.org/:nosubdir
     ${pek.cn}://pek.cn.packages.macports.org/macports/packages/:nosubdir
     ${ywg.ca}://ywg.ca.packages.macports.org/mirror/macports/packages/:nosubdir
 " {:*}]