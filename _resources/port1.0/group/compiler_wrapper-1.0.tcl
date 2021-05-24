macports/macports-ports

commit 97af063d25290d1abacd10c3583d5aad0a663569

compiler_wrapper pg: don't reimplement join (incorrectly)

_resources/port1.0/group/compiler_wrapper-1.0.tcl
@@ -17,13 +17,13 @@ options compwrap.print_compiler_command
 default compwrap.print_compiler_command no
 
 options compwrap.compiler_pre_flags
-default compwrap.compiler_pre_flags {{}}
+default compwrap.compiler_pre_flags [list]
 
 options compwrap.compiler_post_flags
-default compwrap.compiler_post_flags {{}}
+default compwrap.compiler_post_flags [list]
 
 options compwrap.compiler_args_forward
-default compwrap.compiler_args_forward {{\$\{\@\}}}
+default compwrap.compiler_args_forward [list {\${@}}]
 
 options compwrap.compilers_to_wrap
 default compwrap.compilers_to_wrap [list cc objc cxx objcxx fc f77 f90]
@@ -51,16 +51,6 @@ proc compwrap::get_ccache_dir {} {
         return ${ccache_dir}
     } else {
         return [file join $portdbpath build .ccache]
-    }
-}
-
-proc compwrap::trim {c} {
-    # Remove any enclosing "{....}" braces if present
-    if { [string range [option $c] 0   0  ] eq "\{" &&
-         [string range [option $c] end end] eq "\}" } {
-        return [string range [option $c] 1 end-1 ]
-    } else {
-        return [option $c]
     }
 }
 
@@ -112,7 +102,9 @@ proc compwrap::create_wrapper {tag} {
     file delete -force ${wrapcomp}
 
     # Basic option, to pass on all command line arguments
-    set comp_opts "[trim compwrap.compiler_pre_flags] [trim compwrap.compiler_args_forward] [trim compwrap.compiler_post_flags]"
+    set comp_opts [join [option compwrap.compiler_pre_flags]]
+    append comp_opts " [join [option compwrap.compiler_args_forward]]"
+    append comp_opts " [join [option compwrap.compiler_post_flags]]"
 
     # Add MacPorts compiler flags ?
     if { [option compwrap.add_compiler_flags] } {

commit 0d92bdecdf30fd4372232ede2350a9f79fe4d9c1

compiler_wrapper PG: Fix argument quoting Inadvertently broken by commit https://github.com/macports/macports-ports/commit/97af063d25290d1abacd10c3583d5aad0a663569#diff-c4bcf7fad84fe74afb58060187858c995735190cf587676675e77d9f111f94c5 Closes: https://trac.macports.org/ticket/62944

_resources/port1.0/group/compiler_wrapper-1.0.tcl
@@ -145,7 +145,7 @@ proc compwrap::wrap_compiler {tag} {
 
     # Basic option, to pass on all command line arguments
     append comp_opts " [join [option compwrap.compiler_pre_flags]]"
-    append comp_opts " [join [option compwrap.compiler_args_forward]]"
+    append comp_opts " \"[join [option compwrap.compiler_args_forward]]\""
     append comp_opts " [join [option compwrap.compiler_post_flags]]"
 
     # Prepend ccache launcher if active
@@ -162,11 +162,10 @@ proc compwrap::wrap_compiler {tag} {
     if { [compwrap::use_ccache ${tag}] } {
         puts ${f} "export CCACHE_DIR=[compwrap::get_ccache_dir]"
     }
-    puts ${f} "CMD=\"${comp} ${comp_opts}\""
     if {[option compwrap.print_compiler_command]} {
-        puts ${f} "echo \${CMD}"
+        puts ${f} "echo ${comp} ${comp_opts}"
     }
-    puts  ${f} "exec \${CMD}"
+    puts ${f} "exec ${comp} ${comp_opts}"
     close ${f}
     
     return ${wrapcomp}