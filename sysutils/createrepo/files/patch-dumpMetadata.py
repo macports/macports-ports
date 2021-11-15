3773b9eca524e5e28c85b75e8865ea7f7eaf0b28
Zero King <l2dy@macports.org>
2021-11-15 15:20:02 +0000

    yum*: remove yum and related ports
    They are severely outdated and unmaintained.

--- sysutils/createrepo/files/patch-dumpMetadata.py
+++
@@ -1,8 +0,0 @@
---- dumpMetadata.py	2006-06-10 18:28:03.000000000 +0200
-+++ dumpMetadata.py	2006-06-10 19:13:42.000000000 +0200
-@@ -1,4 +1,4 @@
--#!/usr/bin/python -t
-+#!__PYTHON__ -t
- # base classes and functions for dumping out package Metadata
- #
- # This program is free software; you can redistribute it and/or modify
