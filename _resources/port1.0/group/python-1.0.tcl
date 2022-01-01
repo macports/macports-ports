2e40d9575a405d59afbffb5d8f2f8c242796d31c
Joshua Root <jmr@macports.org>
2022-01-01 18:50:45 +1100

    python pg: set recommended default version to 3.10

--- _resources/port1.0/group/python-1.0.tcl
+++ _resources/port1.0/group/python-1.0.tcl
@@ -75,7 +75,7 @@ proc python_get_version {} {
 
 proc python_get_default_version {} {
     global python.versions
-    set def_v 39
+    set def_v 310
     if {[info exists python.versions]} {
         if {${def_v} in ${python.versions}} {
             return ${def_v}
