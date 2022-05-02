b033e63724354dfe04145d106ef9ab98d08d14e4
Vadym-Valdis Yudaiev (Glory to Ukraine) <judaew@macports.org>
2022-05-02 13:24:56 +0300

    arm-none-eabi-gdb, avr-gdb: update to 12.1

--- _resources/port1.0/group/crossgdb-1.0.tcl
+++ _resources/port1.0/group/crossgdb-1.0.tcl
@@ -21,6 +21,11 @@ array set crossgdb.version_info {
         sha256  1497c36a71881b8671a9a84a0ee40faab788ca30d7ba19d8463c3cc787152e32
         size    22039420
     }}
+    12.1 {xz {
+        rmd160  53ce945d3a130f90d164e69f2d3856a9c332bd96 \
+        sha256  0e1793bf8f2b54d53f46dea84ccfd446f48f81b297b28c4f7fc017b818d69fed \
+        size    22470332
+    }}
 }
 
 proc crossgdb.setup {target version} {
