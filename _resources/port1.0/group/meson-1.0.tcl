75aeaf2dbcb436c71fd41c25d483d4125eaf424d
Marcus Calhoun-Lopez <mcalhoun@macports.org>
2022-06-11 08:39:54 -0700

    meson PG: add support for muniversal PG 1.1

--- _resources/port1.0/group/meson-1.0.tcl
+++ _resources/port1.0/group/meson-1.0.tcl
@@ -33,8 +33,16 @@ default destroot.post_args  ""
 namespace eval meson { }
 
 proc meson::get_post_args {} {
-    global configure.dir build_dir muniversal.current_arch
-    if {[info exists muniversal.current_arch]} {
+    global configure.dir build_dir build.dir muniversal.current_arch muniversal.build_arch
+    if {[info exists muniversal.build_arch]} {
+        # muniversal 1.1 PG is being used
+        if {[option muniversal.is_cross.[option muniversal.build_arch]]} {
+            return "${configure.dir} ${build.dir} --cross-file=[option muniversal.build_arch]-darwin"
+        } else {
+            return "${configure.dir} ${build.dir}"
+        }
+    } elseif {[info exists muniversal.current_arch]} {
+        # muniversal 1.0 PG is being used
         return "${configure.dir} ${build_dir}-${muniversal.current_arch} --cross-file=${muniversal.current_arch}-darwin"
     } else {
         return "${configure.dir} ${build_dir}"
