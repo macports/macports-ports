ef27c0c37a37d1fa476e232b4778666f6607c987
kakuhen <kakuhen@ucla.edu>
2022-08-01 14:32:31 -0700

    denemo: remove port
    * No dependents
    * 0 requested installs in the past 90 days
    * Port health is red across the board
    * No maintainer

--- print/denemo/files/autogen.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-aclocal $ACLOCAL_FLAGS -I build -I /usr/share/aclocal && \
-autoheader && \
-glibtoolize --force && \
-intltoolize --copy --force --automake && \
-automake --add-missing --gnu && \
-autoconf
