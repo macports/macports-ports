61de54907181e5c1d36f09cce642f5343d942128
Christopher Nielsen <mascguy@github.com>
2022-08-14 13:51:16 -0400

    pg stub: add option lib_dep - Provides support for stubs intended to be used as a lib dep See: https://github.com/macports/macports-ports/commit/ef9ce94e44f18475e5963b39613cc1f510ab9232#commitcomment-81125173

--- _resources/port1.0/group/stub-1.0.tcl
+++ _resources/port1.0/group/stub-1.0.tcl
@@ -23,6 +23,7 @@
 #
 # Options:
 #   * stub.subport_name - override subport name, for README location
+#   * stub.lib_dep      - allow subport to be used as lib dep; default: false
 #
 #------------------------------------------------------------------------------
 #
@@ -38,26 +39,10 @@ namespace eval stub {}
 options stub.subport_name
 default stub.subport_name ${subport}
 
-if {![info exists PortInfo(maintainers)]} {
-    maintainers     nomaintainer
-}
-
-if {![info exists PortInfo(homepage)]} {
-    homepage
-}
-
-if {![info exists PortInfo(license)] || ${PortInfo(license)} eq "unknown"} {
-    license         none
-}
-
-supported_archs     noarch
-installs_libs       no
-
-distfiles
-patchfiles
-use_configure       no
-
-build {}
+# Designate whether stub is intended to be used as a lib dep.
+# If not, set 'supported_archs noarch' and 'installs_libs no'.
+options stub.lib_dep
+default stub.lib_dep false
 
 proc stub::destroot {} {
     global destroot prefix
@@ -68,7 +53,38 @@ proc stub::destroot {} {
     system "echo ${subport_name} is a stub port > ${docdir}/README"
 }
 
+proc stub::setup_stub {} {
+    global PortInfo
+
+    if {![info exists PortInfo(maintainers)]} {
+        maintainers     nomaintainer
+    }
+
+    if {![info exists PortInfo(homepage)]} {
+        homepage
+    }
+
+    if {![info exists PortInfo(license)] || ${PortInfo(license)} eq "unknown"} {
+        license         none
+    }
+
+    if {[option stub.lib_dep]} {
+        ui_debug "stub::setup_stub: lib_dep: yes"
+    } else {
+        ui_debug "stub::setup_stub: lib_dep: no; mark noarch; set installs_libs no"
+        supported_archs     noarch
+        installs_libs       no
+    }
+
+    distfiles
+    patchfiles
+    use_configure       no
+    build {}
+}
+
 destroot {
     stub::destroot
 }
 
+# callback after port is parsed
+port::register_callback stub::setup_stub
