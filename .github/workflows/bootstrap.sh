030949787e2a54e1543e689d5ce8ecfbd397bda5
Joshua Root <jmr@macports.org>
2022-10-21 13:03:33 +1100

    Use MacPorts 2.8.0 for CI

--- .github/workflows/bootstrap.sh
+++ .github/workflows/bootstrap.sh
@@ -15,7 +15,7 @@ endgroup() {
     printtag "endgroup"
 }
 
-MACPORTS_VERSION=2.7.2
+MACPORTS_VERSION=2.8.0
 
 OS_MAJOR=$(uname -r | cut -f 1 -d .)
 OS_ARCH=$(uname -m)
@@ -25,6 +25,7 @@ case "$OS_ARCH" in
         ;;
 esac
 
+MACPORTS_FILENAME=MacPorts-${MACPORTS_VERSION}-${OS_MAJOR}.tar.bz2
 
 begingroup "Fetching files"
 # Download resources in background ASAP but use later.
@@ -33,7 +34,7 @@ echo "Fetching getopt..."
 /usr/bin/curl -fsSLO "https://distfiles.macports.org/_ci/getopt/getopt-v1.1.6.tar.bz2" &
 curl_getopt_pid=$!
 echo "Fetching MacPorts..."
-/usr/bin/curl -fsSLO "https://github.com/macports/macports-ci-files/releases/download/v${MACPORTS_VERSION}/MacPorts-${OS_MAJOR}.tar.bz2" &
+/usr/bin/curl -fsSLO "https://github.com/macports/macports-ci-files/releases/download/v${MACPORTS_VERSION}/${MACPORTS_FILENAME}" &
 curl_mpbase_pid=$!
 echo "Fetching PortIndex..."
 /usr/bin/curl -fsSLo ports/PortIndex "https://ftp.fau.de/macports/release/ports/PortIndex_darwin_${OS_MAJOR}_${OS_ARCH}/PortIndex" &
@@ -83,8 +84,8 @@ begingroup "Installing MacPorts"
 # Install MacPorts built by https://github.com/macports/macports-base/tree/master/.github
 wait $curl_mpbase_pid
 echo "Extracting..."
-sudo tar -xpf "MacPorts-${OS_MAJOR}.tar.bz2" -C /
-rm -f "MacPorts-${OS_MAJOR}.tar.bz2"
+sudo tar -xpf "${MACPORTS_FILENAME}" -C /
+rm -f "${MACPORTS_FILENAME}"
 endgroup
 
 
