f8aa2ee322c4c66155a63bb3c6e96bf6f054d542
Joshua Root <jmr@macports.org>
2022-12-14 02:45:07 +1100

    openmaintainer ruby/*: update platforms

--- _resources/port1.0/group/ruby-1.0.tcl
+++ _resources/port1.0/group/ruby-1.0.tcl
@@ -173,6 +173,7 @@ proc ruby.setup {module vers {type "install.rb"} {docs {}} {source "custom"} {im
             if {$subport eq $name} {
                 ruby.link_binaries no
                 distfiles
+                platforms       any
                 supported_archs noarch
                 use_configure no
                 build {}
