37515e43a3ab0eecfd2fca33ce68491885da35d2
Sergey Fedorov <vital.had@gmail.com>
2023-01-21 03:27:32 +0800

    fortran PG: implement support for tests, add build dep on git
    Signed-off-by: Sergey Fedorov <vital.had@gmail.com>

--- _resources/port1.0/group/fortran-1.0.tcl
+++ _resources/port1.0/group/fortran-1.0.tcl
@@ -14,10 +14,16 @@ default categories  "fortran"
 depends_build-append \
                     port:fpm
 
+global os.platform os.arch os.major
+if {${os.major} < 14} {
+	# FPM uses Git to fetch modules. Put it here so that we do not keep getting failures.
+	depends_build-append    port:git
+	git.cmd                 ${prefix}/bin/git
+}
+
 compiler.blacklist-append \
                     *gcc-4.*
 
-global os.platform os.arch
 platform darwin powerpc {
     compiler.blacklist-append *clang*
 }
@@ -50,35 +56,47 @@ build.cmd-append    --prefix="${workpath}${prefix}" --profile="${profile}"
 
 global name
 destroot {
-	set bindir ${workpath}${prefix}/bin
-	if {[file exists ${bindir}] && [file isdirectory ${bindir}]} {
-	xinstall -d ${destroot}${prefix}/bin
-		fs-traverse exe ${bindir} {
-			if {[file exists ${exe}] && [file isfile ${exe}]} {
-				xinstall -m 0755 ${exe} ${destroot}${prefix}/bin/
-			}
-		}
-	}
-	set incdir ${workpath}${prefix}/include
-	if {[file exists ${incdir}] && [file isdirectory ${incdir}]} {
-	xinstall -d ${destroot}${prefix}/include/${name}
-		fs-traverse inc ${incdir} {
-			if {[file exists ${inc}] && [file isfile ${inc}]
-				&& ([string match "*.h" ${inc}]
-				|| [string match "*.mod" ${inc}])} {
-					xinstall -m 0644 ${inc} ${destroot}${prefix}/include/${name}/
-			}
-		}
-	}
-	set libdir ${workpath}${prefix}/lib
-	if {[file exists ${libdir}] && [file isdirectory ${libdir}]} {
-	xinstall -d ${destroot}${prefix}/lib
-		fs-traverse lib ${libdir} {
-			if {[file exists ${lib}] && [file isfile ${lib}]
-				&& ([string match "*.a" ${lib}]
-				|| [string match "*.dylib" ${lib}])} {
-					xinstall -m 0644 ${lib} ${destroot}${prefix}/lib/
-			}
-		}
-	}
+    set bindir ${workpath}${prefix}/bin
+    if {[file exists ${bindir}] && [file isdirectory ${bindir}]} {
+    xinstall -d ${destroot}${prefix}/bin
+        fs-traverse exe ${bindir} {
+            if {[file exists ${exe}] && [file isfile ${exe}]} {
+                xinstall -m 0755 ${exe} ${destroot}${prefix}/bin/
+            }
+        }
+    }
+    set incdir ${workpath}${prefix}/include
+    if {[file exists ${incdir}] && [file isdirectory ${incdir}]} {
+    xinstall -d ${destroot}${prefix}/include/${name}
+        fs-traverse inc ${incdir} {
+            if {[file exists ${inc}] && [file isfile ${inc}]
+                && ([string match "*.h" ${inc}]
+                || [string match "*.mod" ${inc}])} {
+                    xinstall -m 0644 ${inc} ${destroot}${prefix}/include/${name}/
+            }
+        }
+    }
+    set libdir ${workpath}${prefix}/lib
+    if {[file exists ${libdir}] && [file isdirectory ${libdir}]} {
+    xinstall -d ${destroot}${prefix}/lib
+        fs-traverse lib ${libdir} {
+            if {[file exists ${lib}] && [file isfile ${lib}]
+                && ([string match "*.a" ${lib}]
+                || [string match "*.dylib" ${lib}])} {
+                    xinstall -m 0644 ${lib} ${destroot}${prefix}/lib/
+            }
+        }
+    }
 }
+
+pre-test {
+    test.env-append \
+        PATH=$env(PATH):${workpath}/bin \
+        FC=${configure.fc}
+}
+
+test.cmd            ${prefix}/bin/fpm test
+test.target
+test.cmd-append     --flag="-I${destroot}${prefix}/include" \
+                    --link-flag="-L${destroot}${prefix}/lib" \
+                    --profile="${profile}"
