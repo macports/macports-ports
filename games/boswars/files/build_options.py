9aa810b0622edf66a8452955dd83d09a6a98aa37
Marcus Calhoun-Lopez <mcalhoun@macports.org>
2023-03-12 04:48:53 -0700

    boswars: use lua PG

--- games/boswars/files/build_options.py
+++ games/boswars/files/build_options.py
@@ -1,7 +1,7 @@
 # put lua51 include path before macports in case lua 5.2 is installed
-CPPPATH = ['__PREFIX__/include/lua-5.1', '__PREFIX__/include']
+CPPPATH = ['__PREFIX__/include/lua5.1', '__PREFIX__/include']
 CPPDEFINES = []
-LIBPATH = ['__PREFIX__/lib/lua-5.1', '__PREFIX__/lib']
+LIBPATH = ['__PREFIX__/lib/lua5.1', '__PREFIX__/lib']
 CCFLAGS = ['-fsigned-char']
 LINKFLAGS = ['$__RPATH']
 CC = ['__CC__']
