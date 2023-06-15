18501593eda8a5c0e3ed120119a37dc0387659fb
Kirill A. Korinsky <kirill@korins.ky>
2023-06-15 11:54:40 +0200

    clang_dependency PG: switch to clang-11-bootstrap
    Instead of blacklisting all clangs I've enforced to use
    clang-11-bootstrap on all systems which ships with something older which
    can't be used by system to build port which is kept inside clang
    bootstrap cycle.
    
    Anyway, I've replaced compiler only if desire one isn't available and if
    desired was MacPorts clang in accepted list of versions.
    
    This dramatically decrease number of compilers which needs to be build
    on old system.
    
    Thus, clang-11-bootstrap contains all necessary patches to be able to
    build LLVM/Clang up to 15 (tested), and probably 16 (untested).
    
    [skip ci]

--- _resources/port1.0/group/clang_dependency-1.0.tcl
+++ _resources/port1.0/group/clang_dependency-1.0.tcl
@@ -1,14 +1,45 @@
 # -*- coding: utf-8; mode: tcl; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- vim:fenc=utf-8:ft=tcl:et:sw=4:ts=4:sts=4
 
 # This portgroup is to help prevent circular dependencies for ports that
-# recent clang builds depend on, by blacklisting all 5+ clang compilers.
+# recent clang builds depend on, switching it to clang-11-bootstrap
 
-# call this if an older clang version depends on the port
-proc clang_dependency.extra_versions {versions} {
-    global prefix
-    foreach ver $versions {
-        compiler.blacklist-append macports-clang-${ver}
+proc clang_dependency.clang_bootstrap {} {
+    global os.platform os.major prefix configure.compiler
+
+    # When MacPorts decided to use MacPorts clang replace it to
+    # clang-11-bootstrap when it possible, to break circular dependencies.
+    if {[regexp {macports-clang-(.*)} ${configure.compiler} -> clang_v]} {
+
+        # detects when decided clang is already installed
+        if {![catch {lindex [registry_active clang-${clang_v}] 0}]} {
+            ui_debug "${configure.compiler} is installed, do not replace by clang-11-bootstrap"
+            return
+        }
+
+        # clang-11-bootstrap can build clang up to 15,
+        # future clang / llvm may requires future patches.
+        if {[vercmp ${clang_v} 15] >= 0} {
+            ui_debug "clang-11-bootstrap can't replace ${configure.compiler}"
+            return
+        }
+
+        ui_debug "Replace ${configure.compiler} by clang-11-bootstrap"
+
+        depends_build-delete    port:clang-${clang_v}
+        depends_build-append    port:clang-11-bootstrap
+        depends_skip_archcheck-append \
+                                clang-11-bootstrap
+        configure.cc            ${prefix}/libexec/clang-11-bootstrap/bin/clang
+        configure.cxx           ${prefix}/libexec/clang-11-bootstrap/bin/clang++
+
+        if {${os.major} < 11} {
+            configure.env-append \
+                                AR=${prefix}/libexec/clang-11-bootstrap/bin/llvm-ar \
+                                RANLIB=${prefix}/libexec/clang-11-bootstrap/bin/llvm-ranlib
+        }
     }
 }
 
-clang_dependency.extra_versions {devel 16 15 14 13 12 11 10 9.0 8.0 7.0 6.0 5.0}
+clang_dependency.clang_bootstrap
+
+port::register_callback clang_dependency.clang_bootstrap
