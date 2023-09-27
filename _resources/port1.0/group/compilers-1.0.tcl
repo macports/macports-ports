3a144bbed74a7358e0bc8d0a16c5f6e6d98e0948
Chris Jones <jonesc@macports.org>
2023-09-27 17:01:07 +0100

    compilers PG: Add clang 17 to known list

--- _resources/port1.0/group/compilers-1.0.tcl
+++ _resources/port1.0/group/compilers-1.0.tcl
@@ -184,7 +184,7 @@ if { ${os.arch} ne "arm" && ${os.platform} eq "darwin" } {
 if { ${os.major} >= 10 || ${os.platform} ne "darwin" } {
     lappend clang_versions 11
     if { ${os.major} >= 11 || ${os.platform} ne "darwin"} {
-        lappend clang_versions 12 13 14 15 16
+        lappend clang_versions 12 13 14 15 16 17
     }
     if { ${os.major} >= 14 } {
         lappend clang_versions devel
