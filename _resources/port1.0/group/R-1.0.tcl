d4a2e7e54e5caba2d72a2daf80e2caac9fe14482
Chris Jones <jonesc@macports.org>
2023-10-05 14:24:58 +0100

    R: Enforce gcc12 via default variants

--- _resources/port1.0/group/R-1.0.tcl
+++ _resources/port1.0/group/R-1.0.tcl
@@ -90,10 +90,8 @@ compiler.blacklist-append   {clang}
 # these get bumped centrally.
 # NOTE : Keep this setting in sync with the one in the R port.
 compiler.blacklist-append   {macports-clang-1[6-9]}
-# Also avoid any new GCC versions beyond gcc12. Again, it is then up to the R
-# maintainers when to migrate to a newer version.
-compilers.setup             -gcc13 -gfortran
-compiler.blacklist-append   {macports-gcc-1[3-9]}
+# manually select gcc12 variant of the compilers PG
+default_variants-append     +gcc12
 
 port::register_callback R.add_dependencies
 
