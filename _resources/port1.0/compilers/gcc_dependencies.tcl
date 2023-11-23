b261b5114896b5e28a339a57cfd1589d520137e4
Joshua Root <jmr@macports.org>
2023-11-24 01:55:18 +1100

    gcc_dependencies.tcl: use list constructor

--- _resources/port1.0/compilers/gcc_dependencies.tcl
+++ _resources/port1.0/compilers/gcc_dependencies.tcl
@@ -12,14 +12,14 @@ if {${os.platform} eq "darwin" && ${os.major} < 10} {
 
 # compiler links against libraries in libgcc\d* and/or libgcc-devel
 if {[vercmp ${gcc_version} 4.6] < 0} {
-    set libgccs "path:share/doc/libgcc/README:libgcc port:libgcc45"
+    set libgccs [list path:share/doc/libgcc/README:libgcc port:libgcc45]
 } elseif {[vercmp ${gcc_version} 7] < 0} {
-    set libgccs "path:share/doc/libgcc/README:libgcc port:libgcc6"
+    set libgccs [list path:share/doc/libgcc/README:libgcc port:libgcc6]
 } elseif {[vercmp ${gcc_version} ${gcc_main_version}] < 0} {
-    set libgccs "path:share/doc/libgcc/README:libgcc port:libgcc${gcc_version}"
+    set libgccs [list path:share/doc/libgcc/README:libgcc port:libgcc${gcc_version}]
 } else {
     # Using primary GCC version
     # Do not depend directly on primary runtime port, as implied by libgcc
     # and doing so prevents libgcc-devel being used as an alternative.
-    set libgccs "path:share/doc/libgcc/README:libgcc"
+    set libgccs [list path:share/doc/libgcc/README:libgcc]
 }
