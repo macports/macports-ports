4dfccd51273972edead03353bdad4cfbd24005cb
Marcus Calhoun-Lopez <mcalhoun@macports.org>
2024-05-11 19:37:35 -0700

    rust PG: sort crate names in alphabetical order

--- _resources/port1.0/group/rust-1.0.tcl
+++ _resources/port1.0/group/rust-1.0.tcl
@@ -282,14 +282,6 @@ proc rust::old_macos_compatibility {cname cversion} {
     global cargo.home subport
 
     switch ${cname} {
-        "kqueue" {
-            if { [vercmp ${cversion} < 1.0.5] && "i386" in [option muniversal.architectures] } {
-                # see https://gitlab.com/worr/rust-kqueue/-/merge_requests/10
-                reinplace {s|all(target_os = "freebsd", target_arch = "x86")|all(any(target_os = "freebsd", target_os = "macos"), any(target_arch = "x86", target_arch = "powerpc"))|g} \
-                    ${cargo.home}/macports/${cname}-${cversion}/src/time.rs
-                cargo.offline_cmd-replace --frozen --offline
-            }
-        }
         "curl-sys" {
             if { [vercmp ${cversion} < 0.4.56] } {
                 # on Mac OS X 10.6, clang exists, but `clang --print-search-dirs` returns an empty library directory
@@ -304,6 +296,14 @@ proc rust::old_macos_compatibility {cname cversion} {
                 #
                 reinplace "s|Command::new(\"clang\")|cc::Build::new().get_compiler().to_command()|g" \
                     ${cargo.home}/macports/${cname}-${cversion}/build.rs
+            }
+        }
+        "kqueue" {
+            if { [vercmp ${cversion} < 1.0.5] && "i386" in [option muniversal.architectures] } {
+                # see https://gitlab.com/worr/rust-kqueue/-/merge_requests/10
+                reinplace {s|all(target_os = "freebsd", target_arch = "x86")|all(any(target_os = "freebsd", target_os = "macos"), any(target_arch = "x86", target_arch = "powerpc"))|g} \
+                    ${cargo.home}/macports/${cname}-${cversion}/src/time.rs
+                cargo.offline_cmd-replace --frozen --offline
             }
         }
         "rustix" {
@@ -359,10 +359,6 @@ proc rust::old_macos_compatibility {cname cversion} {
     }
 
     switch ${cname} {
-        "notify" {
-            reinplace {s|default = \["macos_fsevent"\]|default = \["macos_kqueue"\]|g} \
-                ${cargo.home}/macports/${cname}-${cversion}/Cargo.toml
-        }
         "cargo-util" {
             reinplace {s|#\[cfg(not(target_os = "macos"))\]|#\[cfg(not(target_os = "macos_temp"))\]|g} \
                 ${cargo.home}/macports/${cname}-${cversion}/src/paths.rs
@@ -370,6 +366,10 @@ proc rust::old_macos_compatibility {cname cversion} {
                 ${cargo.home}/macports/${cname}-${cversion}/src/paths.rs
             reinplace {s|#\[cfg(not(target_os = "macos_temp"))\]|#\[cfg(target_os = "macos")\]|g} \
                 ${cargo.home}/macports/${cname}-${cversion}/src/paths.rs
+        }
+        "notify" {
+            reinplace {s|default = \["macos_fsevent"\]|default = \["macos_kqueue"\]|g} \
+                ${cargo.home}/macports/${cname}-${cversion}/Cargo.toml
         }
     }
 }
