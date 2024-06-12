d723437cfffc3d6e73a7daea0c5b4eab855c824b
Marcus Calhoun-Lopez <mcalhoun@macports.org>
2024-06-12 08:45:30 -0700

    muniversal PG: add finer control `-arch` flag

--- _resources/port1.0/group/muniversal-1.1.tcl
+++ _resources/port1.0/group/muniversal-1.1.tcl
@@ -74,6 +74,10 @@ default muniversal.arch_flag {yes}
 options muniversal.arch_compiler
 default muniversal.arch_compiler {no}
 
+# if yes, append architecture flag to build compiler name
+options muniversal.arch_build_compiler
+default muniversal.arch_build_compiler  {${muniversal.arch_compiler}}
+
 ##########################################################################################
 # MacPorts options for different architectures
 ##########################################################################################
@@ -724,7 +728,7 @@ proc parse_environment {command} {
         append_to_environment_value     ${command}  CXXCPP_FOR_BUILD    {*}"[portconfigure::configure_get_compiler cpp]"
         append_to_environment_value     ${command}  CPPFLAGS_FOR_BUILD  {*}[option configure.cppflags_for_build]
 
-        if { [option muniversal.arch_compiler] } {
+        if { [option muniversal.arch_build_compiler] } {
             if { [option configure.append_build_flags_to_compiler] } {
                 append_to_environment_value ${command}  CC_FOR_BUILD    {*}"[portconfigure::configure_get_compiler cc]  [portconfigure::configure_get_archflags cc] [option configure.cflags_for_build]"
                 append_to_environment_value ${command}  CXX_FOR_BUILD   {*}"[portconfigure::configure_get_compiler cxx] [portconfigure::configure_get_archflags cxx] [option configure.cxxflags_for_build]"
