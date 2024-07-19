c8cceb0dec2dfb15e3a54e38260a676cb59fc9af
Chris Jones <jonesc@macports.org>
2024-07-19 11:03:16 +0100

    clang compilers: Allow again clang17 as fallback on older OSes

--- _resources/port1.0/compilers/clang_compilers.tcl
+++ _resources/port1.0/compilers/clang_compilers.tcl
@@ -14,17 +14,17 @@ if {${os.platform} eq "darwin" && [option configure.build_arch] in [list ppc ppc
 
 if {${os.major} >= 11 || ${compiler.cxx_standard} >= 2020 || ${os.platform} ne "darwin"} {
     if {${os.major} >= 22 || ${os.platform} ne "darwin"} {
-        # For now limit exposure of clang-17+ to macOS13+ due to issues like
+        # For now limit exposure of clang-18+ to macOS13+ due to issues like
         # https://github.com/macports/macports-ports/pull/21051
         # https://trac.macports.org/ticket/68640
         if {${compiler.cxx_standard} >= 2017} {
             # Limit clang 18 to c++17 or newer
             lappend compilers macports-clang-18
         }
-        if {${compiler.cxx_standard} >= 2014} {
-            # Limit clang 17 to c++14 or newer
-            lappend compilers macports-clang-17
-        }
+    }
+    if {${compiler.cxx_standard} >= 2014} {
+        # Limit clang 17 to c++14 or newer
+        lappend compilers macports-clang-17
     }
     lappend compilers macports-clang-16 \
                       macports-clang-15 \
