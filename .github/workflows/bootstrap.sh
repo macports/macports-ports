9ba35ca46fc79b7d2e85d6776cb76beb578a8ecf
Joshua Root <jmr@macports.org>
2025-04-30 11:29:38 +1000

    CI: use MacPorts 2.10.6
    It turns out that org and repo variables are not accessible to PRs made
    from forks, so the plan to update this centrally just doesn't work.

--- .github/workflows/bootstrap.sh
+++ .github/workflows/bootstrap.sh
@@ -17,7 +17,7 @@ endgroup() {
     printtag "endgroup"
 }
 
-MACPORTS_VERSION=${MP_CI_RELEASE:-2.10.3}
+MACPORTS_VERSION=${MP_CI_RELEASE:-2.10.6}
 
 OS_MAJOR=$(uname -r | cut -f 1 -d .)
 OS_ARCH=$(uname -m)
