d2cda6900974722c05ca029fa0ff1c83b5b9a755
i0ntempest <i0ntempest@i0ntempest.com>
2025-11-02 16:52:39 +1100

    R: update to 4.5.2

--- _resources/port1.0/group/R-1.0.tcl
+++ _resources/port1.0/group/R-1.0.tcl
@@ -178,7 +178,7 @@ if {${os.platform} eq "darwin" && ${configure.cxx_stdlib} ne "libc++"} {
 
 global prefix frameworks_dir
 # Please update R version here:
-set Rversion        4.5.1
+set Rversion        4.5.2
 set branch          [join [lrange [split ${Rversion} .] 0 1] .]
 set packages        ${frameworks_dir}/R.framework/Versions/${branch}/Resources/library
 set suffix          .tar.gz
