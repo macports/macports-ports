1c1079a3ee6ffe9cb8888a37ba51d821467dae20
Paul Guyot <pguyot@kallisys.net>
2026-03-17 21:54:07 +0100

    crossgcc portgroup: add binutils 2.46.0
    Signed-off-by: Paul Guyot <pguyot@kallisys.net>

--- _resources/port1.0/group/crossbinutils-1.0.tcl
+++ _resources/port1.0/group/crossbinutils-1.0.tcl
@@ -121,6 +121,11 @@ array set crossbinutils.versions_info {
         sha256  5fe101e6fe9d18fdec95962d81ed670fdee5f37e3f48f0bef87bddf862513aa5 \
         size    27962312
     }}
+    2.46.0 {xz {
+        rmd160  cf936fe24a82aa43696ea442b6f52441af4015e2 \
+        sha256  d75a94f4d73e7a4086f7513e67e439e8fcdcbb726ffe63f4661744e6256b2cf2 \
+        size    28548776
+    }}
 }
 
 proc crossbinutils.setup {target version} {
