aed100b7cf42666039486419ec4157facbb82391
Joshua Root <jmr@macports.org>
2026-04-12 11:16:45 +1000

    {archive,mirror}_sites.tcl: make Tcl 9 compatible

--- _resources/port1.0/fetch/archive_sites.tcl
+++ _resources/port1.0/fetch/archive_sites.tcl
@@ -1,4 +1,6 @@
-namespace eval portfetch::mirror_sites { }
+namespace eval ::portfetch::mirror_sites {
+
+variable sites
 
 # Keep these in sync between archive_sites.tcl and mirror_sites.tcl.
 # Some servers only support http; others support https while allowing
@@ -9,35 +11,36 @@ namespace eval portfetch::mirror_sites { }
 # Some servers that support https haven't added the MacPorts hostnames
 # to their SSL certificate as Subject Alternative Names so we can't use
 # https with them yet.
-global os.platform os.major
-set letsencrypt_https_or_http   [expr {${os.platform} ne "darwin" || ${os.major} == 16 || ${os.major} > 18 ? "https" : "http"}]
-set letsencrypt_https_only      [expr {${os.platform} ne "darwin" || ${os.major} == 16 || ${os.major} > 18 ? "https" : ""}]
-set fastly      ${letsencrypt_https_or_http}
+namespace upvar :: os.platform os.platform
+namespace upvar :: os.major os.major
+variable letsencrypt_https_or_http   [expr {${os.platform} ne "darwin" || ${os.major} == 16 || ${os.major} > 18 ? "https" : "http"}]
+variable letsencrypt_https_only      [expr {${os.platform} ne "darwin" || ${os.major} == 16 || ${os.major} > 18 ? "https" : ""}]
+variable fastly      ${letsencrypt_https_or_http}
 # cert doesn't have macports.org SANs; admin notified
-#set aarnet.au   ${letsencrypt_https_or_http}
-set aarnet.au   http
-set atl.us      http
-set bos.us      ${letsencrypt_https_or_http}
-set cph.dk      ${letsencrypt_https_or_http}
-set cjj.kr      http
+#variable aarnet.au   ${letsencrypt_https_or_http}
+variable aarnet.au   http
+variable atl.us      http
+variable bos.us      ${letsencrypt_https_or_http}
+variable cph.dk      ${letsencrypt_https_or_http}
+variable cjj.kr      http
 # cert doesn't have macports.org SANs; admin notified
-#set fco.it      ${letsencrypt_https_or_http}
-set fco.it      http
-set fra.de      ${letsencrypt_https_or_http}
-set jog.id      http
-set kmq.jp      ${letsencrypt_https_or_http}
-set mse.uk      ${letsencrypt_https_or_http}
-set nue.de      ${letsencrypt_https_or_http}
-set pek.cn      ${letsencrypt_https_or_http}
-set vie.at      ${letsencrypt_https_or_http}
+#variable fco.it      ${letsencrypt_https_or_http}
+variable fco.it      http
+variable fra.de      ${letsencrypt_https_or_http}
+variable jog.id      http
+variable kmq.jp      ${letsencrypt_https_or_http}
+variable mse.uk      ${letsencrypt_https_or_http}
+variable nue.de      ${letsencrypt_https_or_http}
+variable pek.cn      ${letsencrypt_https_or_http}
+variable vie.at      ${letsencrypt_https_or_http}
 # cert doesn't have macports.org SANs; admin notified
-#set ykf.ca      ${letsencrypt_https_or_http}
-set ykf.ca      http
-set fcix.net    http
-set sjtu.edu.cn ${letsencrypt_https_only}
+#variable ykf.ca      ${letsencrypt_https_or_http}
+variable ykf.ca      http
+variable fcix.net    http
+variable sjtu.edu.cn ${letsencrypt_https_only}
 
 # Keep the primary packages server first in the list
-set portfetch::mirror_sites::sites(macports_archives) [lsearch -all -glob -inline -not "
+set sites(macports_archives) [lsearch -all -glob -inline -not "
     ${fastly}://packages.macports.org/:nosubdir
     ${nue.de}://nue.de.packages.macports.org/:nosubdir
     ${fcix.net}://mirror.fcix.net/macports/packages/:nosubdir
@@ -55,19 +58,29 @@ set portfetch::mirror_sites::sites(macports_archives) [lsearch -all -glob -inlin
     ${vie.at}://vie.at.packages.macports.org/:nosubdir
 " {:*}]
 
-set portfetch::mirror_sites::archive_type(macports_archives) tbz2
-set portfetch::mirror_sites::archive_prefix(macports_archives) /opt/local
-set portfetch::mirror_sites::archive_frameworks_dir(macports_archives) /opt/local/Library/Frameworks
-set portfetch::mirror_sites::archive_applications_dir(macports_archives) /Applications/MacPorts
+variable archive_type
+set archive_type(macports_archives) tbz2
+variable archive_prefix
+set archive_prefix(macports_archives) /opt/local
+variable archive_frameworks_dir
+set archive_frameworks_dir(macports_archives) /opt/local/Library/Frameworks
+variable archive_applications_dir
+set archive_applications_dir(macports_archives) /Applications/MacPorts
+variable archive_cxx_stdlib
 if {${os.platform} eq "darwin" && ${os.major} >= 10} {
-    set portfetch::mirror_sites::archive_cxx_stdlib(macports_archives) libc++
+    set archive_cxx_stdlib(macports_archives) libc++
 } else {
-    set portfetch::mirror_sites::archive_cxx_stdlib(macports_archives) libstdc++
+    set archive_cxx_stdlib(macports_archives) libstdc++
 }
+variable archive_delete_la_files
 if {${os.platform} eq "darwin" && ${os.major} <= 12} {
-    set portfetch::mirror_sites::archive_delete_la_files(macports_archives) no
+    set archive_delete_la_files(macports_archives) no
 } else {
-    set portfetch::mirror_sites::archive_delete_la_files(macports_archives) yes
+    set archive_delete_la_files(macports_archives) yes
 }
-set portfetch::mirror_sites::archive_sigtype(macports_archives) rmd160
-set portfetch::mirror_sites::archive_pubkey(macports_archives) /opt/local/share/macports/macports-pubkey.pem
+variable archive_sigtype
+set archive_sigtype(macports_archives) rmd160
+variable archive_pubkey
+set archive_pubkey(macports_archives) /opt/local/share/macports/macports-pubkey.pem
+
+}
