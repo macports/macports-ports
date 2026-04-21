1d02ebe851940833ca130ac1a58f6dba4e681e45
dblark <dblark@stu.pku.edu.cn>
2026-04-22 01:01:26 +0800

    riscv64-none-elf-gcc: new port at version 15.2.0

--- _resources/port1.0/group/crossgcc-1.0.tcl
+++ _resources/port1.0/group/crossgcc-1.0.tcl
@@ -174,6 +174,11 @@ array set newlib.versions_info {
         sha256  83a62a99af59e38eb9b0c58ed092ee24d700fff43a22c03e433955113ef35150 \
         size    8832922
     }}
+    4.6.0.20260123 {gz {
+        rmd160  97d1a15f06e550301d2cc68a9cc41bbc3c7b8bd2 \
+        sha256  6ff27e3bf022666f43f7802255be680eeff722ac181b1725d21e2e8318604ee3 \
+        size    9208322
+    }}
 }
 
 proc crossgcc.setup {target version} {
