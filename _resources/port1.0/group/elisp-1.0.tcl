e9aa5b65ca8ada0ac3c92492fbf4bf6c224d3226
Dan R. K. Ports <dports@macports.org>
2026-05-18 08:46:44 -0700

    elisp-1.0: add ELPA package support and default phases
    Adds an elpa.setup helper for GNU/NonGNU ELPA packages and opt-in
    default build (byte-compile + autoloads) and destroot (install into
    site-lisp) phases. Ports opt in via elpa.setup or by setting elisp.files
    or elisp.source_dir; existing ports that do neither are unaffected.

--- _resources/port1.0/group/elisp-1.0.tcl
+++ _resources/port1.0/group/elisp-1.0.tcl
@@ -17,10 +17,42 @@
 #
 # This can be used in the dependency specification, e.g.
 #    depends_lib         path:${emacs_binary}:${emacs_binary_provider}
-# ${emacs_binary} should also be used for compiling the elisp code,
-# but we leave setting that up to the individual portfiles, because
-# there's no standard way to do it.
+# and ${emacs_binary} can be used for byte-compilation either manually
+# or using the optional support below.
 #
+#
+# Optional support for Emacs Lisp module ports
+#
+# Ports that simply install one or more .el files can opt in to
+# default byte-compile and install phases instead of writing their
+# own. A port opts in by either:
+#   * calling  elpa.setup name version (gnu|nongnu)
+#   * setting  elisp.files and/or elisp.source_dir
+# for either ELPA or non-ELPA sources, respecfively.
+#
+# Either of these also marks the port noarch, disables the configure
+# phase, and adds a dependency on an Emacs capable of byte-compiling
+# the code. Ports that do none of the above are unaffected, and can
+# manually add byte-compilation code.
+#
+# Options:
+#   elisp.source_dir    directory containing the .el files
+#                       (default: worksrcpath)
+#   elisp.files         list of .el files to compile/install
+#                       (default: all top-level .el files in
+#                        elisp.source_dir)
+#   elisp.install_dir   subdirectory of site-lisp to install into
+#                       (default: subport name with any .el suffix
+#                        removed)
+#   elisp.byte_compile  whether to byte-compile (default: yes)
+#   elisp.autoloads     whether to generate an autoloads file
+#                       (default: yes)
+#
+# Ports needing custom logic can override the build and/or destroot phases as
+# usual, or set elisp.byte_compile / elisp.autoloads to no.
+#
+
+
 
 set emacs_default_binary             ${prefix}/bin/emacs
 set emacs_default_binary_provider    emacs
@@ -47,3 +79,150 @@ foreach bin ${emacs_binaries} {
 }
 
 set emacs_lispdir            ${prefix}/share/emacs/site-lisp
+
+#
+# Optional configuration for elisp module ports
+#
+
+options elisp.source_dir
+default elisp.source_dir    {${worksrcpath}}
+
+options elisp.files
+default elisp.files         {}
+
+options elisp.install_dir
+default elisp.install_dir   {[regsub -- {\.el$} ${subport} {}]}
+
+options elisp.byte_compile
+default elisp.byte_compile  yes
+
+options elisp.autoloads
+default elisp.autoloads     yes
+
+# Internal flag tracking whether a port has opted in to the managed phases.
+set elisp_managed           0
+
+#
+# elisp.setup: enable noarch settings, an Emacs dependency, and the default
+# build/destroot phases. Idempotent, so it is safe to call more than once
+# (e.g. via both elpa.setup and an elisp.files declaration).
+#
+
+proc elisp.setup {} {
+    global elisp_managed emacs_binary emacs_binary_provider
+
+    if {${elisp_managed}} {
+        return
+    }
+    set elisp_managed 1
+
+    supported_archs     noarch
+    platforms           any
+    use_configure       no
+
+    depends_lib-append  path:${emacs_binary}:${emacs_binary_provider}
+
+    build {
+        set srcdir [option elisp.source_dir]
+        set files [elisp._get_files]
+
+        if {[option elisp.byte_compile]} {
+            foreach el $files {
+                ui_info "Byte-compiling ${el}"
+                system -W $srcdir \
+                    "${emacs_binary} -Q --batch -L . -f batch-byte-compile [shellescape ${el}]"
+            }
+        }
+
+        if {[option elisp.autoloads] && [llength $files] > 0} {
+            set pkg [option elisp.install_dir]
+            set autoload_file "${srcdir}/${pkg}-autoloads.el"
+            ui_info "Generating autoloads: ${pkg}-autoloads.el"
+            system -W $srcdir \
+                "${emacs_binary} -Q --batch --eval '(let ((backup-inhibited t)) (if (fboundp (quote loaddefs-generate)) (loaddefs-generate \"[shellescape ${srcdir}]\" \"[shellescape ${autoload_file}]\") (let ((generated-autoload-file \"[shellescape ${autoload_file}]\")) (update-directory-autoloads \"[shellescape ${srcdir}]\"))))'"
+        }
+    }
+
+    destroot {
+        set srcdir [option elisp.source_dir]
+        set pkg [option elisp.install_dir]
+        set dest ${destroot}${emacs_lispdir}/${pkg}
+
+        xinstall -d ${dest}
+
+        set files [elisp._get_files]
+        foreach el $files {
+            xinstall -m 0644 ${srcdir}/${el} ${dest}
+            # Install corresponding .elc if it exists
+            set elc "${srcdir}/[file rootname ${el}].elc"
+            if {[file exists $elc]} {
+                xinstall -m 0644 $elc ${dest}
+            }
+        }
+
+        # Install autoloads file if generated
+        set autoload_file "${srcdir}/${pkg}-autoloads.el"
+        if {[file exists $autoload_file]} {
+            xinstall -m 0644 $autoload_file ${dest}
+        }
+    }
+}
+
+# Helper: resolve the effective list of .el files.
+proc elisp._get_files {} {
+    set files [option elisp.files]
+    if {$files eq {}} {
+        set srcdir [option elisp.source_dir]
+        set files [glob -nocomplain -tails -directory $srcdir *.el]
+    }
+    return $files
+}
+
+# Opt in automatically when a port declares its elisp sources.
+option_proc elisp.files      elisp._auto_setup
+option_proc elisp.source_dir elisp._auto_setup
+proc elisp._auto_setup {option action args} {
+    if {${action} eq "set" || ${action} eq "append"} {
+        elisp.setup
+    }
+}
+
+#
+# elpa.setup: configure master_sites, distname, homepage, and livecheck for a
+# package distributed via GNU ELPA or NonGNU ELPA, and opt in to the default
+# elisp phases.
+#
+# Usage: elpa.setup name version (gnu|nongnu)
+#        (repository defaults to gnu)
+#
+
+proc elpa.setup {elpa_name elpa_version {elpa_repo gnu}} {
+    global name
+
+    switch -- ${elpa_repo} {
+        gnu {
+            set base_url https://elpa.gnu.org/packages
+        }
+        nongnu {
+            set base_url https://elpa.nongnu.org/nongnu
+        }
+        default {
+            return -code error "elpa.setup: unknown repository '${elpa_repo}' (use gnu or nongnu)"
+        }
+    }
+
+    if {![info exists name] || ${name} eq ""} {
+        name            ${elpa_name}
+    }
+    version             ${elpa_version}
+    master_sites        ${base_url}/
+    distname            ${elpa_name}-${elpa_version}
+    use_tar             yes
+    homepage            ${base_url}/${elpa_name}.html
+
+    livecheck.type      regex
+    livecheck.url       ${base_url}/${elpa_name}.html
+    livecheck.regex     "${elpa_name}-(\\d+(?:\\.\\d+)*)\\.tar"
+
+    elisp.setup
+}
