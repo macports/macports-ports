f967c75a764f4569f60875c5eaed9d8997b73c3f
Nils Breunese <nils@breun.nl>
2026-08-19 22:48:37 +0200

    metal: run xcrun, xcodebuild and grep from trusted locations only

--- _resources/port1.0/group/metal-1.0.tcl
+++ _resources/port1.0/group/metal-1.0.tcl
@@ -13,32 +13,35 @@
 use_xcode   yes
 
 pre-configure {
-    set xcrun_command "/usr/bin/xcrun"
+    set xcrun "/usr/bin/xcrun"
 
     set sdkroot [option configure.sdkroot]
-    if {[file isdirectory $sdkroot]} {
-        set xcrun_command "env SDKROOT=[shellescape $sdkroot] $xcrun_command"
+    if {[file isdirectory ${sdkroot}]} {
+        set xcrun "env SDKROOT=[shellescape ${sdkroot}] ${xcrun}"
     }
 
-    set metal_check_command "$xcrun_command metal --version"
+    set metal_check "${xcrun} metal --version"
 
-    if {[catch {system ${metal_check_command}}]} {
+    if {[catch {system ${metal_check}}]} {
         # The Metal toolchain is not set up correctly, let's see what we can do or recommend
+        
+        set xcodebuild "/usr/bin/xcodebuild"
+        set grep "/usr/bin/grep"
 
         if {[vercmp ${xcodeversion} 26] >= 0 \
-                && [catch {system {xcodebuild -json -showComponent MetalToolchain | grep -qz '"status"[[:space:]]*:[[:space:]]*"installed"'}}]} {
+                && [catch {system {${xcodebuild} -json -showComponent MetalToolchain | ${grep} -qz '"status"[[:space:]]*:[[:space:]]*"installed"'}}]} {
             # The Metal toolchain is provided as an optional component since Xcode 26, but it isn't installed yet
             return -code error "Required Metal toolchain component not installed, \
-                run `xcodebuild -downloadComponent MetalToolchain` and try again."
+                run `${xcodebuild} -downloadComponent MetalToolchain` and try again."
         }
 
         # The Metal toolchain check could be failing due to a corrupt xcrun cache
         # Let's kill the xcrun cache to see if this fixes the problem
-        system "$xcrun_command --kill-cache"
+        system "${xcrun} --kill-cache"
 
-        if {[catch {system ${metal_check_command}}]} {
+        if {[catch {system ${metal_check}}]} {
             return -code error "Required Metal toolchain not set up properly for use with MacPorts. \
-                The toolchain seems to be installed, but `${metal_check_command}` still fails for the `${macportsuser}` \
+                The toolchain seems to be installed, but `${metal_check}` still fails for the `${macportsuser}` \
                 user, even after killing the xcrun cache."
         }
     }
