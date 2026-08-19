7bf409a918da771f6791c46ca511bf298aa26baa
Nils Breunese <nils@breun.nl>
2026-08-20 00:09:22 +0200

    metal: flatten nested if statements

--- _resources/port1.0/group/metal-1.0.tcl
+++ _resources/port1.0/group/metal-1.0.tcl
@@ -22,27 +22,32 @@ pre-configure {
 
     set metal_check "${xcrun} metal --version"
 
-    if {[catch {system ${metal_check}}]} {
-        # The Metal toolchain is not set up correctly, let's see what we can do or recommend
-
-        if {[vercmp ${xcodeversion} 26] >= 0} {
-            # The Metal toolchain is an optional component since Xcode 26
-            set xcodebuild "/usr/bin/xcodebuild"
-            if {[catch {exec ${xcodebuild} -json -showComponent MetalToolchain} metal_toolchain_component_info] \
-                    || ![regexp {"status"[[:space:]]*:[[:space:]]*"installed"} ${metal_toolchain_component_info}]} {
-                return -code error "Required Metal toolchain component is not installed, \
-                    run `${xcodebuild} -downloadComponent MetalToolchain` and try again."
-            }
-        }
-
-        # The Metal toolchain check could be failing due to a corrupt xcrun cache
-        # Let's kill the xcrun cache to see if this fixes the problem
-        catch {system "${xcrun} --kill-cache"}
-
-        if {[catch {system ${metal_check}}]} {
-            return -code error "Required Metal toolchain not set up properly for use with MacPorts. \
-                The toolchain seems to be installed, but `${metal_check}` still fails for the `${macportsuser}` \
-                user, even after killing the xcrun cache."
-        }
+    if {![catch {system ${metal_check}}]} {
+        # Metal toolchain is installed correctly for the MacPorts user
+        return
     }
+
+    set xcodebuild "/usr/bin/xcodebuild"
+    if {[vercmp ${xcodeversion} 26] >= 0 \
+            && ([catch {exec ${xcodebuild} -json -showComponent MetalToolchain} metal_toolchain_component_info] \
+                || ![regexp {"status"[[:space:]]*:[[:space:]]*"installed"} ${metal_toolchain_component_info}])} {
+        # The Metal toolchain is a separately installable component since Xcode 26.
+        # Xcode 26 or later is used, but the Metal toolchain component is not installed.
+        return -code error "Building this port requires the Metal toolchain Xcode component to be installed. \
+            Run `${xcodebuild} -downloadComponent MetalToolchain` and try again."
+    }
+
+    # The Metal toolchain seems to be installed, but executing it could be failing due to a corrupt xcrun cache.
+    # Let's kill the xcrun cache for the MacPorts user to see if this fixes the problem.
+    catch {system "${xcrun} --kill-cache"}
+
+    if {![catch {system ${metal_check}}]} {
+        # Metal toolchain is now installed correctly
+        return
+    }
+
+    # Metal toolchain is still not set up correctly and this PortGroup is out of ideas
+    return -code error "Required Metal toolchain not set up properly for use with MacPorts. \
+        The toolchain seems to be installed, but `${metal_check}` still fails for the `${macportsuser}` \
+        user, even after killing the xcrun cache. The cause of this issue is unknown."
 }
